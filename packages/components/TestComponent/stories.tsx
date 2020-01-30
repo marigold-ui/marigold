d6cc83cf0b34c979752bb4e2c3e211e22da358d5
Timo Zehnle <timo.zehnle@reservix.de>
2020-01-30 09:22:31 +0100

    test(storybook): test story

---
+++ packages/components/TestComponent/stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export default {
+  title: 'Test title',
+  parameters: {
+    componentSubtitle: 'This is the subtitle',
+  },
+};
+
+export const test1 = () => <div>TEST1</div>;
+export const test2 = () => <div>TEST2</div>;
+export const test3 = () => <div>TEST3</div>;
