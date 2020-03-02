99f9067d35e07fb84010c269fab2fbdc6049544e
viktoria.schwarz <viktoria.schwarz@reservix.de>
2020-03-02 11:19:13 +0100

    downgrade react-dom version

--- jest.config.js
+++ jest.config.js
@@ -5,34 +5,34 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transform: {
     '.(ts|tsx)': require.resolve('ts-jest/dist'),
-    },
-    transformIgnorePatterns: ['/node_modules[/\\\\].+\\.(js|jsx)$'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    testMatch: ['**/*.test.ts?(x)'],
-    roots: workspaces.map(
-      workspace => `<rootDir>/${workspace.replace('/*', '')}`
-      ),
-    // coverage
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-      '**/*.{ts,tsx}',
-      // ignore:
-      '!**/stories.tsx',
-      '!**/{*.d.ts,index.ts}',
-      '!**/node_modules/**',
-      '!**/build/**',
-    ],
-    globals: {
-      'ts-jest': {
-        diagnostics: {
-          warnOnly: true,
-        },
+  },
+  transformIgnorePatterns: ['/node_modules[/\\\\].+\\.(js|jsx)$'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/*.test.ts?(x)'],
+  roots: workspaces.map(
+    workspace => `<rootDir>/${workspace.replace('/*', '')}`
+  ),
+  // coverage
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    // ignore:
+    '!**/stories.tsx',
+    '!**/{*.d.ts,index.ts}',
+    '!**/node_modules/**',
+    '!**/build/**',
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true,
       },
-    },  
-    // plugins
-    watchPlugins: [
-      require.resolve('jest-watch-typeahead/filename'),
-      require.resolve('jest-watch-typeahead/testname'),
-    ],
+    },
+  },
+  // plugins
+  watchPlugins: [
+    require.resolve('jest-watch-typeahead/filename'),
+    require.resolve('jest-watch-typeahead/testname'),
+  ],
 };
