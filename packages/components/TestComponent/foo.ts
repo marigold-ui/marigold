3c05b3d370100e54a4229bcb9453a4cb79b1231d
Viktoria Schwarz <59830437+viktoria-schwarz@users.noreply.github.com>
2020-03-09 10:26:02 +0100

    chore: add eslint (#77)

--- packages/components/TestComponent/foo.ts
+++ packages/components/TestComponent/foo.ts
@@ -1,5 +1,5 @@
 export default class Calculator {
-  add(x : number, y : number) : number {
-      return x + y;
+  add(x: number, y: number): number {
+    return x + y;
   }
 }
--- packages/components/TestComponent/testComponent.test.tsx
+++ packages/components/TestComponent/testComponent.test.tsx
@@ -5,20 +5,17 @@ import { render } from '@testing-library/react';
 import Calculator from '@marigold/components/TestComponent/foo';
 
 const renderTest = () => {
-  const { getByTestId } = render(
-    <div data-testid='test'>test</div>
-  );
+  const { getByTestId } = render(<div data-testid="test">test</div>);
   return { getByTestId };
 };
 const { getByTestId } = renderTest();
 test('test', () => {
-  expect(getByTestId('test')).toContainHTML('<div')
+  expect(getByTestId('test')).toContainHTML('<div');
 });
-
 
 describe('Calculator', () => {
   it('should add two numbers together', () => {
-    const calc: Calculator = new Calculator()
-    expect(calc.add(2, 3)).toEqual(5)
-  })
-})
+    const calc: Calculator = new Calculator();
+    expect(calc.add(2, 3)).toEqual(5);
+  });
+});
