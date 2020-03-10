1a2d2e175e506d4e28e11f31f90ba74410416c8e
viktoria.schwarz <viktoria.schwarz@reservix.de>
2020-03-10 09:56:03 +0100

    chore(setup): add src folder to components package

--- packages/components/src/TestComponent/testComponent.test.tsx
+++ packages/components/src/TestComponent/testComponent.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import Calculator from '@marigold/components/TestComponent/foo';
+import Calculator from '@marigold/components/src/TestComponent/foo';
 
 const renderTest = () => {
   const { getByTestId } = render(<div data-testid="test">test</div>);
