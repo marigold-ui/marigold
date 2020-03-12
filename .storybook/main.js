5a9968e92063b46303de80c4685c98787001b890
Sebastian Sebald <sebastian.sebald@reservix.de>
2020-03-12 11:44:33 +0100

    make it work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,50 +1,16 @@
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
-const WORKSPACE_ROOT = '../packages';
-
 module.exports = {
-  stories: [`${WORKSPACE_ROOT}/**/stories.mdx`],
+  stories: ['../packages/**/*.stories.mdx'],
   addons: [
     '@storybook/react',
-    '@storybook/addon-docs/register',
+    '@storybook/preset-typescript',
     '@storybook/addon-a11y',
-  ],
-  webpackFinal: async config => {
-    config.module.rules = config.module.rules.filter(
-      rule =>
-        !(
-          rule.use &&
-          rule.use.length &&
-          rule.use.find(({ loader }) => loader === 'babel-loader')
-        )
-    );
-
-    // Use babel to transpile TypeScript.
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
+    {
+      name: '@storybook/addon-docs',
       options: {
-        presets: [['react-app', { flow: false, typescript: true }]],
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
       },
-    });
-    config.resolve.extensions.push('.ts', '.tsx', '.mdx');
-
-    config.module.rules.push({
-      test: /(stories|story)\.mdx$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-transform-react-jsx'],
-          },
-        },
-        {
-          loader: '@mdx-js/loader',
-          options: {
-            compilers: [createCompiler({})],
-          },
-        },
-      ],
-    });
-    return config;
-  },
+    },
+  ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,4 @@
-import { addDecorator, addParameters } from '@storybook/react';
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
-
+import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 
 addDecorator(withA11y);
-
-addParameters({
-  docs: {
-    container: DocsContainer,
-    page: DocsPage,
-  },
-});
