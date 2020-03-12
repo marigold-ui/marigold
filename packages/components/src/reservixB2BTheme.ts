ece03813cb04689479e2d712c78cdca6eca1b030
Viktoria Schwarz <59830437+viktoria-schwarz@users.noreply.github.com>
2020-03-12 10:50:25 +0100

    feat: Reservix B2B Theme (#96)
    * feat: Reservix B2B Theme
    
    * fix: adjust types in type BaseTheme
    
    * fix: adjust lineHeights

---
+++ packages/components/src/reservixB2BTheme.ts
@@ -0,0 +1,25 @@
+export const ReservixB2BTheme: BaseTheme = {
+  breakpoints: [768, 1200],
+  space: [0, 4, 8, 16, 24, 32, 40, 48, 56, 64, 88],
+  fonts: {
+    body: 'Inter Regular',
+    heading: 'Inter Black',
+  },
+  fontSizes: ['0.875rem', '1rem', '1.125rem', '1.25rem', '1.5rem', '2rem'],
+  fontWeights: {
+    body: 400,
+    heading: 900,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  colors: {
+    text: '#4b4b4b',
+    background: '#fafafa',
+    primary: '#fa8005',
+    secondary: '#4b4b4b',
+    muted: '#8d8d8d',
+  },
+};
