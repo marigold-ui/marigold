20f1beeef7579b73fd7060557213edb82f4d2275
Sebastian Sebald <sebastian.sebald@reservix.de>
2020-03-26 14:56:17 +0100

    wip

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,9 +1,25 @@
+const findUp = require('find-up');
+const path = require('path');
+
+//tsconfig paths package!
+const PROJECT_ROOT = findUp.sync('package.json'); // TODO: only directory
+const TS_CONFIG_PATH = findUp.sync('tsconfig.json'); // Start from project root
+
+// resolve tsconfig path aliases
+
 module.exports = {
-  stories: ['../packages/**/*.stories.mdx'],
+  stories: [path.resolve(PROJECT_ROOT, 'packages/**/*.stories.mdx')],
   addons: [
     '@storybook/react',
-    '@storybook/preset-typescript',
     '@storybook/addon-a11y',
+    {
+      name: '@storybook/preset-typescript',
+      options: {
+        tsLoaderOptions: {
+          configFile: TS_CONFIG_PATH,
+        },
+      },
+    },
     {
       name: '@storybook/addon-docs',
       options: {
