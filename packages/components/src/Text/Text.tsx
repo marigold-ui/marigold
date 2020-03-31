aa9844bbe0b841997fa06070a29fdeb9c59973a0
Timo Zehnle <timo.zehnle@reservix.de>
2020-03-31 09:48:54 +0200

    refa: Text component, make test coverage 100 percent

--- packages/components/src/Text/Text.tsx
+++ packages/components/src/Text/Text.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Box, system } from '@marigold/system';
 
-// Define custom properties for your component
 type TextProps = {};
 
 export const Text = system<TextProps, 'span'>(
-  ({ as = 'span', variant = '', children, ...props }) => {
+  ({ as = 'span', variant, children, ...props }) => {
     return (
       <Box as={as} themeSection="text" variant={variant} {...props}>
         {children}
