df32ccf9d6e1273791c9f6c78a0408acb83238b3
Sebastian Sebald <sebastian.sebald@reservix.de>
2020-04-09 22:55:36 +0200

    feat: Support "reduce-motion" media query
    Closes #190

--- packages/system/src/MarigoldProvider.tsx
+++ packages/system/src/MarigoldProvider.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
-import { ThemeContext } from './emotion';
+import { Global, ThemeContext } from './emotion';
+
+/**
+ * CSS snippet and idea from:
+ * https://css-tricks.com/revisiting-prefers-reduced-motion-the-reduced-motion-media-query/
+ */
+const ReduceMotion = () => (
+  <Global
+    styles={{
+      '@media screen and (prefers-reduced-motion: reduce), (update: slow)': {
+        '*': {
+          animationDuration: '0.001ms !important',
+          animationIterationCount: '1 !important',
+          transitionDuration: '0.001ms !important',
+        },
+      },
+    }}
+  />
+);
 
 // TODO: change any to theme when theme component exists
 export type MarigoldProviderProps<T extends any> = React.PropsWithChildren<{
@@ -10,5 +28,10 @@ export const MarigoldProvider = <T extends any>({
   theme,
   children,
 }: MarigoldProviderProps<T>) => (
-  <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+  <ThemeContext.Provider value={theme}>
+    <>
+      <ReduceMotion />
+      {children}
+    </>
+  </ThemeContext.Provider>
 );
