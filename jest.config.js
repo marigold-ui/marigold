14b6c90d62808ac9752e3d9e712391714fae91a4
Sebastian Sebald <sebastian.sebald@reservix.de>
2020-04-14 11:32:16 +0200

    feat: Support "reduce-motion" media query (#199)
    Closes #190

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const createConfig = require('@marigold/jest-config');
 module.exports = createConfig({
   moduleNameMapper: {
     '^@marigold/theme-(.+)$': '<rootDir>/themes/theme-$1/src',
+    '^@marigold/(.+)-config$': '<rootDir>/config/$1',
     '^@marigold/(.+)$': '<rootDir>/packages/$1/src',
   },
 });
--- packages/system/src/MarigoldProvider.test.tsx
+++ packages/system/src/MarigoldProvider.test.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { Global } from './emotion';
 import { Box } from './Box';
 import { MarigoldProvider } from './MarigoldProvider';
+
+// Mock
+// ---------------
+/**
+ * We mocking emotion's `<Global/>` here even though this will make us test
+ * implementation details. This is currently the only way to test CSS and
+ * media queries.
+ */
+jest.mock('@emotion/core', () => {
+  const original = jest.requireActual('@emotion/core');
+  return {
+    ...original,
+    Global: jest.fn(() => null),
+  };
+});
 
 // Setup
 // ---------------
@@ -38,4 +54,27 @@ test('set theme context', () => {
 
   expect(element).toHaveStyle(`color: ${theme.colors.black}`);
   expect(element).toHaveStyle(`font-size: 14px`);
+});
+
+test('removes animation when "reduce-motion" media query is set', () => {
+  const spy = Global as jest.Mock;
+  spy.mockClear();
+
+  render(<MarigoldProvider theme={theme} />);
+  expect((Global as jest.Mock).mock.calls[0]).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "styles": Object {
+          "@media screen and (prefers-reduced-motion: reduce), (update: slow)": Object {
+            "*": Object {
+              "animationDuration": "0.001ms !important",
+              "animationIterationCount": "1 !important",
+              "transitionDuration": "0.001ms !important",
+            },
+          },
+        },
+      },
+      Object {},
+    ]
+  `);
 });
