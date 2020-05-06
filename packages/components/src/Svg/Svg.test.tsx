bd0fdabd393219874fc24ebf30d7b221e01e4970
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2020-05-06 21:15:58 +0200

    test: svg component test (#211)

---
+++ packages/components/src/Svg/Svg.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Svg } from './Svg';
+
+test('support svg with default props', () => {
+  const t1 = render(
+    <Svg title="test1">
+      <path d="M9.9 20.113V13.8415H14.1V20.113H19.35V11.751H22.5L12 2.34375L1.5 11.751H4.65V20.113H9.9Z" />
+    </Svg>
+  );
+  let element1 = t1.getByTitle('test1');
+
+  expect(element1.getAttribute('fill')).toEqual('currentcolor');
+  expect(element1.getAttribute('width')).toEqual('24');
+  expect(element1.getAttribute('height')).toEqual('24');
+});
+
+test('support fill prop', () => {
+  const t1 = render(
+    <Svg title="test2" fill="orange">
+      <path d="M9.9 20.113V13.8415H14.1V20.113H19.35V11.751H22.5L12 2.34375L1.5 11.751H4.65V20.113H9.9Z" />
+    </Svg>
+  );
+  let element2 = t1.getByTitle('test2');
+
+  expect(element2.getAttribute('fill')).toEqual('orange');
+});
+
+test('support size prop', () => {
+  const t1 = render(
+    <Svg title="test3" size={32}>
+      <path d="M9.9 20.113V13.8415H14.1V20.113H19.35V11.751H22.5L12 2.34375L1.5 11.751H4.65V20.113H9.9Z" />
+    </Svg>
+  );
+  let element3 = t1.getByTitle('test3');
+
+  expect(element3.getAttribute('width')).toEqual('32');
+  expect(element3.getAttribute('height')).toEqual('32');
+});
+
+test('viewbox default value', () => {
+  const t1 = render(
+    <Svg title="test4">
+      <path d="M9.9 20.113V13.8415H14.1V20.113H19.35V11.751H22.5L12 2.34375L1.5 11.751H4.65V20.113H9.9Z" />
+    </Svg>
+  );
+  let element4 = t1.getByTitle('test4');
+
+  expect(element4.getAttribute('viewBox')).toEqual('0 0 24 24');
+});
