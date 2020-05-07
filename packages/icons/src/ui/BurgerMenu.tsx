f14fccdca0ff0348d75155d7026d10839cdd82aa
Timo Zehnle <timo.zehnle@reservix.de>
2020-05-07 10:54:20 +0200

    chore: add UI-icons

---
+++ packages/icons/src/ui/BurgerMenu.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Svg } from '@marigold/components';
+
+export const BurgerMenu = () => (
+  <Svg>
+    <path d="M3.5625 6.375H20.4375V8.25H3.5625V6.375ZM3.5625 16.125H20.4375V18H3.5625V16.125ZM20.4375 11.25H3.5625V13.125H20.4375V11.25Z" />
+  </Svg>
+);
