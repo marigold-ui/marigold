cd73e3ffd9d268af4cba60c0d61236973257b040
Timo Zehnle <timo.zehnle@reservix.de>
2020-05-12 10:08:52 +0200

    merge commit

---
+++ packages/icons/src/ui/ArrowUp.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Svg } from '@marigold/components';
+
+export const ArrowUp = () => (
+  <Svg>
+    <path d="M5.97563 16.8506L12 10.8394L18.0244 16.8506L19.875 15L12 7.125L4.125 15L5.97563 16.8506Z" />
+  </Svg>
+);
