134de865b3b09d075e252ce90481fba170a0dab1
viktoria.schwarz <viktoria.schwarz@reservix.de>
2020-05-12 14:13:13 +0200

    add default variant to text component

--- packages/components/src/Text/Text.tsx
+++ packages/components/src/Text/Text.tsx
@@ -4,7 +4,7 @@ import { Box, system } from '@marigold/system';
 type TextProps = {};
 
 export const Text = system<TextProps, 'span'>(
-  ({ as = 'span', variant, children, ...props }) => {
+  ({ as = 'span', variant = 'body', children, ...props }) => {
     return (
       <Box as={as} themeSection="text" variant={variant} {...props}>
         {children}
