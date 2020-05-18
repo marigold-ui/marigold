10a3ca55cc6a8ca2738d9bfd043bc6af59999941
Timo Zehnle <timo.zehnle@reservix.de>
2020-05-18 10:50:45 +0200

    change name to ExampleIcon

--- packages/icons/src/ExampleIcon.test.tsx
+++ packages/icons/src/ExampleIcon.test.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Icon } from './Icon';
+import { ExampleIcon } from './ExampleIcon';
 
 test('supports default fill color', () => {
-  render(<Icon title="svg" />);
+  render(<ExampleIcon title="svg" />);
   const svg = screen.getByTitle(/svg/);
 
   expect(svg.getAttribute('fill')).toEqual('currentcolor');
 });
 
 test('supports default size', () => {
-  render(<Icon title="svg" />);
+  render(<ExampleIcon title="svg" />);
   const svg = screen.getByTitle(/svg/);
 
   expect(svg.getAttribute('width')).toEqual('24');
 });
 
 test('supports size prop', () => {
-  render(<Icon title="svg" size={30} />);
+  render(<ExampleIcon title="svg" size={30} />);
   const svg = screen.getByTitle(/svg/);
 
   expect(svg.getAttribute('width')).toEqual('30');
 });
 
 test('supports fill prop', () => {
-  render(<Icon title="svg" fill="orange" />);
+  render(<ExampleIcon title="svg" fill="orange" />);
   const svg = screen.getByTitle(/svg/);
 
   expect(svg.getAttribute('fill')).toEqual('orange');
