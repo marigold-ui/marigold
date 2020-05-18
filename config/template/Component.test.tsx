7e0705768f1736768380a5b186ab224f1f93ee23
viktoria.schwarz <viktoria.schwarz@reservix.de>
2020-05-18 11:58:06 +0200

    add templates

---
+++ config/template/Component.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MarigoldProvider } from '@marigold/system';
+
+const theme = {};
+
+test('supports default variant and themeSection', () => {});
+
+test('accepts other variant than default', () => {});
+
+test('renders correct HTML element', () => {});
+
+test('supports custom prop', () => {});
+
+test('variant styles cannot be overridden with CSS prop', () => {});
---
+++ config/template/Component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, system } from '@marigold/system';
+
+type Props = {};
+
+export const New = system<Props, 'div'>(({ variant, children, ...props }) => {
+  return (
+    <Box as="div" themeSection="" variant={variant} {...props}>
+      {children}
+    </Box>
+  );
+});
