e9c432c5d7ba99c87ba5e0435edbd0daf69819b2
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2020-06-23 13:29:49 +0200

    feat: add checkbox component (#335)

---
+++ packages/components/src/Checkbox/Checkbox.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Box, system } from '@marigold/system';
+import { SquareO, SquareOChecked } from '@marigold/icons';
+
+type CheckboxProps = {};
+
+export const Checkbox = system<CheckboxProps, 'input'>(
+  ({ variant = 'checkbox', checked, ...props }) => {
+    const [isCheckedOn, setChecked] = useState(false);
+    const toggleChecked = () => setChecked(!isCheckedOn);
+    return (
+      <Box css={{ display: 'inline-block' }}>
+        <Box
+          as="input"
+          type="checkbox"
+          {...props}
+          css={{
+            position: 'absolute',
+            opacity: 0,
+            zIndex: -1,
+            width: 1,
+            height: 1,
+            overflow: 'hidden',
+          }}
+          onClick={() => toggleChecked()}
+        />
+        <Box
+          as={isCheckedOn || checked ? SquareOChecked : SquareO}
+          aria-hidden="true"
+          themeSection="form"
+          variant={variant}
+          css={{
+            mr: 2,
+            verticalAlign: 'middle',
+            ':hover': { cursor: 'pointer' },
+            'input:disabled ~ &': {
+              color: 'muted',
+              cursor: 'not-allowed',
+            },
+          }}
+        />
+      </Box>
+    );
+  }
+);
