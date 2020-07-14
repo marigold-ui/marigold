5df7db13f12d6a695571a9f792ede18ca4dd94e1
viktoria.schwarz <viktoria.schwarz@reservix.de>
2020-07-14 11:29:34 +0200

    remove <Text>

--- packages/components/src/Link/Link.tsx
+++ packages/components/src/Link/Link.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Box, system } from '@marigold/system';
-import { Text } from '@marigold/components';
 
 type LinkProps = {};
 
 export const Link = system<LinkProps, 'a'>(
   ({ variant = 'link', children, ...props }) => {
     return (
-      <Text>
-        <Box as="a" variant={variant} themeSection="link" {...props}>
-          {children}
-        </Box>
-      </Text>
+      <Box as="a" variant={variant} themeSection="link" {...props}>
+        {children}
+      </Box>
     );
   }
 );
