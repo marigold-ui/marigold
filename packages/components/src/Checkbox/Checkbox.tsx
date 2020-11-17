e7c64cb874bccb3602af0d2f0a2386add5a64823
Timo Zehnle <timo.zehnle@reservix.de>
2020-11-17 14:23:49 +0100

    rewrite Checkbox

--- packages/components/src/Checkbox/Checkbox.tsx
+++ packages/components/src/Checkbox/Checkbox.tsx
@@ -1,42 +1,44 @@
 import React from 'react';
-import { Box, system } from '@marigold/system';
+import { createStyles, system } from '@marigold/system';
 import { SquareUnchecked, SquareChecked } from '@marigold/icons';
 
-type CheckboxProps = {};
+type CheckboxProps = {
+  variant?: string;
+};
+
+const useStyles = createStyles('form');
 
 export const Checkbox = system<CheckboxProps, 'input'>(
   ({ variant = 'checkbox', ...props }) => {
+    const checkboxStyle = useStyles({
+      position: 'absolute',
+      opacity: 0,
+      zIndex: -1,
+      width: 1,
+      height: 1,
+      overflow: 'hidden',
+    });
+    const checkboxIconStyle = useStyles({
+      variant,
+      ariaHidden: 'true',
+      mr: 2,
+      verticalAlign: 'middle',
+      ':hover': { cursor: 'pointer' },
+      'input:disabled ~ &': {
+        color: 'muted',
+        cursor: 'not-allowed',
+      },
+    });
+
     return (
-      <Box css={{ display: 'inline-block' }}>
-        <Box
-          as="input"
-          type="checkbox"
-          {...props}
-          css={{
-            position: 'absolute',
-            opacity: 0,
-            zIndex: -1,
-            width: 1,
-            height: 1,
-            overflow: 'hidden',
-          }}
-        />
-        <Box
-          as={props.checked ? SquareChecked : SquareUnchecked}
-          aria-hidden="true"
-          themeSection="form"
-          variant={variant}
-          css={{
-            mr: 2,
-            verticalAlign: 'middle',
-            ':hover': { cursor: 'pointer' },
-            'input:disabled ~ &': {
-              color: 'muted',
-              cursor: 'not-allowed',
-            },
-          }}
-        />
-      </Box>
+      <div className={useStyles({ display: 'inline-block' })}>
+        <input type="checkbox" className={checkboxStyle} {...props} />
+        {props.checked ? (
+          <SquareChecked className={checkboxIconStyle} />
+        ) : (
+          <SquareUnchecked className={checkboxIconStyle} />
+        )}
+      </div>
     );
   }
 );
