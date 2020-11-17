170fc015b6fcbbeece28a59b00380eeb22d4fe31
Timo Zehnle <timo.zehnle@reservix.de>
2020-11-17 15:08:29 +0100

    refa button

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -10,14 +10,17 @@ const useStyles = createStyles('button');
 export const Button = system<ButtonProps, 'button'>(
   ({ variant = 'primary.large', children, ...props }) => {
     const classNames = useStyles({ variant });
-    const spanStyle = useStyles({
-      display: 'inline-flex',
-      alignItems: 'center',
-    });
 
     return (
       <button className={classNames} {...props}>
-        <span className={spanStyle}>{children}</span>
+        <span
+          className={useStyles({
+            display: 'inline-flex',
+            alignItems: 'center',
+          })}
+        >
+          {children}
+        </span>
       </button>
     );
   }
