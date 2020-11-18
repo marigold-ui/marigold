a186ea7c5feb9d9759fc78fb789a1b64f0f3073d
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2020-11-18 08:45:18 +0100

    Rewrite comps: Hidden, Message, Badge, Divider, Image (#573)
    Co-authored-by: Viktoria Schwarz <59830437+viktoria-schwarz@users.noreply.github.com>
    Co-authored-by: viktoria.schwarz <viktoria.schwarz@reservix.de>

--- packages/components/src/Hidden/Hidden.tsx
+++ packages/components/src/Hidden/Hidden.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Box, system } from '@marigold/system';
+import { createStyles, system } from '@marigold/system';
 
 type HiddenProps = {
+  variant?: string;
   show?: boolean;
 };
 
+const useStyles = createStyles('layout');
+
 export const Hidden = system<HiddenProps, 'span'>(
   ({ variant = 'hidden', show = false, children, ...props }) => {
+    const classNames = useStyles({
+      variant,
+      display: show ? 'display' : 'none',
+    });
+
     return (
-      <Box
-        as="span"
-        css={show ? { display: 'display' } : { display: 'none' }}
-        themeSection="layout"
-        variant={variant}
-        {...props}
-      >
+      <span className={classNames} {...props}>
         {children}
-      </Box>
+      </span>
     );
   }
 );
