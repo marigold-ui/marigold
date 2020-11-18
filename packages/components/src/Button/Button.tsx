12d9ffeee95c5ebd8e8c75eb8c3a0fabd52e2d1c
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2020-11-18 09:54:15 +0100

    Rewrite Button (#593)

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Box, system } from '@marigold/system';
+import { createStyles, system } from '@marigold/system';
 
-type ButtonProps = {};
+type ButtonProps = {
+  variant?: string;
+};
+
+const useStyles = createStyles('button');
 
 export const Button = system<ButtonProps, 'button'>(
   ({ variant = 'primary.large', children, ...props }) => {
+    const classNames = useStyles({ variant });
+
     return (
-      <Box as="button" themeSection="button" variant={variant} {...props}>
-        <Box as="span" css={{ display: 'inline-flex', alignItems: 'center' }}>
+      <button className={classNames} {...props}>
+        <span
+          className={useStyles({
+            display: 'inline-flex',
+            alignItems: 'center',
+          })}
+        >
           {children}
-        </Box>
-      </Box>
+        </span>
+      </button>
     );
   }
 );
