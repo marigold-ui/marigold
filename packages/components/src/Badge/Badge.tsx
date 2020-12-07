0d310c0229572ed841beb552386307586f4d6849
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2020-12-07 12:14:35 +0100

    change useStyles hook to concat custom classNames and edit Text, Alert and Badge component (#631)
    * useStyles in Alert
    
    * useStyles Badge
    
    * change useStyles hook to concat custom classNames
    
    * use custom classNames in Text, Badge and Alert
    
    Co-authored-by: Viktoria Schwarz <59830437+viktoria-schwarz@users.noreply.github.com>

--- packages/components/src/Badge/Badge.tsx
+++ packages/components/src/Badge/Badge.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, system } from '@marigold/system';
+import { useStyles, system } from '@marigold/system';
 
 type BadgeProps = {
   variant?: string;
@@ -7,21 +7,23 @@ type BadgeProps = {
   backgroundColor?: string;
 };
 
-const useStyles = createStyles('content');
-
 export const Badge = system<BadgeProps, 'div'>(
   ({
     variant = 'badge',
     borderColor = 'inherit',
     backgroundColor = 'inherit',
+    className,
     children,
     ...props
   }) => {
-    const classNames = useStyles({
-      variant,
-      border: '1px solid ' + borderColor,
-      bg: backgroundColor,
-    });
+    const classNames = useStyles(
+      {
+        variant: `content.${variant}`,
+        border: '1px solid ' + borderColor,
+        bg: backgroundColor,
+      },
+      className
+    );
 
     return (
       <div className={classNames} {...props}>
