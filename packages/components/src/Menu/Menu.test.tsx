7c83921158d34879b7eeddc8b4e0dca21e851c42
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2021-01-05 11:36:01 +0100

    add MenuItem and Menu component (#669)

---
+++ packages/components/src/Menu/Menu.test.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from '@marigold/system';
+import { Menu, MenuItem } from '@marigold/components';
+
+const theme = {
+  content: {
+    menu: {
+      padding: '4px',
+    },
+    options: {
+      padding: '8px',
+    },
+  },
+};
+
+test('supports default variant and themeSection', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Menu title="menu" onClick={() => {}}>
+        <MenuItem href="#">Home</MenuItem>
+      </Menu>
+    </ThemeProvider>
+  );
+  const menu = screen.getByTitle(/menu/);
+
+  expect(menu).toHaveStyle(`padding: 4px`);
+});
+
+test('accepts other variant than default', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Menu variant="options" title="menu" onClick={() => {}}>
+        <MenuItem href="#">Home</MenuItem>
+      </Menu>
+    </ThemeProvider>
+  );
+  const menu = screen.getByTitle(/menu/);
+
+  expect(menu).toHaveStyle(`padding: 8px`);
+});
+
+test('supports show prop', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Menu show={true} title="menu" onClick={() => {}}>
+        <MenuItem href="#">Home</MenuItem>
+      </Menu>
+    </ThemeProvider>
+  );
+  const menu = screen.getByTitle(/menu/);
+
+  expect(menu).toHaveStyle(`display: block`);
+});
+
+test('renders correct HTML element', () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Menu title="menu" onClick={() => {}}>
+        <MenuItem href="#">Home</MenuItem>
+      </Menu>
+    </ThemeProvider>
+  );
+  const menu = screen.getByTitle(/menu/);
+
+  expect(menu instanceof HTMLDivElement).toBeTruthy();
+});
+
+test('accepts custom styles prop className', () => {
+  render(
+    <Menu title="menu" onClick={() => {}} className="custom-class-name">
+      <MenuItem href="#">Home</MenuItem>
+    </Menu>
+  );
+  const menu = screen.getByTitle(/menu/);
+
+  expect(menu.className).toMatch('custom-class-name');
+});
---
+++ packages/components/src/Menu/Menu.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useStyles, system } from '@marigold/system';
+import { Button } from '@marigold/components';
+
+type MenuProps = {
+  variant?: string;
+  label?: string;
+  onClick: () => void;
+  show?: boolean;
+};
+
+export const Menu = system<MenuProps, 'div'>(
+  ({
+    variant = 'menu',
+    label = 'Menu',
+    onClick,
+    show = false,
+    className,
+    children,
+    ...props
+  }) => {
+    const classNames = useStyles(
+      {
+        variant: `content.${variant}`,
+      },
+      className
+    );
+    const itemStyles = useStyles({
+      position: 'absolute',
+      minWidth: '120px',
+      display: 'block',
+      textAlign: 'left',
+      borderRadius: '2px',
+    });
+
+    return (
+      <div className={classNames} {...props}>
+        <Button onClick={onClick} variant="menu">
+          {label}
+        </Button>
+        {show ? <div className={itemStyles}>{children}</div> : null}
+      </div>
+    );
+  }
+);
