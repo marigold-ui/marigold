055c16b3ab7ca3bcb3451f13790937aa9487b078
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-08 10:42:56 +0100

    remove variant from column

--- packages/components/src/Column/Column.tsx
+++ packages/components/src/Column/Column.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { useStyles, system } from '@marigold/system';
 
 type ColumnProps = {
-  variant?: string;
   width?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 };
 
 export const Column = system<ColumnProps, 'div'>(
-  ({ variant = 'column', width = 12, className, children, ...props }) => {
+  ({ width = 12, className, children, ...props }) => {
     const classNames = useStyles(
       {
-        variant: `layout.${variant}`,
         width: `${(width / 12) * 100}%`,
       },
       className
