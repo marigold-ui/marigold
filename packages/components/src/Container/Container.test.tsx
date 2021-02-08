5860d47ed1efb68a4258d5b358277fe816c19272
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-08 10:29:16 +0100

    edit container component

--- packages/components/src/Container/Container.test.tsx
+++ packages/components/src/Container/Container.test.tsx
@@ -1,52 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { ThemeProvider } from '@marigold/system';
 import { Container, Text } from '@marigold/components';
-
-const theme = {
-  layout: {
-    container: {
-      margin: 0,
-    },
-    notContainer: {
-      margin: '2px',
-    },
-  },
-};
-
-test('supports default variant and themeSection', () => {
-  render(
-    <ThemeProvider theme={theme}>
-      <Container title="container">
-        <Text>sdf</Text>
-      </Container>
-    </ThemeProvider>
-  );
-  const container = screen.getByTitle(/container/);
-
-  expect(container).toHaveStyle(`margin: 0`);
-});
-
-test('accepts other variant than default', () => {
-  render(
-    <ThemeProvider theme={theme}>
-      <Container variant="notContainer" title="container">
-        <Text>sdf</Text>
-      </Container>
-    </ThemeProvider>
-  );
-  const container = screen.getByTitle(/container/);
-
-  expect(container).toHaveStyle(`margin: 2px`);
-});
 
 test('renders correct HTML element', () => {
   render(
-    <ThemeProvider theme={theme}>
-      <Container title="container">
-        <Text>sdf</Text>
-      </Container>
-    </ThemeProvider>
+    <Container title="container">
+      <Text>sdf</Text>
+    </Container>
   );
   const container = screen.getByTitle(/container/);
 
@@ -55,11 +15,9 @@ test('renders correct HTML element', () => {
 
 test('accepts custom styles prop className', () => {
   render(
-    <ThemeProvider theme={theme}>
-      <Container className="custom-class-name" title="container">
-        <Text>text</Text>
-      </Container>
-    </ThemeProvider>
+    <Container className="custom-class-name" title="container">
+      <Text>text</Text>
+    </Container>
   );
   const container = screen.getByTitle(/container/);
 
