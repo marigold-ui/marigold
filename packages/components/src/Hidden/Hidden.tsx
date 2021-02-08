e1a8a934c1cc147eb65b84177220165d985dc64a
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-08 10:42:21 +0100

    remove variant from hidden

--- packages/components/src/Hidden/Hidden.tsx
+++ packages/components/src/Hidden/Hidden.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { useStyles, system } from '@marigold/system';
 
 type HiddenProps = {
-  variant?: string;
   show?: boolean;
 };
 
 export const Hidden = system<HiddenProps, 'span'>(
-  ({ variant = 'hidden', show = false, className, children, ...props }) => {
+  ({ show = false, className, children, ...props }) => {
     const classNames = useStyles(
       {
-        variant: `layout.${variant}`,
         display: show ? 'display' : 'none',
       },
       className
