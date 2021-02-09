685d69508c7e81575abca95ef1baeb547a9a29f3
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-09 14:47:14 +0100

    use Box in Column

--- packages/components/src/Column/Column.tsx
+++ packages/components/src/Column/Column.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStyles, system } from '@marigold/system';
+import { Box } from '../Box';
 
 type ColumnProps = {
   width?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
@@ -7,17 +8,12 @@ type ColumnProps = {
 
 export const Column = system<ColumnProps, 'div'>(
   ({ width = 12, className, children, ...props }) => {
-    const classNames = useStyles(
-      {
-        width: `${(width / 12) * 100}%`,
-      },
-      className
-    );
+    const classNames = useStyles({}, className);
 
     return (
-      <div className={classNames} {...props}>
+      <Box width={`${(width / 12) * 100}%`} className={classNames} {...props}>
         {children}
-      </div>
+      </Box>
     );
   }
 );
