6608627ea89e287efc5dae26f8fabf422d4311ef
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-02-10 22:36:08 +0100

    refa(components): Remove system helper from <Button>
    Ref #753

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import { useStyles, system } from '@marigold/system';
+import { useStyles } from '@marigold/system';
+import { Box } from '../Box';
 
 type ButtonProps = {
+  className?: string;
   variant?: string;
 };
 
-export const Button = system<ButtonProps, 'button'>(
-  ({ variant = 'primary.large', className, children, ...props }) => {
-    const classNames = useStyles(
-      {
-        variant: `button.${variant}`,
-      },
-      className
-    );
-    return (
-      <button className={classNames} {...props}>
-        <span
-          className={useStyles({
-            display: 'inline-flex',
-            alignItems: 'center',
-          })}
-        >
-          {children}
-        </span>
-      </button>
-    );
-  }
-);
+export const Button: React.FC<ButtonProps> = ({
+  variant = 'primary.large',
+  className,
+  children,
+  ...props
+}) => {
+  const classNames = useStyles(
+    {
+      variant: `button.${variant}`,
+    },
+    className
+  );
+  return (
+    <button className={classNames} {...props}>
+      <Box as="span" display="inline-flex" alignItems="center">
+        {children}
+      </Box>
+    </button>
+  );
+};
