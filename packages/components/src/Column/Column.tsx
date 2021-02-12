4f6ecec1b30c16c208fcb568e196935fa94b0b99
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-12 09:00:49 +0100

    wip columns

--- packages/components/src/Column/Column.tsx
+++ packages/components/src/Column/Column.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { useStyles, system } from '@marigold/system';
 import { Box } from '../Box';
 
-type ColumnProps = {
+export type ColumnProps = {
   width?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 };
 
 export const Column = system<ColumnProps, 'div'>(
   ({ width = 12, className, children, ...props }) => {
-    const classNames = useStyles({}, className);
+    let percent = `${(width / 12) * 100}%`;
+    const classNames = useStyles({ flexBasis: percent }, className);
 
     return (
-      <Box width={`${(width / 12) * 100}%`} className={classNames} {...props}>
+      <Box
+        // width={`${(width / 12) * 100}%`}
+        className={classNames}
+        {...props}
+      >
         {children}
       </Box>
     );
--- packages/components/src/Columns/Columns.tsx
+++ packages/components/src/Columns/Columns.tsx
@@ -1,23 +1,62 @@
 import React from 'react';
 import { useStyles, system } from '@marigold/system';
+import { Box } from '../Box';
 
 type ColumnsProps = {
-  width?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  space?: 0 | 4 | 8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 88;
+  // align?: 'left' | 'right' | 'center';
 };
 
 export const Columns = system<ColumnsProps, 'div'>(
-  ({ width = 12, className, children, ...props }) => {
-    const classNames = useStyles(
-      {
-        width: `${(width / 12) * 100}%`,
-      },
-      className
-    );
+  ({
+    space = 0,
+    // align = 'left',
+    className,
+    children,
+    ...props
+  }) => {
+    const classNames = useStyles({}, className);
+
+    let display = 'flex';
+    let flexDirection = 'row';
+    // let alignItems = 'flex-start';
+
+    // if (align === 'left') {
+    //   display = 'flex';
+    //   flexDirection = 'row';
+    //   alignItems = 'flex-start';
+    // }
 
     return (
-      <div className={classNames} {...props}>
-        {children}
-      </div>
+      <Box
+        p={space}
+        display={display}
+        flexDirection={flexDirection}
+        maxWidth="100%"
+        className={classNames}
+        {...props}
+      >
+        {React.Children.map(children, (child, index) => {
+          let marginLeft = index === 0 ? -space : 0;
+
+          return React.cloneElement(child as React.ReactElement<any>, {
+            className: useStyles({ pl: space, ml: marginLeft }),
+          });
+          // return (
+          //   <Box
+          //     key={index.toString()}
+          //     display={display}
+          //     flexDirection={flexDirection}
+          //     // alignItems={alignItems}
+          //     pl={space}
+          //     ml={index === 0 ? -space : 0}
+          //     width="33%"
+          //   >
+          //     {child}
+          //   </Box>
+          // );
+        })}
+      </Box>
     );
   }
 );
