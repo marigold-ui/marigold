141634025ceaf6a1d87d0161702bb07be189a6a2
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-02-15 11:46:26 +0100

    jup jup

--- packages/components/src/Alert/Alert.tsx
+++ packages/components/src/Alert/Alert.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
-import { useStyles } from '@marigold/system';
+import { HTMLProps } from '@marigold/types';
 import { Box } from '../Box';
 
 type AlertProps = {
   className?: string;
   variant?: string;
-};
+} & HTMLProps<'div'>;
 
 export const Alert: React.FC<AlertProps> = ({
   variant = 'info',
-  className,
   children,
   ...props
-}) => {
-  const classNames = useStyles(
-    {
-      variant: `alerts.${variant}`,
-    },
-    className
-  );
-
-  return (
-    <Box
-      {...props}
-      className={classNames}
-      display="flex"
-      alignItems="center"
-      px={3}
-      py={2}
-      borderRadius={4}
-    >
-      {children}
-    </Box>
-  );
-};
+}) => (
+  <Box
+    {...props}
+    display="flex"
+    alignItems="center"
+    px={3}
+    py={2}
+    borderRadius={4}
+    variant={`alerts.${variant}`}
+  >
+    {children}
+  </Box>
+);
