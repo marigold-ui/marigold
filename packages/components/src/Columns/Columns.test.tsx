844856726df2a309ba117bf412b327a41c48968a
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-16 14:25:54 +0100

    add working Columns comp - space missing

--- packages/components/src/Columns/Columns.test.tsx
+++ packages/components/src/Columns/Columns.test.tsx
@@ -1,20 +1,101 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Column } from '@marigold/components';
+import { Column, Columns } from '@marigold/components';
 
-test('supports width prop', () => {
+test('supports default space prop', () => {
   render(
-    <Column title="column" width={6}>
-      column
-    </Column>
+    <Columns title="column">
+      <Column>column</Column>
+    </Columns>
   );
   const column = screen.getByTitle(/column/);
 
-  expect(column).toHaveStyle(`width: 50%`);
+  expect(column).toHaveStyle(`margin: 0px`);
+});
+
+test('supports custom space prop', () => {
+  render(
+    <Columns space={12} title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`margin: -6px`);
+});
+
+test('supports default horizontalAlign prop: left', () => {
+  render(
+    <Columns title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`justify-content: flex-start`);
+});
+
+test('supports custom horizontalAlign prop: center', () => {
+  render(
+    <Columns horizontalAlign="center" title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`justify-content: center`);
+});
+
+test('supports custom horizontalAlign prop: right', () => {
+  render(
+    <Columns horizontalAlign="right" title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`justify-content: flex-end`);
+});
+
+test('supports default verticalAlign prop: top', () => {
+  render(
+    <Columns title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`align-items: flex-start`);
+});
+
+test('supports custom verticalAlign prop: center', () => {
+  render(
+    <Columns verticalAlign="center" title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`align-items: center`);
+});
+
+test('supports custom verticalAlign prop: bottom', () => {
+  render(
+    <Columns verticalAlign="bottom" title="column">
+      <Column>column</Column>
+    </Columns>
+  );
+  const column = screen.getByTitle(/column/);
+
+  expect(column).toHaveStyle(`align-items: flex-end`);
 });
 
 test('renders correct HTML element', () => {
-  render(<Column title="column">Default</Column>);
+  render(
+    <Columns title="column">
+      <Column>column</Column>
+    </Columns>
+  );
   const column = screen.getByTitle(/column/);
 
   expect(column instanceof HTMLDivElement).toBeTruthy();
@@ -23,7 +104,7 @@ test('renders correct HTML element', () => {
 test('accepts custom styles prop className', () => {
   render(
     <Column className="custom-class-name" title="column">
-      text
+      column
     </Column>
   );
   const column = screen.getByTitle(/column/);
--- packages/components/src/Columns/Columns.tsx
+++ packages/components/src/Columns/Columns.tsx
@@ -3,59 +3,61 @@ import { useStyles, system } from '@marigold/system';
 import { Box } from '../Box';
 
 type ColumnsProps = {
-  space?: 0 | 4 | 8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 88;
-  // align?: 'left' | 'right' | 'center';
+  space?: 0 | 12 | 24;
+  horizontalAlign?: 'left' | 'right' | 'center';
+  verticalAlign?: 'top' | 'bottom' | 'center';
 };
 
 export const Columns = system<ColumnsProps, 'div'>(
   ({
     space = 0,
-    // align = 'left',
+    horizontalAlign = 'left',
+    verticalAlign = 'top',
     className,
     children,
     ...props
   }) => {
-    const classNames = useStyles({}, className);
+    // horizontal Alignment
+    let justify = 'flex-start';
+    if (horizontalAlign === 'right') {
+      justify = 'flex-end';
+    } else if (horizontalAlign === 'center') {
+      justify = 'center';
+    }
 
-    let display = 'flex';
-    let flexDirection = 'row';
-    // let alignItems = 'flex-start';
-
-    // if (align === 'left') {
-    //   display = 'flex';
-    //   flexDirection = 'row';
-    //   alignItems = 'flex-start';
-    // }
-
+    // vertical Alignment
+    let alignItems = 'flex-start';
+    if (verticalAlign === 'bottom') {
+      alignItems = 'flex-end';
+    } else if (verticalAlign === 'center') {
+      alignItems = 'center';
+    }
     return (
-      <Box
-        p={space}
-        display={display}
-        flexDirection={flexDirection}
-        maxWidth="100%"
-        className={classNames}
-        {...props}
-      >
-        {React.Children.map(children, (child, index) => {
-          let marginLeft = index === 0 ? -space : 0;
-
-          return React.cloneElement(child as React.ReactElement<any>, {
-            className: useStyles({ pl: space, ml: marginLeft }),
-          });
-          // return (
-          //   <Box
-          //     key={index.toString()}
-          //     display={display}
-          //     flexDirection={flexDirection}
-          //     // alignItems={alignItems}
-          //     pl={space}
-          //     ml={index === 0 ? -space : 0}
-          //     width="33%"
-          //   >
-          //     {child}
-          //   </Box>
-          // );
-        })}
+      <Box p={space} display="flex" className={className}>
+        <Box
+          width={`calc(100% + ${space}px)`}
+          m={`${-(space / 2)}px`}
+          display="flex"
+          flexWrap="wrap"
+          alignItems={alignItems}
+          justifyContent={justify}
+          {...props}
+        >
+          {React.Children.map(
+            children as React.ReactElement,
+            (child: React.ReactElement) => {
+              return React.cloneElement(
+                child,
+                {
+                  className: useStyles({ p: `${space / 2}px` }),
+                },
+                <Box className={child && child.props.className}>
+                  {child.props.children}
+                </Box>
+              );
+            }
+          )}
+        </Box>
       </Box>
     );
   }
