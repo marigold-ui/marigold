0c346afdd32a28f0ab4bc7868e56dcf19cb38bc2
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-02-18 10:31:16 +0100

    yay!

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { HTMLProps } from '@marigold/types';
-import { Box, BoxProps } from '../Box';
-import { system } from '@marigold/system';
+import { ComponentPropsWithRef } from '@marigold/types';
+import { Box } from '../Box';
 
 type ButtonProps = {
   variant?: string;
-} & HTMLProps<'button'>;
+} & ComponentPropsWithRef<'button'>;
 
 export const Button: React.FC<ButtonProps> = ({
   variant = 'primary.large',
@@ -20,24 +19,3 @@ export const Button: React.FC<ButtonProps> = ({
     </Box>
   );
 };
-
-// ----
-
-const Boxx = system<BoxProps, 'div'>(({ as = 'div', children, ...props }) => {
-  const Component = as;
-  return <Component {...props}>{children}</Component>;
-});
-
-export const Button2 = system<ButtonProps, 'button'>(
-  ({ variant = 'primary.large', children, ...props }) => (
-    <Boxx as="button" {...props} variant={`button.${variant}`}>
-      <Box as="span" display="inline-flex" alignItems="center">
-        {children}
-      </Box>
-    </Boxx>
-  )
-);
-
-<Button2 as="a" ref={e => e}>
-  asd
-</Button2>;
