df6550a83bf39f9dceeccc17764b3c2d2028eeb6
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-02-22 09:13:08 +0100

    refa(components): Remove system helper from <Hidden> (#804)

--- packages/components/src/Hidden/Hidden.test.tsx
+++ packages/components/src/Hidden/Hidden.test.tsx
@@ -3,37 +3,22 @@ import { render, screen } from '@testing-library/react';
 import { Hidden } from '@marigold/components';
 
 test('text is not visible, show prop = false', () => {
-  render(<Hidden title="hidden">Hidden</Hidden>);
-  const hidden = screen.getByTitle(/hidden/);
+  render(<Hidden>Default</Hidden>);
+  const hidden = screen.getByText('Default');
 
   expect(hidden).not.toBeVisible();
 });
 
 test('support show prop = true', () => {
-  render(
-    <Hidden title="hidden" show={true}>
-      Default
-    </Hidden>
-  );
-  const hidden = screen.getByTitle(/hidden/);
+  render(<Hidden show={true}>Default</Hidden>);
+  const hidden = screen.getByText('Default');
 
   expect(hidden).toBeVisible();
 });
 
 test('renders correct HTML element', () => {
-  render(<Hidden title="hidden">Default</Hidden>);
-  const hidden = screen.getByTitle(/hidden/);
+  render(<Hidden>Default</Hidden>);
+  const hidden = screen.getByText('Default');
 
   expect(hidden instanceof HTMLSpanElement).toBeTruthy();
-});
-
-test('accepts custom styles prop className', () => {
-  render(
-    <Hidden className="custom-class-name" title="hidden">
-      text
-    </Hidden>
-  );
-  const hidden = screen.getByTitle(/hidden/);
-
-  expect(hidden.className).toMatch('custom-class-name');
 });
