c3be6659f0f63d7115ebc613d595d5d838380aa6
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-22 13:42:15 +0100

    add as prop to Heading

--- packages/components/src/Heading/Heading.test.tsx
+++ packages/components/src/Heading/Heading.test.tsx
@@ -38,12 +38,26 @@ test('accepts other variant than default', () => {
   expect(heading).toHaveStyle(`font-family: Roboto`);
 });
 
-test('renders correct HTML element', () => {
+test('supports default as prop', () => {
+  render(<Heading title="default">Default</Heading>);
+  const heading = screen.getByTitle(/default/);
+
+  expect(heading.tagName).toEqual('H2');
+});
+
+test('accepts other as prop than default', () => {
   render(
-    <ThemeProvider theme={theme}>
-      <Heading title="default">Default</Heading>
-    </ThemeProvider>
+    <Heading as="h3" title="default" variant="h3">
+      Default
+    </Heading>
   );
+  const heading = screen.getByTitle(/default/);
+
+  expect(heading.tagName).toEqual('H3');
+});
+
+test('renders correct HTML element', () => {
+  render(<Heading title="default">Default</Heading>);
   const heading = screen.getByTitle(/default/);
 
   expect(heading instanceof HTMLHeadingElement).toBeTruthy();
