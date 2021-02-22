9205a439b2eee4f1c5c83f8dfb5d0635dac25523
Timo Zehnle <timo.zehnle@reservix.de>
2021-02-22 16:45:51 +0100

    first step of area design

--- packages/components/src/Textarea/Textarea.tsx
+++ packages/components/src/Textarea/Textarea.tsx
@@ -1,22 +1,58 @@
 import React from 'react';
-import { useStyles, system } from '@marigold/system';
+import { useStyles } from '@marigold/system';
+import { Exclamation, Required } from '@marigold/icons';
+import { ComponentProps } from '@marigold/types';
+
+import { ValidationMessage } from '../ValidationMessage';
+import { Label } from '../Label';
+import { Box } from '../Box';
 
 type TextareaProps = {
+  // how to check if theres a label and then htmlFor is required
   variant?: string;
-};
+  label?: string;
+  htmlFor?: string;
+  error?: string;
+  required?: boolean;
+} & ComponentProps<'textarea'>;
 
-export const Textarea = system<TextareaProps, 'textarea'>(
-  ({ variant = 'textarea', children, className, ...props }) => {
-    const classNames = useStyles(
-      {
-        variant: `form.${variant}`,
-      },
-      className
-    );
-    return (
-      <textarea className={classNames} {...props}>
+export const Textarea: React.FC<TextareaProps> = ({
+  variant = 'textarea',
+  htmlFor = 'textarea',
+  label,
+  error,
+  required = false,
+  children,
+  ...props
+}) => {
+  let errorColor = '#dd4142';
+  const errorClassName = useStyles({ color: errorColor });
+
+  return (
+    <Box display="block">
+      {label && (
+        <Label {...props} htmlFor={htmlFor}>
+          {label}
+          {(error || required) && (
+            <Required size={16} className={errorClassName} />
+          )}
+        </Label>
+      )}
+      <Box
+        as="textarea"
+        display="block"
+        border={error && `1px solid ${errorColor}`}
+        variant={`form.${variant}`}
+        {...props}
+      >
         {children}
-      </textarea>
-    );
-  }
-);
+      </Box>
+      {error && (
+        <ValidationMessage>
+          <Exclamation size={16} />
+          {error}
+        </ValidationMessage>
+      )}
+    </Box>
+  );
+};
