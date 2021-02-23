ce16297c97d8e5ac26cde633f20d8fc8b83c507d
Viktoria Schwarz <59830437+viktoria-schwarz@users.noreply.github.com>
2021-02-23 13:15:31 +0100

    chore: add type exports and remove absolute imports (#821)

--- packages/components/src/Checkbox/Checkbox.test.tsx
+++ packages/components/src/Checkbox/Checkbox.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { Checkbox } from '@marigold/components';
+import { Checkbox } from './Checkbox';
 
 test('supports label prop', () => {
   render(<Checkbox label="Test" id="test" title="checkbox" />);
--- packages/components/src/Heading/Heading.test.tsx
+++ packages/components/src/Heading/Heading.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { ThemeProvider } from '@marigold/system';
-import { Heading } from '@marigold/components';
+import { Heading } from './Heading';
 
 const theme = {
   text: {
