ca366ca070bc9e021463ae1fa4246384f4cdb24a
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-03-08 11:14:10 +0100

    refa(components): Remove system helper from <Svg> (#853)
    * refa(components): Remove system helper from <Svg>
    
    * fix lint

--- packages/components/src/Svg/Svg.tsx
+++ packages/components/src/Svg/Svg.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
-import { useStyles, system } from '@marigold/system';
+import { useStyles } from '@marigold/system';
+import { ComponentProps } from '@marigold/types';
 
 export type SvgProps = {
   variant?: string;
   size?: number;
+  title?: string; // For Testing
+} & ComponentProps<'svg'>;
+
+export const Svg: React.FC<SvgProps> = ({
+  variant = 'icon',
+  size = 24,
+  className,
+  children,
+  ...props
+}) => {
+  const classNames = useStyles(
+    {
+      variant: `icon.${variant}`,
+    },
+    className
+  );
+
+  return (
+    <svg
+      className={classNames}
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="currentcolor"
+      {...props}
+    >
+      {children}
+    </svg>
+  );
 };
-
-export const Svg = system<SvgProps, 'svg'>(
-  ({ variant = 'icon', size = 24, className = '', children, ...props }) => {
-    const classNames = useStyles(
-      {
-        variant: `icon.${variant}`,
-      },
-      className
-    );
-
-    return (
-      <svg
-        className={classNames}
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        fill="currentcolor"
-        {...props}
-      >
-        {children}
-      </svg>
-    );
-  }
-);
