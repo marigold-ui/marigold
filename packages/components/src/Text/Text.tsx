0f567f567d89e2d2109fb143f214677cf5f63602
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-03-08 12:08:29 +0100

    refa(components): Remove system helper from <Text> (#861)

--- packages/components/src/Text/Text.tsx
+++ packages/components/src/Text/Text.tsx
@@ -1,34 +1,35 @@
 import React from 'react';
-import { useStyles, system } from '@marigold/system';
+import { useStyles } from '@marigold/system';
+import { ComponentPropsWithRef } from '@marigold/types';
+import { Box, BoxProps } from '../Box';
 
 export type TextProps = {
-  as?: 'p' | 'span';
-  variant?: 'body' | 'heading';
+  className?: string;
+  as?: 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  variant?: string;
   textColor?: string;
+} & ComponentPropsWithRef<'span'> &
+  BoxProps;
+
+export const Text: React.FC<TextProps> = ({
+  as = 'span',
+  variant = 'body',
+  textColor = 'inherit',
+  className,
+  children,
+  ...props
+}) => {
+  const classNames = useStyles(
+    {
+      variant: `text.${variant}`,
+      color: textColor,
+    },
+    className
+  );
+
+  return (
+    <Box as={as} className={classNames} {...props}>
+      {children}
+    </Box>
+  );
 };
-
-export const Text = system<TextProps, 'span'>(
-  ({
-    as = 'span',
-    variant = 'body',
-    textColor = 'inherit',
-    className,
-    children,
-    ...props
-  }) => {
-    const classNames = useStyles(
-      {
-        variant: `text.${variant}`,
-        color: textColor,
-      },
-      className
-    );
-    const Cmp = as;
-
-    return (
-      <Cmp className={classNames} {...props}>
-        {children}
-      </Cmp>
-    );
-  }
-);
