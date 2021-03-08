1bbcc1e82d1282276a6919ae437c4dd41e197101
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-03-08 11:51:12 +0100

    refa(components): Remove system helper from <ValidationMessage>

--- packages/components/src/ValidationMessage/ValidationMessage.tsx
+++ packages/components/src/ValidationMessage/ValidationMessage.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import { useStyles, system } from '@marigold/system';
+import { useStyles } from '@marigold/system';
+import { ComponentProps } from '@marigold/types';
 
 export type ValidationMessageProps = {
   variant?: string;
+} & ComponentProps<'span'>;
+
+export const ValidationMessage: React.FC<ValidationMessageProps> = ({
+  variant = 'negative',
+  children,
+  className,
+  ...props
+}) => {
+  const classNames = useStyles(
+    {
+      variant: `validation.${variant}`,
+      display: 'flex',
+      alignItems: 'center',
+    },
+    className
+  );
+
+  return (
+    <span className={classNames} {...props}>
+      {children}
+    </span>
+  );
 };
-
-export const ValidationMessage = system<ValidationMessageProps, 'span'>(
-  ({ variant = 'negative', children, className, ...props }) => {
-    const classNames = useStyles(
-      {
-        variant: `validation.${variant}`,
-        display: 'flex',
-        alignItems: 'center',
-      },
-      className
-    );
-
-    return (
-      <span className={classNames} {...props}>
-        {children}
-      </span>
-    );
-  }
-);
