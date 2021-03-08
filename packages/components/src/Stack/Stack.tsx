2181f518e7737cbdd374d8624fbaa4183305b6f1
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-03-08 10:06:11 +0100

    refa(components): Remove system helper from <Stack>

--- packages/components/src/Stack/Stack.tsx
+++ packages/components/src/Stack/Stack.tsx
@@ -1,5 +1,5 @@
 import React, { Children } from 'react';
-import { ResponsiveStyleValue, system } from '@marigold/system';
+import { ResponsiveStyleValue } from '@marigold/system';
 import { Box } from '../Box';
 import flattenChildren from 'react-keyed-flatten-children';
 
@@ -8,36 +8,35 @@ export type StackProps = {
   align?: 'left' | 'right' | 'center';
 };
 
-export const Stack = system<StackProps, 'div'>(
-  ({ space = 0, align = 'left', children, ...props }) => {
-    let stackItems = flattenChildren(children);
-    let display = 'flex';
-    let flexDirection = 'column';
-    let alignItems = align === 'right' ? 'flex-end' : 'center';
-    if (align === 'left') {
-      display = 'block';
-      flexDirection = 'row';
-      alignItems = 'flex-start';
-    }
-
-    return (
-      <Box p={space} display={display} flexDirection={flexDirection} {...props}>
-        {Children.map(stackItems, (child, index) => {
-          return (
-            <>
-              <Box
-                display={display}
-                flexDirection={flexDirection}
-                alignItems={alignItems}
-                pt={space}
-                mt={index === 0 ? -space : 0}
-              >
-                {child}
-              </Box>
-            </>
-          );
-        })}
-      </Box>
-    );
+export const Stack: React.FC<StackProps> = ({
+  space = 0,
+  align = 'left',
+  children,
+  ...props
+}) => {
+  let stackItems = flattenChildren(children);
+  let display = 'flex';
+  let flexDirection = 'column';
+  let alignItems = align === 'right' ? 'flex-end' : 'center';
+  if (align === 'left') {
+    display = 'block';
+    flexDirection = 'row';
+    alignItems = 'flex-start';
   }
-);
+
+  return (
+    <Box p={space} display={display} flexDirection={flexDirection} {...props}>
+      {Children.map(stackItems, (child, index) => (
+        <Box
+          display={display}
+          flexDirection={flexDirection}
+          alignItems={alignItems}
+          pt={space}
+          mt={index === 0 ? -space : 0}
+        >
+          {child}
+        </Box>
+      ))}
+    </Box>
+  );
+};
