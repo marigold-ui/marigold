b3ed020a6a7cb503572fb694c213ebe79fcf281c
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2021-04-12 14:25:35 +0200

    Add Layout Navigation and first pages to main project (#924)

---
+++ docs/src/components/Navigation.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import { Text } from '@marigold/components';
+
+export const Navigation: React.FC = () => {
+  return (
+    <div>
+      <Link to="/start">
+        <Text>Home</Text>
+      </Link>
+      <ul>
+        <li>
+          <Text>Design Language</Text>
+          <ul>
+            <li>
+              <Text>Brand</Text>
+            </li>
+            <li>
+              <Text>Logos</Text>
+            </li>
+            <li>
+              <Text>Guidelines</Text>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Text>Design Tokens</Text>
+          <ul>
+            <li>
+              <Text>
+                <Link to="/designTokens/colours">Colours</Link>
+              </Text>
+            </li>
+            <li>
+              <Text>Layout</Text>
+            </li>
+            <li>
+              <Text>Typography</Text>
+            </li>
+            <li>
+              <Text>
+                <Link to="/designTokens/icons">Icons</Link>
+              </Text>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Text>Components</Text>
+          <ul>
+            <li>
+              <Text>
+                <Link to="/components/button">Button</Link>
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Link to="/components/checkbox">Checkbox</Link>
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Link to="/components/field">Field</Link>
+              </Text>
+            </li>
+            <li>
+              <Text>
+                <Link to="/components/input">Input</Link>
+              </Text>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
