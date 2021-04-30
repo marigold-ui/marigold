dec657902b7c0dee5c85c9eb9c3ba60eda816a98
Timo Zehnle <timo.zehnle@reservix.de>
2021-04-30 09:06:53 +0200

    use prettier for this file

--- docs/gatsby-browser.js
+++ docs/gatsby-browser.js
@@ -1,51 +1,60 @@
 import React from 'react';
-import { MDXProvider } from '@mdx-js/react'
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/github'
+import { MDXProvider } from '@mdx-js/react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/github';
 
 const CodeBlock = props => {
-  const className = props.children.props.className || ''
-  const matches = className.match(/language-(?<lang>.*)/)
+  const className = props.children.props.className || '';
+  const matches = className.match(/language-(?<lang>.*)/);
   return (
-    <Highlight {...defaultProps} code={props.children.props.children.trim()} language={
+    <Highlight
+      {...defaultProps}
+      code={props.children.props.children.trim()}
+      language={
         matches && matches.groups && matches.groups.lang
           ? matches.groups.lang
           : ''
       }
-      theme={theme}>
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
+      theme={theme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
           {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({line, key: i})}>
+            <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
-                <span key={key} {...getTokenProps({token, key})} />
+                <span key={key} {...getTokenProps({ token, key })} />
               ))}
             </div>
           ))}
         </pre>
       )}
     </Highlight>
-  )
-}
+  );
+};
 
 const components = {
   table: props => {
-    return <table style={{width: '100%'}} {...props}></table>
+    return <table style={{ width: '100%' }} {...props}></table>;
   },
   th: props => {
-    return <th style={{backgroundColor:'#e3e3e3', padding: '8px'}} {...props}></th>
+    return (
+      <th
+        style={{ backgroundColor: '#e3e3e3', padding: '8px' }}
+        {...props}
+      ></th>
+    );
   },
   tr: props => {
-    return <tr style={{padding: '8px'}} {...props}></tr>
+    return <tr style={{ padding: '8px' }} {...props}></tr>;
   },
   td: props => {
-    return <td style={{padding: '8px'}} {...props}></td>
+    return <td style={{ padding: '8px' }} {...props}></td>;
   },
   pre: props => {
-    return <CodeBlock {...props}/>
-  }
-}
+    return <CodeBlock {...props} />;
+  },
+};
 
-export const wrapRootElement = ({element}) => {
-  return (<MDXProvider components={components}>{element}</MDXProvider>)
-}
+export const wrapRootElement = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>;
+};
