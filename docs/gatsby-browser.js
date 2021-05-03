48da7608b214f01bcc549957b1a3b04ef9db7fff
Timo Zehnle <timo.zehnle@reservix.de>
2021-05-03 15:47:11 +0200

    copy code button

--- docs/gatsby-browser.js
+++ docs/gatsby-browser.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/github';
+import { Button } from '@marigold/components';
 
 const CodeBlock = props => {
+  const [isCopied, setIsCopied] = React.useState(false);
   const className = props.children.props.className || '';
   const matches = className.match(/language-(?<lang>.*)/);
+  const code = props.children.props.children.trim();
   return (
     <Highlight
       {...defaultProps}
-      code={props.children.props.children.trim()}
+      code={code}
       language={
         matches && matches.groups && matches.groups.lang
           ? matches.groups.lang
@@ -18,7 +21,33 @@ const CodeBlock = props => {
       theme={theme}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
+        <pre
+          className={className}
+          style={{ ...style, padding: '20px', position: 'relative' }}
+        >
+          <Button
+            onClick={() => {
+              copyToClipboard(code);
+              setIsCopied(true);
+              setTimeout(() => setIsCopied(false), 3000);
+            }}
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              margin: '8px',
+              padding: '8px 12px',
+              background: '#cccccc',
+              color: 'white',
+              borderRadius: '4px',
+              cursor: 'pointer',
+              fontSize: '14px',
+              fontFamily: 'Inter',
+              lineHeight: '1',
+            }}
+          >
+            {isCopied ? 'Copied 🎉' : 'Copy'}
+          </Button>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
@@ -53,6 +82,18 @@ const components = {
   pre: props => {
     return <CodeBlock {...props} />;
   },
+};
+
+const copyToClipboard = props => {
+  const element = document.createElement('textarea');
+  element.value = props;
+  element.setAttribute('readonly', '');
+  element.style.position = 'absolute';
+  element.style.left = '-9999px';
+  document.body.appendChild(element);
+  element.select();
+  document.execCommand('copy');
+  document.body.removeChild(element);
 };
 
 export const wrapRootElement = ({ element }) => {
