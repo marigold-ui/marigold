3f3116c022e8200ddfbc1ac069caa37553c99b74
viktoria.schwarz <viktoria.schwarz@reservix.de>
2021-05-10 13:35:30 +0200

    format nav

--- docs/src/components/Navigation.tsx
+++ docs/src/components/Navigation.tsx
@@ -5,35 +5,25 @@ import { Text } from '@marigold/components';
 export const Navigation: React.FC = () => {
   return (
     <Text>
-      <Link to="/start">
-        Home
-      </Link>
+      <Link to="/start">Home</Link>
       <ul>
         <li>
           <b>Guides</b>
           <ul>
-            <li>
-              Installation
-            </li>
-            <li>
-              Theme & Variants
-            </li>
+            <li>Installation</li>
+            <li>Theme & Variants</li>
           </ul>
         </li>
         <li>
           <b>Foundation</b>
           <ul>
             <li>
-                <Link to="/designTokens/theme">Theming</Link>
+              <Link to="/designTokens/theme">Theming</Link>
             </li>
+            <li>Layout</li>
+            <li>Box Primitive</li>
             <li>
-             Layout
-            </li>
-            <li>
-              Box Primitive
-            </li>
-            <li>
-                <Link to="/designTokens/icons">Iconography</Link>
+              <Link to="/designTokens/icons">Iconography</Link>
             </li>
             {/* <li>
                 <Link to="/designTokens/colours">Colours</Link>
@@ -43,26 +33,20 @@ export const Navigation: React.FC = () => {
         <li>
           <b>Components</b>
           <ul>
+            <li>Alert</li>
+            <li>Badge</li>
             <li>
-                Alert
+              <Link to="/components/button">Button</Link>
             </li>
             <li>
-                Badge
+              <Link to="/components/checkbox">Checkbox</Link>
+            </li>
+            <li>Column</li>
+            <li>
+              <Link to="/components/field">Field</Link>
             </li>
             <li>
-                <Link to="/components/button">Button</Link>
-            </li>
-            <li>
-                <Link to="/components/checkbox">Checkbox</Link>
-            </li>
-            <li>
-                Column
-            </li>
-            <li>
-                <Link to="/components/field">Field</Link>
-            </li>
-            <li>
-                <Link to="/components/input">Input</Link>
+              <Link to="/components/input">Input</Link>
             </li>
           </ul>
         </li>
