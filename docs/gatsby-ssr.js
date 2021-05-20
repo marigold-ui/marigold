815a8826e00114c957d8808965a8a43ffee7bace
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-05-20 12:34:13 +0200

    remove logs

--- docs/gatsby-ssr.js
+++ docs/gatsby-ssr.js
@@ -3,8 +3,6 @@ import { renderToString } from 'react-dom/server';
 import { createStyleTag } from '@marigold/server';
 
 export const replaceRenderer = ({ bodyComponent, setHeadComponents }) => {
-  console.log(bodyComponent);
-  console.log(renderToString(bodyComponent));
   const Style = createStyleTag(renderToString(bodyComponent));
   setHeadComponents([<Style />]);
 };
