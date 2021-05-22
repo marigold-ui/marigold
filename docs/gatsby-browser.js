6eb75baee11e8afdd3fe3afd71929634c8f8ea3b
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2021-05-22 14:58:10 +0200

    docs: marigold text components to mdx (#1043)

--- docs/gatsby-browser.js
+++ docs/gatsby-browser.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { preToCodeBlock } from 'mdx-utils';
+import { Heading, Text } from '@marigold/components';
 import b2bTheme from '@marigold/theme-b2b';
 import { CodeBlock } from './src/components/CodeBlock';
 
 const components = {
+  p: props => {
+    return <Text as="p" variant="body" {...props}></Text>;
+  },
+  h1: props => {
+    return <Heading as="h1" variant="h1" {...props}></Heading>;
+  },
+  h2: props => {
+    return <Heading as="h2" {...props}></Heading>;
+  },
+  h3: props => {
+    return <Heading as="h3" variant="h3" {...props}></Heading>;
+  },
+  h4: props => {
+    return <Heading as="h4" variant="h4" {...props}></Heading>;
+  },
+  h5: props => {
+    return <Heading as="h5" variant="h5" {...props}></Heading>;
+  },
+  h6: props => {
+    return <Heading as="h6" variant="h6" {...props}></Heading>;
+  },
   inlineCode: props => {
     return (
       <code
@@ -23,7 +45,9 @@ const components = {
     );
   },
   table: props => {
-    return <table style={{ width: '100%' }} {...props}></table>;
+    return (
+      <table style={{ width: '100%', fontFamily: 'Inter' }} {...props}></table>
+    );
   },
   th: props => {
     return (
