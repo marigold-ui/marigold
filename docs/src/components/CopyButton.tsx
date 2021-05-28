90799fbf489e31c8a7223b726c0329c4a1dab44e
Timo Zehnle <timo.zehnle@reservix.de>
2021-05-28 09:50:06 +0200

    docs: copy button variant to ghost.small

--- docs/src/components/CopyButton.tsx
+++ docs/src/components/CopyButton.tsx
@@ -1,39 +1,26 @@
 import React from 'react';
 import { Button } from '@marigold/components';
-import b2bTheme from '@marigold/theme-b2b';
-import { useStyles } from '@marigold/system';
 
 type CopyProps = {
   codeString: string;
 };
 
-const copyButtonStyles = {
-  position: 'absolute',
-  bottom: b2bTheme.space.none,
-  right: b2bTheme.space.none,
-  padding: '8px 12px',
-  background: '#f6f8fa',
-  color: b2bTheme.colors.gray60,
-  border: 'none',
-  cursor: 'pointer',
-  fontSize: b2bTheme.fontSizes.xxsmall,
-  fontFamily: b2bTheme.fonts.body,
-  lineHeight: b2bTheme.lineHeights.heading,
-};
-
 export const CopyButton: React.FC<CopyProps> = ({ codeString }) => {
   const [isCopied, setIsCopied] = React.useState(false);
   return (
-    <Button
-      onClick={() => {
-        copyToClipboard(codeString);
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 3000);
-      }}
-      className={useStyles({ css: copyButtonStyles })}
-    >
-      {isCopied ? 'Copied 🎉' : 'Copy'}
-    </Button>
+    <>
+      <br />
+      <Button
+        variant="ghost.small"
+        onClick={() => {
+          copyToClipboard(codeString);
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 3000);
+        }}
+      >
+        {isCopied ? 'Copied 🎉' : 'Copy'}
+      </Button>
+    </>
   );
 };
 
