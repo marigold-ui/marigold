20e38632dac662d9b30f7925c58f577012cea2ac
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-06-17 15:40:19 +0200

    move provider to where it belongs

--- docs/src/wrapper.tsx
+++ docs/src/wrapper.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { GatsbyBrowser } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
 
+import { ThemeProvider } from '@marigold/system';
+import b2bTheme from '@marigold/theme-b2b';
+
 import { Layout } from './components/Layout';
 import * as mdxComponents from './mdx';
 
@@ -12,6 +15,8 @@ export const WrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
 
 export const WrapRootElement: GatsbyBrowser['wrapRootElement'] = ({
   element,
-}) => {
-  return <MDXProvider components={mdxComponents}>{element}</MDXProvider>;
-};
+}) => (
+  <ThemeProvider theme={b2bTheme}>
+    <MDXProvider components={mdxComponents}>{element}</MDXProvider>;
+  </ThemeProvider>
+);
