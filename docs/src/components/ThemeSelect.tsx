d51fed1b16fc53388b6f4e4903631af29d82f3a2
Timo Zehnle <timo.zehnle@reservix.de>
2021-06-28 17:10:24 +0200

    change ThemeSelect

--- docs/src/components/ThemeSelect.tsx
+++ docs/src/components/ThemeSelect.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useThemeSwitch } from './ThemeSwitch';
-import { useStyles } from '@marigold/system';
+import { Box, SelectProps } from '@marigold/components';
 import unicornTheme from '@marigold/theme-unicorn';
 import b2bTheme from '@marigold/theme-b2b';
 
@@ -9,34 +9,22 @@ export const marigoldThemes = {
   unicornTheme,
 };
 
-const themeSelectStyles = {
-  position: 'absolute',
-  right: 0,
-  padding: '8px',
-  marginTop: '4px',
-  marginRight: '12px',
-  background: '#f6f8fa',
-  color: '#6d6d6d',
-  border: 'none',
-  cursor: 'pointer',
-  fontFamily: 'Inter',
-};
-
-export const ThemeSelect: React.FC = () => {
+export const ThemeSelect: React.FC<SelectProps> = () => {
   const { current, themes, setTheme } = useThemeSwitch();
 
   return (
-    <select
-      className={useStyles({ css: themeSelectStyles })}
+    <Box
+      as="select"
+      variant="select.themeSwitch"
       id="theme-select"
       value={current}
-      onChange={e => setTheme(e.target.value)}
+      onChange={(e: { target: { value: any } }) => setTheme(e.target.value)}
     >
       {Object.keys(themes).map(name => (
         <option key={name} value={name}>
           {name}
         </option>
       ))}
-    </select>
+    </Box>
   );
 };
