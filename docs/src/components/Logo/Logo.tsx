cb533c5f3ebdc221ac8b7585e5eb518d2ec24834
Viktoria Schwarz <59830437+viktoria-schwarz@users.noreply.github.com>
2021-07-12 14:58:30 +0200

    fix: add withPrefix for correct image path (#1178)
    * fix: add withPrefix for correct image path
    
    * add prefix-path
    
    * revert withPrefix

--- docs/src/components/Logo/Logo.tsx
+++ docs/src/components/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { withPrefix } from 'gatsby';
 
 export const Logo: React.FC = () => (
-  <StaticImage src={withPrefix("/logo.png")} alt="Marigold Logo" />
+  <StaticImage src="./logo.png" alt="Marigold Logo" />
 );
