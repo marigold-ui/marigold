f6a08db67acde19d2acbb7edc176bd6b8e51541f
sebald <sebald@users.noreply.github.com>
2021-07-28 06:47:08 +0000

    deploy: 3351fe36adb1666f816b719a6dc851627cf1b3f4

---
+++ component---src-pages-404-tsx-1840383b47f2b4442b1a.js
@@ -0,0 +1,2 @@
+(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[218],{50457:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return o}});var c=n(27378),r=n(12416),u=n(77401),a=n(23497);function o(){return c.createElement(u.f6,{theme:a.Z},c.createElement(r.Heading,{variant:"h1"},"404"),c.createElement(r.Text,null,"oooops"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1840383b47f2b4442b1a.js.map
