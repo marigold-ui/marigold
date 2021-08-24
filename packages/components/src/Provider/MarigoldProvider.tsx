5adf82944332c9ba4b77ea82d31ed897d46b6006
Timo Zehnle <timo.zehnle@reservix.de>
2021-08-24 15:45:09 +0200

    check if OverlayProvider is set

--- packages/components/src/Provider/MarigoldProvider.tsx
+++ packages/components/src/Provider/MarigoldProvider.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { OverlayProvider } from '@react-aria/overlays';
-import { ThemeProvider, ThemeProviderProps } from '@marigold/system';
+import {
+  Theme,
+  ThemeProvider,
+  ThemeProviderProps,
+  useTheme,
+} from '@marigold/system';
+
+export interface ThemeContextValue {
+  theme: Theme;
+}
+
+export const defaultThemeValue: ThemeContextValue = {
+  theme: {},
+};
 
 // a merge of the ThemeProvider and the react-aria OverlayProvider
 export const MarigoldProvider: React.FC<ThemeProviderProps> = ({
   theme,
   children,
 }) => {
+  const outerTheme = useTheme();
+  const outerThemeJson = JSON.stringify(outerTheme, null, 2);
+  const defaultThemeValueJson = JSON.stringify(defaultThemeValue, null, 2);
+  const isTopLevel = outerThemeJson === defaultThemeValueJson;
+
   return (
     <ThemeProvider theme={theme}>
-      <OverlayProvider>{children}</OverlayProvider>
+      {isTopLevel ? <OverlayProvider>{children}</OverlayProvider> : children}
     </ThemeProvider>
   );
 };
