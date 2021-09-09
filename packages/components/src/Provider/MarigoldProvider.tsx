5cb9a5588a16483568cccaaf6288c36080ff06e7
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2021-09-09 10:13:36 +0200

    Revert "fix: remove leaking theme-ui to other packages (#1261)"
    This reverts commit 99632c6e8a427bdfb4f90b9c068af5003150a278.

--- packages/components/src/Provider/MarigoldProvider.tsx
+++ packages/components/src/Provider/MarigoldProvider.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { OverlayProvider } from '@react-aria/overlays';
 import { ThemeProvider, ThemeProviderProps, useTheme } from '@marigold/system';
 import { Global } from '@emotion/react';
+import { css } from '@theme-ui/css';
 
 const GlobalStyles = () => {
-  const { css } = useTheme();
+  const theme = useTheme();
   const styles = css({
     body: { variant: 'root.body' },
     html: { variant: 'root.html' },
-  });
+  })(theme);
 
   return <Global styles={styles} />;
 };
@@ -16,7 +17,7 @@ const GlobalStyles = () => {
 export const MarigoldProvider: React.FC<ThemeProviderProps> = ({
   theme,
   children,
-}) => {
+}) => { 
   return (
     <ThemeProvider theme={theme}>
       <OverlayProvider>
