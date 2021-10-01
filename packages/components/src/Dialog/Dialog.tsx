321f4380ffeecb05304a17842ac7aab212dc78e0
Timo Zehnle <timo.zehnle@reservix.de>
2021-10-01 14:38:03 +0200

    first changes to renew dialog

--- packages/components/src/Dialog/Dialog.tsx
+++ packages/components/src/Dialog/Dialog.tsx
@@ -1,32 +1,83 @@
 import React from 'react';
 import { ComponentProps } from '@marigold/types';
-
+import {
+  OverlayTriggerState,
+  useOverlayTriggerState,
+} from '@react-stately/overlays';
+import { OverlayContainer } from '@react-aria/overlays';
+import { useButton } from '@react-aria/button';
+import { LegacyRef, RefObject } from 'react-helmet/node_modules/@types/react';
 import { Close } from '@marigold/icons';
-import { Button } from '../Button';
+
 import { Box } from '../Box';
+import { Button } from '../Button';
+import { Text } from '../Text';
+
+import { ModalDialog } from './ModalDialog';
 
 export type DialogProps = {
   onClose?: ComponentProps<typeof Button>['onClick'];
-} & ComponentProps<'div'>;
+  title?: string;
+} & OverlayTriggerState &
+  ComponentProps<'div'>;
 
 export const Dialog: React.FC<DialogProps> = ({
   onClose,
   children,
+  title,
   className,
   ...props
 }) => {
+  const closeButtonRef = React.useRef<HTMLElement>() as RefObject<HTMLElement>;
+
+  // useButton ensures that focus management is handled correctly,
+  // across all browsers. Focus is restored to the button once the
+  // dialog closes.
+  const { buttonProps: closeButtonProps } = useButton(
+    {
+      onPress: () => props.close(),
+    },
+    closeButtonRef
+  );
+
   return (
-    <Box display="flex" width="100%">
-      <Box {...props} variant="dialog.wrapper" className={className}>
-        <Box display="flex">
-          <Box variant="dialog.body">{children}</Box>
-          <Box variant="dialog.onClose">
-            <Button variant="close" size="xsmall" onClick={onClose}>
-              <Close size={16} />
-            </Button>
+    <OverlayContainer>
+      <ModalDialog isOpen onClose={props.close} isDismissable>
+        <Box variant="dialog.wrapper" className={className} {...props}>
+          <Box display="flex" justifyContent="space-between">
+            <Box variant="dialog.body">
+              <Box pb="xsmall">
+                <Text>{title}</Text>
+              </Box>
+              {children}
+            </Box>
+            <Box variant="dialog.onClose">
+              <Button
+                variant="close"
+                size="xsmall"
+                {...closeButtonProps}
+                ref={closeButtonRef as LegacyRef<HTMLButtonElement>}
+              >
+                <Close size={16} />
+              </Button>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Box>
+      </ModalDialog>
+    </OverlayContainer>
   );
+};
+
+// use this hook to get the overlayTriggerState and Button props for using the dialog component9i
+export const useDialogButtonProps = () => {
+  const state = useOverlayTriggerState({});
+  const openButtonRef = React.useRef<HTMLElement>() as RefObject<HTMLElement>;
+  const { buttonProps: openButtonProps } = useButton(
+    {
+      onPress: () => state.open(),
+    },
+    openButtonRef
+  );
+
+  return { state, openButtonProps, openButtonRef };
 };
---
+++ packages/components/src/Dialog/ModalDialog.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+  useOverlay,
+  usePreventScroll,
+  useModal,
+  OverlayProps,
+} from '@react-aria/overlays';
+import { useDialog } from '@react-aria/dialog';
+import { FocusScope } from '@react-aria/focus';
+import { AriaDialogProps } from '@react-types/dialog';
+
+import { Box } from '../Box';
+import { RefObject } from 'react-helmet/node_modules/@types/react';
+import { useStyles } from 'packages/system/src/useStyles';
+
+export type ModalDialogProps = OverlayProps & AriaDialogProps;
+
+export const ModalDialog: React.FC<ModalDialogProps> = ({
+  children,
+  ...props
+}) => {
+  // Handle interacting outside the dialog and pressing
+  // the Escape key to close the modal.
+  const ref = React.useRef<HTMLElement>() as RefObject<HTMLElement>;
+  const { overlayProps, underlayProps } = useOverlay(props, ref);
+
+  // Prevent scrolling while the modal is open, and hide content
+  // outside the modal from screen readers.
+  usePreventScroll();
+  const { modalProps } = useModal();
+
+  // Get props for the dialog
+  const { dialogProps } = useDialog(props, ref);
+
+  const modalWrapperClassname = useStyles({
+    css: {
+      position: 'fixed',
+      zIndex: 100,
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      background: 'rgba(0, 0, 0, 0.5)',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  const modalBodyClassname = useStyles({
+    css: {
+      background: 'white',
+      minWidth: '510px',
+      minHeight: '240px',
+    },
+  });
+
+  return (
+    <Box className={modalWrapperClassname} {...underlayProps}>
+      <FocusScope contain restoreFocus autoFocus>
+        <Box
+          {...overlayProps}
+          {...dialogProps}
+          {...modalProps}
+          ref={ref}
+          className={modalBodyClassname}
+        >
+          {children}
+        </Box>
+      </FocusScope>
+    </Box>
+  );
+};
