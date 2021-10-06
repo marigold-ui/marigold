2d32102a9652962904e6b7691be8223be6c824e8
Timo Zehnle <timo.zehnle@reservix.de>
2021-10-06 15:45:45 +0200

    create variants for modal dialog

--- packages/components/src/Dialog/ModalDialog.tsx
+++ packages/components/src/Dialog/ModalDialog.tsx
@@ -8,7 +8,6 @@ import {
 import { useDialog } from '@react-aria/dialog';
 import { FocusScope } from '@react-aria/focus';
 import { AriaDialogProps } from '@react-types/dialog';
-import { useStyles } from '@marigold/system';
 
 import { Box } from '../Box';
 
@@ -29,37 +28,16 @@ export const ModalDialog: React.FC<ModalDialogProps> = ({
 
   const { modalProps } = useModal();
   const { dialogProps } = useDialog(props, ref);
-  const modalWrapperClassname = useStyles({
-    css: {
-      position: 'fixed',
-      zIndex: 100,
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-      background: 'rgba(0, 0, 0, 0.5)',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  const modalBodyClassname = useStyles({
-    css: {
-      background: 'white',
-      minWidth: '510px',
-      minHeight: '240px',
-    },
-  });
 
   return (
-    <Box className={modalWrapperClassname} {...underlayProps}>
+    <Box variant="dialog.modalWrapper" {...underlayProps}>
       <FocusScope contain restoreFocus autoFocus>
         <Box
           {...overlayProps}
           {...dialogProps}
           {...modalProps}
           ref={ref}
-          className={modalBodyClassname}
+          variant="dialog.modalBody"
         >
           {children}
         </Box>
