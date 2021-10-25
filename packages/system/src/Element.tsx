de42ad102bb096e8b61b1a779aafab9f079a767d
Timo Zehnle <timo.zehnle@reservix.de>
2021-10-25 12:14:45 +0200

    create Element component + normalize file

---
+++ packages/system/src/Element.tsx
@@ -0,0 +1,69 @@
+import { jsx } from '@emotion/react';
+import { forwardRef } from 'react';
+import {
+  PolymorphicPropsWithRef,
+  PolymorphicComponentWithRef,
+} from '@marigold/types';
+
+import { getNormalizedStyles } from './normalize';
+import { CSSObject } from './types';
+import { useTheme } from './useTheme';
+
+export type ElementOwnProps = {
+  css?: CSSObject;
+  variant?: string | string[];
+};
+
+export type ElementProps = PolymorphicPropsWithRef<ElementOwnProps, 'div'>;
+
+const isNotEmpty = (val: any) =>
+  !(val && Object.keys(val).length === 0 && val.constructor === Object);
+
+const baseStyles = getNormalizedStyles('base');
+
+/**
+ * Props that we have to remove and want to process to styling the component.
+ */
+const SKIP_PROPS = ['css', 'variant'];
+
+export const Element: PolymorphicComponentWithRef<ElementOwnProps, 'div'> =
+  forwardRef(
+    ({ as = 'div', css: styles = {}, variant, children, ...props }, ref) => {
+      const { css } = useTheme();
+
+      /**
+       * Gather styling related props (css, variant, space props, ...) and put them in a
+       * single `css` prop for emotion. All gathered props will be passed to `@theme-ui/css`
+       * before emotion will process them. This way CSS properties will interpolated based on
+       * the given theme.
+       */
+      const parseProps = (props: { [key: string]: any }) => {
+        const next: any = {};
+
+        for (let key in props) {
+          if (SKIP_PROPS.includes(key)) continue;
+          next[key] = props[key];
+        }
+
+        /**
+         * Transform variant input for `@theme-ui/css`
+         */
+        const variants = Array.isArray(variant)
+          ? variant.map(v => ({ variant: v }))
+          : [{ variant }];
+
+        next.css = () => {
+          return [
+            baseStyles,
+            getNormalizedStyles(as),
+            ...variants.map(v => css(v)),
+            css(styles),
+          ].filter(isNotEmpty);
+        };
+
+        return next;
+      };
+
+      return jsx(as, { ...props, ...parseProps(props), ref }, children);
+    }
+  );
