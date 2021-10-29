e557d2aa965c2aa494276d22f05ab6f5b806e9dd
ti10le <ti10le@users.noreply.github.com>
2021-10-29 05:47:26 +0000

    deploy: 69aa6becf12aa4fddcb771ef97e3677ad7da7276

--- component---src-pages-404-tsx-ea078e785eabef17cfae.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[218],{50457:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var c=n(27378),r=n(12416),u=n(2302),a=n(23497);function o(){return c.createElement(u.f6,{theme:a.Z},c.createElement(r.Heading,{variant:"h1"},"404"),c.createElement(r.Text,null,"oooops"))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-ea078e785eabef17cfae.js.map
