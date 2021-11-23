58cd1fba3b11e0952d0f356807423260b9dbc057
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-11-23 15:24:45 +0100

    docs: add all marigold components to mdx

--- docs/src/wrapper.tsx
+++ docs/src/wrapper.tsx
@@ -2,12 +2,81 @@ import React from 'react';
 import { GatsbyBrowser } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
 
-import { MarigoldProvider, SSRProvider } from '@marigold/components';
+import {
+  MarigoldProvider,
+  SSRProvider,
+  ActionGroup,
+  Alert,
+  Badge,
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  Column,
+  Columns,
+  Dialog,
+  Divider,
+  Field,
+  Heading,
+  Hidden,
+  Image,
+  Label,
+  Link,
+  Menu,
+  MenuItem,
+  Message,
+  Radio,
+  Slider,
+  Select,
+  Stack,
+  Text,
+  Textarea,
+  Input,
+  Container,
+  ValidationMessage,
+  Item,
+  Section,
+} from '@marigold/components';
 import { theme } from './theme';
 
 import { Layout } from './components/Layout';
 import { MarigoldThemeSwitch, themes } from './components/ThemeSwitch';
 import * as mdxComponents from './mdx';
+
+const components = {
+  ActionGroup,
+  Alert,
+  Badge,
+  Box,
+  Button,
+  Card,
+  Checkbox,
+  Column,
+  Columns,
+  Dialog,
+  Divider,
+  Field,
+  Heading,
+  Hidden,
+  Image,
+  Label,
+  Link,
+  Menu,
+  MenuItem,
+  Message,
+  Radio,
+  Slider,
+  Select,
+  Stack,
+  Text,
+  Textarea,
+  Input,
+  Container,
+  ValidationMessage,
+  Item,
+  Section,
+  ...mdxComponents,
+};
 
 export const WrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
   element,
@@ -20,7 +89,7 @@ export const WrapRootElement: GatsbyBrowser['wrapRootElement'] = ({
   <SSRProvider>
     <MarigoldProvider theme={theme}>
       <MarigoldThemeSwitch themes={themes} initial="b2bTheme">
-        <MDXProvider components={mdxComponents}>{element}</MDXProvider>
+        <MDXProvider components={components}>{element}</MDXProvider>
       </MarigoldThemeSwitch>
     </MarigoldProvider>
   </SSRProvider>
