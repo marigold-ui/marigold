cda920f2979acd373a1584fb79439353d9527d16
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-11-30 14:00:43 +0100

    normalize and __css

--- packages/system/src/Element.tsx
+++ packages/system/src/Element.tsx
@@ -12,24 +12,27 @@ import { useTheme } from './useTheme';
 export type ElementOwnProps = {
   css?: CSSObject;
   variant?: string | string[];
+  /**
+   * Use to set base styles for the component
+   * @private - **use with caution ... please!**
+   */
+  __baseCSS?: CSSObject;
 };
 
 export type ElementProps = PolymorphicPropsWithRef<ElementOwnProps, 'div'>;
 
 /**
- * Function expression to check if there is any falsy value or empty object
+ * Check if there is any falsy value or empty object
  */
 const isNotEmpty = (val: any) =>
   !(val && Object.keys(val).length === 0 && val.constructor === Object);
 
-/**
- * Get the normalized base styles
- */
-const baseStyles = getNormalizedStyles('base');
-
 export const Element: PolymorphicComponentWithRef<ElementOwnProps, 'div'> =
   forwardRef(
-    ({ as = 'div', css: styles = {}, variant, children, ...props }, ref) => {
+    (
+      { as = 'div', __baseCSS, css: styles = {}, variant, children, ...props },
+      ref
+    ) => {
       const { css } = useTheme();
 
       /**
@@ -45,8 +48,8 @@ export const Element: PolymorphicComponentWithRef<ElementOwnProps, 'div'> =
           ...props,
           ...{
             css: [
-              baseStyles,
               getNormalizedStyles(as),
+              __baseCSS,
               ...variants.map(v => css(v)),
               css(styles),
             ].filter(isNotEmpty),
