83d8a3739b0c3b3419abe18a4fb6357127fe1c38
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-12-03 22:48:07 +0100

    ups

--- packages/components/src/Provider/MarigoldProvider.tsx
+++ packages/components/src/Provider/MarigoldProvider.tsx
@@ -20,7 +20,6 @@ export const MarigoldProvider: React.FC<ThemeProviderProps> = ({
       {isTopLevel ? (
         <>
           <Global />
-          <Global />
           <OverlayProvider>{children}</OverlayProvider>
         </>
       ) : (
