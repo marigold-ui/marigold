b0ac467edb083e4d079565860e236b92e89a0af2
Timo Zehnle <timo.zehnle@reservix.de>
2021-12-06 13:52:29 +0100

    use Inline in ActionGroup

--- packages/components/src/ActionGroup/ActionGroup.tsx
+++ packages/components/src/ActionGroup/ActionGroup.tsx
@@ -1,43 +1,28 @@
-import React, { Children } from 'react';
-import flattenChildren from 'react-keyed-flatten-children';
+import React from 'react';
 
 import { ResponsiveStyleValue } from '@marigold/system';
 import { ComponentProps } from '@marigold/types';
 
-import { Box } from '../Box';
+import { Inline } from '../Inline';
+import { Stack } from '../Stack';
 
 export type ActionGroupProps = {
-  variant?: string;
-  space?: ResponsiveStyleValue<number | string>;
+  space?: ResponsiveStyleValue<string>;
   verticalAlignment?: boolean;
 } & ComponentProps<'div'>;
 
 export const ActionGroup: React.FC<ActionGroupProps> = ({
-  variant = 'default',
   space = 'none',
   verticalAlignment = false,
   children,
-  className,
   ...props
-}) => {
-  const childStyle = verticalAlignment
-    ? { marginBottom: space }
-    : { marginRight: space };
-  return (
-    <Box variant={`actionGroup.${variant}`} className={className} {...props}>
-      {Children.map(
-        flattenChildren(children),
-        (child, i) =>
-          child !== null &&
-          child !== undefined && (
-            <Box
-              as={verticalAlignment ? 'div' : 'span'}
-              css={i < Children.count(children) - 1 ? childStyle : undefined}
-            >
-              {child}
-            </Box>
-          )
-      )}
-    </Box>
+}) =>
+  verticalAlignment ? (
+    <Stack space={space} {...props}>
+      {children}
+    </Stack>
+  ) : (
+    <Inline space={space} {...props}>
+      {children}
+    </Inline>
   );
-};
