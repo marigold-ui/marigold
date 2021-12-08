8b65071f066d6e4de7b0affbbcb66f7714c64bd4
Sebastian Sebald <sebastian.sebald@reservix.de>
2021-12-08 16:37:31 +0100

    yay but broke svg

--- packages/system/src/Element.tsx
+++ packages/system/src/Element.tsx
@@ -1,5 +1,6 @@
 import { jsx, Theme } from '@emotion/react';
 import { css as transformStyleObject } from '@theme-ui/css';
+import { Property } from 'csstype';
 import { forwardRef } from 'react';
 import {
   PolymorphicPropsWithRef,
@@ -9,6 +10,48 @@ import {
 import { getNormalizedStyles } from './normalize';
 import { CSSObject } from './types';
 
+export type StyleProps = Pick<
+  CSSObject,
+  | 'display'
+  | 'height'
+  | 'width'
+  | 'minWidth'
+  | 'maxWidth'
+  | 'position'
+  | 'top'
+  | 'bottom'
+  | 'right'
+  | 'left'
+  | 'zIndex'
+  | 'p'
+  | 'px'
+  | 'py'
+  | 'pt'
+  | 'pb'
+  | 'pl'
+  | 'pr'
+  | 'm'
+  | 'mx'
+  | 'my'
+  | 'mt'
+  | 'mb'
+  | 'ml'
+  | 'mr'
+  | 'flexDirection'
+  | 'flexWrap'
+  | 'flexShrink'
+  | 'flexGrow'
+  | 'alignItems'
+  | 'justifyContent'
+  | 'bg'
+  | 'border'
+  | 'borderRadius'
+  | 'boxShadow'
+  | 'opacity'
+  | 'overflow'
+  | 'transition'
+>;
+
 export type ElementOwnProps = {
   css?: CSSObject;
   variant?: string | string[];
@@ -17,7 +60,7 @@ export type ElementOwnProps = {
    * @internal Used to set default styles for Marigold components
    */
   __baseCSS?: CSSObject;
-};
+} & StyleProps;
 
 export type ElementProps = PolymorphicPropsWithRef<ElementOwnProps, 'div'>;
 
@@ -27,16 +70,103 @@ export type ElementProps = PolymorphicPropsWithRef<ElementOwnProps, 'div'>;
 const isNotEmpty = (val: any) =>
   !(val && Object.keys(val).length === 0 && val.constructor === Object);
 
+const pickStyleProps = (props: ElementOwnProps) => {
+  const {
+    display,
+    height,
+    width,
+    minWidth,
+    maxWidth,
+    position,
+    top,
+    bottom,
+    right,
+    left,
+    zIndex,
+    p,
+    px,
+    py,
+    pt,
+    pb,
+    pl,
+    pr,
+    m,
+    mx,
+    my,
+    mt,
+    mb,
+    ml,
+    mr,
+    flexDirection,
+    flexWrap,
+    flexShrink,
+    flexGrow,
+    alignItems,
+    justifyContent,
+    bg,
+    border,
+    borderRadius,
+    boxShadow,
+    opacity,
+    overflow,
+    transition,
+    ...rest
+  } = props;
+
+  const styles = {
+    display,
+    height,
+    width,
+    minWidth,
+    maxWidth,
+    position,
+    top,
+    bottom,
+    right,
+    left,
+    zIndex,
+    p,
+    px,
+    py,
+    pt,
+    pb,
+    pl,
+    pr,
+    m,
+    mx,
+    my,
+    mt,
+    mb,
+    ml,
+    mr,
+    flexDirection,
+    flexWrap,
+    flexShrink,
+    flexGrow,
+    alignItems,
+    justifyContent,
+    bg,
+    border,
+    borderRadius,
+    boxShadow,
+    opacity,
+    overflow,
+    transition,
+  };
+
+  return { styles, rest };
+};
+
 const ensureArray = <T extends any>(val?: T | T[]) =>
   Array.isArray(val) ? val : [val];
 
 type CreateStyleProps = Pick<
   ElementProps,
   'as' | '__baseCSS' | 'variant' | 'css'
->;
+> & { styles: StyleProps };
 
 const createThemedStyle =
-  ({ as, __baseCSS, variant, css }: CreateStyleProps) =>
+  ({ as, __baseCSS, variant, styles, css }: CreateStyleProps) =>
   (theme: Theme) => {
     return [
       getNormalizedStyles(as),
@@ -44,22 +174,25 @@ const createThemedStyle =
       ...ensureArray(variant).map(v =>
         transformStyleObject({ variant: v })(theme)
       ),
+      transformStyleObject(styles)(theme),
       transformStyleObject(css)(theme),
     ].filter(isNotEmpty);
   };
 
 export const Element: PolymorphicComponentWithRef<ElementOwnProps, 'div'> =
   forwardRef(
-    ({ as = 'div', __baseCSS, css = {}, variant, children, ...props }, ref) =>
-      jsx(
+    ({ as = 'div', __baseCSS, css = {}, variant, children, ...props }, ref) => {
+      const { styles, rest } = pickStyleProps(props);
+      return jsx(
         as,
         {
-          ...props,
+          ...rest,
           ...{
-            css: createThemedStyle({ as, __baseCSS, variant, css }),
+            css: createThemedStyle({ as, __baseCSS, variant, styles, css }),
           },
           ref,
         },
         children
-      )
+      );
+    }
   );
