4cc0dae60a478532d2e89b2723f390e470f9bb98
Timo Zehnle <timo.zehnle@reservix.de>
2021-12-08 08:58:01 +0100

    feat(docs): copy svg icon to clipboard

---
+++ docs/src/components/CopySVGIcon.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement, useState } from 'react';
+import { renderToString } from 'react-dom/server';
+import Snackbar from '@mui/material/Snackbar';
+import Tooltip from '@mui/material/Tooltip';
+
+import { Button } from '@marigold/components';
+
+import { copyToClipboard } from './CopyButton';
+
+export const CopySVGIcon: React.FC = ({ children }) => {
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  // remove class prop from stringified html element
+  const stringifiedChildren = renderToString(children as ReactElement);
+  const removeClassRegex = / class="[a-zA-Z0-9:;.\s()\-,]*"/;
+  const finalString = stringifiedChildren.replace(removeClassRegex, '');
+
+  return (
+    <>
+      <Tooltip title="Copy SVG" placement="right" arrow>
+        <Button
+          variant="action"
+          onClick={() => {
+            copyToClipboard(finalString);
+            setOpenSnackbar(true);
+          }}
+        >
+          {children}
+        </Button>
+      </Tooltip>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={5000}
+        onClose={() => setOpenSnackbar(false)}
+        message="Icon was copied as SVG to clipboard"
+      />
+    </>
+  );
+};
