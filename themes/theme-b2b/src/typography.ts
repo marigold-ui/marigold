811a07da430641df41533ef8e146f2e7c5cbe7dc
Timo Zehnle <timo.zehnle@reservix.de>
2021-12-20 12:07:01 +0100

    feat(b2b-theme): split theme + make mor consistent

---
+++ themes/theme-b2b/src/typography.ts
@@ -0,0 +1,96 @@
+export const text = {
+  root: {
+    fontFamily: 'body',
+    fontSize: 'xsmall',
+    lineHeight: 'body',
+    fontWeight: 'body',
+  },
+  span: {
+    display: 'inline-block',
+  },
+  p: {
+    display: 'inline-block',
+    mt: 'none',
+    mr: 'none',
+    mb: 'xsmall',
+    ':last-child': {
+      marginBottom: 'small',
+    },
+  },
+} as const;
+
+export const typography = {
+  fonts: {
+    body: 'Inter',
+    heading: 'Inter Black',
+  },
+  fontSizes: {
+    xxsmall: '0.875rem',
+    xsmall: '1rem',
+    small: '1.125rem',
+    medium: '1.25rem',
+    large: '1.5rem',
+    xlarge: '2rem',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 900,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  text: {
+    body: {
+      ...text.root,
+      ...text.span,
+    },
+    headline1: {
+      ...text.root,
+      fontWeight: 'heading',
+      fontSize: 'xlarge',
+      m: 'none',
+    },
+    headline2: {
+      ...text.root,
+      fontWeight: 'heading',
+      fontSize: 'large',
+      m: 'none',
+    },
+    headline3: {
+      ...text.root,
+      fontWeight: 'heading',
+      fontSize: 'medium',
+      m: 'none',
+    },
+    headline4: {
+      ...text.root,
+      fontWeight: 'heading',
+      fontSize: 'small',
+      m: 'none',
+    },
+    headline5: {
+      ...text.root,
+      fontWeight: 'heading',
+      fontSize: 'xsmall',
+      m: 'none',
+    },
+    headline6: {
+      ...text.root,
+      fontSize: 'xsmall',
+      textTransform: 'uppercase',
+      m: 'none',
+    },
+    link: {
+      color: 'blue60',
+      ':hover': {
+        textDecoration: 'none',
+      },
+    },
+    menuItemLink: {
+      color: 'text',
+      textDecoration: 'none',
+    },
+  },
+} as const;
