832a2718fb5513699b81f3fc3c028c7188bd0ee8
Timo Zehnle <timo.zehnle@reservix.de>
2022-01-19 08:03:08 +0100

    change test

--- packages/system/src/conditional.test.tsx
+++ packages/system/src/conditional.test.tsx
@@ -15,18 +15,7 @@ test('returns the variant and the conditional states', () => {
 });
 
 test('returns only truthy states', () => {
-  const variant = conditional('input', { checked: true, disabled: false });
-  expect(variant).toMatchInlineSnapshot(`
-    [
-      "input",
-      "input.:checked",
-    ]
-  `);
-});
-
-test('removes dot from the end of the variant string', () => {
-  // This happens if you have variant='' in your component and define __default in your theme
-  const variant = conditional('input.', { checked: true, disabled: false });
+  const variant = conditional('input', { checked: true, error: false });
   expect(variant).toMatchInlineSnapshot(`
     [
       "input",
