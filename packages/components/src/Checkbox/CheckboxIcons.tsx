4452b39c822fa82671530e4c475c0cf1df967c60
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2022-01-21 12:12:48 +0100

    feat(comp): Checkbox component rewrite (#1651)
    Co-authored-by: Sebastian Sebald <sebastian.sebald@reservix.de>

--- packages/components/src/Checkbox/CheckboxIcons.tsx
+++ packages/components/src/Checkbox/CheckboxIcons.tsx
@@ -1,49 +1,59 @@
 import React from 'react';
-import { SVG } from '@marigold/system';
+import { conditional, State, SVG } from '@marigold/system';
 
 import { Box } from '../Box';
 
-export const CheckboxChecked = ({ disabled = false, ...props }) => (
-  <SVG width="16" height="32" viewBox="0 0 16 32" fill="none" {...props}>
-    <Box
-      as="rect"
-      x="0.5"
-      y="8.5"
-      width="15px"
-      height="15px"
-      rx="1.5"
-      variant={disabled ? 'checkbox.checked.disabled' : 'checkbox.checked'}
-    />
-    <Box
-      as="path"
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M13.9571 12.8338L12.4085 11.2852L6.08699 17.6007L3.59887 15.1126L2.04163 16.6588L6.08682 20.704L13.9571 12.8338Z"
-      variant="checkbox.checked.icon"
-    />
-  </SVG>
-);
+// Checkbox Icon
+// ---------------
+export type CheckboxIconProps = {
+  variant?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  error?: boolean;
+  children?: never;
+};
 
-export const CheckboxUnchecked = ({
+export const CheckboxIcon: React.FC<CheckboxIconProps> = ({
+  variant = '',
+  checked = false,
   disabled = false,
   error = false,
-  ...props
-}) => (
-  <SVG width="16" height="32" viewBox="0 0 16 32" fill="none" {...props}>
-    <Box
-      as="rect"
-      x="0.5"
-      y="8.5"
-      width="15px"
-      height="15px"
-      rx="1.5"
-      variant={
-        disabled
-          ? 'checkbox.unchecked.disabled'
-          : error
-          ? 'checkbox.unchecked.error'
-          : 'checkbox.unchecked'
+}) => {
+  const conditionalStates: State = disabled
+    ? {
+        disabled: disabled,
       }
-    />
-  </SVG>
-);
+    : {
+        checked: checked,
+        error: error,
+      };
+
+  return (
+    <SVG
+      width="16"
+      height="32"
+      viewBox="0 0 16 32"
+      fill="none"
+      aria-hidden="true"
+    >
+      <Box
+        as="rect"
+        x="0.5"
+        y="8.5"
+        width="15px"
+        height="15px"
+        rx="1.5"
+        variant={conditional(`checkbox.${variant}`, conditionalStates)}
+      />
+      {checked && (
+        <Box
+          __baseCSS={{ fill: 'gray00' }}
+          as="path"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M13.9571 12.8338L12.4085 11.2852L6.08699 17.6007L3.59887 15.1126L2.04163 16.6588L6.08682 20.704L13.9571 12.8338Z"
+        />
+      )}
+    </SVG>
+  );
+};
