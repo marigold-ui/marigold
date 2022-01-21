8f21241923c395a950f38f0b6fd922774f2a1f80
sebald <sebald@users.noreply.github.com>
2022-01-21 08:26:52 +0000

    deploy: af4855058358e75a6ae9f8594f1b4d3d4d383f3c

--- component---src-pages-404-tsx-b42592274c85848f8290.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_marigold_docs=self.webpackChunk_marigold_docs||[]).push([[218],{50457:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var a=t(27378),r=t(16263);function l(){return a.createElement(a.Fragment,null,a.createElement(r.Text,{as:"h1",variant:"headline1"},"404"),a.createElement(r.Text,null,"oooops"))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-b42592274c85848f8290.js.map
