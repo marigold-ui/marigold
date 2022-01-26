cdeb395fe3d8d12cd991fc22ff6c43cc98f227f9
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-01-26 21:00:48 +0100

    Updates

---
+++ component---src-pages-404-tsx-92dabc53d067d3b2c128.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_marigold_docs=self.webpackChunk_marigold_docs||[]).push([[218],{50457:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var a=t(27378),r=t(40053);function l(){return a.createElement(a.Fragment,null,a.createElement(r.Text,{as:"h1",variant:"headline1"},"404"),a.createElement(r.Text,null,"oooops"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-92dabc53d067d3b2c128.js.map
---
+++ component---src-pages-index-tsx-dfc5ed1c4b34be1a095a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[691],{86237:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var a=n(27378),c=n(12416),r=n(26796),u=n(23497);function o(){return a.createElement(r.f6,{theme:u.Z},a.createElement(c.Heading,{variant:"h1"},"Welcome to Marigold"),a.createElement(c.Text,null,"The design system based on Emotion and theme-ui"))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-dfc5ed1c4b34be1a095a.js.map
