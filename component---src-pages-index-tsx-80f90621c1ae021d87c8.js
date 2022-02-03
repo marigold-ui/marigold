47394233878087033f6433cfcd81903b4be6f5d2
sebald <sebald@users.noreply.github.com>
2022-02-03 19:46:14 +0000

    deploy: 9b010ca93ced32a5796f220dac787fb42b4a2ea0

--- component---src-pages-index-tsx-80f90621c1ae021d87c8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_marigold_docs=self.webpackChunk_marigold_docs||[]).push([[691],{86237:function(n,u,t){t.r(u),t.d(u,{default:function(){return a}});var e=t(27378),i=t(87296);function a(){return(0,e.useEffect)((function(){(0,i.navigate)("/guides/installation")}),[]),null}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-80f90621c1ae021d87c8.js.map
