c6a16a4f58db5d9f0b72a7c994aa5c7d6c4277d5
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-02-22 13:54:32 +0100

    lineheights

--- themes/create-theme/src/vars/typography.ts
+++ themes/create-theme/src/vars/typography.ts
@@ -1,11 +1,22 @@
 // Typography (from https://open-props.style/#typography)
 // ---------------
-const family = {
+const font = {
   sans: 'system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif',
   serif: 'ui-serif,serif',
   mono: 'Dank Mono,Operator Mono,Inconsolata,Fira Mono,ui-monospace,SF Mono,Monaco,Droid Sans Mono,Source Code Pro,monospace',
 } as const;
 
+const lineHeight = {
+  '00': 0.95,
+  '0': 1.1,
+  '1': 1.25,
+  '2': 1.375,
+  '3': 1.5,
+  '4': 1.75,
+  '5': 2,
+};
+
 export const typography = {
-  font: family,
+  font,
+  lineHeight,
 };
