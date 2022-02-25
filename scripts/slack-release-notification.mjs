aeedd60b64c2c4dcda83a714bc44cfe1c3b81836
sarahgm <sarah.gosmann@reservix.de>
2022-02-25 10:42:52 +0100

    - promises to async

--- scripts/slack-release-notification.mjs
+++ scripts/slack-release-notification.mjs
@@ -2,46 +2,40 @@
 
 import SlackNotify from 'slack-notify';
 import {config} from 'dotenv';
-import { createRequire } from "module"; 
 
-const require = createRequire(import.meta.url); 
 const json = require("../packages/components/package.json");
-
 const result = config({path: 'local.env'});
 const slack = SlackNotify(result.parsed.SLACK_NOTIFICATION);
 
-const message = ':information_source: New Release v' + json.version + ' :tada:';
-
-slack.send({
-    "blocks": [
-        {   
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": message
-            }
-              
-        }, 
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": 'Click below to see all *changes*'
-            },
-        },
-        {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": " <https://github.com/marigold-ui/marigold/blob/main/packages/components/CHANGELOG.md|:marigold: *View Changelog* >"
-			}
-		}
-    ]
-
-}).then(() => {
-    console.log('done!');
-}).catch((err) => {
-    console.error(err);
-});
-
-
+async function slackNotification() {
+    const message = ':information_source: New Release v' + json.version + ' :tada:';
+    try{
+        await slack.send({
+            "blocks": [
+                {   
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": message
+                    }
+                },{
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": 'Click below to see all *changes*'
+                    },
+                },{
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": " <https://github.com/marigold-ui/marigold/blob/main/packages/components/CHANGELOG.md|:marigold: *View Changelog*>"
+                    }
+                }
+            ]
+        });
+        console.log('Done!');
+    } catch (e){
+        console.log('Failed! '+e);
+    }
+};
+slackNotification()
