abfea0755aae3817c37db17a891a46e7d9e23cff
Timo Zehnle <timo.zehnle@reservix.de>
2022-03-15 11:41:26 +0100

    feat(comp): renew Container component

--- packages/components/src/Container/Container.tsx
+++ packages/components/src/Container/Container.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { ComponentProps } from '@marigold/types';
+import { size as tokenSize } from '@marigold/tokens';
 
 import { Box } from '../Box';
 
-export interface ContainerProps extends ComponentProps<'div'> {}
+export interface ContainerProps extends ComponentProps<'div'> {
+  contentType?: 'content' | 'header';
+  size?: keyof typeof tokenSize.content | keyof typeof tokenSize.header;
+  align?: 'left' | 'right' | 'center';
+}
 
-export const Container: React.FC<ContainerProps> = ({ children, ...props }) => (
-  <Box {...props} width="100%">
+const ALIGNMENT = {
+  left: 'flex-start',
+  center: 'center',
+  right: 'flex-end',
+};
+
+export const Container: React.FC<ContainerProps> = ({
+  contentType = 'content',
+  size = 'medium',
+  align = 'left',
+  children,
+  ...props
+}) => (
+  <Box
+    display="flex"
+    flexDirection="column"
+    maxWidth={tokenSize[contentType][size]}
+    alignItems={ALIGNMENT[align]}
+    {...props}
+  >
     {children}
   </Box>
 );
