ec4f8f228f05e88f2b9f17784b59f3cfc8ceabe7
Timo Zehnle <59875255+ti10le@users.noreply.github.com>
2022-03-16 08:29:56 +0100

    feat(comp): renew Container component (#1868)
    * release: changelog
    
    * feat(comp): renew Container component
    
    * Create smooth-ligers-dream.md
    
    * fix text in mdx file

--- packages/components/src/Container/Container.test.tsx
+++ packages/components/src/Container/Container.test.tsx
@@ -4,6 +4,96 @@ import { render, screen } from '@testing-library/react';
 import { Container } from './Container';
 import { Text } from '../Text';
 
+test('supports default contentType content', () => {
+  render(
+    <Container data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`maxWidth: 45ch`);
+});
+
+test('supports contentType header', () => {
+  render(
+    <Container contentType="header" data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`maxWidth: 25ch`);
+});
+
+test('supports default size', () => {
+  render(
+    <Container data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`maxWidth: 45ch`);
+});
+
+test('supports size small', () => {
+  render(
+    <Container size="small" data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`maxWidth: 20ch`);
+});
+
+test('supports size large', () => {
+  render(
+    <Container size="large" data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`maxWidth: 60ch`);
+});
+
+test('supports size and contentType', () => {
+  render(
+    <Container size="large" contentType="header" data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`maxWidth: 35ch`);
+});
+
+test('supports default align left', () => {
+  render(
+    <Container data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`alignItems: flex-start`);
+});
+
+test('supports align center', () => {
+  render(
+    <Container align="center" data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`alignItems: center`);
+});
+
+test('supports align right', () => {
+  render(
+    <Container align="right" data-testid="container">
+      <Text>sdf</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveStyle(`alignItems: flex-end`);
+});
+
 test('renders correct HTML element', () => {
   render(
     <Container data-testid="container">
@@ -12,14 +102,4 @@ test('renders correct HTML element', () => {
   );
   const container = screen.getByTestId(/container/);
   expect(container instanceof HTMLDivElement).toBeTruthy();
-});
-
-test('has default width', () => {
-  render(
-    <Container data-testid="container">
-      <Text>text</Text>
-    </Container>
-  );
-  const container = screen.getByTestId(/container/);
-  expect(container).toHaveStyle(`width: 100%`);
 });
