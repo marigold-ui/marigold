803e9e596e87cecbf2da97f9294e0d436dcf04f0
Timo Zehnle <timo.zehnle@reservix.de>
2022-03-22 10:31:26 +0100

    feat(comp): introduce Tiles component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -28,6 +28,7 @@ export * from './Switch';
 export * from './Stack';
 export * from './Text';
 export * from './Textarea';
+export * from './Tiles';
 export * from './Tooltip';
 export * from './Input';
 export * from './Container';
