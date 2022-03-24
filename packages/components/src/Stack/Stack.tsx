3cf378e9c80f700e78eaafcfc0701a20e29e37d0
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-03-24 16:51:58 +0100

    feat: `<Stack>` supports rendering as list (#1898)

--- packages/components/src/Stack/Stack.tsx
+++ packages/components/src/Stack/Stack.tsx
@@ -4,6 +4,7 @@ import { ResponsiveStyleValue } from '@marigold/system';
 import { Box } from '../Box';
 
 export interface StackProps {
+  as?: 'div' | 'ul' | 'ol';
   space?: ResponsiveStyleValue<string>;
   align?: 'left' | 'right' | 'center';
 }
@@ -22,10 +23,11 @@ export const Stack: React.FC<StackProps> = ({
 }) => (
   <Box
     {...props}
-    __baseCSS={{ gap: space }}
     display="flex"
     flexDirection="column"
     alignItems={ALIGNMENT[align]}
+    p="0"
+    css={{ gap: space }}
   >
     {children}
   </Box>
