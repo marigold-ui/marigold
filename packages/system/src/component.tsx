396c63daf07fd8f837335dd5d9eb1a6332a1c917
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-04-05 11:38:57 +0200

    starting

---
+++ packages/system/src/component.tsx
@@ -0,0 +1,15 @@
+/**
+ * 1. transform object to "variant string"
+ * 2. pass to Box' variant prop
+ */
+
+// const Button = ({}) => {
+//   const styles = f({ component: 'Button', size: 'small', variant: 'primary' }); // base + size + variant
+
+//   return <Box css={{}} />;
+// };
+
+export const useStyleConfig = (
+  componentName: string,
+  { size, variant, state }
+) => {};
---
+++ packages/system/src/components.test.ts
@@ -0,0 +1,11 @@
+test('get base styles for a component');
+test('get variant styles for a component');
+test('get size styles for a component');
+test('get state styles for a component');
+
+test('override order: base < variant < size < state');
+
+// example 'Button.state.hover' => 'Button:hover'
+test('allow to transform styles');
+
+test('usage with <Box>');
