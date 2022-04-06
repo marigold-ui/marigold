89f30f76a49dbad2ec7bdb3403dd35bc3a0c8f9b
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-04-06 09:48:31 +0200

    add test cases

--- packages/system/src/components.test.tsx
+++ packages/system/src/components.test.tsx
@@ -1,18 +1,23 @@
 import { useComponentStyles } from './component';
 
 test('get base styles for a component');
+test('get base styles for a component (with parts)');
 test('get variant styles for a component');
+test('get variant styles for a component (with parts)');
 test('get size styles for a component');
+test('get size styles for a component (with parts)');
 test('get state styles for a component');
+test('get state styles for a component (with parts)');
 
 test('override order: base < variant < size < state');
+test('override order: base < variant < size < state (with parts)');
 
 // example 'Button.state.hover' => 'Button:hover'
 test('transform state styles');
-
-test('support multiple parts');
+test('transform state styles (with parts)');
 
 test('usage with <Box>');
+test('usage with <Box> (with parts)');
 
 const Component = () => {
   const r = useComponentStyles('name');
