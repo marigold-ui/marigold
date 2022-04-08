96e29df524ce9df379f077b6b8dba9b5d4b24777
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-04-08 22:35:52 +0200

    transform pseudos working

---
+++ packages/system/src/components/Box/utils.test.ts
@@ -0,0 +1,132 @@
+import { transformPseudos } from './utils';
+
+describe('transform states', () => {
+  test('returns a css object', () => {
+    expect(transformPseudos({})).toEqual({});
+    expect(
+      transformPseudos({
+        color: '#fa5252',
+        padding: 10,
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "color": "#fa5252",
+        "padding": 10,
+      }
+    `);
+    expect({
+      maring: 10,
+      '> *': {
+        marginTop: 4,
+      },
+    }).toMatchInlineSnapshot(`
+      {
+        "> *": {
+          "marginTop": 4,
+        },
+        "maring": 10,
+      }
+    `);
+  });
+
+  test.each([
+    {
+      name: ':hover',
+      input: {
+        '&:hover': {},
+      },
+      expected: {
+        '&:hover, &[data-hover]': {},
+      },
+    },
+    {
+      name: ':focus',
+      input: {
+        '&:focus': {},
+      },
+      expected: {
+        '&:focus, &[data-focus]': {},
+      },
+    },
+    {
+      name: ':active',
+      input: {
+        '&:active': {},
+      },
+      expected: {
+        '&:active, &[data-active]': {},
+      },
+    },
+    {
+      name: ':disabled',
+      input: {
+        '&:disabled': {},
+      },
+      expected: {
+        '&[disabled], &[aria-disabled=true], &[data-disabled]': {},
+      },
+    },
+    {
+      name: ':read-only',
+      input: {
+        '&:read-only': {},
+      },
+      expected: {
+        '&[readonly], &[aria-readonly=true], &[data-readonly]': {},
+      },
+    },
+    {
+      name: ':checked',
+      input: {
+        '&:checked': {},
+      },
+      expected: {
+        '&[aria-checked=true], &[data-checked]': {},
+      },
+    },
+    {
+      name: ':indeterminate',
+      input: {
+        '&:indeterminate': {},
+      },
+      expected: {
+        '&:indeterminate, &[aria-checked=mixed], &[data-indeterminate]': {},
+      },
+    },
+    {
+      name: ':invalid',
+      input: {
+        '&:invalid': {},
+      },
+      expected: {
+        '&:invalid, &[aria-invalid=true], &[data-invalid]': {},
+      },
+    },
+  ])('tramsform "$name" to selector', ({ input, expected }) => {
+    expect(transformPseudos(input)).toEqual(expected);
+  });
+
+  test('transform nested pseudos', () => {
+    expect(
+      transformPseudos({
+        '&:hover': {
+          color: '#1864ab',
+          '&:focus': {
+            outline: '1px solid #228be6',
+          },
+        },
+      })
+    ).toMatchInlineSnapshot(`
+      {
+        "&:hover, &[data-hover]": {
+          "&:focus, &[data-focus]": {
+            "outline": "1px solid #228be6",
+          },
+          "color": "#1864ab",
+        },
+      }
+    `);
+  });
+});
+
+// -> add to box and test it there
