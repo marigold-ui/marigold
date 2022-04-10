44079118273808008f555fd10e36aaefaca26014
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-04-10 12:44:52 +0200

    remove unused import

--- packages/system/src/components/Box/Box.test.tsx
+++ packages/system/src/components/Box/Box.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { ThemeProvider } from '../../hooks';
 import { normalize } from '../../normalize';
