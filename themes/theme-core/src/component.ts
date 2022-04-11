5655157e9dadbe95f6269b7d5e557a08808fd0d5
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-04-11 12:59:54 +0200

    update alle themes with ne badge styles

--- themes/theme-core/src/component.ts
+++ themes/theme-core/src/component.ts
@@ -327,21 +327,7 @@ const slider = {
   },
 } as const;
 
-export const components = {
-  badge: {
-    __default: {
-      display: 'inline-flex',
-      alignItems: 'center',
-      fontFamily: 'body',
-      fontSize: 'xsmall',
-      fontWeight: 'body',
-      borderRadius: 'large',
-      border: '2px solid transparent',
-      whiteSpace: 'nowrap',
-      padding: '0.25rem 0.75rem',
-      mx: '0.5rem',
-    },
-  },
+export const component = {
   button: {
     __default: {
       p: 0,
