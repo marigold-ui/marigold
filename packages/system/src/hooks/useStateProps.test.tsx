212ff5357a3eb05ba7d094e0548db29996032899
Timo Zehnle <timo.zehnle@reservix.de>
2022-04-11 14:03:58 +0200

    fix typo

--- packages/system/src/hooks/useStateProps.test.tsx
+++ packages/system/src/hooks/useStateProps.test.tsx
@@ -53,7 +53,7 @@ test('return only set states', () => {
   `);
 });
 
-test('worls with empty object', () => {
+test('works with empty object', () => {
   const { result } = renderHook(() => useStateProps({}));
 
   expect(result.current).toMatchInlineSnapshot(`{}`);
