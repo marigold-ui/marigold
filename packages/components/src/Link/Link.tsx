45116af56f06e26579321719a02f111d549488ba
sarahgm <sarah.gosmann@reservix.de>
2022-04-13 16:11:13 +0200

    fix

--- packages/components/src/Link/Link.tsx
+++ packages/components/src/Link/Link.tsx
@@ -1,10 +1,6 @@
 import React, { useRef } from 'react';
 import { useLink } from '@react-aria/link';
-import {
-  conditional,
-  ThemeExtension,
-  useComponentStyles,
-} from '@marigold/system';
+import { ThemeExtension, useComponentStyles } from '@marigold/system';
 import { PolymorphicComponent, PolymorphicProps } from '@marigold/types';
 
 import { Box, BoxOwnProps } from '../Box';
