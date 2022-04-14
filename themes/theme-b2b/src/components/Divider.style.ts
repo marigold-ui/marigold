07eb414935a8748e8f5bae8bdd7abbb66e2676dd
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-04-14 16:14:48 +0200

    remove margin

--- themes/theme-b2b/src/components/Divider.style.ts
+++ themes/theme-b2b/src/components/Divider.style.ts
@@ -2,18 +2,15 @@ import type { Theme } from '@marigold/components';
 
 export const Divider: Theme['components']['Divider'] = {
   base: {
-    my: 'xsmall',
     width: '100%',
     height: '1px',
     bg: 'text',
   },
   variant: {
     bold: {
-      my: 'xsmall',
       height: '2px',
     },
     section: {
-      my: 'xxsmall',
       bg: 'gray50',
     },
   },
--- themes/theme-core/src/components/Divider.style.ts
+++ themes/theme-core/src/components/Divider.style.ts
@@ -2,14 +2,12 @@ import type { Theme } from '@marigold/components';
 
 export const Divider: Theme['components']['Divider'] = {
   base: {
-    my: 'xsmall',
     width: '100%',
     height: '1px',
     bg: 'text',
   },
   variant: {
     bold: {
-      my: 'xsmall',
       height: '2px',
     },
   },
