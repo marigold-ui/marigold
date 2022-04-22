18b519b898cdcc176a2a866c2e9014a5eecb9225
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-04-22 13:16:18 +0200

    docs: fix styling

--- docs/src/theme/components/Text.style.ts
+++ docs/src/theme/components/Text.style.ts
@@ -2,10 +2,8 @@ import type { Theme } from '@marigold/components';
 
 export const Text: Theme['components']['Text'] = {
   base: {
-    body: {
-      color: 'gray.40',
-      marginBottom: 'medium',
-    },
+    color: 'gray.40',
+    marginBottom: 'medium',
   },
   variant: {
     muted: {
