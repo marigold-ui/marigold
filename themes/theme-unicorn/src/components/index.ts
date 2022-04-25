b2ff460e866cdde44e7fb7074f902866f29cf8a8
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-04-25 11:10:34 +0200

    styles

--- themes/theme-unicorn/src/components/index.ts
+++ themes/theme-unicorn/src/components/index.ts
@@ -11,6 +11,7 @@ export * from './Input.style';
 export * from './Label.style';
 export * from './Link.style';
 export * from './Menu.style';
+export * from './Radio.style';
 export * from './Table.style';
 export * from './Text.style';
 export * from './TextArea.style';
