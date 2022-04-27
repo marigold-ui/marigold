02d13e84a27ac075587f7278c8c858aeee884f64
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-04-27 10:48:50 +0200

    feat: Support for `:focus-visible` and `:selected` pseudos (#2041)

--- packages/system/src/hooks/useStateProps.ts
+++ packages/system/src/hooks/useStateProps.ts
@@ -1,18 +1,21 @@
 import { useRef } from 'react';
 import isEqual from 'react-fast-compare';
+import { KebabCase } from '@marigold/types';
 
 export type ComponentState =
   | 'hover'
   | 'focus'
+  | 'focusVisible'
   | 'active'
   | 'visited'
   | 'disabled'
   | 'readOnly'
   | 'checked'
+  | 'selected'
   | 'indeterminate'
   | 'error';
 
-export type StateAttrKeyProps = `data-${Lowercase<ComponentState>}`;
+export type StateAttrKeyProps = `data-${KebabCase<ComponentState>}`;
 
 export type StateAttrProps = {
   [key in StateAttrKeyProps]?: '';
@@ -21,6 +24,13 @@ export type StateAttrProps = {
 export type UseStateProps = {
   [key in ComponentState]?: boolean;
 };
+
+/**
+ * Stolen from https://github.com/joakimbeng/kebab-case
+ */
+const KEBAB_REGEX = /[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g;
+const toKebap = (val: string): KebabCase<string> =>
+  val.replace(KEBAB_REGEX, match => `-${match.toLocaleLowerCase()}`);
 
 /**
  * Given a map of states (e.g. `{ hover: true, focus: false }`) returns an
@@ -34,7 +44,7 @@ export const useStateProps = (states: UseStateProps): StateAttrProps => {
   let stateProps: StateAttrProps = {};
   for (let state in states) {
     if (states[state as ComponentState]) {
-      const key = `data-${state.toLocaleLowerCase()}` as StateAttrKeyProps;
+      const key = `data-${toKebap(state)}` as StateAttrKeyProps;
       stateProps[key] = '';
     }
   }
