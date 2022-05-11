cf2f0a65d2d071f9ac9bee2c37d483547be0fa51
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-05-11 14:10:55 +0200

    putting stuff together

--- packages/components/src/Tooltip-old/TooltipTrigger.tsx
+++ packages/components/src/Tooltip-old/TooltipTrigger.tsx
@@ -1,3 +1,18 @@
-export interface TooltipTriggerProps {}
+import React, { ReactNode } from 'react';
+import { useTooltipTriggerState } from '@react-stately/tooltip';
 
-export const TooltipTrigger = ({}: TooltipTriggerProps) => {};
+export interface TooltipTriggerProps {
+  children: [trigger: ReactNode, menu: ReactNode];
+  disabled?: boolean;
+}
+
+export const TooltipTrigger = ({
+  disabled: isDisabled,
+  children,
+  ...rest
+}: TooltipTriggerProps) => {
+  const [trigger, tooltip] = React.Children.toArray(children);
+  const props = { ...rest, isDisabled };
+
+  const state = useTooltipTriggerState(props);
+};
