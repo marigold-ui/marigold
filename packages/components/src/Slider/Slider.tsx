3cb25da14435883db07ed3738ce61f82bf8d31db
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-06-07 12:48:52 +0200

    feat: forward ref for `<Slider>`
    Ref #2145

--- packages/components/src/Slider/Slider.tsx
+++ packages/components/src/Slider/Slider.tsx
@@ -1,10 +1,11 @@
 /**
  * Thanks to react-aria: https://react-spectrum.adobe.com/react-aria/useSlider.html
  */
-import React, { ReactNode, useRef } from 'react';
+import React, { forwardRef, ReactNode, useRef } from 'react';
 import { useSlider } from '@react-aria/slider';
 import { useSliderState } from '@react-stately/slider';
 import { useNumberFormatter } from '@react-aria/i18n';
+import { useObjectRef } from '@react-aria/utils';
 import { AriaSliderProps } from '@react-types/slider';
 
 import { ThemeExtensionsWithParts, useComponentStyles } from '@marigold/system';
@@ -57,76 +58,73 @@ export interface SliderProps
  * A label + the output value and the slider functionality itself.
  * The slider itself consists of a track line and a thumb.
  */
-export const Slider = ({
-  variant,
-  size,
-  width = '100%',
-  ...props
-}: SliderProps) => {
-  const { formatOptions } = props;
-  const trackRef = useRef<HTMLElement>(null);
-  const numberFormatter = useNumberFormatter(formatOptions);
-  const state = useSliderState({ ...props, numberFormatter });
-  const { groupProps, trackProps, labelProps, outputProps } = useSlider(
-    {
-      label: props.children,
-      ...props,
-    },
-    state,
-    trackRef
-  );
+export const Slider = forwardRef<HTMLDivElement, SliderProps>(
+  ({ variant, size, width = '100%', ...props }, ref) => {
+    const { formatOptions } = props;
+    const trackRef = useObjectRef(ref);
+    const numberFormatter = useNumberFormatter(formatOptions);
+    const state = useSliderState({ ...props, numberFormatter });
+    const { groupProps, trackProps, labelProps, outputProps } = useSlider(
+      {
+        label: props.children,
+        ...props,
+      },
+      state,
+      trackRef
+    );
 
-  const styles = useComponentStyles(
-    'Slider',
-    { variant, size },
-    { parts: ['track', 'thumb', 'label', 'output'] }
-  );
+    const styles = useComponentStyles(
+      'Slider',
+      { variant, size },
+      { parts: ['track', 'thumb', 'label', 'output'] }
+    );
 
-  return (
-    <Box
-      __baseCSS={{
-        display: 'flex',
-        flexDirection: 'column',
-        touchAction: 'none',
-        width,
-      }}
-      {...groupProps}
-    >
-      {/* Flex container for the label and output element. */}
-      <Box __baseCSS={{ display: 'flex', alignSelf: 'stretch' }}>
-        {props.children && (
-          <Box as="label" __baseCSS={styles.label} {...labelProps}>
-            {props.children}
+    return (
+      <Box
+        __baseCSS={{
+          display: 'flex',
+          flexDirection: 'column',
+          touchAction: 'none',
+          width,
+        }}
+        {...groupProps}
+      >
+        {/* Flex container for the label and output element. */}
+        <Box __baseCSS={{ display: 'flex', alignSelf: 'stretch' }}>
+          {props.children && (
+            <Box as="label" __baseCSS={styles.label} {...labelProps}>
+              {props.children}
+            </Box>
+          )}
+          <Box
+            as="output"
+            {...outputProps}
+            __baseCSS={{ flex: '1 0 auto', textAlign: 'end' }}
+            css={styles.output}
+          >
+            {state.getThumbValueLabel(0)}
           </Box>
-        )}
+        </Box>
+        {/* The track element holds the visible track line and the thumb. */}
         <Box
-          as="output"
-          {...outputProps}
-          __baseCSS={{ flex: '1 0 auto', textAlign: 'end' }}
-          css={styles.output}
+          {...trackProps}
+          ref={trackRef}
+          __baseCSS={{
+            position: 'relative',
+            height: 32,
+            width: '100%',
+            cursor: props.disabled ? 'not-allowed' : 'pointer',
+          }}
         >
-          {state.getThumbValueLabel(0)}
+          <Box __baseCSS={styles.track} />
+          <Thumb
+            state={state}
+            trackRef={trackRef}
+            disabled={props.disabled}
+            styles={styles.thumb}
+          />
         </Box>
       </Box>
-      {/* The track element holds the visible track line and the thumb. */}
-      <Box
-        {...trackProps}
-        ref={trackRef}
-        __baseCSS={{
-          position: 'relative',
-          height: 32,
-          width: '100%',
-          cursor: props.disabled ? 'not-allowed' : 'pointer',
-        }}
-      >
-        <Box __baseCSS={styles.track} />
-        <Thumb
-          state={state}
-          trackRef={trackRef}
-          disabled={props.disabled}
-          styles={styles.thumb}
-        />
-      </Box>
-    </Box>
-  );
-};
+    );
+  }
+);
