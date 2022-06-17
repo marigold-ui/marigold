24af7f9b61f60c0a3bce1ae195e29f967ebb4b6d
sarahgm <sarah.gosmann@reservix.de>
2022-06-17 11:10:14 +0200

    fixed image

--- docs-next/src/components/Logo/Logo.tsx
+++ docs-next/src/components/Logo/Logo.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
+import logo from '../Logo/logo.png';
 
 const Small = () => (
-  <Image src="/Logo/logo.png" alt="Marigold Logo" width={140} height={60} />
+  <Image src={logo} alt="Marigold Logo" width={140} height={60} />
 );
 
 const Large = () => (
-  <Image src="/Logo/logo.png" alt="Marigold Logo" width={750} height={325} />
+  <Image src={logo} alt="Marigold Logo" width={750} height={325} />
 );
 
-const Fit = () => (
-  <Image src="/Logo/logo.png" layout="fill" alt="Marigold Logo" />
-);
+const Fit = () => <Image src={logo} layout="fill" alt="Marigold Logo" />;
 
 const sizes = {
   small: Small,
