8b3c05c9dbe0da5650c9186510369540855047be
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-06-23 14:20:33 +0200

    add stacked layout

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,52 +1,111 @@
 import React from 'react';
 
-import { StoryFn } from '@storybook/react';
+import isChromatic from 'chromatic/isChromatic';
+import type { StoryFn } from '@storybook/react';
 
-import { MarigoldProvider } from '@marigold/components';
+import { Box, MarigoldProvider } from '@marigold/components';
 import b2bTheme from '@marigold/theme-b2b';
 import coreTheme from '@marigold/theme-core';
 import unicornTheme from '@marigold/theme-unicorn';
 
+// Helpers
+// ---------------
 const THEME = {
   b2b: b2bTheme,
   core: coreTheme,
   unicorn: unicornTheme,
 };
 
+type ThemeNames = keyof typeof THEME;
+
+const Divider = () => (
+  <Box
+    css={{
+      width: '100vw',
+      height: 1,
+      bg: '#adb5bd',
+      my: '1.5rem',
+      mx: '-1rem',
+    }}
+  />
+);
+
+// Parameters
+// ---------------
 export const parameters = {
   a11y: {
     element: '#root',
-    config: {},
-    options: {},
   },
   controls: { expanded: true },
 };
 
+// Decorators
+// ---------------
 export const decorators = [
   (Story: StoryFn, { globals, parameters }: any) => {
-    const theme: keyof typeof THEME =
-      globals.theme || parameters.theme || 'b2b';
+    const theme: ThemeNames = globals.theme || parameters.theme || 'b2b';
+    const layout: 'single' | 'stacked' =
+      globals.layout ||
+      parameters.layout ||
+      (isChromatic() ? 'stacked' : 'single');
 
-    return (
-      <MarigoldProvider theme={THEME[theme]}>
-        <Story />
-      </MarigoldProvider>
-    );
+    switch (layout) {
+      case 'stacked': {
+        return (
+          <>
+            {Object.keys(THEME).map((key, idx) => (
+              <React.Fragment key={key}>
+                {idx > 0 ? <Divider /> : null}
+                <MarigoldProvider theme={THEME[key as ThemeNames]}>
+                  <Story />
+                </MarigoldProvider>
+              </React.Fragment>
+            ))}
+          </>
+        );
+      }
+      default: {
+        return (
+          <MarigoldProvider theme={THEME[theme]}>
+            <Story />
+          </MarigoldProvider>
+        );
+      }
+    }
   },
 ];
 
+// Global Types
+// ---------------
 export const globalTypes = {
   // Add theme selector to toolbar
   theme: {
     name: 'Theme',
     description: 'Global theme for components',
-    defaultValue: { value: 'b2b', title: 'b2b' },
     toolbar: {
       icon: 'paintbrush',
       items: Object.keys(THEME).map(key => ({
         value: key,
         title: key,
       })),
+    },
+  },
+  // Change layout
+  layout: {
+    name: 'Layout',
+    description: 'Toggle theme display',
+    toolbar: {
+      icon: 'grid',
+      items: [
+        {
+          title: 'Single',
+          value: 'single',
+        },
+        {
+          title: 'Stacked',
+          value: 'stacked',
+        },
+      ],
     },
   },
 };
