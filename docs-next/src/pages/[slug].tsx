44b097e5c8e6e93e1eaafdfe3a71572b2f7d21a7
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-07-05 09:37:41 +0200

    load mdx files
    Co-authored-by: sarahgm <sarahgm@users.noreply.github.com>

---
+++ docs-next/src/pages/[slug].tsx
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import { MDXRemote } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Link from 'next/link';
+import path from 'path';
+
+import { POSTS_PATH, postFilePaths } from '../utils';
+
+const components = {
+  Head,
+};
+
+export default function PostPage({ source, frontMatter }: any) {
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>👈 Go back home</a>
+          </Link>
+        </nav>
+      </header>
+      <div className="post-header">
+        <h1>{frontMatter.title}</h1>
+        {frontMatter.description && (
+          <p className="description">{frontMatter.description}</p>
+        )}
+      </div>
+      <main>
+        <MDXRemote {...source} />
+      </main>
+
+      <style jsx>{`
+        .post-header h1 {
+          margin-bottom: 0;
+        }
+
+        .post-header {
+          margin-bottom: 2rem;
+        }
+        .description {
+          opacity: 0.6;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export const getStaticProps = async ({ params }: any) => {
+  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
+  const source = fs.readFileSync(postFilePath);
+
+  const { content, data } = matter(source);
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: data,
+  });
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const paths = postFilePaths
+    // Remove file extensions for page paths
+    .map(path => path.replace(/\.mdx?$/, ''))
+    // Map the path into the static paths object required by Next.js
+    .map(slug => ({ params: { slug } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
