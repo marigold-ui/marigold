3e3503f5a229b67d84ef27290e0c63907f831193
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-07-07 11:32:48 +0200

    chore: Remove G maps from stories to make it static

--- packages/components/src/Aspect/Aspect.stories.tsx
+++ packages/components/src/Aspect/Aspect.stories.tsx
@@ -20,11 +20,7 @@ export default {
         'golden',
       ],
       description: 'choose between content and header',
-      table: {
-        defaultValue: {
-          summary: 'square',
-        },
-      },
+      defaultValue: 'square',
     },
     maxWidth: {
       control: {
@@ -38,11 +34,9 @@ export default {
 
 export const Basic: ComponentStory<typeof Aspect> = args => (
   <Aspect {...args}>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4820.000043444012!2d7.826018541821473!3d48.020383262446884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47911b1e29425703%3A0xbe342117a976e59!2sEuropa-Park%20Stadion!5e1!3m2!1sde!2sde!4v1647595604899!5m2!1sde!2sde"
-      title="map_sc"
-      width="100%"
-      height="100%"
+    <Image
+      src="https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80"
+      alt="some image"
     />
   </Aspect>
 );
