507ff35e6caa9923f796f4949b32ffb02aebdb97
sarahgm <sarah.gosmann@reservix.de>
2022-07-08 16:16:37 +0200

    LandingPage and DevMode

--- docs-next/src/pages/_app.tsx
+++ docs-next/src/pages/_app.tsx
@@ -2,11 +2,26 @@ import { MDXProvider } from '@mdx-js/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { MarigoldProvider } from '@marigold/components';
+import { Box, Text, MarigoldProvider } from '@marigold/components';
 import * as MarigoldComponents from '@marigold/components';
 
 import * as MdxComponents from '../mdx';
 import { theme } from '../theme';
+
+const DevMode = () => {
+  const devMode = process.env.NODE_ENV === 'development';
+  if (devMode) {
+    return (
+      <Box
+        bg="#f3f3f3"
+        css={{ textAlign: 'center', textTransform: 'uppercase' }}
+      >
+        <Text color="#1d67b6">localhost</Text>
+      </Box>
+    );
+  }
+  return null;
+};
 
 const components = {
   Head,
@@ -16,6 +31,7 @@ const components = {
 const App = ({ Component, pageProps }: AppProps) => (
   <MarigoldProvider theme={theme}>
     <MDXProvider components={components as any}>
+      <DevMode />
       <Component {...pageProps} />
     </MDXProvider>
   </MarigoldProvider>
--- docs-next/src/pages/index.tsx
+++ docs-next/src/pages/index.tsx
@@ -1,66 +1,32 @@
 import React from 'react';
-import { MDXProvider } from '@mdx-js/react';
-import * as mdxComponents from '../mdx';
-import { MarigoldTheme } from '../components/MarigoldTheme';
+import { Box, Inline, Stack } from '@marigold/components';
+import { FigmaLink } from '../components/FigmaLink';
+import { Link } from '../components/Link';
+import { Logo } from '../components/Logo';
+import { ResaleLogbook } from '@marigold/icons';
 
-import { theme } from '../theme';
-import { Helmet } from 'react-helmet';
-import { LandingPage } from './landingpage';
-import * as MarigoldComponents from '@marigold/components';
-import * as MarigoldIcons from '@marigold/icons';
-import { getAllPosts, getPostBySlug } from '../../lib/posts';
-
-import { Box, Text, MarigoldProvider, SSRProvider } from '@marigold/components';
-import { MarigoldThemeSwitch, themes } from '../components/ThemeSwitch';
-import type { ReactElement } from 'react';
-import markdownToHtml from 'docs-next/lib/markdownToHtml';
-import { useRouter } from 'next/router';
-
-const DevMode = () => {
-  const devMode = process.env.NODE_ENV === 'development';
-  if (devMode) {
-    return (
-      <Box
-        bg="#f3f3f3"
-        css={{ textAlign: 'center', textTransform: 'uppercase' }}
-      >
-        <Text color="#1d67b6">localhost</Text>
-      </Box>
-    );
-  }
-  return null;
-};
-
-export function Page() {
+export function LandingPage() {
   return (
     <div>
-      <Helmet>
-        <title>Marigold Design System</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
-      <DevMode />
-
-      <SSRProvider>
-        <MarigoldProvider theme={theme}>
-          <MarigoldThemeSwitch themes={themes} initial="b2bTheme">
-            <MDXProvider
-              components={{
-                ...mdxComponents,
-                ...(MarigoldComponents as any),
-                ...(MarigoldIcons as any),
-                MarigoldTheme,
-              }}
-            >
-              {/* for testing landingpage */}
-              <LandingPage />
-            </MDXProvider>
-          </MarigoldThemeSwitch>
-        </MarigoldProvider>
-      </SSRProvider>
+      <Box
+        __baseCSS={{
+          display: 'grid',
+          placeItems: 'center',
+          height: '100vh',
+          my: '-xsmall',
+        }}
+      >
+        <Stack space="xxlarge" alignX="center">
+          <Logo size="large" />
+          <Inline space="xxlarge">
+            <Link variant="outline" to="/introduction/00-getting-started">
+              <ResaleLogbook size="16" />
+              View in Documentation
+            </Link>
+            <FigmaLink to="https://www.figma.com/file/DFKyTGHAoDxOsUBPszLLxP/%F0%9F%8F%B5%EF%B8%8FMarigold?node-id=0%3A1" />
+          </Inline>
+        </Stack>
+      </Box>
     </div>
   );
 }
@@ -75,4 +41,4 @@ if (module.hot) {
   });
 }
 
-export default Page;
+export default LandingPage;
