3ae796c12c6d549f7786f7db7c4edd8bc65d49c6
sarahgm <sarah.gosmann@reservix.de>
2022-07-08 15:44:10 +0200

    deleted unused files

--- docs-next/lib/markdownToHtml.js
+++
@@ -1,8 +0,0 @@
-import remark from 'remark';
-import html from 'remark-html';
-
-export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown);
-  console.log('RESULT', result.toString());
-  return result.toString();
-}
