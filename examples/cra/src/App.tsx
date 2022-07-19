e2a48720bc0bcbe7d8790f06c719867d1d084e28
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-07-19 20:12:30 +0200

    chore: add example on how to setup marigold with CRA

---
+++ examples/cra/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button } from '@marigold/components';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="primary">I am button!</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
