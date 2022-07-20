e3d05151ac9e393d0c1311dcb7b6aa18bd986982
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-07-20 12:57:31 +0200

    fix lint and tests

--- examples/cra/src/reportWebVitals.ts
+++ examples/cra/src/reportWebVitals.ts
@@ -2,6 +2,7 @@ import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    //@ts-ignore
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
