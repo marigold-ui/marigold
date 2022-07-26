b53c756eacfd835ee349345f29c127eb668db41e
sarahgm <sarah.gosmann@reservix.de>
2022-07-26 09:30:22 +0200

    styled the versoion number

--- docs-next/src/components/Version.tsx
+++ docs-next/src/components/Version.tsx
@@ -4,7 +4,7 @@ import { Text } from '@marigold/components';
 export const Version = () => {
   const version = process.env.version;
   return (
-    <Text variant="muted" display="block" align="right" p="xsmall">
+    <Text variant="muted" display="block" align="right" p="small-1">
       v{version}
     </Text>
   );
---
+++ docs-next/src/theme/components/Text.style.ts
@@ -0,0 +1,11 @@
+import type { Theme } from '@marigold/components';
+
+export const Text: Theme['components']['Text'] = {
+  base: {},
+  variant: {
+    muted: {
+      fontFamily: 'headline',
+      color: 'brand.secondary',
+    },
+  },
+};
