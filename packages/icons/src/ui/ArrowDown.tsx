f0e90d8f0a877e10d9eeb95c4b93ece45a0cf1bc
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-08-08 11:52:18 +0200

    feat: Improve naming
    BREAKING: Arrows are now correctly named "chevron" and we removed the trailing A from the Arrows.
    
    e.g. `ArrowDownA` -> `ArrowDown` and `ArrowUp` -> `ChevronUp`

--- packages/icons/src/ui/ArrowDown.tsx
+++ packages/icons/src/ui/ArrowDown.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { SVG } from '@marigold/system';
 
-export const ArrowDown = ({ ...props }) => (
-  <SVG {...props}>
-    <path d="M5.97563 7.125L12 13.1363L18.0244 7.125L19.875 8.97563L12 16.8506L4.125 8.97563L5.97563 7.125Z" />
+export const ArrowDown = ({ className = '', ...props }) => (
+  <SVG className={className} {...props}>
+    <path d="M21 12L19.4137 10.4138L13.125 16.6912V3H10.875V16.6912L4.5975 10.4025L3 12L12 21L21 12Z" />
   </SVG>
 );
---
+++ packages/icons/src/ui/ChevronUp.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SVG } from '@marigold/system';
+
+export const ChevronUp = ({ className = '', ...props }) => (
+  <SVG className={className} {...props}>
+    <path d="M5.97563 16.8506L12 10.8394L18.0244 16.8506L19.875 15L12 7.125L4.125 15L5.97563 16.8506Z" />
+  </SVG>
+);
