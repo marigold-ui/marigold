42d4d372fcf2792bc4a5ec22a232ec40417af4ff
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-08-09 23:13:16 +0200

    make svg ref-able

--- packages/system/src/components/SVG/SVG.tsx
+++ packages/system/src/components/SVG/SVG.tsx
@@ -1,6 +1,8 @@
+import { forwardRef } from 'react';
 import { jsx } from '@emotion/react';
 import { ComponentProps } from '@marigold/types';
 import { useTheme } from '../../hooks';
+import { CSSObject } from '../../types';
 
 // Make sure that numbered values are converted to px.
 const toDimension = (value: number | string | number[] | string[]) =>
@@ -12,27 +14,30 @@ const ensureNumberOrToken = (value: number | string) =>
 
 export interface SVGProps extends ComponentProps<'svg'> {
   size?: number | string | number[] | string[];
+  css?: CSSObject;
 }
 
-export const SVG = ({
-  size = 24,
-  fill = 'currentcolor',
-  children,
-  ...props
-}: SVGProps) => {
-  const { css } = useTheme();
+export const SVG = forwardRef<SVGElement, SVGProps>(
+  (
+    { size = 24, fill = 'currentcolor', children, css: styles, ...props },
+    ref
+  ) => {
+    const { css } = useTheme();
 
-  return jsx(
-    'svg',
-    {
-      viewBox: '0 0 24 24',
-      css: css({
-        fill,
-        width: toDimension(props.width || size),
-        height: toDimension(props.height || size),
-      }),
-      ...props,
-    },
-    children
-  );
-};
+    return jsx(
+      'svg',
+      {
+        ...props,
+        viewBox: '0 0 24 24',
+        css: css({
+          ...styles,
+          fill,
+          width: toDimension(props.width || size),
+          height: toDimension(props.height || size),
+        }),
+        ref,
+      },
+      children
+    );
+  }
+);
