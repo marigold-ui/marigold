52df2911717df110cf224c728fbeca0dd9cd4949
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-08-16 09:55:49 +0200

    feat: clean up `<Slider>` base styles

--- packages/components/src/Slider/Thumb.tsx
+++ packages/components/src/Slider/Thumb.tsx
@@ -44,24 +44,15 @@ export const Thumb = ({ state, trackRef, styles, ...props }: ThumbProps) => {
   }, [disabled, state]);
 
   return (
-    <Box
-      __baseCSS={{
-        position: 'absolute',
-        top: 16,
-        transform: 'translateX(-50%)',
-        left: `${state.getThumbPercent(0) * 100}%`,
-      }}
-    >
-      <Box {...thumbProps} __baseCSS={styles} {...stateProps}>
-        <VisuallyHidden>
-          <Box
-            as="input"
-            type="range"
-            ref={inputRef}
-            {...mergeProps(inputProps, focusProps)}
-          />
-        </VisuallyHidden>
-      </Box>
+    <Box css={styles} {...thumbProps} {...stateProps}>
+      <VisuallyHidden>
+        <Box
+          as="input"
+          type="range"
+          ref={inputRef}
+          {...mergeProps(inputProps, focusProps)}
+        />
+      </VisuallyHidden>
     </Box>
   );
 };
