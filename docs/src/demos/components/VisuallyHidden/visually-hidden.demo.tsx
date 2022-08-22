940ba9338c9b6630a7c3a0e1df485f6cea298b25
sarahgm <sarah.gosmann@reservix.de>
2022-08-22 15:17:51 +0200

    docs: visuallyHidden demo

---
+++ docs/src/demos/components/VisuallyHidden/visually-hidden.demo.tsx
@@ -0,0 +1,8 @@
+import { Text, VisuallyHidden } from '@marigold/components';
+
+export const BasicVisuallyHidden = () => (
+  <>
+    <Text>The Text below is visually hidden</Text>
+    <VisuallyHidden>I am invisible</VisuallyHidden>
+  </>
+);
--- docs/src/demos/components/index.ts
+++ docs/src/demos/components/index.ts
@@ -19,3 +19,4 @@ export * from './Container';
 export * from './Dialog';
 export * from './Footer';
 export * from './Header';
+export * from './VisuallyHidden';
