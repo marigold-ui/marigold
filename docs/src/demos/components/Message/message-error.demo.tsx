7b812264fd65a2485f02bb310f66292562ffadb8
sarahgm <sarah.gosmann@reservix.de>
2022-08-22 15:49:47 +0200

    message mdx

--- docs/src/demos/components/Message/message-error.demo.tsx
+++ docs/src/demos/components/Message/message-error.demo.tsx
@@ -1,6 +1,6 @@
 import { Message, Text } from '@marigold/components';
 
-export const MessageError = () => (
+export const ErrorMessage = () => (
   <Message variant="error" messageTitle="Wrong here.">
     <Text>Hello, I am a simple error message.</Text>
   </Message>
