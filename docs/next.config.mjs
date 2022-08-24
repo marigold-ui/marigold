b422dd7c0765423451409856041656384338361d
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-08-24 14:35:50 +0200

    meh

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,12 +1,26 @@
+// @ts-check
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import pkg from './package.json' assert { type: 'json' };
+import mdx from '@next/mdx';
+
 import createNavigationTree from './src/navigation.mjs';
+
+import pkg from './package.json' assert { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const isProduction = process.env.NODE_ENV === 'production';
+
+const withMdx = mdx({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    recmaPlugins: [],
+    providerImportSource: '@mdx-js/react',
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const config = async () => {
@@ -25,17 +39,18 @@ const config = async () => {
       ignoreBuildErrors: isProduction,
       tsconfigPath: './tsconfig.json',
     },
+    // pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     webpack: (config, { defaultLoaders: { babel } }) => {
-      config.module.rules.push({
+      config.module.rules.unshift({
         include: [
           path.resolve(__dirname, '..', 'packages'),
           path.resolve(__dirname, '..', 'themes'),
         ],
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(jsx?|tsx?)$/,
 
         use: [babel],
       });
-
+      console.log('###################', config.module.rules);
       config.resolve.alias.root = path.resolve(__dirname, '..');
 
       return config;
@@ -43,4 +58,9 @@ const config = async () => {
   };
 };
 
-export default config;
+// TODO: withMdx odes not work with async function?
+export default async () => {
+  // Make navigation function sync?
+  const base = await config();
+  return withMdx(base);
+};
