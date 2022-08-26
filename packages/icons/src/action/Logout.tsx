69a13fa671c6d8fbc08c11f03103909b6ee9b64e
sarahgm <sarah.gosmann@reservix.de>
2022-08-26 08:53:40 +0200

    remove fill

--- packages/icons/src/action/Logout.tsx
+++ packages/icons/src/action/Logout.tsx
@@ -3,9 +3,6 @@ import { SVG, SVGProps } from '@marigold/system';
 
 export const Logout = forwardRef<SVGElement, SVGProps>((props, ref) => (
   <SVG {...props} viewBox="0 0 24 24" ref={ref}>
-    <path
-      d="M13 3h-2v10h2V3Zm4.83 2.17-1.42 1.42A6.92 6.92 0 0 1 19 12c0 3.87-3.13 7-7 7A6.995 6.995 0 0 1 7.58 6.58L6.17 5.17A8.932 8.932 0 0 0 3 12a9 9 0 0 0 18 0c0-2.74-1.23-5.18-3.17-6.83Z"
-      fill="#8D8D8D"
-    />
+    <path d="M13 3h-2v10h2V3Zm4.83 2.17-1.42 1.42A6.92 6.92 0 0 1 19 12c0 3.87-3.13 7-7 7A6.995 6.995 0 0 1 7.58 6.58L6.17 5.17A8.932 8.932 0 0 0 3 12a9 9 0 0 0 18 0c0-2.74-1.23-5.18-3.17-6.83Z" />
   </SVG>
 ));
