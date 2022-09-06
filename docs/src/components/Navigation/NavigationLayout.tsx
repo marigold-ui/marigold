29d367d1530fccdb88ab9a269591698e9f766be6
Benedikt Grether <info@benediktgrether.de>
2022-09-06 11:28:43 +0200

    fix: add title to button in navigation for a11y

--- docs/src/components/Navigation/NavigationLayout.tsx
+++ docs/src/components/Navigation/NavigationLayout.tsx
@@ -24,6 +24,7 @@ export const NavigationLayout = ({ navigation }: NavigationLayoutProps) => {
             <Button
               variant="navigationSmall"
               onPress={() => setShowNavigation(!showNavigation)}
+              title="navigation"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
