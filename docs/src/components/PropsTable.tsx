ed04b4c789760e1e6e8d84fc3266c3f1f2a08f46
sarahgm <sarah.gosmann@reservix.de>
2022-09-08 14:46:35 +0200

    remove box

--- docs/src/components/PropsTable.tsx
+++ docs/src/components/PropsTable.tsx
@@ -7,44 +7,40 @@ export interface PropsTableProps {
 
 export const PropsTable = ({ props }: PropsTableProps) => {
   return (
-    <Box
-      css={{ overflow: ['scroll', 'unset'], whiteSpace: ['nowrap', 'unset'] }}
-    >
-      <Card px="medium-1" py="medium-2">
-        {props ? (
-          <Table aria-label="Table with component props" variant="propsTable">
-            <Table.Header>
-              <Table.Column key="property">Property</Table.Column>
-              <Table.Column key="type">Type</Table.Column>
-              <Table.Column key="default">Default</Table.Column>
-              <Table.Column key="description">Description</Table.Column>
-            </Table.Header>
-            <Table.Body items={props}>
-              {item => (
-                <Table.Row key={item.property}>
-                  <Table.Cell>
-                    <code>{item.property}</code>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <code>{item.type}</code>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <code>{item.default}</code>
-                  </Table.Cell>
-                  <Table.Cell>{item.description}</Table.Cell>
-                </Table.Row>
-              )}
-            </Table.Body>
-          </Table>
-        ) : (
-          <Inline>
-            <BlankCanvas />
-            <Text variant="content">
-              Sorry! There are currently no props availible.
-            </Text>
-          </Inline>
-        )}
-      </Card>
-    </Box>
+    <Card px="medium-1" py="medium-2">
+      {props ? (
+        <Table aria-label="Table with component props" variant="propsTable">
+          <Table.Header>
+            <Table.Column key="property">Property</Table.Column>
+            <Table.Column key="type">Type</Table.Column>
+            <Table.Column key="default">Default</Table.Column>
+            <Table.Column key="description">Description</Table.Column>
+          </Table.Header>
+          <Table.Body items={props}>
+            {item => (
+              <Table.Row key={item.property}>
+                <Table.Cell>
+                  <code>{item.property}</code>
+                </Table.Cell>
+                <Table.Cell>
+                  <code>{item.type}</code>
+                </Table.Cell>
+                <Table.Cell>
+                  <code>{item.default}</code>
+                </Table.Cell>
+                <Table.Cell>{item.description}</Table.Cell>
+              </Table.Row>
+            )}
+          </Table.Body>
+        </Table>
+      ) : (
+        <Inline>
+          <BlankCanvas />
+          <Text variant="content">
+            Sorry! There are currently no props availible.
+          </Text>
+        </Inline>
+      )}
+    </Card>
   );
 };
