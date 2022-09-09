b1f23b63248e71637f26d3abe6625dfbbf070d0e
sarahgm <38324334+sarahgm@users.noreply.github.com>
2022-09-09 16:48:05 +0200

    chore: made container more responsive (#2418)
    * chore: made container more responsive
    
    * fix tests
    
    * used undefined instead of unset
    
    * fix test again

--- packages/components/src/Container/Container.tsx
+++ packages/components/src/Container/Container.tsx
@@ -9,26 +9,29 @@ export interface ContainerProps extends ComponentProps<'div'> {
   contentType?: 'content' | 'header';
   size?: keyof typeof tokenSize.content | keyof typeof tokenSize.header;
   align?: 'left' | 'right' | 'center';
-  alignItems?: 'left' | 'right' | 'center';
+  alignItems?: 'left' | 'right' | 'center' | 'none';
 }
 
+// for the case that elements whit overflow were used we needed to set the align-items to undefined(it's now default behavior)
 const ALIGN_ITEMS = {
   left: 'start',
   center: 'center',
   right: 'end',
+  none: 'undefined',
 };
 
+// for responsive reasons we needed to use the `minmax(0, 60ch)` value instead of `fit-content(60ch)`
 const ALIGN = {
   left: (maxWidth: string) => ({
-    gridTemplateColumns: `fit-content(${maxWidth}) 1fr 1fr`,
+    gridTemplateColumns: `minmax(0, ${maxWidth}) 1fr 1fr`,
     gridColumn: 1,
   }),
   center: (maxWidth: string) => ({
-    gridTemplateColumns: `1fr fit-content(${maxWidth}) 1fr`,
+    gridTemplateColumns: `1fr minmax(0, ${maxWidth}) 1fr`,
     gridColumn: 2,
   }),
   right: (maxWidth: string) => ({
-    gridTemplateColumns: `1fr 1fr fit-content(${maxWidth})`,
+    gridTemplateColumns: `1fr 1fr minmax(0, ${maxWidth})`,
     gridColumn: 3,
   }),
 };
@@ -37,7 +40,7 @@ export const Container = ({
   contentType = 'content',
   size = 'medium',
   align = 'left',
-  alignItems = 'left',
+  alignItems = 'none',
   children,
   ...props
 }: ContainerProps) => {
