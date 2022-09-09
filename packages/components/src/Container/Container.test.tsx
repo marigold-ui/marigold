905924ea00c0cc1754e848d6455de22f77e425a6
sarahgm <sarah.gosmann@reservix.de>
2022-09-09 15:27:41 +0200

    fix test again

--- packages/components/src/Container/Container.test.tsx
+++ packages/components/src/Container/Container.test.tsx
@@ -107,7 +107,7 @@ test('supports default align items none', () => {
     </Container>
   );
   const container = screen.getByTestId(/container/);
-  expect(container).toHaveStyle(`placeItems: unset`);
+  expect(container).toHaveStyle(`placeItems: undefined`);
 });
 
 test('supports align items center', () => {
