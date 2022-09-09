76b29677fa37cdeae24c860b2271bfba9395f46c
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-09-09 11:10:48 +0200

    update doku

--- docs/src/demos/components/Container/large-content-container.demo.tsx
+++ docs/src/demos/components/Container/large-content-container.demo.tsx
@@ -1,7 +1,7 @@
 import { Container, Text } from '@marigold/components';
 
 export const LargeContentContainer = () => (
-  <Container size="medium" alignItems="center">
+  <Container size="medium" align="center">
     <Text>
       The evil lord Darth Vader, obsessed with finding young Skywalker, has
       dispatched thousands of remote probes into the far reaches of space....
