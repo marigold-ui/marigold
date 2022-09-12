6c5f25e6bef6870bc823937ca402d7f5b3018267
Benedikt Grether <info@benediktgrether.de>
2022-09-12 16:30:14 +0200

    docs: Add styles for focus and  active state to navigation

--- docs/src/theme/components/Link.style.ts
+++ docs/src/theme/components/Link.style.ts
@@ -1,13 +1,21 @@
 import { Theme } from '@marigold/components';
+import { colors } from '../colors';
 
 export const Link: Theme['components']['Link'] = {
   base: {
     position: 'relative',
     '&:hover': {
-      color: 'brand.secondary',
+      color: `brand.secondary`,
     },
     '&:focus': {
-      outline: 'none',
+      outline: `2px solid ${colors.brand.secondary}`,
+    },
+    '&[data-active]': {
+      pl: '8px',
+      ml: '-10px',
+      fontSize: 'fixed.small-3',
+      borderLeft: `2px solid  ${colors.brand.primary}`,
+      fontWeight: 'medium',
     },
   },
   variant: {
