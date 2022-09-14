ae75ea072b4f85f769155b64378a20d3bc34c864
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-09-14 09:33:26 +0200

    improve layout image and find a bug in <text>

--- docs/src/demos/concepts-layouts.demo.tsx
+++ docs/src/demos/concepts-layouts.demo.tsx
@@ -3,31 +3,31 @@ import { Box, Columns, Stack, Text, Tiles } from '@marigold/components';
 import { Rectangle, Squirecle } from '~/components';
 
 const Annotation = ({ children }: { children: ReactNode }) => (
-  <Text align="center" m="none">
+  <Text fontFamily="heading" align="center" m="none">
     {children}
   </Text>
 );
 
 export const LayoutsDemo = () => (
-  <Box>
+  <Box p="small-1" bg="background.light" borderRadius="medium-1">
     <Columns columns={[1, 1, 1]} space="medium-2" collapseAt="0em">
-      <div>
+      <>
         <Stack space="small-1">
           <Rectangle />
           <Rectangle />
           <Rectangle />
         </Stack>
         <Annotation>Stack</Annotation>
-      </div>
-      <div>
+      </>
+      <>
         <Columns columns={[3, 1, 2]} space="small-1" collapseAt="0em">
           <Rectangle height="126px" />
           <Rectangle height="126px" />
           <Rectangle height="126px" />
         </Columns>
         <Annotation>Columns</Annotation>
-      </div>
-      <div>
+      </>
+      <>
         <Tiles space="small-1" itemMinWidth="32px">
           <Squirecle />
           <Squirecle />
@@ -40,7 +40,7 @@ export const LayoutsDemo = () => (
           <Squirecle />
         </Tiles>
         <Annotation>Tiles</Annotation>
-      </div>
+      </>
     </Columns>
   </Box>
 );
