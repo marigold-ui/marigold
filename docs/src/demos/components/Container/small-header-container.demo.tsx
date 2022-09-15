d88986d1be5085cca57f0a912b08824fb60b0f57
sarahgm <38324334+sarahgm@users.noreply.github.com>
2022-09-15 15:59:10 +0200

    docs: refactor container docs page and demos (#2461)

--- docs/src/demos/components/Container/small-header-container.demo.tsx
+++
@@ -1,7 +0,0 @@
-import { Container, Headline } from '@marigold/components';
-
-export const SmallHeaderContainer = () => (
-  <Container contentType="header" size="small">
-    <Headline level="2">Star Wars - The Empire Strikes Back</Headline>
-  </Container>
-);
