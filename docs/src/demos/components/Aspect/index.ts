7eaf10196e5e320dd2f6d1e8b630ed9bebc4c397
Benedikt Grether <info@benediktgrether.de>
2022-09-16 10:38:22 +0200

    docs: refactor aspect docs

--- docs/src/demos/components/Aspect/index.ts
+++ docs/src/demos/components/Aspect/index.ts
@@ -1,5 +1,5 @@
 export * from './simple-aspect.demo';
-export * from './widescreen-aspect.demo';
+export * from './golden-aspect.demo';
 export * from './ratio-square.demo';
 export * from './map.demo';
 export * from './video.demo';
