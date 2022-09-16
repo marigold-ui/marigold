49b1cb85a5e656a9171d324bb04b93d061fb703b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2022-09-16 11:32:59 +0200

    chore: rename Content to Body and write docs (#2467)

--- docs/src/demos/components/Headline/headline.demo.tsx
+++ docs/src/demos/components/Headline/headline.demo.tsx
@@ -1,12 +1,12 @@
-import { Content, Headline, Text } from '@marigold/components';
+import { Body, Headline, Text } from '@marigold/components';
 
 export const BasicHeadline = () => (
-  <Content>
+  <Body>
     <Headline level="3">Lorem ipsum</Headline>
     <Text>
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
       eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
       voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
     </Text>
-  </Content>
+  </Body>
 );
