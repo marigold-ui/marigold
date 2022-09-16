f835e68da1cc085c244c0fa7b2194063d9213276
sarahgm <sarah.gosmann@reservix.de>
2022-09-16 09:17:41 +0200

    docs: add Box docs

--- docs/src/demos/components/Box/box-text.demo.tsx
+++ docs/src/demos/components/Box/box-text.demo.tsx
@@ -1,7 +1,13 @@
 import { Box } from '@marigold/components';
 
 export const TextBox = () => (
-  <Box as="p" border="1px solid" p="8px">
-    I am a paragraph with a border!
+  <Box
+    as="p"
+    border="1px solid"
+    borderRadius="5px"
+    p="8px"
+    css={{ bg: 'lightseagreen', color: 'white' }}
+  >
+    I am a paragraph with a lot of props!
   </Box>
 );
