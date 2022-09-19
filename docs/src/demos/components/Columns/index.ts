99dcbbc7e4fa3da0ce7df3addef56ef13c88aaa1
Benedikt Grether <info@benediktgrether.de>
2022-09-19 11:36:42 +0200

    docs: refactor columns (#2465)

--- docs/src/demos/components/Columns/index.ts
+++ docs/src/demos/components/Columns/index.ts
@@ -1,5 +1,4 @@
 export * from './basic-columns.demo';
-export * from './breadcrumb-columns.demo';
 export * from './collapse-columns.demo';
 export * from './layout-columns.demo';
 export * from './space-columns.demo';
