24904d4de867d6ed6044260c47aadd10667c1e3c
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-09-20 08:54:41 +0200

    feat: better `Logout` icon

--- packages/icons/src/action/Logout.tsx
+++ packages/icons/src/action/Logout.tsx
@@ -3,6 +3,6 @@ import { SVG, SVGProps } from '@marigold/system';
 
 export const Logout = forwardRef<SVGElement, SVGProps>((props, ref) => (
   <SVG {...props} viewBox="0 0 24 24" ref={ref}>
-    <path d="M13 3h-2v10h2V3Zm4.83 2.17-1.42 1.42A6.92 6.92 0 0 1 19 12c0 3.87-3.13 7-7 7A6.995 6.995 0 0 1 7.58 6.58L6.17 5.17A8.932 8.932 0 0 0 3 12a9 9 0 0 0 18 0c0-2.74-1.23-5.18-3.17-6.83Z" />
+    <path d="M5 21q-.825 0-1.413-.587Q3 19.825 3 19V5q0-.825.587-1.413Q4.175 3 5 3h7v2H5v14h7v2Zm11-4-1.375-1.45 2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5Z" />
   </SVG>
 ));
