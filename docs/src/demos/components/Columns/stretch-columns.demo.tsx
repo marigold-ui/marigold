ae2d61948d832e1c6512494463e918046fab6dfe
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-09-21 09:14:02 +0200

    docs: fix column stretch demo (#2501)

--- docs/src/demos/components/Columns/stretch-columns.demo.tsx
+++ docs/src/demos/components/Columns/stretch-columns.demo.tsx
@@ -1,41 +1,21 @@
-import type { ReactNode } from 'react';
-import { Box, Columns, Stack } from '@marigold/components';
-
-const Block = ({
-  children,
-  height,
-}: {
-  children: ReactNode;
-  height?: number | string;
-}) => (
-  <Box
-    css={{
-      height,
-      background: 'hsla(218 16% 77% / 50%)',
-      border: '1px solid hsla(218 16% 70% / 50%)',
-      borderRadius: 12,
-      p: 12,
-    }}
-  >
-    {children}
-  </Box>
-);
+import { Columns, Stack } from '@marigold/components';
+import { Block } from '~/components';
 
 export const StretchColumns = () => (
   <Block>
     <Stack space="medium">
       <Block height={500}>
         <Columns columns={[4, 4, 4]} space="small" stretch>
-          <Block height="100%">one</Block>
-          <Block>two</Block>
-          <Block height={200}>three</Block>
+          <Block height="100%">I have a height set to 100%!</Block>
+          <Block>I space myself</Block>
+          <Block height={200}>I have a height set to 200px.</Block>
         </Columns>
       </Block>
       <Columns columns={[2, 1]} space="small">
         <Block>
           Columns will stretch if they get longer, like a regular CSS element.
         </Block>
-        <Block>five</Block>
+        <Block>I am here too!</Block>
       </Columns>
     </Stack>
   </Block>
