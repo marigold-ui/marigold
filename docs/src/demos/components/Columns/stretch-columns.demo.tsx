d55bc313c86193fafd07cd4a5671652cf7f5cb51
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-09-21 09:03:25 +0200

    text

--- docs/src/demos/components/Columns/stretch-columns.demo.tsx
+++ docs/src/demos/components/Columns/stretch-columns.demo.tsx
@@ -6,16 +6,16 @@ export const StretchColumns = () => (
     <Stack space="medium">
       <Block height={500}>
         <Columns columns={[4, 4, 4]} space="small" stretch>
-          <Block height="100%">one</Block>
-          <Block>two</Block>
-          <Block height={200}>three</Block>
+          <Block height="100%">I have a height set to 100%!</Block>
+          <Block>I space myself</Block>
+          <Block height={200}>I have a height set to 200px.</Block>
         </Columns>
       </Block>
       <Columns columns={[2, 1]} space="small">
         <Block>
           Columns will stretch if they get longer, like a regular CSS element.
         </Block>
-        <Block>five</Block>
+        <Block>I am here too!</Block>
       </Columns>
     </Stack>
   </Block>
