6bc71364943a020b8fa7f2e5ffa563b223d19d34
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-09-22 12:11:56 +0200

    working

--- docs/src/demos/components/Tooltip/tooltip-pressed.demo.tsx
+++ docs/src/demos/components/Tooltip/tooltip-pressed.demo.tsx
@@ -5,8 +5,14 @@ export const PressedTooltip = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Tooltip.Trigger open={open} onOpenChange={setOpen}>
-      <Button>Press me!</Button>
+    <Tooltip.Trigger open={open}>
+      <Button
+        variant="primary"
+        onPressStart={() => setOpen(true)}
+        onPressEnd={() => setOpen(false)}
+      >
+        Press and hold me!
+      </Button>
       <Tooltip>I am a tooltip!</Tooltip>
     </Tooltip.Trigger>
   );
