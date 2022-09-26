9773db08863b4b2deb5766dfe131533a7d39a2e9
sarahgm <sarah.gosmann@reservix.de>
2022-09-26 14:27:54 +0200

    docs: add radio docs

--- docs/src/demos/components/Radio/radio-required.demo.tsx
+++ docs/src/demos/components/Radio/radio-required.demo.tsx
@@ -1,7 +1,7 @@
 import { Radio } from '@marigold/components';
 
 export const RequiredRadio = () => (
-  <Radio.Group label="Radio Group" required>
+  <Radio.Group label="Radio Group" required orientation="horizontal">
     <Radio value="1">Option 1</Radio>
     <Radio value="2">Option 2</Radio>
     <Radio value="3" disabled>
