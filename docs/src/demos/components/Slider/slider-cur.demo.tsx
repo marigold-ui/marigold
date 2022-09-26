3614cf5405bb42bed1adac6662840daf86fe359e
sarahgm <sarah.gosmann@reservix.de>
2022-09-26 10:50:32 +0200

    docs: slider docs

--- docs/src/demos/components/Slider/slider-cur.demo.tsx
+++
@@ -1,5 +0,0 @@
-import { Slider } from '@marigold/components';
-
-export const CurrencySlider = () => (
-  <Slider formatOptions={{ style: 'currency', currency: 'EUR' }}>Price</Slider>
-);
--- docs/src/demos/components/Slider/slider-per.demo.tsx
+++
@@ -1,7 +0,0 @@
-import { Slider } from '@marigold/components';
-
-export const PercentageSlider = () => (
-  <Slider formatOptions={{ style: 'percent' }} step={0.01} maxValue={1}>
-    Percent
-  </Slider>
-);
