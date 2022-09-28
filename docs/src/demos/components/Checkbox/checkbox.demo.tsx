efec45a0d5fccd95e48d8065ab3530cc34a59364
sarahgm <38324334+sarahgm@users.noreply.github.com>
2022-09-28 11:21:32 +0200

    docs: checkbox docs (#2538)
    * docs: checkbox docs
    
    * grammar

--- docs/src/demos/components/Checkbox/checkbox.demo.tsx
+++ docs/src/demos/components/Checkbox/checkbox.demo.tsx
@@ -1,7 +1,5 @@
 import { Checkbox } from '@marigold/components';
 
 export const CheckboxDemo = () => (
-  <Checkbox defaultChecked readOnly>
-    I will not talk about Fight Club
-  </Checkbox>
+  <Checkbox defaultChecked>I will not talk about Fight Club</Checkbox>
 );
