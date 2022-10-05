459ad4586d5aae459194bcf6e46df6fc527c9528
sarahgm <sarah.gosmann@reservix.de>
2022-10-05 14:38:18 +0200

    add required property for styling reasons

--- packages/components/src/Label/Label.tsx
+++ packages/components/src/Label/Label.tsx
@@ -29,18 +29,18 @@ export const Label = ({
 }: LabelProps) => {
   const styles = useComponentStyles('Label', { size, variant });
 
+  console.log(props);
   return (
     <Box
       {...props}
       as={as}
+      // aria-required is set on the field and will already be announced,
+      // so we don't need to add it here. BUT we need it for styling the required label, so this is needed.
+      aria-required={required}
       __baseCSS={{ display: 'flex', alignItems: 'center', gap: 4 }}
       css={styles}
     >
       {children}
-      {/*
-       * aria-required is set on the field and will already be announced,
-       * so we don't need to add it here.
-       */}
       {required && <Required role="presentation" size={16} fill="error" />}
     </Box>
   );
