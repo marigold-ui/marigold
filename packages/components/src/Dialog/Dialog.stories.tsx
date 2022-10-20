6912330655e9edbc1c93d813beae98a8df2d557a
Sebastian Sebald <sebastian.sebald@gmail.com>
2022-10-20 12:14:27 +0200

    docs: Add some more complex dialog examples
    Co-authored-by: sarahgm <sarahgm@users.noreply.github.com>

--- packages/components/src/Dialog/Dialog.stories.tsx
+++ packages/components/src/Dialog/Dialog.stories.tsx
@@ -8,6 +8,12 @@ import { Text } from '../Text';
 import { Dialog } from './Dialog';
 import { TextField } from '../TextField';
 import { Stack } from '../Stack';
+import { Body } from '../Body';
+import { Container } from '../Container';
+import { Footer } from '../Footer';
+import { Header } from '../Header';
+import { Checkbox, CheckboxGroup } from '../Checkbox';
+import { Box } from '@marigold/system';
 
 export default {
   title: 'Components/Dialog',
@@ -66,16 +72,89 @@ export const Form: ComponentStory<typeof Dialog.Trigger> = args => {
   );
 };
 
-export const CustomTitleProps: ComponentStory<typeof Dialog.Trigger> = args => {
-  return (
-    <>
-      <Dialog.Trigger {...args}>
-        <Button variant="primary">Open</Button>
-        <Dialog closeButton aria-labelledby="my-cool-headline">
+export const CustomTitleProps: ComponentStory<typeof Dialog.Trigger> = args => (
+  <Dialog.Trigger {...args}>
+    <Button variant="primary">Open</Button>
+    <Dialog closeButton aria-labelledby="my-cool-headline">
+      <Headline id="my-cool-headline">This is a headline!</Headline>
+      <Text>This is some not so very long text.</Text>
+    </Dialog>
+  </Dialog.Trigger>
+);
+
+export const ScrollableContent: ComponentStory<
+  typeof Dialog.Trigger
+> = args => (
+  <Dialog.Trigger {...args}>
+    <Button variant="primary">Open</Button>
+    <Dialog closeButton aria-labelledby="my-cool-headline">
+      <Container>
+        <Header>
           <Headline id="my-cool-headline">This is a headline!</Headline>
-          <Text>This is some not so very long text.</Text>
-        </Dialog>
-      </Dialog.Trigger>
-    </>
-  );
-};
+        </Header>
+        <Body>
+          <Stack space="small">
+            <Text>This is some not so very long text.</Text>
+            <Box
+              pl="small"
+              pb="xsmall"
+              css={{ maxHeight: '200px', overflowY: 'auto' }}
+            >
+              <CheckboxGroup>
+                <Checkbox>One</Checkbox>
+                <Checkbox>Two</Checkbox>
+                <Checkbox>Three</Checkbox>
+                <Checkbox>Four</Checkbox>
+                <Checkbox>Five</Checkbox>
+                <Checkbox>Six</Checkbox>
+                <Checkbox>Seven</Checkbox>
+                <Checkbox>Eight</Checkbox>
+                <Checkbox>Nine</Checkbox>
+                <Checkbox>Ten</Checkbox>
+              </CheckboxGroup>
+            </Box>
+          </Stack>
+        </Body>
+        <Footer>
+          <Button variant="primary">Ok</Button>
+        </Footer>
+      </Container>
+    </Dialog>
+  </Dialog.Trigger>
+);
+
+export const StickyFooter: ComponentStory<typeof Dialog.Trigger> = args => (
+  <Dialog.Trigger {...args}>
+    <Button variant="primary">Open</Button>
+    <Dialog closeButton aria-labelledby="my-cool-headline">
+      <Box css={{ display: 'flex', flexDirection: 'column', maxHeight: 400 }}>
+        <Header>
+          <Headline id="my-cool-headline">This is a headline!</Headline>
+          <Text>This is some additional text that is always visible!</Text>
+        </Header>
+        <Box css={{ flex: 1, overflowY: 'auto', maxWidth: 400 }}>
+          <Body>
+            <Text>
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat
+              vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+              amet quam egestas semper. Aenean ultricies mi vitae est. Mauris
+              placerat eleifend leo. Quisque sit amet est et sapien ullamcorper
+              pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae,
+              ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt
+              condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
+              dui. Donec non enim in turpis pulvinar facilisis. Ut felis.
+              Praesent dapibus, neque id cursus faucibus, tortor neque egestas
+              augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam
+              dui mi, tincidunt quis, accumsan porttitor, facilisis luctus,
+              metus
+            </Text>
+          </Body>
+        </Box>
+        <Footer>
+          <Button variant="primary">Ok</Button>
+        </Footer>
+      </Box>
+    </Dialog>
+  </Dialog.Trigger>
+);
