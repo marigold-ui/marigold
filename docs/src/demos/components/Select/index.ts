5e3b8edd55b5a82e67b2482658ce1492a9fa1821
sarahgm <sarah.gosmann@reservix.de>
2022-10-20 15:51:37 +0200

    add some examples

--- docs/src/demos/components/Select/index.ts
+++ docs/src/demos/components/Select/index.ts
@@ -3,3 +3,4 @@ export * from './select-disabled.demo';
 export * from './select-default-selected.demo';
 export * from './select-diabled-keys.demo';
 export * from './select-section.demo';
+export * from './select-side.demo';
---
+++ docs/src/demos/components/TextField/textfield-side.demo.tsx
@@ -0,0 +1,5 @@
+import { TextField } from '@marigold/components';
+
+export const SideTextField = () => (
+  <TextField label="Vorname" required side="left" />
+);
