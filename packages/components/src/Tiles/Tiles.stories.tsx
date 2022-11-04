9c681069cfdd2dfc40760aa5ebd11e5c0c78d625
Sebastian Sebald <sebastian.sebald@reservix.de>
2022-11-04 08:27:41 +0100

    tiles stretch

--- packages/components/src/Tiles/Tiles.stories.tsx
+++ packages/components/src/Tiles/Tiles.stories.tsx
@@ -49,16 +49,23 @@ export default {
 } as Meta;
 
 export const Boxes: ComponentStory<typeof Tiles> = args => (
-  <Tiles {...args}>
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-    <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
-  </Tiles>
+  <>
+    <Tiles {...args}>
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+    </Tiles>
+    <br />
+    <Tiles {...args}>
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+      <Box border="1px solid #ced4da" bg="#e9ecef" height="100px" />
+    </Tiles>
+  </>
 );
 
 export const DifferentHights: ComponentStory<typeof Tiles> = args => (
--- packages/components/src/Tiles/Tiles.tsx
+++ packages/components/src/Tiles/Tiles.tsx
@@ -5,32 +5,26 @@ import { ResponsiveStyleValue } from '@marigold/system';
 export interface TilesProps {
   children: ReactNode;
   space?: ResponsiveStyleValue<string>;
+  tilesWidth?: ResponsiveStyleValue<string>;
   itemMinWidth?: ResponsiveStyleValue<string>;
-  gridAutoRows?: boolean;
 }
 
 export const Tiles = React.forwardRef<HTMLDivElement, TilesProps>(
-  (
-    {
-      space = 'none',
-      itemMinWidth = '250px',
-      gridAutoRows,
-      children,
-      ...props
-    },
-    ref
-  ) => (
-    <Box
-      ref={ref}
-      display="grid"
-      __baseCSS={{
-        gap: space,
-        gridTemplateColumns: `repeat(auto-fit, minmax(min(${itemMinWidth}, 100%), 1fr))`,
-        gridAutoRows: gridAutoRows ? '1fr' : 'none',
-      }}
-      {...props}
-    >
-      {children}
-    </Box>
-  )
+  ({ space = 'none', itemMinWidth = '250px', children, ...props }, ref) => {
+    return (
+      <Box
+        ref={ref}
+        display="grid"
+        __baseCSS={{
+          gap: space,
+          gridTemplateColumns: `repeat(auto-fit, minmax(min(${itemMinWidth}, 100%), 1fr))`,
+          // Make height of all tiles equal
+          gridAutoRows: '1fr',
+        }}
+        {...props}
+      >
+        {children}
+      </Box>
+    );
+  }
 );
