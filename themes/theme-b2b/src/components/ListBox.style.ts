57ff5ea50bc684ad1a6ab5edad17e3776669e634
Benedikt Grether <info@benediktgrether.de>
2022-12-05 14:09:35 +0100

    fix set maxheight to 75 and 45vh

--- themes/theme-b2b/src/components/ListBox.style.ts
+++ themes/theme-b2b/src/components/ListBox.style.ts
@@ -9,7 +9,7 @@ export const ListBox: Theme['components']['ListBox'] = {
       border: '1px solid',
       borderColor: 'gray40',
       borderRadius: 'small',
-      height: '75vh',
+      maxHeight: ['75vh', '75vh', '45vh'],
       overflow: 'hidden auto',
 
       '&:error': {
