7aeaf0fa7c397c5c53514bf2df25ebbdb21c346b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2022-12-09 11:26:42 +0100

    docs: refa theme select as menu (#2678)

---
+++ docs/src/theme/components/Menu.style.ts
@@ -0,0 +1,23 @@
+import type { Theme } from '@marigold/components';
+
+export const Menu: Theme['components']['Menu'] = {
+  base: {
+    container: {
+      fontFamily: 'headline',
+      bg: 'background.page',
+      border: '1px solid',
+      borderColor: 'background.light',
+      borderRadius: 'small-2',
+      maxHeight: ['75vh', '75vh', '45vh'],
+      overflow: 'hidden auto',
+    },
+    item: {
+      p: 'small-1',
+      cursor: 'pointer',
+
+      '&:hover': {
+        color: 'brand.primary',
+      },
+    },
+  },
+};
