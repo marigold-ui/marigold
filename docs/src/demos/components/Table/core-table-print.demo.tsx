cb4a660de8c2c7b9d7ca1e6e004d18218ab5855e
sarahgm <sarah.gosmann@reservix.de>
2022-12-12 10:33:31 +0100

    docs: improve table recipe for core

--- docs/src/demos/components/Table/core-table-print.demo.tsx
+++ docs/src/demos/components/Table/core-table-print.demo.tsx
@@ -1,5 +1,7 @@
 import {
+  Box,
   Button,
+  Headline,
   Inline,
   Select,
   Split,
@@ -11,52 +13,59 @@ import {
 import React from 'react';
 
 import coreTheme from '@marigold/theme-core';
-import { ResaleLogbook, AutoRenew, ExternalLink } from '@marigold/icons';
+import { ResaleLogbook, AutoRenew, ExternalLink, Lock } from '@marigold/icons';
 
 const columns = [
   { name: 'Veranstaltung', key: 'event' },
-  { name: 'Kategorie', key: 'category' },
-  { name: 'Preisstufe', key: 'price' },
   { name: 'Platz', key: 'seat' },
   { name: 'Platzcode', key: 'code' },
   { name: 'Ticketcode', key: 'ticketcode' },
   { name: 'Gedruckt', key: 'print' },
+  { name: 'Aktueller Tickettyp', key: 'type' },
   { name: 'Aktionen', key: 'actions' },
 ];
 
-const rows: { [key: string]: string }[] = [
+const rows: { [key: string]: any }[] = [
   {
-    id: '0',
-    event: '01.01.2022 20:00 Uhr - Test Veranstaltung',
-    category: '',
-    price: '',
+    id: '0-header',
+    event: '01.01.2022, 20:00 Uhr - Test Veranstaltung',
     seat: '',
     code: '',
-    ticketcode: 'Gruppenticket',
+    ticketcode: '',
     print: '',
+    type: '',
     actions: 'Gruppenticket drucken',
   },
   {
     id: '1',
     event: 'Test Veranstaltung 1',
-    category: '1. Kategorie',
-    price: 'Normalpreis',
     seat: 'Mitte - Reihe 12 - Platz 36',
     code: '1234',
     ticketcode: '1234567890',
     print: '1x',
+    type: 'print@home',
     actions: 'Einzelticket drucken',
   },
   {
     id: '2',
     event: 'Test Veranstaltung 1',
-    category: '1. Kategorie',
-    price: 'Normalpreis',
     seat: 'Mitte - Reihe 12 - Platz 37',
     code: '4321',
     ticketcode: '1234567890',
     print: '1x',
+    type: 'print@home',
     actions: 'Einzelticket drucken',
+  },
+  {
+    id: '3',
+    event: 'Test Veranstaltung 2',
+    seat: 'Mitte - Reihe 10 - Platz 7',
+    code: '4321',
+    ticketcode: '1234567890',
+    print: '1x',
+    type: 'print@home',
+    actions: 'Einzelticket drucken',
+    locked: true,
   },
 ];
 export const CorePrintTable = () => {
@@ -70,14 +79,21 @@ export const CorePrintTable = () => {
     }
     return state;
   };
+  console.log(rows.map(item => item.id.includes('header')) ? 'header' : '');
   return (
     <ThemeProvider theme={coreTheme}>
-      <Stack space="small">
+      <Stack space="xsmall">
         <Inline space="small">
-          <Text variant="bold">Tickets</Text>
+          <Headline level="4">Veranstaltungen</Headline>
           <Split />
-          <ResaleLogbook />
-          <AutoRenew />
+          <Stack alignX="center">
+            <ResaleLogbook />
+            <Text>Logbuch</Text>
+          </Stack>
+          <Stack alignX="center">
+            <AutoRenew />
+            <Text>Refresh</Text>
+          </Stack>
         </Inline>
         <Table
           aria-label="Example dynamic collection table"
@@ -90,22 +106,56 @@ export const CorePrintTable = () => {
           <Table.Body items={rows}>
             {rows.map(item => (
               <Table.Row key={item.id}>
-                <Table.Cell>{item.event}</Table.Cell>
-                <Table.Cell>{item.category}</Table.Cell>
-                <Table.Cell>{item.price}</Table.Cell>
+                <Table.Cell>
+                  {item.id.includes('header') ? (
+                    <strong>{item.event}</strong>
+                  ) : (
+                    <>
+                      {item.locked === true ? (
+                        <Inline>
+                          <Lock size={14} />
+                          {item.event}
+                        </Inline>
+                      ) : (
+                        <Text>{item.event}</Text>
+                      )}
+                    </>
+                  )}
+                </Table.Cell>
                 <Table.Cell>{item.seat}</Table.Cell>
                 <Table.Cell>{item.code}</Table.Cell>
                 <Table.Cell>
-                  <Button variant="link">
-                    {item.ticketcode}
-                    <ExternalLink size={14} />
-                  </Button>
+                  {item.ticketcode && (
+                    <>
+                      {item.locked === true ? (
+                        <Inline>
+                          <Button variant="link">
+                            <Lock size={14} />
+                            {item.ticketcode}
+                            <ExternalLink size={14} />
+                          </Button>
+                        </Inline>
+                      ) : (
+                        <Button variant="link">
+                          {item.ticketcode}
+                          <ExternalLink size={14} />
+                        </Button>
+                      )}
+                    </>
+                  )}
                 </Table.Cell>
                 <Table.Cell>
                   <Button variant="link">{item.print}</Button>
                 </Table.Cell>
                 <Table.Cell>
-                  <Button variant="link">{item.actions}</Button>
+                  <Button variant="link">{item.type}</Button>
+                </Table.Cell>
+                <Table.Cell>
+                  <Button variant="link">
+                    {item.locked === true
+                      ? 'Ticketsperre bearbeiten'
+                      : 'Einzelticket drucken'}
+                  </Button>
                 </Table.Cell>
               </Table.Row>
             ))}
