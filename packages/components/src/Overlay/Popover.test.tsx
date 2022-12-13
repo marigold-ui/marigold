4aeb3c28cb49c7d78515c772ecb011eb8dbe4cd7
sarahgm <sarah.gosmann@reservix.de>
2022-12-13 10:54:44 +0100

    fix test

--- packages/components/src/Overlay/Popover.test.tsx
+++ packages/components/src/Overlay/Popover.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-node-access */
 import React from 'react';
 import { OverlayProvider } from '@react-aria/overlays';
 import { useObjectRef } from '@react-aria/utils';
