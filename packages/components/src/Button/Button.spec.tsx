76d48984658f7f5f141c7bb87fddd06efc683e45
sarahgm <sarah.gosmann@reservix.de>
2023-01-17 15:23:38 +0100

    save

--- packages/components/src/Button/Button.spec.tsx
+++ packages/components/src/Button/Button.spec.tsx
@@ -1,8 +1,18 @@
 import { test, expect } from '@playwright/test';
+import {
+  test as cttest,
+  expect as ctexpect,
+} from '@playwright/experimental-ct-react';
+import { Button } from './Button';
 
 test('example test', async ({ page }) => {
   await page.goto(
-    'http://localhost:1337/iframe.html?globals=theme:stacked&id=components-button--basic&viewMode=story'
+    'http://localhost:1337/?path=/story/components-button--basic&globals=theme:stacked'
   ); // The baseURL here is the webServer URL
   await expect(page).toHaveScreenshot();
+});
+
+cttest('render props', async ({ mount }) => {
+  const component = await mount(<Button title="Submit" />);
+  await ctexpect(component).toContainText('Submit');
 });
