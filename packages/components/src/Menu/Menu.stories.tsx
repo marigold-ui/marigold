123076e8ef812e1c7854838206af13ff3fff487e
sarahgm <sarah.gosmann@reservix.de>
2023-01-19 16:05:50 +0100

    chore: test responsiveness in Chromatic

--- packages/components/src/Menu/Menu.stories.tsx
+++ packages/components/src/Menu/Menu.stories.tsx
@@ -59,3 +59,8 @@ export const DotMenu: ComponentStory<typeof Menu> = args => (
     </Menu>
   </Menu.Trigger>
 );
+
+Basic.parameters = {
+  // Set the viewports in Chromatic at a story level.
+  chromatic: { viewports: [320, 1200] },
+};
