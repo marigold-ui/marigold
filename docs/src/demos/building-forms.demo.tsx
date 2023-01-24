ce31363a61aa6fb6cf496cacfbeefa93badb9863
sarahgm <sarah.gosmann@reservix.de>
2023-01-24 15:30:21 +0100

    saves

--- docs/src/demos/building-forms.demo.tsx
+++ docs/src/demos/building-forms.demo.tsx
@@ -1,42 +1,36 @@
-import {
-  Button,
-  Stack,
-  NumberField,
-  TextField,
-  ThemeProvider,
-} from '@marigold/components';
-import { useState } from 'react';
 import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import { Button, Select, Stack, TextField } from '@marigold/components';
+import { useState } from 'react';
 
 interface IFormInputs {
   firstName: string;
   name: string;
-  phone: number;
+  phone: string;
   mail: string;
+  country: string | number;
 }
 
 export const Form = () => {
-  const { control, handleSubmit, setValue } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({
     defaultValues: {
       firstName: '',
       name: '',
-      phone: 0,
+      phone: '',
       mail: '',
+      country: '',
     },
   });
-
-  const [error, setError] = useState<boolean>(false);
-  if (
-    /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
-      value
-    )
-  ) {
-    setError(true);
-  }
+  const [selected, setSelected] = useState<string | number>('');
   const onSubmit: SubmitHandler<IFormInputs> = data => {
+    console.log(data);
     alert(JSON.stringify(data));
   };
 
+  console.log(selected);
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -44,24 +38,32 @@ export const Form = () => {
           <Controller
             name="firstName"
             control={control}
+            rules={{ required: true }}
             render={({ field }) => (
               <TextField
+                {...field}
                 label="Firstname:"
+                required
                 description="Please enter your firstname"
                 placeholder="Firstname"
-                {...field}
+                error={field.value.length < 0 ? true : false}
+                errorMessage="The field is required. Please enter your firstname."
               />
             )}
           />
           <Controller
             name="name"
             control={control}
+            rules={{ required: true }}
             render={({ field }) => (
               <TextField
+                {...field}
                 label="Name:"
+                required
                 description="Please enter your name"
                 placeholder="Name"
-                {...field}
+                error={field.value.length < 0 ? true : false}
+                errorMessage="The field is required. Please enter your name."
               />
             )}
           />
@@ -69,12 +71,23 @@ export const Form = () => {
           <Controller
             name="phone"
             control={control}
+            rules={{
+              required: true,
+              minLength: 6,
+            }}
             render={({ field }) => (
-              <NumberField
-                label="Phone:"
-                description="Please enter your phone number"
-                hideStepper
+              <TextField
                 {...field}
+                label="Phone:"
+                required
+                description="Please enter your phone number"
+                error={
+                  field.value.length <= 6 && /^\d+$/.test(field.value)
+                    ? true
+                    : false
+                }
+                errorMessage="The field is required. Please enter a valid phone number."
+                pattern="[0-9]*"
               />
             )}
           />
@@ -82,19 +95,59 @@ export const Form = () => {
           <Controller
             name="mail"
             control={control}
+            rules={{ required: true }}
             render={({ field }) => (
               <TextField
+                {...field}
                 label="E-Mail:"
                 description="Please enter your E-Mail adress"
                 placeholder="E-Mail"
-                error={error}
-                value={field.value}
-                errorMessage="Please enter a valid E-Mail adress."
-                {...field}
+                required
+                error={
+                  field.value.length > 0 && !/^\S+@\S+\.\S+$/.test(field.value)
+                    ? true
+                    : false
+                }
+                errorMessage="The field is required. Please enter a valid E-Mail adress."
               />
             )}
           />
-          <Button variant="primary" size="small" type="submit">
+
+          <Controller
+            name="country"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                label="Country:"
+                name="country"
+                description="Please select your country."
+                onSelectionChange={setSelected}
+                selectedKey={selected}
+                onChange={e => {
+                  field.onChange(e);
+                  console.log('log', e.target.value); // let's say this is the additional processing
+                }}
+              >
+                <Select.Option key={'germany'} textValue={'germany'}>
+                  Germany
+                </Select.Option>
+                <Select.Option key={'austria'} textValue={'austria'}>
+                  Austria
+                </Select.Option>
+                <Select.Option key={'switzerland'} textValue={'switzerland'}>
+                  Switzerland
+                </Select.Option>
+              </Select>
+            )}
+          />
+
+          <Button
+            variant="primary"
+            size="small"
+            type="submit"
+            disabled={!isValid}
+          >
             Submit
           </Button>
         </Stack>
