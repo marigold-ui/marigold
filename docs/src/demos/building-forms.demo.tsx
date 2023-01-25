1fdf61582281393a6be0c69d1f89d460ef0abc8b
sarahgm <sarah.gosmann@reservix.de>
2023-01-25 16:20:58 +0100

    save

--- docs/src/demos/building-forms.demo.tsx
+++ docs/src/demos/building-forms.demo.tsx
@@ -1,5 +1,14 @@
 import { useForm, SubmitHandler, Controller } from 'react-hook-form';
-import { Button, Select, Stack, TextField } from '@marigold/components';
+import {
+  Button,
+  Aside,
+  FieldGroup,
+  Select,
+  Stack,
+  TextField,
+  Headline,
+  Columns,
+} from '@marigold/components';
 import { useState } from 'react';
 
 interface IFormInputs {
@@ -29,118 +38,115 @@ export const Form = () => {
   const [selected, setSelected] = useState<string | number>('');
 
   const onSubmit: SubmitHandler<IFormInputs> = data => {
-    console.log(data);
     alert(JSON.stringify(data));
   };
 
-  console.log(isValid);
   return (
-    <>
+    <FieldGroup labelWidth="medium">
+      <Headline level="2">Example Form</Headline>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Stack space="medium">
-          <Controller
-            name="firstName"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Firstname:"
-                required
-                description="Please enter your firstname"
-                placeholder="Firstname"
-                error={field.value.length < 0 ? true : false}
-                errorMessage="The field is required. Please enter your firstname."
-              />
-            )}
-          />
-          <Controller
-            name="name"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Name:"
-                required
-                description="Please enter your name"
-                placeholder="Name"
-                error={field.value.length < 0 ? true : false}
-                errorMessage="The field is required. Please enter your name."
-              />
-            )}
-          />
-
-          <Controller
-            name="phone"
-            control={control}
-            rules={{
-              required: true,
-              minLength: 6,
-            }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Phone:"
-                required
-                description="Please enter your phone number"
-                error={
-                  field.value.length <= 5 && /^[0-9]*$/.test(field.value)
-                    ? true
-                    : false
-                }
-                errorMessage="The field is required. Please enter a valid phone number."
-              />
-            )}
-          />
-
-          <Controller
-            name="mail"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="E-Mail:"
-                description="Please enter your E-Mail adress"
-                placeholder="E-Mail"
-                required
-                error={
-                  field.value.length > 0 && !/^\S+@\S+\.\S+$/.test(field.value)
-                    ? true
-                    : false
-                }
-                errorMessage="The field is required. Please enter a valid E-Mail adress."
-              />
-            )}
-          />
-
-          <Controller
-            name="country"
-            control={control}
-            render={({ field }) => (
-              <Select
-                {...field}
-                label="Country:"
-                onChange={field.onChange}
-                value={field.value}
-                description="Please select your country."
-                onSelectionChange={setSelected}
-                selectedKey={selected}
-              >
-                <Select.Option key={'germany'} textValue={'germany'}>
-                  Germany
-                </Select.Option>
-                <Select.Option key={'austria'} textValue={'austria'}>
-                  Austria
-                </Select.Option>
-                <Select.Option key={'switzerland'} textValue={'switzerland'}>
-                  Switzerland
-                </Select.Option>
-              </Select>
-            )}
-          />
-
+          <Columns columns={[2, 2]} space="medium">
+            <Controller
+              name="firstName"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Firstname:"
+                  required
+                  description="Please enter your firstname"
+                  placeholder="Firstname"
+                  error={field.value.length < 0 ? true : false}
+                  errorMessage="The field is required. Please enter your firstname."
+                />
+              )}
+            />
+            <Controller
+              name="name"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Name:"
+                  required
+                  description="Please enter your name"
+                  placeholder="Name"
+                  error={field.value.length < 0 ? true : false}
+                  errorMessage="The field is required. Please enter your name."
+                />
+              )}
+            />
+          </Columns>
+          <Stack space="medium">
+            <Controller
+              name="phone"
+              control={control}
+              rules={{
+                required: true,
+                min: 6,
+              }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Phone:"
+                  required
+                  placeholder="Phone"
+                  type="tel"
+                  description="Please enter your phone number"
+                  error={!/^[0-9]*$/.test(field.value) ? true : false}
+                  errorMessage="The field is required. Please enter a valid phone number."
+                />
+              )}
+            />
+            <Controller
+              name="mail"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="E-Mail:"
+                  description="Please enter your E-Mail adress"
+                  placeholder="E-Mail"
+                  required
+                  error={
+                    field.value.length > 0 &&
+                    !/^\S+@\S+\.\S+$/.test(field.value)
+                      ? true
+                      : false
+                  }
+                  errorMessage="The field is required. Please enter a valid E-Mail adress."
+                />
+              )}
+            />
+            <Controller
+              name="country"
+              control={control}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  label="Country:"
+                  description="Please select your country."
+                  onSelectionChange={setSelected}
+                >
+                  <Select.Option key={'germany'} textValue={'germany'}>
+                    Germany
+                  </Select.Option>
+                  <Select.Option key={'austria'} textValue={'austria'}>
+                    Austria
+                  </Select.Option>
+                  <Select.Option key={'switzerland'} textValue={'switzerland'}>
+                    Switzerland
+                  </Select.Option>
+                </Select>
+              )}
+            />
+          </Stack>
+        </Stack>
+        <Stack alignX="right">
           <Button
             variant="primary"
             size="small"
@@ -152,6 +158,6 @@ export const Form = () => {
           </Button>
         </Stack>
       </form>
-    </>
+    </FieldGroup>
   );
 };
