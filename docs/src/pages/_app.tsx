916d57694c292d4824210fe1fd604db6c68a319e
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-02-10 12:55:36 +0100

    docs: improve `<Badge>` styles (#2761)

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MDXProvider } from '@mdx-js/react';
 
-import { Badge, MarigoldProvider, SSRProvider } from '@marigold/components';
+import { MarigoldProvider, SSRProvider } from '@marigold/components';
 import * as MarigoldComponents from '@marigold/components';
 import * as MarigoldIcons from '@marigold/icons';
 import unicornTheme from '@marigold/theme-unicorn';
@@ -70,10 +70,6 @@ const components = {
   ...MarigoldIcons,
 };
 
-const badgeNameToLowercase = (badge: string) => {
-  return badge.toLowerCase();
-};
-
 export interface PageProps {
   title?: string;
   caption?: string;
@@ -95,11 +91,6 @@ const MyApp = ({ Component, pageProps }: AppProps<PageProps>) => {
               <Title title={pageProps?.title} />
               {pageProps?.title && (
                 <Header>
-                  {pageProps?.badge && (
-                    <Badge variant={badgeNameToLowercase(pageProps.badge)}>
-                      {pageProps.badge}
-                    </Badge>
-                  )}
                   <GradientHeadline>{pageProps.title}</GradientHeadline>
                   {pageProps.caption && (
                     <Text variant="page-caption">{pageProps.caption}</Text>
