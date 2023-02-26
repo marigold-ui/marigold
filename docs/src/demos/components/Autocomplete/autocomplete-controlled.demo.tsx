c82a3bffb9f39638360da792dc22673012e2d43b
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-02-26 07:50:08 +0100

    better controlled demo

--- docs/src/demos/components/Autocomplete/autocomplete-controlled.demo.tsx
+++ docs/src/demos/components/Autocomplete/autocomplete-controlled.demo.tsx
@@ -1,31 +1,47 @@
-import { Key, useState } from 'react';
+import { Key, useMemo, useState } from 'react';
 import { Autocomplete, Stack, Text } from '@marigold/components';
 
 export const AutocompleteControlledDemo = () => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const vegetables = [
+    { id: 'spinach', name: 'Spinach' },
+    { id: 'carrots', name: 'Carrots' },
+    { id: 'broccoli', name: 'Broccoli' },
+    { id: 'garlic', name: 'Garlic' },
+    { id: 'brussels-sprouts', name: 'Brussels Sprouts' },
+    { id: 'kale', name: 'Kale' },
+    { id: 'peas', name: 'Peas' },
+    { id: 'beets', name: 'Beets' },
+  ];
+
   const [input, setInput] = useState('');
   const [submitted, setSubmitted] = useState<[Key | null, string | null]>([
     '',
     '',
   ]);
-
+  const filteredVeggies = useMemo(
+    () =>
+      vegetables.filter(item =>
+        item.name
+          .toLocaleLowerCase()
+          .replace(/\s/g, '')
+          .includes(input.toLocaleLowerCase().replace(/\s/g, ''))
+      ),
+    [vegetables, input]
+  );
+  console.log(filteredVeggies);
   return (
     <Stack space="large">
       <Autocomplete
         label="Favorite vegetable:"
+        items={filteredVeggies}
         value={input}
         onChange={setInput}
         onSubmit={(key, val) => setSubmitted([key, val])}
       >
-        <Autocomplete.Item key="spinach">Spinach</Autocomplete.Item>
-        <Autocomplete.Item key="carrots">Carrots</Autocomplete.Item>
-        <Autocomplete.Item key="broccoli">Broccoli</Autocomplete.Item>
-        <Autocomplete.Item key="garlic">Garlic</Autocomplete.Item>
-        <Autocomplete.Item key="brussels-sprouts">
-          Brussels Sprouts
-        </Autocomplete.Item>
-        <Autocomplete.Item key="kale">Kale</Autocomplete.Item>
-        <Autocomplete.Item key="peas">Peas</Autocomplete.Item>
-        <Autocomplete.Item key="beets">Beets</Autocomplete.Item>
+        {(item: (typeof vegetables)[number]) => (
+          <Autocomplete.Item>{item.name}</Autocomplete.Item>
+        )}
       </Autocomplete>
       <Stack>
         <Text fontWeight="900">User input: "{input}"</Text>
