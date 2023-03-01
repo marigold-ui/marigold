4ce20a8a45b0556679a397148b56cdee1456bed6
sarahgm <sarah.gosmann@reservix.de>
2023-03-01 15:40:46 +0100

    styles

--- themes/theme-core/src/components/Input.style.ts
+++ themes/theme-core/src/components/Input.style.ts
@@ -2,9 +2,22 @@ import type { Theme } from '@marigold/components';
 
 export const Input: Theme['components']['Input'] = {
   base: {
-    border: '1px solid',
-    borderColor: '#aaa',
-    borderRadius: 'small',
-    lineHeight: 'medium',
+    container: {
+      border: '1px solid',
+      borderColor: '#aaa',
+      borderRadius: 'small',
+      lineHeight: 'medium',
+    },
+    icon: {
+      left: 'xxsmall',
+    },
+    input: {
+      pl: 'xsmall',
+      pr: 'xsmall',
+
+      '&:has-icon': {
+        pl: 'large',
+      },
+    },
   },
 };
