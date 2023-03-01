419b51b9a9fbaddd6534b0a85934c0ace2e283ac
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-03-01 09:00:40 +0100

    update stories to new format

--- packages/components/src/Autocomplete/Autocomplete.stories.tsx
+++ packages/components/src/Autocomplete/Autocomplete.stories.tsx
@@ -1,12 +1,12 @@
 import React, { Key, useState } from 'react';
-import { Meta, ComponentStory } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { Container } from '../Container';
 import { Stack } from '../Stack';
 import { Autocomplete } from './Autocomplete';
 import { useAsyncList } from '@react-stately/data';
 
-export default {
+const meta = {
   title: 'Components/Autocomplete',
   argTypes: {
     label: {
@@ -57,80 +57,90 @@ export default {
       description: 'The width of the field',
     },
   },
-} as Meta;
+} satisfies Meta;
 
-export const Basic: ComponentStory<typeof Autocomplete> = args => (
-  <Container size="large">
-    <Autocomplete {...args}>
-      <Autocomplete.Item key="Harry Potter">Harry Potter</Autocomplete.Item>
-      <Autocomplete.Item key="Lord of the Rings">
-        Lord of the Rings
-      </Autocomplete.Item>
-      <Autocomplete.Item key="Star Wars">Star Wars</Autocomplete.Item>
-      <Autocomplete.Item key="Star Trek">Star Trek</Autocomplete.Item>
-      <Autocomplete.Item key="Firefly">Firefly</Autocomplete.Item>
-    </Autocomplete>
-  </Container>
-);
+export default meta;
 
-export const Controlled: ComponentStory<typeof Autocomplete> = args => {
-  const [submitted, setSubmitted] = useState<[Key | null, string | null]>([
-    '',
-    '',
-  ]);
-  const [current, setCurrent] = useState<string>('');
-  return (
+export const Basic: StoryObj<typeof Autocomplete> = {
+  render: args => (
     <Container size="large">
-      <Stack space="medium">
-        <Autocomplete
-          {...args}
-          value={current}
-          onChange={setCurrent}
-          onSubmit={(key, val) => setSubmitted([key, val])}
-          disabledKeys={['star-trek']}
-        >
-          <Autocomplete.Item key="harry-potter">Harry Potter</Autocomplete.Item>
-          <Autocomplete.Item key="lord-of-the-rings">
-            Lord of the Rings
-          </Autocomplete.Item>
-          <Autocomplete.Item key="star-wars">Star Wars</Autocomplete.Item>
-          <Autocomplete.Item key="star-trek">Star Trek</Autocomplete.Item>
-          <Autocomplete.Item key="firefly">Firefly</Autocomplete.Item>
-        </Autocomplete>
-        <pre>current: {current}</pre>
-        <pre>
-          submitted: (key: {submitted[0]}, value: {submitted[1]})
-        </pre>
-      </Stack>
+      <Autocomplete {...args}>
+        <Autocomplete.Item key="Harry Potter">Harry Potter</Autocomplete.Item>
+        <Autocomplete.Item key="Lord of the Rings">
+          Lord of the Rings
+        </Autocomplete.Item>
+        <Autocomplete.Item key="Star Wars">Star Wars</Autocomplete.Item>
+        <Autocomplete.Item key="Star Trek">Star Trek</Autocomplete.Item>
+        <Autocomplete.Item key="Firefly">Firefly</Autocomplete.Item>
+      </Autocomplete>
     </Container>
-  );
+  ),
 };
 
-export const Async: ComponentStory<typeof Autocomplete> = args => {
-  const list = useAsyncList<{ name: string }>({
-    async load({ signal, filterText }) {
-      const res = await fetch(
-        `https://swapi.py4e.com/api/people/?search=${filterText}`,
-        { signal }
-      );
-      const json = await res.json();
+export const Controlled: StoryObj<typeof Autocomplete> = {
+  render: args => {
+    const [submitted, setSubmitted] = useState<[Key | null, string | null]>([
+      '',
+      '',
+    ]);
+    const [current, setCurrent] = useState<string>('');
+    return (
+      <Container size="large">
+        <Stack space="medium">
+          <Autocomplete
+            {...args}
+            value={current}
+            onChange={setCurrent}
+            onSubmit={(key, val) => setSubmitted([key, val])}
+            disabledKeys={['star-trek']}
+          >
+            <Autocomplete.Item key="harry-potter">
+              Harry Potter
+            </Autocomplete.Item>
+            <Autocomplete.Item key="lord-of-the-rings">
+              Lord of the Rings
+            </Autocomplete.Item>
+            <Autocomplete.Item key="star-wars">Star Wars</Autocomplete.Item>
+            <Autocomplete.Item key="star-trek">Star Trek</Autocomplete.Item>
+            <Autocomplete.Item key="firefly">Firefly</Autocomplete.Item>
+          </Autocomplete>
+          <pre>current: {current}</pre>
+          <pre>
+            submitted: (key: {submitted[0]}, value: {submitted[1]})
+          </pre>
+        </Stack>
+      </Container>
+    );
+  },
+};
 
-      return {
-        items: json.results,
-      };
-    },
-  });
+export const Async: StoryObj<typeof Autocomplete> = {
+  render: () => {
+    const list = useAsyncList<{ name: string }>({
+      async load({ signal, filterText }) {
+        const res = await fetch(
+          `https://swapi.py4e.com/api/people/?search=${filterText}`,
+          { signal }
+        );
+        const json = await res.json();
 
-  return (
-    <Autocomplete
-      label="Search Star Wars Characters"
-      items={list.items}
-      value={list.filterText}
-      onChange={list.setFilterText}
-    >
-      {(item: any) => (
-        <Autocomplete.Item key={item.name}>{item.name}</Autocomplete.Item>
-      )}
-    </Autocomplete>
-  );
+        return {
+          items: json.results,
+        };
+      },
+    });
+
+    return (
+      <Autocomplete
+        label="Search Star Wars Characters"
+        items={list.items}
+        value={list.filterText}
+        onChange={list.setFilterText}
+      >
+        {(item: any) => (
+          <Autocomplete.Item key={item.name}>{item.name}</Autocomplete.Item>
+        )}
+      </Autocomplete>
+    );
+  },
 };
