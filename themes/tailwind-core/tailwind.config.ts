38916a77a2f11a1783bfcbf268a17c88238b8030
sarahgm <sarah.gosmann@reservix.de>
2023-04-12 16:37:58 +0200

    "__baseCSS"

--- themes/tailwind-core/tailwind.config.ts
+++ themes/tailwind-core/tailwind.config.ts
@@ -7,7 +7,11 @@ const preset = createPreset({ name: 'unicorn' });
 // Figma File: https://www.figma.com/file/RiWJBV4Z8L8ycVvUuMYXbm/%F0%9F%93%93-CR---Components-2022?node-id=1452-1785&t=YaLGVHzniD5mOtbJ-0
 
 export default {
-  content: ['src/root.ts', 'src/**/*.styles.ts'],
+  content: [
+    'src/root.ts',
+    'src/**/*.styles.ts',
+    '../../packages/components/**/**.{tsx,ts}',
+  ],
   presets: [preset],
   theme: {
     extend: {
