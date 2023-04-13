78063788828812a0d6557e32f65aedbb3dd3b717
sarahgm <sarah.gosmann@reservix.de>
2023-04-13 08:53:26 +0200

    add prefix to preset

--- themes/tailwind-core/src/preset.ts
+++ themes/tailwind-core/src/preset.ts
@@ -2,11 +2,16 @@ import { Config } from 'tailwindcss/types/config';
 
 export interface PresetConfig {
   name: string;
+  preflight?: boolean;
 }
 
-export const createPreset = ({}: PresetConfig) => {
+export const createPreset = ({ preflight = true }: PresetConfig) => {
   const preset: Config = {
+    important: '[data-theme="core"]',
     content: [],
+    corePlugins: {
+      preflight,
+    },
     plugins: [
       //TODO: Create prefix plugin using `name`
     ],
--- themes/tailwind-core/tailwind.config.ts
+++ themes/tailwind-core/tailwind.config.ts
@@ -2,12 +2,11 @@ import type { Config } from 'tailwindcss';
 
 import { createPreset } from './src/preset';
 
-const preset = createPreset({ name: 'unicorn' });
+const preset = createPreset({ name: 'core', preflight: false });
 
 // Figma File: https://www.figma.com/file/RiWJBV4Z8L8ycVvUuMYXbm/%F0%9F%93%93-CR---Components-2022?node-id=1452-1785&t=YaLGVHzniD5mOtbJ-0
 
 export default {
-  important: '#tailwind-core',
   content: [
     'src/root.ts',
     'src/**/*.styles.ts',
