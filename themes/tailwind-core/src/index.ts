9c42694842678677b55c163ecb0368afc0e36d79
sarahgm <sarah.gosmann@reservix.de>
2023-04-13 08:46:16 +0200

    add prefix

--- themes/tailwind-core/src/index.ts
+++ themes/tailwind-core/src/index.ts
@@ -1,1 +1,7 @@
-export const theme = {};
+import { button } from './components/Button.styles';
+
+export const theme = {
+  components: {
+    button,
+  },
+};
--- themes/tailwind-core/tailwind.config.ts
+++ themes/tailwind-core/tailwind.config.ts
@@ -7,6 +7,7 @@ const preset = createPreset({ name: 'unicorn' });
 // Figma File: https://www.figma.com/file/RiWJBV4Z8L8ycVvUuMYXbm/%F0%9F%93%93-CR---Components-2022?node-id=1452-1785&t=YaLGVHzniD5mOtbJ-0
 
 export default {
+  important: '#tailwind-core',
   content: [
     'src/root.ts',
     'src/**/*.styles.ts',
