29685d5eb86c00b35010904199ba96e19592d593
sarahgm <sarah.gosmann@reservix.de>
2023-04-17 11:35:42 +0200

    add comment

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,9 @@ module.exports = createConfig({
     '!packages/components/src/Accordion/useAccordionItem.ts',
     '!packages/components/src/Accordion/Accordion.tsx',
   ],
+  // needed 'modulePathIgnorePatterns' because without it throws errors according to:  Haste module map
+  // It cannot be resolved, because there exists several different files, or packages,
+  // that provide a module for that particular name and platform. The platform is generic (no extension).
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   moduleNameMapper: {
     '^@marigold/theme-(.+)$': '<rootDir>/themes/theme-$1/src',
