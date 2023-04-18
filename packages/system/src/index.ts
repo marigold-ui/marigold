dfe9c028d6f92ea15d8aaa929817100bf4507044
sarahgm <sarah.gosmann@reservix.de>
2023-04-18 14:09:42 +0200

    remove keyframes

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,5 +1,4 @@
 export * from './components';
 export * from './hooks';
-export * from './keyframes';
 export * from './types';
 export * from './defaultTheme';
--- packages/system/src/keyframes.ts
+++
@@ -1,1 +0,0 @@
-export { keyframes } from '@emotion/react';
