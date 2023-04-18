8fc8aee8f5df5ea594b60bfe61f1fc0e41e547bb
marcel.koehler <aromkoapps@gmail.com>
2023-04-18 13:20:50 +0200

    add tag group stories

--- packages/components/src/TagGroup/TagGroup.stories.tsx
+++ packages/components/src/TagGroup/TagGroup.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { TagGroup } from './TagGroup';
@@ -45,36 +45,14 @@ const meta = {
       description: 'The width of the field',
     },
   },
-  args: {
-    label: 'Label',
-  },
 } satisfies Meta<typeof TagGroup>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const items = [
-  { id: 1, name: 'News' },
-  { id: 2, name: 'Travel' },
-  { id: 3, name: 'Gaming' },
-  { id: 4, name: 'Shopping' },
-];
-
-export const Base: Story = {
+export const Basic: Story = {
   render: args => (
-    <TagGroup
-      {...args}
-      items={items}
-      aria-label="Dynamic TagGroup items example"
-    >
-      {item => <Item>{item.name}</Item>}
-    </TagGroup>
-  ),
-};
-
-export const DefaultSelected: Story = {
-  render: args => (
-    <TagGroup {...args} label="Categories">
+    <TagGroup {...args} aria-label="Static TagGroup items example">
       <Item key="news">News</Item>
       <Item key="travel">Travel</Item>
       <Item key="gaming">Gaming</Item>
@@ -83,7 +61,59 @@ export const DefaultSelected: Story = {
   ),
 };
 
-DefaultSelected.parameters = {
+export const Error: Story = {
+  render: args => (
+    <TagGroup errorMessage="Das ist ein Error" error {...args}>
+      <TagGroup.Tag key="news">News</TagGroup.Tag>
+      <TagGroup.Tag key="travel">Travel</TagGroup.Tag>
+      <TagGroup.Tag key="gaming">Gaming</TagGroup.Tag>
+      <TagGroup.Tag key="shopping">Shopping</TagGroup.Tag>
+    </TagGroup>
+  ),
+};
+
+export const WithLabel: Story = {
+  render: args => (
+    <TagGroup {...args} label="Categories">
+      <TagGroup.Tag key="news">News</TagGroup.Tag>
+      <TagGroup.Tag key="travel">Travel</TagGroup.Tag>
+      <TagGroup.Tag key="gaming">Gaming</TagGroup.Tag>
+      <TagGroup.Tag key="shopping">Shopping</TagGroup.Tag>
+    </TagGroup>
+  ),
+};
+
+export const RemovableTags: Story = {
+  render: args => {
+    let defaultItems = [
+      { id: 1, name: 'News' },
+      { id: 2, name: 'Travel' },
+      { id: 3, name: 'Gaming' },
+      { id: 4, name: 'Shopping' },
+    ];
+
+    let [items, setItems] =
+      useState<{ id: number; name: string }[]>(defaultItems);
+
+    let removeItem = (key: number) => {
+      setItems(prevItems => prevItems.filter(item => key !== item.id));
+    };
+
+    return (
+      <TagGroup
+        {...args}
+        items={items}
+        aria-label="TagGrooup removing example"
+        allowsRemoving
+        onRemove={removeItem}
+      >
+        {item => <Item>{item.name}</Item>}
+      </TagGroup>
+    );
+  },
+};
+
+Basic.parameters = {
   chromatic: { viewports: [320, 1200] },
   theme: isChromatic() ? 'b2b' : 'stacked',
 };
