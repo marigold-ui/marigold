1dc09f50d53c810dff9694a4df823979a55d849a
sarahgm <sarah.gosmann@reservix.de>
2023-04-19 09:11:06 +0200

    change to SVGSVGElement

--- packages/icons/src/action/Crop.tsx
+++ packages/icons/src/action/Crop.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
 import { SVG, SVGProps } from '@marigold/system';
 
-export const Crop = forwardRef<SVGElement, SVGProps>((props, ref) => (
+export const Crop = forwardRef<SVGSVGElement, SVGProps>((props, ref) => (
   <SVG {...props} viewBox="0 0 24 24" ref={ref}>
     <path d="M16.0909 14.4545H17.7273V7.90909C17.7273 7.00909 16.9909 6.27273 16.0909 6.27273H9.54545V7.90909H16.0909V14.4545ZM7.90909 16.0909V3H6.27273V6.27273H3V7.90909H6.27273V16.0909C6.27273 16.9909 7.00909 17.7273 7.90909 17.7273H16.0909V21H17.7273V17.7273H21V16.0909H7.90909Z" />
   </SVG>
--- packages/icons/src/action/SortDown.tsx
+++ packages/icons/src/action/SortDown.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
 import { SVG, SVGProps } from '@marigold/system';
 
-export const SortDown = forwardRef<SVGElement, SVGProps>((props, ref) => (
+export const SortDown = forwardRef<SVGSVGElement, SVGProps>((props, ref) => (
   <SVG {...props} viewBox="0 0 24 24" ref={ref}>
     <path d="M17.3962 10.0496L12.5042 14.9416C12.3731 15.0727 12.1984 15.1492 12.0128 15.1492C11.8272 15.1492 11.6524 15.0727 11.5214 14.9416L6.62934 10.0496C6.49827 9.91854 6.42188 9.7439 6.42188 9.55816C6.42188 9.17606 6.73856 8.85938 7.12078 8.85938H16.9048C17.287 8.85938 17.6037 9.17606 17.6037 9.55816C17.6037 9.7439 17.5273 9.91854 17.3962 10.0496Z" />
   </SVG>
