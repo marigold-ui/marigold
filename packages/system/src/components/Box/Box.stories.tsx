bb89b73bb545c24f277a0ba97389328e6331057b
sarahgm <sarah.gosmann@reservix.de>
2023-04-24 11:57:43 +0200

    box stories

--- packages/system/src/components/Box/Box.stories.tsx
+++ packages/system/src/components/Box/Box.stories.tsx
@@ -32,54 +32,20 @@ const meta = {
       },
       description: 'The variant of the box',
     },
-    css: {
-      control: {
-        type: 'object',
-      },
-      description: 'The css object to apply to the box',
-      table: {
-        type: { summary: 'object' },
-        defaultValue: {
-          summary: {
-            width: 100,
-            borderRadius: 5,
-            px: 8,
-            py: 2,
-            marginInline: 'auto',
-            color: '#0b7285',
-            bg: '#99e9f2',
-            cursor: 'pointer',
-            '&:hover': {
-              bg: '#c5f6fa',
-              transform: 'scale(2)',
-            },
-          },
-        },
-      },
-    },
   },
   args: {
     children: 'I am a box!',
     as: 'div',
-    css: {
-      width: 100,
-      borderRadius: 5,
-      px: 8,
-      py: 2,
-      marginInline: 'auto',
-      color: '#0b7285',
-      bg: '#99e9f2',
-      cursor: 'pointer',
-      '&:hover': {
-        bg: '#c5f6fa',
-        transform: 'scale(2)',
-      },
-    },
   },
 } satisfies Meta;
 
 export default meta;
 
 export const Basic: StoryObj<typeof Box> = {
-  render: args => <Box {...args} />,
+  render: args => (
+    <Box
+      className="ms-auto me-auto w-6/12 rounded bg-[#99e9f2] px-2 py-1 text-[#0b7285] hover:scale-150 hover:bg-[#c5f6fa]"
+      {...args}
+    />
+  ),
 };
