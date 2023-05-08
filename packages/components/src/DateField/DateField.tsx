68af55276fcdaed91f8f31eaacdcb7812c6a30e2
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-05-08 16:22:46 +0300

    resolving comments

--- packages/components/src/DateField/DateField.tsx
+++ packages/components/src/DateField/DateField.tsx
@@ -15,7 +15,6 @@ import { FieldBase } from '../FieldBase';
 import { mergeProps } from '@react-aria/utils';
 import { useHover } from '@react-aria/interactions';
 import { useFocusRing } from '@react-aria/focus';
-import { Calendar } from '@marigold/icons';
 
 // Props
 // ----------------
@@ -201,7 +200,13 @@ export const DateField = ({
             marginRight: '8px',
           }}
         >
-          {action ? action : <Calendar />}
+          {action ? (
+            action
+          ) : (
+            <svg width="28" height="28" viewBox="0 0 24 24">
+              <path d="M20.0906 19.2V6.6C20.0906 5.61 19.2806 4.8 18.2906 4.8H17.3906V3H15.5906V4.8H8.39062V3H6.59062V4.8H5.69063C4.69163 4.8 3.89962 5.61 3.89962 6.6L3.89062 19.2C3.89062 20.19 4.69163 21 5.69063 21H18.2906C19.2806 21 20.0906 20.19 20.0906 19.2ZM9.29062 11.1001H7.49061V12.9001H9.29062V11.1001ZM5.69062 8.40009H18.2906V6.60008H5.69062V8.40009ZM18.2906 10.2V19.2H5.69062V10.2H18.2906ZM14.6906 12.9001H16.4906V11.1001H14.6906V12.9001ZM12.8906 12.9001H11.0906V11.1001H12.8906V12.9001Z"></path>
+            </svg>
+          )}
         </Box>
       </Box>
     </FieldBase>
