e143313d633332b8845599c3bd9fae429a64ba08
sarahgm <sarah.gosmann@reservix.de>
2023-05-10 14:10:42 +0200

    update

---
+++ themes/tailwind-core/src/components/Accordion.styles.ts
@@ -0,0 +1,9 @@
+import { Theme } from '@marigold/system';
+import { cva } from 'class-variance-authority';
+
+export const Accordion: Theme['components']['Accordion'] = {
+  button: cva(
+    'border-none p-0 w-full justify-between bg-button-base-background'
+  ),
+  item: cva(''),
+};
