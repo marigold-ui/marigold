ab0320f7b75bfea924b97944e970ad65faf3429d
sarahgm <sarah.gosmann@reservix.de>
2023-05-12 11:13:27 +0200

    root

--- themes/theme-core/src/root.ts
+++ themes/theme-core/src/root.ts
@@ -1,4 +1,4 @@
-import { tv, type TVReturnType } from 'tailwind-variants';
-export const root: TVReturnType<any, any, never, never, any, any> = tv({
-  base: 'font-body text-[13px] text-root-body bg-root-background',
-});
+import { cva } from 'class-variance-authority';
+export const root = cva(
+  'font-body text-[13px] text-root-body bg-root-background'
+);
