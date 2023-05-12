4a2ce7c1c3c466c34c0672532c96c03ca483e400
sarahgm <sarah.gosmann@reservix.de>
2023-05-12 09:44:48 +0200

    save

--- themes/theme-core/src/components/Input.styles.ts
+++ themes/theme-core/src/components/Input.styles.ts
@@ -1,13 +1,15 @@
-import { tv, type TVReturnType } from 'tailwind-variants';
-export const input: TVReturnType<any, any, any, any, any, any> = tv({
-  slots: {
-    input: [
+import { cva } from 'class-variance-authority';
+
+export const Input = {
+  input: cva(
+    [
       'border border-solid rounded-sm border-input-border',
       'leading-normal pl-2 pr-2',
       'data-[has-icon]:pl-8',
       'mg-error:border-error-text',
     ],
-    icon: ['left-1'],
-    container: [''],
-  },
-});
+    {}
+  ),
+  icon: cva('left-1', {}),
+  container: cva({}),
+};
