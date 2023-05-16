97749ca85e8ce7b16652a7e67c52904e86826fdb
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-05-16 15:52:23 +0200

    t

--- themes/theme-core/src/components/Input.styles.ts
+++ themes/theme-core/src/components/Input.styles.ts
@@ -6,7 +6,7 @@ export const Input = {
   container: cva([
     'border rounded-sm border-input-border bg-white',
     'focus-within:outline-focus-bg focus-within:outline-2 focus-within:outline -outline-offset-1',
-    inputHeight, // makes sure that container will not grow larger than input.
+    inputHeight, // makes sure that container will not grow larger than input
   ]),
   input: cva([`${inputHeight} leading-[22px]`, 'px-2 peer-data-[icon]:pl-6']),
   icon: cva('left-0.5 [&>*]:h-[18px] [&>*]:w-[18px]'),
