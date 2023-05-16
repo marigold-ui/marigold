fb487cfd37ab3eddcc46e693a4ae1090eee7fde8
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-05-16 15:52:08 +0200

    input focus styles

--- themes/theme-core/src/components/Input.styles.ts
+++ themes/theme-core/src/components/Input.styles.ts
@@ -5,6 +5,7 @@ const inputHeight = 'h-[22px]';
 export const Input = {
   container: cva([
     'border rounded-sm border-input-border bg-white',
+    'focus-within:outline-focus-bg focus-within:outline-2 focus-within:outline -outline-offset-1',
     inputHeight, // makes sure that container will not grow larger than input.
   ]),
   input: cva([`${inputHeight} leading-[22px]`, 'px-2 peer-data-[icon]:pl-6']),
