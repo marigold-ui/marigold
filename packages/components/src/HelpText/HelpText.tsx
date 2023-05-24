1aff6a79c4ee06b3e2ce9c5b29b751d48ca4db48
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-05-24 21:46:25 +0200

    helptext

--- packages/components/src/HelpText/HelpText.tsx
+++ packages/components/src/HelpText/HelpText.tsx
@@ -1,12 +1,16 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
-import { Box, SVG, cn, useClassNames } from '@marigold/system';
+import { SVG, cn, useClassNames } from '@marigold/system';
 import { HtmlProps } from '@marigold/types';
 
 // Props
 // ---------------
-export interface HelpTextProps extends HtmlProps<'div'> {
+export interface HelpTextProps extends Omit<HtmlProps<'div'>, 'className'> {
   variant?: string;
   size?: string;
+  className?: {
+    container?: string;
+    icon?: string;
+  };
   disabled?: boolean;
   description?: ReactNode;
   descriptionProps?: HTMLAttributes<HTMLElement>;
@@ -38,14 +42,18 @@ export const HelpText = ({
   });
 
   return (
-    <Box
+    <div
       {...props}
       {...(hasErrorMessage ? errorMessageProps : descriptionProps)}
-      className={cn('flex items-center gap-1', classNames)}
+      className={cn('flex items-center gap-1', classNames.container)}
     >
       {hasErrorMessage ? (
         <>
-          <SVG viewBox="0 0 24 24" role="presentation">
+          <SVG
+            className={cn('h-4 w-4', classNames.icon)}
+            viewBox="0 0 24 24"
+            role="presentation"
+          >
             <path d="M2.25 20.3097H21.75L12 3.46875L2.25 20.3097ZM12.8864 17.2606H11.1136V15.4879H12.8864V17.2606ZM12.8864 13.7151H11.1136V10.1697H12.8864V13.7151Z" />
           </SVG>
 
@@ -54,6 +62,6 @@ export const HelpText = ({
       ) : (
         <>{description}</>
       )}
-    </Box>
+    </div>
   );
 };
