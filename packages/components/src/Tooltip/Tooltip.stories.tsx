f8a104aa6657f76e0bcb30545a9fc3347b6c5228
sarahgm <sarah.gosmann@reservix.de>
2023-05-24 13:59:22 +0200

    remove unused

--- packages/components/src/Tooltip/Tooltip.stories.tsx
+++ packages/components/src/Tooltip/Tooltip.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Box } from '@marigold/system';
 
 import { Tooltip } from './Tooltip';
 import { Button } from '../Button';
