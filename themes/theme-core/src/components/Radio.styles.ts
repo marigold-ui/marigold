c46487082411b37438793efe4cfc62c979f105fc
sarahgm <sarah.gosmann@reservix.de>
2023-05-25 13:27:22 +0200

    Tailwind: Radio

---
+++ themes/theme-core/src/components/Radio.styles.ts
@@ -0,0 +1,14 @@
+import { ThemeComponent } from '@marigold/system';
+import { cva } from 'class-variance-authority';
+
+export const Radio: ThemeComponent<'Radio'> = {
+  container: cva('disabled:text-disabled-text'),
+  radio: cva([
+    'border-radio-border',
+    'group-focus/radio:bg-black group-focus/radio:outline-2 group-focus/radio:outline-radio-focus group-focus/radio:outline-offset-[3]',
+    'group-hover/radio:border-radio-hover',
+    'group-checked/radio:text-secondary-50 group-checked/radio:bg-radio-checked checked:border-radio-checkedBorder',
+    'group-disabled/radio:bg-disabled-bg group-disabled/radio:border-disabled-border',
+  ]),
+  label: cva(),
+};
