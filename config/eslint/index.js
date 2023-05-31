d62c303f18c9e500c0f219ec9d833e2fc441c052
sarahgm <sarah.gosmann@reservix.de>
2023-05-31 12:25:22 +0200

    save this

--- config/eslint/index.js
+++ config/eslint/index.js
@@ -1,10 +1,16 @@
 module.exports = {
-  extends: ['react-app', 'react-app/jest', 'prettier'],
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'prettier',
+    'plugin:tailwindcss/recommended',
+  ],
   settings: {
     react: {
       version: 'detect',
     },
   },
+  root: true,
   rules: {
     // Disabled cause it doesn't currently work as of 01/02/2021
     'no-redeclare': 'off',
