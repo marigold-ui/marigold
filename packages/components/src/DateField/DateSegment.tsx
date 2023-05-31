2cce5b67100dd222d4cb238d411269bfe2aa6755
sarahgm <sarah.gosmann@reservix.de>
2023-05-31 16:00:39 +0200

    fixes styling

--- packages/components/src/DateField/DateSegment.tsx
+++ packages/components/src/DateField/DateSegment.tsx
@@ -42,7 +42,7 @@ export const DateSegment = ({
       {...mergeProps(segmentProps, stateProps, focusProps)}
       ref={ref}
       className={cn(
-        'group/segment',
+        'group/segment peer',
         maxValue != null && `min-w-[${String(maxValue).length}ch]`,
         'box-content rounded-sm text-center outline-0',
         classNames.segment
