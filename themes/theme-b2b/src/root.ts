b85ebc286ebe965d498b70505a22857568b1c08b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-06-05 20:39:46 +0200

    Tailwind: b2b theme setup + root styles (#3012)
    setup b2b theme and root styles

---
+++ themes/theme-b2b/src/root.ts
@@ -0,0 +1,2 @@
+import { cva } from 'class-variance-authority';
+export const root = cva('font-body text-text-primary bg-bg-body m-0 p-0');
---
+++ themes/theme-b2b/src/tokens.ts
@@ -0,0 +1,45 @@
+const brand = {
+  primary: {
+    100: '#fff8f1',
+    200: '#fdcca0',
+    400: '#f8ac67',
+    600: '#fa8005',
+    700: '#ea5200',
+    800: '#ae440a',
+  },
+  secondary: {
+    50: '#ffffff',
+    100: '#fafafa',
+    200: '#f3f3f3',
+    300: '#e3e3e3',
+    400: '#cccccc',
+    500: '#8d8d8d',
+    600: '#6d6d6d',
+    700: '#4b4b4b',
+    800: '#2b2b2b',
+    900: '#0f0f0f',
+  },
+} as const;
+
+export const colors = {
+  // Brand
+  // ---------------
+  brand,
+
+  // Text
+  // ---------------
+  text: {
+    primary: {
+      DEFAULT: brand.secondary[700],
+    },
+  },
+
+  // Background
+  // ---------------
+  bg: {
+    /**
+     * Should be set to either html or body element.
+     */
+    body: brand.secondary[50],
+  },
+};
