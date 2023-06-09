2943e27b47080942d24152f76fa545e08560061b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-06-09 14:22:43 +0200

    Tailwind: Message b2b (#3030)

--- themes/theme-b2b/src/components/index.ts
+++ themes/theme-b2b/src/components/index.ts
@@ -3,4 +3,5 @@ export * from './Field.styles';
 export * from './HelpText.styles';
 export * from './Input.styles';
 export * from './Label.styles';
+export * from './Message.styles';
 export * from './Underlay.styles';
