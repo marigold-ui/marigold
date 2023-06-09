7b72658069b212c7cf88ca91120cbd7efcf9ff61
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-06-09 14:30:38 +0200

    Tailwind: add text and headline b2b style (#3029)
    Co-authored-by: Sebastian Sebald <sebastian.sebald@reservix.de>

---
+++ themes/theme-b2b/src/components/Headline.styles.ts
@@ -0,0 +1,15 @@
+import { ThemeComponent } from '@marigold/system';
+import { cva } from 'class-variance-authority';
+
+export const Headline: ThemeComponent<'Headline'> = cva('', {
+  variants: {
+    size: {
+      'level-1': 'text-2xl',
+      'level-2': 'text-base',
+      'level-3': 'text-sm',
+      'level-4': 'text-[1.125rem]',
+      'level-5': 'text-xs',
+      'level-6': 'text-xs uppercase',
+    },
+  },
+});
---
+++ themes/theme-b2b/src/components/Text.styles.ts
@@ -0,0 +1,4 @@
+import { ThemeComponent } from '@marigold/system';
+import { cva } from 'class-variance-authority';
+
+export const Text: ThemeComponent<'Text'> = cva();
