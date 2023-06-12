4b5aeea59ad1252b7c080125082a9313bc7ba394
sarahgm <sarah.gosmann@reservix.de>
2023-06-12 16:36:06 +0200

    save

--- themes/theme-b2b/src/components/Select.styles.ts
+++ themes/theme-b2b/src/components/Select.styles.ts
@@ -7,12 +7,12 @@ export const Select: ThemeComponent<'Select'> = {
   select: cva([
     'appearance-none leading-8',
     'px-2',
-    'bg-bg-surface border-border-light rounded-sm border',
+    'bg-bg-surface border-border-light rounded-sm border border-solid',
     'cursor-pointer outline-none',
     'hover:border-border-hover',
     'disabled:border-border-disabled disabled:text-text-disabled disabled:bg-bg-disabled disabled:cursor-not-allowed',
-    'focus-visible:border-border-focus focus-visible:shadow-shadow-focus shadow-[0_0_0_1px]',
+    'focus:border-border-focus focus:shadow-shadow-focus focus:shadow-[0px_0px_0px_1px]',
     'aria-expanded:bg-bg-neutral aria-expanded:border-border-light',
-    'data-[error]:border-border-error',
+    'data-[error]:border-border-error data-[error]:shadow-none',
   ]),
 };
