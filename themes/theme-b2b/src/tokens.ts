6e68c1889337a9e854aa115f95902ea9a32e4140
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-06-13 12:36:25 +0300

    Tailwind:button stlyes

--- themes/theme-b2b/src/tokens.ts
+++ themes/theme-b2b/src/tokens.ts
@@ -31,15 +31,12 @@ export const colors = {
   text: {
     primary: {
       DEFAULT: brand.secondary[700],
-      light: brand.secondary[500],
     },
-    link: '#3ab3d5',
 
     light: brand.secondary[100],
 
     // State
     disabled: brand.secondary[400],
-    error: '#dd4142',
   },
 
   // Background
@@ -49,75 +46,49 @@ export const colors = {
      * Should be set to either html or body element.
      */
     body: brand.secondary[50],
+    primary: {
+      DEFAULT: brand.primary[600],
+      hover: brand.primary[400],
+    },
+    dark: {
+      DEFAULT: brand.secondary[700],
+      hover: brand.secondary[600],
+    },
 
     surface: {
       /**
        * Use this for e.g. card backgrounds.
        */
       DEFAULT: brand.secondary[50],
-      /**
-       * Use this when you need to separate a specific layer from
-       * the surface color, e.g. table headers.
-       */
-      raised: brand.secondary[500],
-      /**
-       * Use this when you need to make a specific layer
-       * to appear lower than the surface color, e.g. progress bar.
-       */
-      lowered: brand.secondary[700],
-
-      underlay: 'rgba(206, 212, 218, 0.5)',
     },
 
     // State
     disabled: brand.secondary[200],
-    focus: brand.primary[200],
-    selected: brand.primary[600],
-
-    // Status
-    info: '#1d67b6',
+    hover: brand.secondary[400],
   },
-  shadow: {
-    // state
-    focus: brand.primary[600],
 
-    // status
-    error: '#dd4142',
-  },
   // Border
   // ---------------
   border: {
     light: brand.secondary[400],
+    dark: brand.secondary[700],
 
     // State
-    disabled: brand.secondary[400],
     hover: brand.secondary[500],
     focus: brand.primary[600],
-    selected: brand.primary[800],
 
     // Status
     error: '#dd4142',
-    info: '#1d67b6',
-    warning: '#eac500',
   },
 
   // Outline
   // ---------------
   outline: {
+    dark: brand.secondary[700],
     // State
     focus: brand.primary[600],
 
     // Status
     error: '#dd4142',
-  },
-
-  // Fill
-  // ---------------
-  fill: {
-    // Status
-    required: 'rgb(221, 65, 66)',
-    error: '#dd4142',
-    info: '#1d67b6',
-    warning: '#eac500',
   },
 };
