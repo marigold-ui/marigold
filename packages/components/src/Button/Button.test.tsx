ae19d9d5ea5547973e8696972095b3da429fe78b
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-06-13 12:41:52 +0300

    fixing tests

--- packages/components/src/Button/Button.test.tsx
+++ packages/components/src/Button/Button.test.tsx
@@ -44,7 +44,7 @@ test('supports base styling classes', () => {
 
   expect(button).toMatchInlineSnapshot(`
     <button
-      class="items-center justify-center gap-[0.5ch] align-center flex disabled:bg-gray-600"
+      class="align-center flex disabled:bg-gray-600"
       type="button"
     >
       button
@@ -74,7 +74,7 @@ test('accepts other variants', () => {
   expect(button).toHaveClass('text-secondary-800');
   expect(button).toMatchInlineSnapshot(`
     <button
-      class="items-center justify-center gap-[0.5ch] align-center flex disabled:bg-gray-600 text-secondary-800"
+      class="align-center flex disabled:bg-gray-600 text-secondary-800"
       type="button"
     >
       button
