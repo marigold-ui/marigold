7836914bcf1b566ec9dbbfc144a17199791eb483
sarahgm <sarah.gosmann@reservix.de>
2023-06-14 14:53:34 +0200

    add simple spacing

--- themes/theme-b2b/src/components/Accordion.styles.ts
+++ themes/theme-b2b/src/components/Accordion.styles.ts
@@ -2,6 +2,8 @@ import { ThemeComponent } from '@marigold/system';
 import { cva } from 'class-variance-authority';
 
 export const Accordion: ThemeComponent<'Accordion'> = {
-  button: cva(),
-  item: cva(),
+  button: cva(
+    'bg-bg-surface flex w-full items-center justify-between border-none px-2 py-1 leading-normal'
+  ),
+  item: cva('p-2'),
 };
