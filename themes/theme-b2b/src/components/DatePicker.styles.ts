bf2a762ce3beab9d3316579e926b15062f8dd6c7
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-06-16 11:17:41 +0300

    Styling date picker button

--- themes/theme-b2b/src/components/DatePicker.styles.ts
+++ themes/theme-b2b/src/components/DatePicker.styles.ts
@@ -2,14 +2,12 @@ import { ThemeComponent } from '@marigold/system';
 import { cva } from 'class-variance-authority';
 
 export const DatePicker: ThemeComponent<'DatePicker'> = {
-  container: cva('[&_button]:[&_button[data-[hover]]]:bg-green-700'),
+  container: cva(),
   button: cva([
     'h-full w-9 p-0',
     'flex items-center justify-center ',
-    'text-text-primary/80 hover:text-text-primary border-none',
-    'group-focus/field:bg-red-600 group-focus/field:[&_svg]:fill-white',
-    'overflow-hidden',
+    'text-text-primary/80',
+    'data-[hover]:bg-bg-hover group-focus/field:bg-bg-hover group-focus/field:[&[aria-expanded=true]]:bg-bg-dark focus-visible:border-none focus-visible:outline-0 group-focus/field:text-white data-[hover]:text-white',
     'disabled:hidden',
-    '-outline-offset-1',
   ]),
 };
