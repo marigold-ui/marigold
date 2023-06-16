b03a5d771eb4f9d65ea0362e6d59b67d8bc3a98b
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-06-16 09:56:51 +0300

    Tailwind: Adding b2b styles

--- themes/theme-b2b/src/components/DatePicker.styles.ts
+++ themes/theme-b2b/src/components/DatePicker.styles.ts
@@ -2,13 +2,13 @@ import { ThemeComponent } from '@marigold/system';
 import { cva } from 'class-variance-authority';
 
 export const DatePicker: ThemeComponent<'DatePicker'> = {
-  container: cva(),
+  container: cva('[&_button]:[&_button[data-[hover]]]:bg-green-700'),
   button: cva([
-    ' h-full w-6',
-    'p-0',
-    'text-text-primary/80 hover:text-text-primary border-none bg-transparent',
-    'outline-none',
-
+    'h-full w-9 p-0',
+    'flex items-center justify-center ',
+    'text-text-primary/80 hover:text-text-primary border-none',
+    'group-focus/field:bg-red-600 group-focus/field:[&_svg]:fill-white',
+    'overflow-hidden',
     'disabled:hidden',
     '-outline-offset-1',
   ]),
