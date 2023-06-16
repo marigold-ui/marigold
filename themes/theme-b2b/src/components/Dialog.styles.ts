011dc45bd03ffeecb4c8fe770130dd107887b2f7
sarahgm <sarah.gosmann@reservix.de>
2023-06-16 11:39:46 +0200

    fix: underlay styles

--- themes/theme-b2b/src/components/Dialog.styles.ts
+++ themes/theme-b2b/src/components/Dialog.styles.ts
@@ -5,6 +5,6 @@ import { cva } from 'class-variance-authority';
 export const Dialog: ThemeComponent<'Dialog'> = {
   closeButton: cva('relative -right-6 -top-2'),
   container: cva(
-    'font-body rounded-sm px-8 pb-8 pt-4 shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)]'
+    'font-body bg-bg-surface rounded-sm px-8 pb-8 pt-4 shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)]'
   ),
 };
