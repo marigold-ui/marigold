e64a59bdb793290a8cf0005daee896e01e013b4b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-06-16 13:04:29 +0200

    fix: underlay base styles (#3063)
    fix: underlay styles

--- themes/theme-b2b/src/components/Underlay.styles.ts
+++ themes/theme-b2b/src/components/Underlay.styles.ts
@@ -2,7 +2,7 @@ import { ThemeComponent } from '@marigold/system';
 
 import { cva } from 'class-variance-authority';
 
-export const Underlay: ThemeComponent<'Underlay'> = cva('', {
+export const Underlay: ThemeComponent<'Underlay'> = cva('fixed inset-0 z-[1]', {
   variants: {
     variant: {
       modal: ' bg-bg-surface-underlay blur-[1]',
