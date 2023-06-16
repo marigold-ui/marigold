014451a52122f2dcd5fc6302cad31abbe4292f1b
Osama Abdul Latif <62595605+OsamaAbdellateef@users.noreply.github.com>
2023-06-16 11:55:39 +0300

    Tailwind: `DatePicker` b2b styles (#3061)

--- packages/components/src/DatePicker/DatePicker.tsx
+++ packages/components/src/DatePicker/DatePicker.tsx
@@ -81,20 +81,22 @@ export const DatePicker = ({
           description={!state.isOpen && description}
           triggerRef={ref}
           action={
-            <Button
-              {...mergeProps(buttonProps, stateProps)}
-              className={cn('absolute right-0 top-0', classNames.button)}
-              disabled={isDisabled}
-            >
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="currentColor"
+            <div className={classNames.container}>
+              <Button
+                {...mergeProps(buttonProps, stateProps)}
+                className={cn('absolute right-0 top-0', classNames.button)}
+                disabled={isDisabled}
               >
-                <path d="M20.0906 19.2V6.6C20.0906 5.61 19.2806 4.8 18.2906 4.8H17.3906V3H15.5906V4.8H8.39062V3H6.59062V4.8H5.69063C4.69163 4.8 3.89962 5.61 3.89962 6.6L3.89062 19.2C3.89062 20.19 4.69163 21 5.69063 21H18.2906C19.2806 21 20.0906 20.19 20.0906 19.2ZM9.29062 11.1001H7.49061V12.9001H9.29062V11.1001ZM5.69062 8.40009H18.2906V6.60008H5.69062V8.40009ZM18.2906 10.2V19.2H5.69062V10.2H18.2906ZM14.6906 12.9001H16.4906V11.1001H14.6906V12.9001ZM12.8906 12.9001H11.0906V11.1001H12.8906V12.9001Z"></path>
-              </svg>
-            </Button>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M20.0906 19.2V6.6C20.0906 5.61 19.2806 4.8 18.2906 4.8H17.3906V3H15.5906V4.8H8.39062V3H6.59062V4.8H5.69063C4.69163 4.8 3.89962 5.61 3.89962 6.6L3.89062 19.2C3.89062 20.19 4.69163 21 5.69063 21H18.2906C19.2806 21 20.0906 20.19 20.0906 19.2ZM9.29062 11.1001H7.49061V12.9001H9.29062V11.1001ZM5.69062 8.40009H18.2906V6.60008H5.69062V8.40009ZM18.2906 10.2V19.2H5.69062V10.2H18.2906ZM14.6906 12.9001H16.4906V11.1001H14.6906V12.9001ZM12.8906 12.9001H11.0906V11.1001H12.8906V12.9001Z"></path>
+                </svg>
+              </Button>
+            </div>
           }
         />
       </div>
