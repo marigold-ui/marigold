5d39a44c266122ce0331e1e21954fb304d6cbfb9
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-06-21 11:38:46 +0200

    chore: delete old theme (#3064)

--- themes-old/theme-b2b/src/components/List.style.ts
+++
@@ -1,5 +0,0 @@
-import type { Theme } from '@marigold/components';
-
-export const List: Theme['components']['List'] = {
-  base: {},
-};
--- themes-old/theme-b2b/src/components/Text.style.ts
+++
@@ -1,3 +0,0 @@
-import type { Theme } from '@marigold/components';
-
-export const Text: Theme['components']['Text'] = {};
