a5429e535a0ed6e6e0396d46997af683a5fc4ada
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-06-29 10:42:42 +0300

    Adding typescript examples

--- docs/components/button-basic.demo.tsx
+++ docs/components/button-basic.demo.tsx
@@ -1,15 +1,37 @@
 import DemoLiveEditor from '@/DemoLiveEditor';
 import { Button } from '@marigold/components/src';
 
-const BasicButton = () => {
+export const code = `
+type Props = {
+  label: string;
+}
+const Counter = (props: Props) => {
+  const [count, setCount] =
+    React.useState<number>(0)
   return (
-    <DemoLiveEditor
-      code={`
-<Button>save</Button>
-       `}
-      scope={{ Button }}
-    />
-  );
+    <div>
+      <h3 style={{
+        background: 'darkslateblue',
+        color: 'white',
+        padding: 8,
+        borderRadius: 4
+      }}>
+        {props.label}: {count} 
+      </h3>
+      <button
+        onClick={() =>
+          setCount(c => c + 1)
+        }>
+        Increment
+      </button>
+    </div>
+  )
+}
+render(<Counter label="Counter" />)
+`.trim();
+
+const BasicButton = () => {
+  return <DemoLiveEditor code={code} noInline />;
 };
 
 export default BasicButton;
