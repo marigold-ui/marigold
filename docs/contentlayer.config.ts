65915dfef4dac8201c29cebef51c93afe16bbfb4
sarahgm <sarah.gosmann@reservix.de>
2023-06-29 16:34:56 +0200

    added rehype plugins to config

--- docs/contentlayer.config.ts
+++ docs/contentlayer.config.ts
@@ -4,6 +4,10 @@ import {
   type ComputedFields,
 } from 'contentlayer/source-files';
 
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 const computedFields: ComputedFields = {
   slug: {
     type: 'string',
@@ -11,17 +15,35 @@ const computedFields: ComputedFields = {
   },
   slugAsParams: {
     type: 'string',
-    resolve: doc => doc._raw.flattenedPath.split('/').slice(1).join('/'),
+    // Slugs are matched agains the name of the component or rather the file name
+    resolve: doc => doc._raw.sourceFileName.split('.')[0],
   },
 };
 
 export const ComponentPage = defineDocumentType(() => ({
   name: 'ComponentPage',
-  filePathPattern: 'components/*.mdx',
+  filePathPattern: 'components/**/*.mdx',
   contentType: 'mdx',
   fields: {
     title: {
       type: 'string',
+      required: true,
+    },
+    caption: {
+      type: 'string',
+      required: true,
+    },
+    group: {
+      type: 'enum',
+      options: [
+        'applicaiton',
+        'collection',
+        'content',
+        'form',
+        'layout',
+        'navigation',
+        'overlay',
+      ],
       required: true,
     },
   },
@@ -30,5 +52,9 @@ export const ComponentPage = defineDocumentType(() => ({
 
 export default makeSource({
   contentDirPath: './content',
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
+  },
   documentTypes: [ComponentPage],
 });
