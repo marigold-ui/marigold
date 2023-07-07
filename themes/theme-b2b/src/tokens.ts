94365848d815ea6aca5420c6630f6951380e1496
sarahgm <sarah.gosmann@reservix.de>
2023-07-07 09:40:33 +0200

    add color tokens and rename dark to secondary

--- themes/theme-b2b/src/tokens.ts
+++ themes/theme-b2b/src/tokens.ts
@@ -1,23 +1,86 @@
 const brand = {
   primary: {
-    100: '#fff8f1',
-    200: '#fdcca0',
-    400: '#f8ac67',
-    600: '#fa8005',
-    700: '#ea5200',
-    800: '#ae440a',
+    '50': '#fffaec',
+    '100': '#fff4d4',
+    '200': '#ffe5a7',
+    '300': '#ffd170',
+    '400': '#ffb136',
+    '500': '#ff980f',
+    '600': '#fa8005',
+    '700': '#c85e06',
+    '800': '#9e490e',
+    '900': '#7f3d0f',
+    '950': '#451d05',
   },
+
   secondary: {
-    50: '#ffffff',
-    100: '#fafafa',
-    200: '#f3f3f3',
-    300: '#e3e3e3',
-    400: '#cccccc',
-    500: '#8d8d8d',
-    600: '#6d6d6d',
-    700: '#4b4b4b',
-    800: '#2b2b2b',
-    900: '#0f0f0f',
+    '50': '#f9fafb',
+    '100': '#f3f4f6',
+    '200': '#e5e7eb',
+    '300': '#d1d5db',
+    '400': '#9ca3af',
+    '500': '#6b7280',
+    '600': '#4b5563',
+    '700': '#374151',
+    '800': '#1f2937',
+    '900': '#111827',
+    '950': '#030712',
+  },
+
+  blue: {
+    '50': '#eefbfd',
+    '100': '#d5f2f8',
+    '200': '#b0e5f1',
+    '300': '#79d0e7',
+    '400': '#3ab3d5',
+    '500': '#1f96bb',
+    '600': '#1d799d',
+    '700': '#1e6280',
+    '800': '#215269',
+    '900': '#1f455a',
+    '950': '#0f2c3d',
+  },
+
+  red: {
+    '50': '#fdf3f3',
+    '100': '#fce4e4',
+    '200': '#f9cfcf',
+    '300': '#f4adad',
+    '400': '#ec7d7e',
+    '500': '#dd4142',
+    '600': '#cc3637',
+    '700': '#ab2a2b',
+    '800': '#8e2627',
+    '900': '#772526',
+    '950': '#400f0f',
+  },
+
+  yellow: {
+    '50': '#ffffe7',
+    '100': '#feffc1',
+    '200': '#fffd86',
+    '300': '#fff441',
+    '400': '#ffe60d',
+    '500': '#eac500',
+    '600': '#d19e00',
+    '700': '#a67102',
+    '800': '#89580a',
+    '900': '#74480f',
+    '950': '#442504',
+  },
+
+  green: {
+    '50': '#f7fce9',
+    '100': '#eef8cf',
+    '200': '#dcf1a5',
+    '300': '#c3e670',
+    '400': '#a8d744',
+    '500': '#8bbd26',
+    '600': '#6b961a',
+    '700': '#527318',
+    '800': '#425b19',
+    '900': '#394e19',
+    '950': '#1c2a09',
   },
 } as const;
 
@@ -33,13 +96,13 @@ export const colors = {
       DEFAULT: brand.secondary[700],
       light: brand.secondary[500],
     },
-    link: '#3ab3d5',
+    link: brand.blue[400],
 
     light: brand.secondary[100],
 
     // State
     disabled: brand.secondary[400],
-    error: '#dd4142',
+    error: brand.red[500],
     hover: brand.secondary[800],
   },
 
@@ -58,7 +121,7 @@ export const colors = {
       hover: brand.primary[400],
     },
 
-    dark: {
+    secondary: {
       DEFAULT: brand.secondary[700],
       hover: brand.secondary[600],
     },
@@ -104,8 +167,8 @@ export const colors = {
     focus: brand.primary[600],
 
     // status
-    error: '#dd4142',
-    selected: '#ae440a',
+    error: brand.red[500],
+    selected: brand.red[700],
     disabled: brand.secondary[400],
   },
 
@@ -122,9 +185,9 @@ export const colors = {
     selected: brand.primary[800],
 
     // Status
-    error: '#dd4142',
+    error: brand.red[500],
     info: '#1d67b6',
-    warning: '#eac500',
+    warning: brand.yellow[500],
   },
 
   // Outline
@@ -136,16 +199,16 @@ export const colors = {
     focus: brand.primary[600],
 
     // Status
-    error: '#dd4142',
+    error: brand.red[500],
   },
 
   // Fill
   // ---------------
   fill: {
     // Status
-    required: 'rgb(221, 65, 66)',
-    error: '#dd4142',
+    required: brand.red[500],
+    error: brand.red[500],
     info: '#1d67b6',
-    warning: '#eac500',
+    warning: brand.yellow[500],
   },
 };
