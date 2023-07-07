e4947fde2d12a3b44a052ba7b80fd17f5598ede8
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-07 09:53:47 +0200

    feat: typography for docs

--- docs/theme/components/Text.styles.ts
+++ docs/theme/components/Text.styles.ts
@@ -1,3 +1,15 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
-export const Text: ThemeComponent<'Text'> = cva();
+export const Text: ThemeComponent<'Text'> = cva(
+  'leading-7 [&:not(:first-child)]:mt-6',
+  {
+    variants: {
+      variant: {
+        lead: 'text-xl text-muted-foreground',
+        large: 'text-lg font-semibold',
+        small: 'text-sm font-medium leading-none',
+        muted: 'text-sm text-muted-foreground',
+      },
+    },
+  }
+);
