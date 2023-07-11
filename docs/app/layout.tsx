96ba624916c02cc4de3554dc2c92dd2949858fb3
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-11 20:41:53 +0200

    make large layout

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -5,6 +5,7 @@ import { theme } from '@/theme';
 import { fontSans } from '@/theme/fonts';
 
 import { SiteHeader } from './_components/SiteHeader';
+import { Navigation } from './_components/Navigation';
 
 // Metadata
 // ---------------
@@ -26,8 +27,11 @@ export default function RootLayout({
         <MarigoldProvider theme={theme}>
           <div className="flex min-h-screen flex-col">
             <SiteHeader />
-            <div className="flex-1">
-              <div className="container">{children}</div>
+            <div className="container flex-1">
+              <aside className="fixed top-14 z-20 -ml-2 hidden w-60 overflow-y-auto md:block">
+                <Navigation />
+              </aside>
+              <div className="md:pl-60">{children}</div>
             </div>
             <footer className="container">Marigold Footer</footer>
           </div>
