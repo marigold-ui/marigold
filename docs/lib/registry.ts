6dda6e83f43d4dc94206aee7a04b2d95f7ce59c2
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-13 13:14:00 +0200

    fix ts

--- docs/lib/registry.ts
+++ docs/lib/registry.ts
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
 
-export const registry = {
+export const registry: any = {
   'button-variant': dynamic(
     () => import('@/content/components/button/button-variant.demo')
   ),
