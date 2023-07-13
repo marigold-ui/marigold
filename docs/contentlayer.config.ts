1742c55eeea1d2265231f501634d80f3e4b9ae69
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-13 12:05:49 +0200

    first working demo

--- docs/contentlayer.config.ts
+++ docs/contentlayer.config.ts
@@ -9,6 +9,8 @@ import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
+import { rehypeComponentDemo } from './lib/mdx/rehype-component-demo';
+
 // Helpers
 // ---------------
 const commonFields: FieldDefs = {
@@ -82,11 +84,17 @@ export const ComponentPage = defineDocumentType(() => ({
 
 // Config
 // ---------------
+const contentDirPath = './content';
+
 export default makeSource({
-  contentDirPath: './content',
+  contentDirPath,
   mdx: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
+    rehypePlugins: [
+      [rehypeComponentDemo, { contentDirPath }],
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+    ],
   },
   documentTypes: [ContentPage, ComponentPage],
 });
--- docs/ui/ComponentDemo.tsx
+++ docs/ui/ComponentDemo.tsx
@@ -1,4 +1,11 @@
-export const ComponentDemo = () => {
+import { ReactNode } from 'react';
+
+export interface ComponentDemoProps {
+  name: string;
+  children?: ReactNode;
+}
+
+export const ComponentDemo = ({ children }: ComponentDemoProps) => {
   // Use React-live
-  return <div className="bg-cyan-800 p-10 text-cyan-100">hello</div>;
+  return <div className="bg-cyan-800 p-10 text-cyan-100">{children}</div>;
 };
