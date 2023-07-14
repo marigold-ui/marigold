a542114656049756b15bb4d32182721b62ea3827
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-07-14 12:16:04 +0300

    Fixing errors

--- packages/system/src/hooks/useSmallScreen.ts
+++ packages/system/src/hooks/useSmallScreen.ts
@@ -2,6 +2,9 @@ import { useCallback, useEffect, useState } from 'react';
 
 export const useSmallScreen = (): boolean => {
   const getMatches = (): boolean => {
+    if (typeof window == 'undefined') {
+      return false;
+    }
     return window.matchMedia('(max-width: 600px)').matches;
   };
 
@@ -12,7 +15,7 @@ export const useSmallScreen = (): boolean => {
   }, []);
   useEffect(() => {
     // Triggered at the first client-side load and if query changes
-    if (!window) return;
+    if (typeof window == 'undefined') return;
     handleResize();
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
