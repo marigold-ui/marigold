a63a0315d0526a2e2ae0af408bd787dcf4f81ea0
sarahgm <sarah.gosmann@reservix.de>
2023-07-18 15:50:02 +0200

    use marigold image

--- docs/content/components/aspect/ratio-square.demo.tsx
+++ docs/content/components/aspect/ratio-square.demo.tsx
@@ -2,7 +2,7 @@ import { Aspect, Image } from '@marigold/components';
 
 export default () => (
   <Aspect maxWidth="25vw">
-    <img
+    <Image
       src="https://images.unsplash.com/photo-1603910234616-3b5f4a6be2b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       alt="event_image"
     />
