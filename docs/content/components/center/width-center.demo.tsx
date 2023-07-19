ab1c6ebe42b67ac2a7ade410debe3cd5e93b0910
sarahgm <sarah.gosmann@reservix.de>
2023-07-19 13:33:50 +0200

    added width to 50%

--- docs/content/components/center/width-center.demo.tsx
+++ docs/content/components/center/width-center.demo.tsx
@@ -2,6 +2,6 @@ import { Center } from '@marigold/components';
 
 export default () => (
   <Center maxWidth="xxlarge">
-    <div className="h-28 w-full border border-slate-300 bg-slate-200" />
+    <div className="h-28 w-1/2 border border-slate-300 bg-slate-200" />
   </Center>
 );
