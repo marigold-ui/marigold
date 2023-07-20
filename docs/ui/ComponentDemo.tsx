d36196e3604047ed01fb79aae3b1db4a3493696a
sarahgm <sarah.gosmann@reservix.de>
2023-07-20 10:58:36 +0200

    save

--- docs/ui/ComponentDemo.tsx
+++ docs/ui/ComponentDemo.tsx
@@ -1,24 +1,24 @@
 import { ReactNode, useState } from 'react';
 import { Button, MarigoldProvider } from './marigold';
-import { useThemeSwitch } from '@/app/components/[...slug]/_components';
-import { Theme } from '../../packages/system/src';
 import { type Theme } from '@marigold/system';
 
 import { registry } from '@/registry';
-import { MarigoldProvider } from './marigold';
 import { useThemeSwitch } from '@/app/_components/ThemeSwitch';
 export interface ComponentDemoProps {
-  name: string;
+  name: keyof typeof registry;
+  source: string;
   children?: ReactNode;
 }
 
 export const ComponentDemo = ({ name, children }: ComponentDemoProps) => {
-  const Demo = (registry as any)[name].demo;
-  const { current, themes } = useThemeSwitch();
+  const Demo = registry[name].demo;
+
   const { current, themes } = useThemeSwitch();
   const [showCode, setShowCode] = useState(true);
+
   const buttonStyles =
     'rounded-none border-solid  border-transparent border-b-slate-800 p-2';
+
   return (
     <MarigoldProvider theme={(current && themes[current]) as Theme}>
       <div className="max-h-[650px] max-w-[800px]">
