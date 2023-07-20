8859fc10ffbc2328e553bf0b14eddc7e2d94d742
sarahgm <sarah.gosmann@reservix.de>
2023-07-20 12:19:09 +0200

    save

--- docs/ui/ComponentDemo.tsx
+++ docs/ui/ComponentDemo.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, useState } from 'react';
-import { Button, MarigoldProvider } from './marigold';
+import { ReactNode } from 'react';
+import { MarigoldProvider, Tabs } from './marigold';
 import { type Theme } from '@marigold/system';
 
 import { registry } from '@/registry';
@@ -12,39 +12,25 @@ export interface ComponentDemoProps {
 
 export const ComponentDemo = ({ name, children }: ComponentDemoProps) => {
   const Demo = registry[name].demo;
-
   const { current, themes } = useThemeSwitch();
-  const [showCode, setShowCode] = useState(true);
-
-  const buttonStyles =
-    'rounded-none border-solid  border-transparent border-b-slate-800 p-2';
 
   return (
-    <MarigoldProvider theme={(current && themes[current]) as Theme}>
-      <div className="max-h-[650px] max-w-[800px]">
-        <div className="mb-4 mt-10 border-b border-b-slate-200">
-          <Button
-            className={`${buttonStyles} ${showCode && 'border-b-[3px]'}`}
-            onClick={() => setShowCode(true)}
-          >
-            Code
-          </Button>
-          <Button
-            className={`${buttonStyles} ${!showCode && 'border-b-[3px]'}`}
-            onClick={() => setShowCode(false)}
-          >
-            Preview
-          </Button>
-        </div>
-        <div className={!showCode ? 'hidden' : ''}>{children}</div>
-        <div data-theme={current}>
+    <Tabs defaultSelectedKey="preview">
+      <Tabs.Item key="preview" title="Preview">
+        <div
+          data-theme={current}
+          className="flex min-h-[150px] flex-col [&>*:first-child]:grid [&>*:first-child]:flex-1 [&>*:first-child]:place-items-center [&>*:first-child]:rounded-xl [&>*:first-child]:border"
+        >
           <MarigoldProvider theme={(current && themes[current]) as Theme}>
-            <div className="px-4 py-6" className={showCode ? 'hidden' : ''}>
+            <div className="w-full p-4">
               <Demo />
             </div>
           </MarigoldProvider>
         </div>
-      </div>
-    </MarigoldProvider>
+      </Tabs.Item>
+      <Tabs.Item key="code" title="Code">
+        {children}
+      </Tabs.Item>
+    </Tabs>
   );
 };
