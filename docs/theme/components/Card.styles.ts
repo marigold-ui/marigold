0f6505ce5b66c441da63c7df133075333e5cb328
sarahgm <sarah.gosmann@reservix.de>
2023-07-20 15:52:39 +0200

    fix: bg token for card

--- docs/theme/components/Card.styles.ts
+++ docs/theme/components/Card.styles.ts
@@ -1,7 +1,7 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Card: ThemeComponent<'Card'> = cva(
-  ['bg-card text-card-foreground rounded-xl border shadow', 'p-6'],
+  ['bg-bg-surface rounded-xl border shadow', 'p-6'],
   {
     variants: {
       variant: {
