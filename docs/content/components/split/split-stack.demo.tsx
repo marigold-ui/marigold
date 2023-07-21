e1b64e11a4794c423334b074564c278b1ddf21a7
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-07-21 13:24:02 +0200

    docs: add split docs and demo (#3163)
    * docs: add split docs and demo
    
    * fix

---
+++ docs/content/components/split/split-stack.demo.tsx
@@ -0,0 +1,12 @@
+import { Stack, Split, Text } from '@marigold/components';
+
+export default () => (
+  <div className="h-48">
+    <Stack space={1} stretch>
+      <Text>It's gonna be ...</Text>
+      <Text>wait for it!</Text>
+      <Split />
+      <Text>... legendary!</Text>
+    </Stack>
+  </div>
+);
