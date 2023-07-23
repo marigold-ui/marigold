58d46428c4070527752578c71820fe4ed8996c76
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-23 23:53:26 +0200

    use bg token

--- docs/theme/components/Menu.styles.ts
+++ docs/theme/components/Menu.styles.ts
@@ -2,7 +2,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const Menu: ThemeComponent<'Menu'> = {
   container: cva([
-    'border-border overflow-hidden rounded-md border bg-white px-1 py-1.5 text-sm shadow-md',
+    'border-border bg-bg-surface overflow-hidden rounded-md border px-1 py-1.5 text-sm shadow-md',
     'sm:max-h-[45vh] md:max-h-[75vh] lg:max-h-[75vh]',
   ]),
   item: cva('hover:bg-bg-hover cursor-pointer p-2 outline-none'),
