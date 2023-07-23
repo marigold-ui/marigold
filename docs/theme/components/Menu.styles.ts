9d274b7845d1912bd5c9483f5ba1ef9ac956334a
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-07-23 23:52:43 +0200

    docs-menu-styles

--- docs/theme/components/Menu.styles.ts
+++ docs/theme/components/Menu.styles.ts
@@ -2,9 +2,9 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const Menu: ThemeComponent<'Menu'> = {
   container: cva([
-    'overflow-y-auto overflow-x-hidden rounded-sm border border-solid bg-white ',
+    'border-border overflow-hidden rounded-md border bg-white px-1 py-1.5 text-sm shadow-md',
     'sm:max-h-[45vh] md:max-h-[75vh] lg:max-h-[75vh]',
   ]),
-  item: cva('hover:bg-bg-hover cursor-pointer px-2 py-1 outline-none'),
+  item: cva('hover:bg-bg-hover cursor-pointer p-2 outline-none'),
   section: cva(''),
 };
