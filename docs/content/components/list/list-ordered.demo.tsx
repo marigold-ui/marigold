a9a3284d08d7bf6ef2cfbd182e0b4c2744fa542b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-07-24 13:54:56 +0200

    docs: list demo and docs (#3196)
    * docs: list demo and docs
    
    * save

---
+++ docs/content/components/list/list-ordered.demo.tsx
@@ -0,0 +1,11 @@
+import { List } from '@marigold/components';
+
+export default () => (
+  <div className="p-4">
+    <List as="ol">
+      <List.Item>Black Tea</List.Item>
+      <List.Item>Green Tea</List.Item>
+      <List.Item>Chai Tea</List.Item>
+    </List>
+  </div>
+);
