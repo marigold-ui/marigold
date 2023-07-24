9787670407839615e672fa968397f7b626595fb9
sarahgm <sarah.gosmann@reservix.de>
2023-07-24 11:39:27 +0200

    save

--- docs/content/components/list/list-ordered.demo.tsx
+++ docs/content/components/list/list-ordered.demo.tsx
@@ -1,9 +1,11 @@
 import { List } from '@marigold/components';
 
 export default () => (
-  <List as="ol">
-    <List.Item>Black Tea</List.Item>
-    <List.Item>Green Tea</List.Item>
-    <List.Item>Chai Tea</List.Item>
-  </List>
+  <div className="p-4">
+    <List as="ol">
+      <List.Item>Black Tea</List.Item>
+      <List.Item>Green Tea</List.Item>
+      <List.Item>Chai Tea</List.Item>
+    </List>
+  </div>
 );
--- docs/content/components/list/simple-list.demo.tsx
+++ docs/content/components/list/simple-list.demo.tsx
@@ -1,9 +1,11 @@
 import { List } from '@marigold/components';
 
 export default () => (
-  <List>
-    <List.Item>Cheese</List.Item>
-    <List.Item>Milk</List.Item>
-    <List.Item>Bread</List.Item>
-  </List>
+  <div className="p-4">
+    <List>
+      <List.Item>Cheese</List.Item>
+      <List.Item>Milk</List.Item>
+      <List.Item>Bread</List.Item>
+    </List>
+  </div>
 );
