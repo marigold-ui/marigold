a2a84ce9fd7ba343a59c5bf33322c908b8ba9cc8
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-07-24 19:32:16 +0300

    Docs: Add `TextArea` docs page

---
+++ docs/content/components/text-area/text-area-basic.demo.tsx
@@ -0,0 +1,3 @@
+import { TextArea } from '@marigold/components';
+
+export default () => <TextArea label="Simple" placeholder="Placeholder" />;
---
+++ docs/content/components/text-area/text-area-req.demo.tsx
@@ -0,0 +1,3 @@
+import { TextArea } from '@marigold/components';
+
+export default () => <TextArea label="Required Textarea" required rows={6} />;
