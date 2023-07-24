edd91c1050d7aaee8834ff012a4824d58607777b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-07-24 12:59:33 +0200

    docs: header docs and demo (#3191)

---
+++ docs/content/components/header/simple-header.demo.tsx
@@ -0,0 +1,8 @@
+import { Header, Headline, Text } from '@marigold/components';
+
+export default () => (
+  <Header>
+    <Headline level="3">Header!</Headline>
+    <Text>awesome</Text>
+  </Header>
+);
