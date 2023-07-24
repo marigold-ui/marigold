3b1a779a85bea8fe04de4e744f54766cc98514d3
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-07-24 11:19:40 +0300

    docs: adding  docs page

---
+++ docs/content/components/datefield/date-field-disabled.demo.tsx
@@ -0,0 +1,9 @@
+import { DateField } from '@marigold/components';
+
+export default () => (
+  <DateField
+    label="Field Disabled"
+    disabled
+    description="This field is disabled"
+  />
+);
---
+++ docs/content/components/datefield/date-field-error.demo.tsx
@@ -0,0 +1,5 @@
+import { DateField } from '@marigold/components';
+
+export default () => (
+  <DateField label="Date field" error errorMessage="Something went wrong !" />
+);
