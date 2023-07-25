d718e55f59dd0475c733caa938d76ea446ba380c
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-07-25 16:15:44 +0300

    Adding args to  stories

--- packages/components/src/SearchField/SearchField.stories.tsx
+++ packages/components/src/SearchField/SearchField.stories.tsx
@@ -51,7 +51,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-  render: args => <SearchField required label="search field" />,
+  render: args => <SearchField required label="search field" {...args} />,
 };
 
 export const Controlled: Story = {
@@ -64,6 +64,7 @@ export const Controlled: Story = {
           onChange={setValue}
           required
           label="search field"
+          {...args}
         />
         <pre>
           <strong>Input Value:</strong>
