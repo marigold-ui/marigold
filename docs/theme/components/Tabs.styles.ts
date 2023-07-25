0fa6bcbc1232adb7d416555e25aaf219d9b8e3e8
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-07-25 11:51:26 +0200

    docs: small tweaks to the nav spacing and y overflow of previews (#3220)

--- docs/theme/components/Tabs.styles.ts
+++ docs/theme/components/Tabs.styles.ts
@@ -3,7 +3,8 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const Tabs: ThemeComponent<'Tabs'> = {
   tabs: cva('mb-4 border-b'),
   tab: cva([
-    'text-text-primary-muted aria-selected:text-text-primary px-2 py-1 font-medium',
-    'aria-selected:border-border -m-px border-b-2 border-transparent',
+    'text-text-primary-muted aria-selected:text-text-primary px-2 py-1 text-sm font-medium',
+    'aria-selected:border-border-primary -m-px border-b-2 border-transparent',
+    'focus:outline-none focus-visible:outline',
   ]),
 };
