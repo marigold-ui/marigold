d1e45200a9093a4ac38a02c17423ee9f91c3f061
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-07-26 11:41:02 +0200

    docs: adding concept pages and demos (#3222)

---
+++ docs/content/pages/concepts/layout-forms.demo.tsx
@@ -0,0 +1,58 @@
+import {
+  Button,
+  Checkbox,
+  Columns,
+  FieldGroup,
+  Headline,
+  Select,
+  Stack,
+  TextField,
+} from '@marigold/components';
+
+export default () => (
+  <FieldGroup labelWidth="medium">
+    <Headline level="2">Account Registration</Headline>
+    <Stack space={4}>
+      <Columns columns={[2, 2]} space={4}>
+        <TextField
+          label="Firstname:"
+          required
+          description="Please enter your first name."
+          placeholder="Firstname"
+        />
+        <TextField
+          label="Name:"
+          required
+          description="Please enter your name."
+          placeholder="Name"
+        />
+      </Columns>
+      <Stack space={4}>
+        <TextField
+          label="Phone:"
+          required
+          placeholder="Phone"
+          type="tel"
+          description="Please enter your phone number."
+        />
+        <TextField
+          label="E-Mail:"
+          description="Please enter your E-Mail adress."
+          placeholder="E-Mail"
+          required
+        />
+        <Select label="Country:" description="Please select your country.">
+          <Select.Option key={'germany'}>Germany</Select.Option>
+          <Select.Option key={'austria'}>Austria</Select.Option>
+          <Select.Option key={'switzerland'}>Switzerland</Select.Option>
+        </Select>
+        <Checkbox>Agree to the terms</Checkbox>
+      </Stack>
+    </Stack>
+    <Stack alignX="right">
+      <Button variant="primary" size="small" type="submit">
+        Submit
+      </Button>
+    </Stack>
+  </FieldGroup>
+);
