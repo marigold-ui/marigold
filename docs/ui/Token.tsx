286a16bdaa43434c3dec7ab619be4260ba731285
sarahgm <sarah.gosmann@reservix.de>
2023-08-03 13:35:52 +0200

    radius

---
+++ docs/ui/Token.tsx
@@ -0,0 +1,50 @@
+import { Card, Inline, Stack, Text } from '@/ui';
+
+export const BorderRadius = () => (
+  <Card>
+    <Inline space={8}>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded 2px
+        </code>
+        <div className="h-14 w-full rounded-sm bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded 4px
+        </code>
+        <div className="h-14 w-full rounded bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded-md 6px
+        </code>
+        <div className="h-14 w-full rounded-md bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded-lg 8px
+        </code>
+        <div className="h-14 w-full rounded-lg bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded-xl 12px
+        </code>
+        <div className="h-14 w-full rounded-xl bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded-2xl 16px
+        </code>
+        <div className="h-14 w-full rounded-2xl bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+      <Stack alignX="center" space={2}>
+        <code className="before:content-none after:content-none">
+          rounded-full 9999px
+        </code>
+        <div className="h-14 w-full rounded-full bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)] px-3 py-2 shadow"></div>
+      </Stack>
+    </Inline>
+  </Card>
+);
