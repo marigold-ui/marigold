5b981b3e1239ed7163e97c2d84ff9aa60ea0ca99
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-08-03 15:54:14 +0300

    Ref: Unifying common inputs styles

--- themes/theme-b2b/src/components/Input.styles.ts
+++ themes/theme-b2b/src/components/Input.styles.ts
@@ -1,17 +1,28 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
+export const inputBox =
+  'border-border-light rounded-sm border outline-none hover:border-border-hover';
+export const inputFocus =
+  'focus:border-border-focus focus:outline-outline-focus focus:-outline-offset-1';
+export const inputBackground = 'bg-bg-surface';
+export const inputDisabled =
+  'disabled:bg-bg-disabled disabled:text-text-disabled disabled:hover:border-border-disabled disabled:border-border-disabled disabled:cursor-not-allowed';
+export const inputError = (selector: string = 'error') =>
+  `${selector}:border-border-error ${selector}:outline-outline-error ${selector}:-outline-offset-1`;
+export const inputSpacing = 'px-2';
+
 export const Input: ThemeComponent<'Input'> = {
   icon: cva('left-2'),
   action: cva('right-0'),
   input: cva([
-    'bg-bg-surface',
-    'border-border-light rounded-sm border  outline-none',
-    'px-2 leading-[2]',
-    'hover:border-border-hover',
-    'read-only:focus:border-border-light read-only:focus:outline-none',
-    'focus:border-border-focus focus:outline-outline-focus focus:-outline-offset-1',
-    'disabled:bg-bg-disabled disabled:text-text-disabled disabled:cursor-not-allowed ',
-    'data-[error]:border-border-error data-[error]:outline-outline-error data-[error]:-outline-offset-1',
+    inputBox,
+    inputBackground,
+    inputFocus,
+    inputDisabled,
+    inputSpacing,
+    inputError('data-[error]'),
+    'leading-[2]',
+    'read-only:focus:border-border-light read-only:focus:outline-none ',
     // Extra padding for when an icon/action is present
     'group-data-[icon]/input:pl-8',
   ]),
--- themes/theme-b2b/src/components/Select.styles.ts
+++ themes/theme-b2b/src/components/Select.styles.ts
@@ -1,16 +1,24 @@
 import { ThemeComponent, cva } from '@marigold/system';
+import {
+  inputBackground,
+  inputBox,
+  inputDisabled,
+  inputError,
+  inputFocus,
+  inputSpacing,
+} from './Input.styles';
 
 export const Select: ThemeComponent<'Select'> = {
   icon: cva(),
   select: cva([
+    inputBox,
+    inputDisabled,
+    inputFocus,
+    inputBackground,
+    inputSpacing,
+    inputError('data-[error]'),
     'appearance-none leading-8',
-    'px-2',
-    'bg-bg-surface border-border-light rounded-sm border border-solid',
-    'cursor-pointer outline-none',
-    'hover:border-border-hover',
-    'disabled:border-border-disabled disabled:text-text-disabled disabled:bg-bg-disabled disabled:cursor-not-allowed',
-    'focus:border-border-focus',
-    'aria-expanded:bg-bg-neutral aria-expanded:border-border-light',
-    'data-[error]:border-border-error data-[error]:shadow-none',
+    'cursor-pointer',
+    'aria-expanded:bg-bg-neutral ',
   ]),
 };
