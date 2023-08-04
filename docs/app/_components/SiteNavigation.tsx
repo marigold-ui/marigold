666f33a80cd9fdd72fea7864b30fb2a4c99489ba
sarahgm <sarah.gosmann@reservix.de>
2023-08-04 15:32:28 +0200

    add link to old docs

--- docs/app/_components/SiteNavigation.tsx
+++ docs/app/_components/SiteNavigation.tsx
@@ -1,14 +1,22 @@
-import { Split } from '@/ui';
+import { Text, Link } from '@/ui';
 import { Logo } from './Logo';
 import { ThemeMenu } from './ThemeMenu';
 
 export const SiteNavigation = () => (
-  <div className="hidden w-full md:flex">
+  <div className="hidden w-full justify-between gap-3 md:flex">
     <div className="flex items-center gap-2 text-lg font-bold uppercase tracking-tight text-[#46505a]">
       <Logo className="h-6 w-6" />
       Marigold
     </div>
-    <Split />
+    <div className="bg-bg-muted rounded p-2">
+      <Link
+        href="https://marigold-ui.io/"
+        className="no-underline"
+        target="_blank"
+      >
+        <Text fontSize="sm">Previous Docs Version</Text>
+      </Link>
+    </div>
     <ThemeMenu />
   </div>
 );
