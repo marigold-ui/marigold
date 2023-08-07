2a4f4d6003f243e9ad31f72bd5dafb7fdaf728b6
Sebastian Sebald <sebastian.sebald@reservix.de>
2023-08-07 11:03:29 +0200

    feat([DST-120]): expose `preset` from themes (#3249)

--- themes/theme-core/src/theme.ts
+++ themes/theme-core/src/theme.ts
@@ -1,19 +1,15 @@
 import { Theme } from '@marigold/system';
-import { colors } from './tokens';
-import * as components from './components';
 
 import { root } from './root';
+import { screens } from './screens';
+import { colors } from './tokens';
+
+import * as components from './components';
 
 export const theme: Theme = {
   name: 'core',
-  screens: {
-    sm: '640px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    '2xl': '1536px',
-  },
+  screens,
   root,
-  colors: colors,
+  colors,
   components,
 };
