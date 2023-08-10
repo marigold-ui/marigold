651eecb058aa13ac2cbb3fa37f47c107f951be13
sarahgm <sarah.gosmann@reservix.de>
2023-08-10 08:59:25 +0200

    chore[DST-131]: replace fill colors with text colors

--- themes/theme-b2b/src/components/Label.styles.ts
+++ themes/theme-b2b/src/components/Label.styles.ts
@@ -3,6 +3,6 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const Label: ThemeComponent<'Label'> = {
   container: cva('group-disabled/field:text-text-disabled text-sm '),
   indicator: cva(
-    'group-required/field:fill-fill-required group-required/field:block'
+    'group-required/field:text-text-required group-required/field:block'
   ),
 };
