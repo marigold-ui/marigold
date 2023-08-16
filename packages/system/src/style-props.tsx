e3d498a01a1f66ba04a32aa90fd828d63db6117e
sarahgm <sarah.gosmann@reservix.de>
2023-08-16 11:15:33 +0200

    update fontsize

--- packages/system/src/style-props.tsx
+++ packages/system/src/style-props.tsx
@@ -85,6 +85,10 @@ export const textSize = {
   '3xl': 'text-3xl',
   '4xl': 'text-4xl',
   '5xl': 'text-5xl',
+  '6xl': 'text-6xl',
+  '7xl': 'text-7xl',
+  '8xl': 'text-8xl',
+  '9xl': 'text-9xl',
 };
 
 export const textStyle = {
