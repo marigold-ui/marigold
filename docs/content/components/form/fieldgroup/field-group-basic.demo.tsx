1bcb21b5ebe0f225bc5bf38f84f81bbfe953778b
Osama Abdul Latif <62595605+OsamaAbdellateef@users.noreply.github.com>
2023-08-28 13:57:44 +0300

    [DST-82]: Documention: <FieldGroup> (#3308)

---
+++ docs/content/components/form/fieldgroup/field-group-basic.demo.tsx
@@ -0,0 +1,45 @@
+import { coreTheme } from '@/theme';
+import { MarigoldProvider } from '@/ui';
+
+import {
+  Checkbox,
+  CheckboxGroup,
+  FieldBase,
+  FieldGroup,
+  Input,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from '@marigold/components';
+
+export default () => (
+  <MarigoldProvider theme={coreTheme}>
+    <FieldGroup labelWidth="150px">
+      <FieldBase label="First Name">
+        <Input />
+      </FieldBase>
+      <FieldBase label="Last Name">
+        <input type="text" />
+      </FieldBase>
+      <TextField type="email" label="Email" />
+      <TextField
+        type="tel"
+        label="Phone Number"
+        description="Please enter your phone number"
+      />
+      <Select label="Country">
+        <Select.Option key="usa">USA</Select.Option>
+        <Select.Option key="canada">Canada</Select.Option>
+      </Select>
+      <RadioGroup label="Preferred Language">
+        <Radio value="english">English</Radio>
+        <Radio value="spanish">Spanish</Radio>
+      </RadioGroup>
+      <CheckboxGroup label="Hobbies">
+        <Checkbox value="reading">Reading</Checkbox>
+        <Checkbox value="painting">Painting</Checkbox>
+      </CheckboxGroup>
+    </FieldGroup>
+  </MarigoldProvider>
+);
