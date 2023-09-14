f50c5a4ae5a5d947b126e43228ab37cd35c8403b
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-09-14 16:23:30 +0200

    docs[DST-189]: write utils documentation (#3340)
    * save
    
    * added cva in docs
    
    * Create silver-coats-report.md
    
    * add review changes
    
    * added badge

---
+++ docs/content/hooks-and-utils/cva/cva.demo.tsx
@@ -0,0 +1,16 @@
+import { cva } from '@marigold/system';
+
+export default () => {
+  const styledDiv = cva('p-4 text-4xl', {
+    variants: {
+      variant: {
+        myOwnVariant: 'bg-lime-200',
+      },
+    },
+  });
+  return (
+    <div className={styledDiv({ variant: 'myOwnVariant' })}>
+      this is some text
+    </div>
+  );
+};
