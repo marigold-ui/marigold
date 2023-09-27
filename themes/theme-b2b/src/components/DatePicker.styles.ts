99fb0aa7a9198f348e2b9da2edf73ff8af0aed0e
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-09-27 17:25:08 +0300

    Revert changes

--- themes/theme-b2b/src/components/DatePicker.styles.ts
+++ themes/theme-b2b/src/components/DatePicker.styles.ts
@@ -6,7 +6,7 @@ export const DatePicker: ThemeComponent<'DatePicker'> = {
     'h-full w-9 p-0',
     'flex items-center justify-center ',
     'text-text-body/80',
-    'focus-visible:border-none focus-visible:outline-0',
+    'group-focus/field:[&[aria-expanded=true]]:bg-bg-secondary focus-visible:border-none focus-visible:outline-0 group-focus/field:[&[aria-expanded=true]]:text-white',
     'disabled:hidden',
   ]),
 };
