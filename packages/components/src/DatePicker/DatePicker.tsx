72817b2fa2d5e17343f1fe91e9a256f85e729c6c
sarahgm <sarah.gosmann@reservix.de>
2023-10-02 12:20:14 +0200

    fix datepicker button

--- packages/components/src/DatePicker/DatePicker.tsx
+++ packages/components/src/DatePicker/DatePicker.tsx
@@ -1,6 +1,7 @@
 import { CalendarDate } from '@internationalized/date';
 import { useRef } from 'react';
 
+import { useButton } from '@react-aria/button';
 import { AriaDatePickerProps, useDatePicker } from '@react-aria/datepicker';
 import { mergeProps } from '@react-aria/utils';
 
@@ -12,6 +13,23 @@ import { Button } from '../Button';
 import { Calendar } from '../Calendar/Calendar';
 import { DateField } from '../DateField';
 import { Popover } from '../Overlay';
+
+interface OpenButtonProps {
+  className?: string;
+  disabled?: boolean;
+}
+
+const OpenButton = ({ className, disabled, ...props }: OpenButtonProps) => {
+  const ref = useRef(null);
+  const { buttonProps } = useButton({ ...props }, ref);
+  return (
+    <button className={className} {...buttonProps} disabled={disabled}>
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M20.0906 19.2V6.6C20.0906 5.61 19.2806 4.8 18.2906 4.8H17.3906V3H15.5906V4.8H8.39062V3H6.59062V4.8H5.69063C4.69163 4.8 3.89962 5.61 3.89962 6.6L3.89062 19.2C3.89062 20.19 4.69163 21 5.69063 21H18.2906C19.2806 21 20.0906 20.19 20.0906 19.2ZM9.29062 11.1001H7.49061V12.9001H9.29062V11.1001ZM5.69062 8.40009H18.2906V6.60008H5.69062V8.40009ZM18.2906 10.2V19.2H5.69062V10.2H18.2906ZM14.6906 12.9001H16.4906V11.1001H14.6906V12.9001ZM12.8906 12.9001H11.0906V11.1001H12.8906V12.9001Z"></path>
+      </svg>
+    </button>
+  );
+};
 
 // Props
 // -----------------
@@ -67,7 +85,7 @@ export const DatePicker = ({
     ref
   );
 
-  const { isDisabled, errorMessage, description, label } = props;
+  const { errorMessage, description, label } = props;
   const classNames = useClassNames({
     component: 'DatePicker',
     size,
@@ -89,20 +107,11 @@ export const DatePicker = ({
           width={width}
           action={
             <div className={classNames.container}>
-              <Button
+              <OpenButton
                 {...mergeProps(buttonProps, stateProps)}
                 className={cn('absolute right-0 top-0', classNames.button)}
-                disabled={isDisabled}
-              >
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M20.0906 19.2V6.6C20.0906 5.61 19.2806 4.8 18.2906 4.8H17.3906V3H15.5906V4.8H8.39062V3H6.59062V4.8H5.69063C4.69163 4.8 3.89962 5.61 3.89962 6.6L3.89062 19.2C3.89062 20.19 4.69163 21 5.69063 21H18.2906C19.2806 21 20.0906 20.19 20.0906 19.2ZM9.29062 11.1001H7.49061V12.9001H9.29062V11.1001ZM5.69062 8.40009H18.2906V6.60008H5.69062V8.40009ZM18.2906 10.2V19.2H5.69062V10.2H18.2906ZM14.6906 12.9001H16.4906V11.1001H14.6906V12.9001ZM12.8906 12.9001H11.0906V11.1001H12.8906V12.9001Z"></path>
-                </svg>
-              </Button>
+                disabled={disabled}
+              />
             </div>
           }
         />
