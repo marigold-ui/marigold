1732e78f93c3001ed896296d70ca49e7cd52be39
sarahgm <sarah.gosmann@reservix.de>
2023-10-05 09:24:04 +0200

    fix: fix links in docs navigation

--- docs/app/_components/SiteNavigation.tsx
+++ docs/app/_components/SiteNavigation.tsx
@@ -5,19 +5,14 @@ import { ThemeMenu } from './ThemeMenu';
 
 export const SiteNavigation = () => (
   <div className="hidden w-full justify-between gap-3 md:flex">
-    <Link
-      href="/"
-      className="flex items-center gap-2 text-lg font-bold uppercase tracking-tight text-[#46505a] no-underline"
-    >
-      <Logo className="h-6 w-6" />
-      Marigold
-    </Link>
-    <div className="bg-bg-muted rounded p-2">
-      <Link
-        href="https://marigold-ui.github.io/marigold/"
-        className="no-underline"
-        target="_blank"
-      >
+    <div className="text-lg font-bold uppercase tracking-tight text-[#46505a] [&>*]:flex [&>*]:items-center [&>*]:gap-2  [&>*]:no-underline">
+      <Link href="/">
+        <Logo className="h-6 w-6" />
+        Marigold
+      </Link>
+    </div>
+    <div className="bg-bg-muted rounded p-2 [&>*]:no-underline">
+      <Link href="https://marigold-ui.github.io/marigold/" target="_blank">
         <Text fontSize="sm">Visit old Docs (v5.6.0)</Text>
       </Link>
     </div>
