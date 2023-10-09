315385eb01b734107fdc0993076e9c87a32e9f5e
sarahgm <sarah.gosmann@reservix.de>
2023-10-09 09:55:19 +0200

    testttt

--- docs/next.config.js
+++ docs/next.config.js
@@ -12,10 +12,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    loader: 'akamai',
-    path: '/',
-  },
   transpilePackages: [
     '@marigold/components',
     '@marigold/system',
