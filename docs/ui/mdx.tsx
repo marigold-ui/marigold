bef9f2b60b9912731bf00b40a52b54311ee96c0f
sarahgm <sarah.gosmann@reservix.de>
2023-10-09 15:35:24 +0200

    save

--- docs/ui/mdx.tsx
+++ docs/ui/mdx.tsx
@@ -82,10 +82,6 @@ const typography = {
       </div>
     );
   },
-  img: (props: any) => (
-    // height and width are part of the props, so they get automatically passed here with {...props}
-    <Image {...props} layout="responsive" loading="lazy" />
-  ),
 };
 
 // MDX Components
