5679204c208fd76e5f5de71e4d15e39158a37ca3
sarahgm <sarah.gosmann@reservix.de>
2023-10-10 12:11:55 +0200

    fix: button active disabled state

--- themes/theme-b2b/src/components/Button.styles.ts
+++ themes/theme-b2b/src/components/Button.styles.ts
@@ -4,7 +4,7 @@ export const Button: ThemeComponent<'Button'> = cva(
   [
     'rounded-sm border-none px-8 leading-[48px] outline-none',
     'focus-visible:outline-outline-focus focus-visible:outline focus-visible:outline-1 focus-visible:outline-offset-1',
-    'disabled:text-text-disabled disabled:bg-bg-disabled',
+    'disabled:text-text-disabled disabled:bg-bg-disabled disabled:cursor-not-allowed',
   ],
   {
     variants: {
@@ -12,20 +12,20 @@ export const Button: ThemeComponent<'Button'> = cva(
         primary: [
           'text-text-light bg-bg-primary',
           'data-[hover]:text-text-light data-[hover]:bg-bg-primary-hover',
-          'active:hover:bg-bg-primary-active',
+          'enabled:active:hover:bg-bg-primary-active',
         ],
         secondary: [
           'text-text-light bg-bg-secondary data-[hover]:text-text-light data-[hover]:bg-bg-secondary-hover',
-          'active:hover:bg-bg-secondary-active',
+          'enabled:active:hover:bg-bg-secondary-active',
         ],
         ghost: [
           'border-border-dark border border-solid',
           'text-text-body',
           'hover:bg-bg-hover',
-          'active:hover:bg-bg-secondary-active active:hover:text-text-light',
+          'enabled:active:hover:bg-bg-secondary-active enabled:active:hover:text-text-light',
         ],
         text: [
-          'text-text-body data-[hover]:bg-bg-hover active:hover:bg-bg-secondary-active active:hover:text-text-light',
+          'text-text-body data-[hover]:bg-bg-hover enabled:active:hover:bg-bg-secondary-active enabled:active:hover:text-text-light',
         ],
         menu: [
           'text-text-body bg-bg-body hover:text-text-light hover:bg-bg-secondary-hover',
