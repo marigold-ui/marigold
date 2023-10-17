ea8f77bb296adfa0670f9a2dfe2fc41abafd94c1
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-10-17 06:25:04 +0000

    Deploying to docs from @ marigold-ui/marigold@824a25e836f1819eed14521b7b2bfbbf36f270fe 🚀

--- packages/components/src/Select/Select.tsx
+++ packages/components/src/Select/Select.tsx
@@ -149,7 +149,7 @@ export const Select = forwardRef<HTMLButtonElement, SelectProps>(
         />
         <button
           className={cn(
-            'flex w-full items-center justify-between gap-1',
+            'flex w-full items-center justify-between gap-1 overflow-hidden',
             classNames.select
           )}
           ref={buttonRef}
