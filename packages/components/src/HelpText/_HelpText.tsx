43cadd8fe9d1e4ec146fc2c4e83fa1d4a903a5f0
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-10-19 15:25:04 +0200

    feat: Move `<HelpText>` to RAC

---
+++ packages/components/src/HelpText/_HelpText.tsx
@@ -0,0 +1,53 @@
+import type { ReactNode } from 'react';
+import { Text } from 'react-aria-components';
+
+import { cn, useClassNames } from '@marigold/system';
+
+export interface HelpTextProps {
+  variant?: string;
+  size?: string;
+  description?: ReactNode;
+  error?: boolean;
+  errorMessage?: ReactNode;
+}
+
+export const HelpText = ({
+  variant,
+  size,
+  description,
+  error,
+  errorMessage,
+}: HelpTextProps) => {
+  const hasErrorMessage = errorMessage && error;
+  const classNames = useClassNames({
+    component: 'HelpText',
+    variant,
+    size,
+  });
+
+  if (!description && !errorMessage) {
+    return null;
+  }
+
+  return (
+    <Text
+      slot={hasErrorMessage ? 'errorMessage' : 'description'}
+      className={cn('flex items-center gap-1', classNames.container)}
+    >
+      {hasErrorMessage ? (
+        <>
+          <svg
+            className={cn('h-4 w-4', classNames.icon)}
+            viewBox="0 0 24 24"
+            role="presentation"
+          >
+            <path d="M2.25 20.3097H21.75L12 3.46875L2.25 20.3097ZM12.8864 17.2606H11.1136V15.4879H12.8864V17.2606ZM12.8864 13.7151H11.1136V10.1697H12.8864V13.7151Z" />
+          </svg>
+          {errorMessage}
+        </>
+      ) : (
+        description
+      )}
+    </Text>
+  );
+};
