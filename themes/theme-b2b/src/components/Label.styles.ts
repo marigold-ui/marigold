0609c34db627d123236efaee6621e38f1cbfdfec
sarahgm <sarah.gosmann@reservix.de>
2023-10-20 16:38:58 +0200

    fix: label size in b2b

--- themes/theme-b2b/src/components/Label.styles.ts
+++ themes/theme-b2b/src/components/Label.styles.ts
@@ -1,7 +1,7 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Label: ThemeComponent<'Label'> = {
-  container: cva('group-disabled/field:text-text-disabled text-sm '),
+  container: cva('group-disabled/field:text-text-disabled'),
   indicator: cva(
     'group-required/field:text-text-required group-required/field:block'
   ),
