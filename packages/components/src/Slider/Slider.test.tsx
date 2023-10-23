ba6e62beb3dc815df5b976cf41f71a42a0173529
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-10-23 15:44:27 +0200

    fix tests

--- packages/components/src/Slider/Slider.test.tsx
+++ packages/components/src/Slider/Slider.test.tsx
@@ -16,6 +16,7 @@ const theme: Theme = {
       indicator: cva(),
     },
     Slider: {
+      container: cva(''),
       track: cva([
         'absolute top-4 h-2 w-full',
         'rounded-lg border-none border-transparent',
@@ -29,7 +30,6 @@ const theme: Theme = {
         ' focus:border-slider-thumb-focus',
         ' disabled:bg-slider-thumb-disabled-background  disabled:border-slider-thumb-disabled-border',
       ]),
-      label: cva('text-slider-label-text text-base font-normal'),
       output: cva('text-slider-ouput-text text-base font-normal'),
     },
   },
@@ -119,7 +119,7 @@ test('takes full width by default', () => {
 
   const container = screen.getByRole('group');
   expect(container.className).toMatchInlineSnapshot(
-    `"flex touch-none flex-col w-full"`
+    `"grid grid-cols-[auto_1fr] gap-y-1 w-full"`
   );
 });
 
@@ -128,7 +128,7 @@ test('allows to set width via prop', () => {
 
   const container = screen.getByRole('group');
   expect(container.className).toMatchInlineSnapshot(
-    `"flex touch-none flex-col w-44"`
+    `"grid grid-cols-[auto_1fr] gap-y-1 w-44"`
   );
 });
 
