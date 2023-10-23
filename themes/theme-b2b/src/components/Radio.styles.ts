002fe47441263507a2ca453050971ec8e672590e
sarahgm <sarah.gosmann@reservix.de>
2023-10-23 21:57:31 +0200

    core theme

--- themes/theme-b2b/src/components/Radio.styles.ts
+++ themes/theme-b2b/src/components/Radio.styles.ts
@@ -6,9 +6,8 @@ export const Radio: ThemeComponent<'Radio'> = {
   radio: cva([
     'border-border-light',
     'group-hover/radio:border-border-hover',
-    'group-[selected]/radio:border-border-selected-input group-[selected]/radio:bg-bg-selected-input group-[selected]/radio:text-white',
+    'group-selected/radio:border-border-selected-input group-selected/radio:bg-bg-selected-input group-selected/radio:text-white',
     'group-[focus-visible]/radio:outline-outline-focus group-[focus-visible]/radio:outline group-[focus-visible]/radio:outline-2 group-[focus-visible]/radio:outline-offset-2',
-
     'group-disabled/radio:bg-bg-disabled group-disabled/radio:border-border-disabled',
   ]),
   group: cva(),
