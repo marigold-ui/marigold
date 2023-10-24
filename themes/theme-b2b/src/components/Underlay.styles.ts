b74af0e3810cfb9f5a1add5207a72949a6c16966
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-10-24 10:11:17 +0200

    fix: underlay in b2b theme is transparent again

--- themes/theme-b2b/src/components/Underlay.styles.ts
+++ themes/theme-b2b/src/components/Underlay.styles.ts
@@ -3,7 +3,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const Underlay: ThemeComponent<'Underlay'> = cva('', {
   variants: {
     variant: {
-      modal: ' bg-bg-surface-underlay backdrop-blur-sm',
+      modal: ' bg-bg-surface-underlay/40 backdrop-blur-sm',
     },
   },
 });
