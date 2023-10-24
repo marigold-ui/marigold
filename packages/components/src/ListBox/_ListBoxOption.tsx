6c106496a49fcf854492770ec3a09a9d25c6c135
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-10-24 20:02:54 +0300

    edit styles selectors

--- packages/components/src/ListBox/_ListBoxOption.tsx
+++ packages/components/src/ListBox/_ListBoxOption.tsx
@@ -1,11 +1,18 @@
+import { Key } from 'react';
 import { Item } from 'react-aria-components';
 import type RAC from 'react-aria-components';
 
 import { useListBoxContext } from './Context';
 
-export interface ItemProps extends RAC.ItemProps {}
+export interface ItemProps extends Omit<RAC.ItemProps, 'id'> {
+  key?: Key;
+}
 
-export const _Item = (props: ItemProps) => {
+export const _Item = ({ key, ...rest }: ItemProps) => {
+  const props: RAC.ItemProps = {
+    id: key,
+    ...rest,
+  };
   const { classNames } = useListBoxContext();
   return <Item {...props} className={classNames.option} />;
 };
