737a870a04358ba518518563c424a742d12389db
Sebastian Sebald <sebastian.sebald@gmail.com>
2023-11-10 12:56:51 +0100

    fix error message styling

--- packages/components/src/HelpText/_HelpText.tsx
+++ packages/components/src/HelpText/_HelpText.tsx
@@ -30,7 +30,7 @@ export const HelpText = ({
   if (!description && !errorMessage) {
     return null;
   }
-
+  console.log(hasErrorMessage);
   return (
     <Text
       {...props}
@@ -43,6 +43,7 @@ export const HelpText = ({
             className={cn('h-4 w-4', classNames.icon)}
             viewBox="0 0 24 24"
             role="presentation"
+            fill="currentColor"
           >
             <path d="M2.25 20.3097H21.75L12 3.46875L2.25 20.3097ZM12.8864 17.2606H11.1136V15.4879H12.8864V17.2606ZM12.8864 13.7151H11.1136V10.1697H12.8864V13.7151Z" />
           </svg>
