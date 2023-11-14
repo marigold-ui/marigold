8793cc313d3c8d0859e9c4078598650c8967a856
sarahgm <sarah.gosmann@reservix.de>
2023-11-14 08:57:18 +0100

    save

--- packages/components/src/HelpText/_HelpText.tsx
+++ packages/components/src/HelpText/_HelpText.tsx
@@ -4,7 +4,7 @@ import type { ValidationResult } from 'react-aria-components';
 
 import { cn, useClassNames } from '@marigold/system';
 
-export interface HelpTextProps extends Omit<ValidationResult, 'isInvalid'> {
+export interface HelpTextProps {
   variant?: string;
   size?: string;
   description?: ReactNode;
@@ -35,26 +35,23 @@ export const HelpText = ({
     return null;
   }
 
-  console.log(hasErrorMessage);
   return (
     <div {...props} className={cn(classNames.container)}>
       {hasErrorMessage ? (
-        <>
-          <FieldError
-            {...props}
-            className="grid grid-flow-col items-center gap-1"
+        <FieldError
+          {...props}
+          className="grid grid-flow-col items-center gap-1"
+        >
+          <svg
+            className={cn('h-4 w-4', classNames.icon)}
+            viewBox="0 0 24 24"
+            role="presentation"
+            fill="currentColor"
           >
-            <svg
-              className={cn('h-4 w-4', classNames.icon)}
-              viewBox="0 0 24 24"
-              role="presentation"
-              fill="currentColor"
-            >
-              <path d="M2.25 20.3097H21.75L12 3.46875L2.25 20.3097ZM12.8864 17.2606H11.1136V15.4879H12.8864V17.2606ZM12.8864 13.7151H11.1136V10.1697H12.8864V13.7151Z" />
-            </svg>
-            {errorMessage as ReactNode}
-          </FieldError>
-        </>
+            <path d="M2.25 20.3097H21.75L12 3.46875L2.25 20.3097ZM12.8864 17.2606H11.1136V15.4879H12.8864V17.2606ZM12.8864 13.7151H11.1136V10.1697H12.8864V13.7151Z" />
+          </svg>
+          {errorMessage as ReactNode}
+        </FieldError>
       ) : (
         <Text slot="description">{description}</Text>
       )}
