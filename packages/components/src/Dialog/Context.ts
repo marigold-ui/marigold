7fa02487781022cb874e673572add4bb45bfa150
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-11-21 13:14:06 +0200

    remove unwanted changes

--- packages/components/src/Dialog/Context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export interface DialogContextProps {
-  open?: boolean;
-  close?: () => void;
-}
-
-export const DialogContext = createContext<DialogContextProps>({});
-export const useDialogContext = () => useContext(DialogContext);
