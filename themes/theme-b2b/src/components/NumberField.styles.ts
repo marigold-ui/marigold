7ca9778afc62c0e62803f4490eecfc05f59323a3
aromko <aromkoapps@gmail.com>
2023-11-21 11:17:32 +0100

    RAC: fix outline style

--- themes/theme-b2b/src/components/NumberField.styles.ts
+++ themes/theme-b2b/src/components/NumberField.styles.ts
@@ -6,13 +6,13 @@ export const NumberField: ThemeComponent<'NumberField'> = {
     'group-hover/field:border-border-hover',
     'group-readonly/field:group-focus/field:border-border-light group-readonly/field:group-focus/field:shadow-none',
     'group-disabled/field:bg-bg-disabled group-disabled/field:text-text-disabled group-disabled/field:cursor-not-allowed ',
+    'data-[focus-within]:outline-outline-focus data-[focus-within]:outline',
+    'outline-2 -outline-offset-1 ',
   ]),
   stepper: cva([
     'text-text-body w-7',
     'border-border-light border-solid first-of-type:border-r',
     'border-border-light border-solid last-of-type:border-l ',
-    'outline-2 -outline-offset-1 ',
-    'group-focus/field:outline-outline-focus group-focus/field:outline',
     'group-error/field:outline-outline-error group-error/field:outline',
   ]),
 };
