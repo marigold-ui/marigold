3987d8ca1964e4bc58dd4585227539e2dbc029a2
sarahgm <sarah.gosmann@reservix.de>
2023-11-21 13:21:51 +0100

    save

--- packages/components/src/Tooltip/Tooltip.stories.tsx
+++ packages/components/src/Tooltip/Tooltip.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Button } from '../Button';
 import { Tooltip } from './Tooltip';
