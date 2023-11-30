cdc17ee83771dc367d29126903feca831c43a421
Marcel Köhler <77496890+aromko@users.noreply.github.com>
2023-11-30 19:36:06 +0100

    RAC: migrate `<TagGroup>` component (#3513)
    * RAC: migrate taggroup component
    
    * Create sour-trainers-promise.md
    
    * RAC: move tag documentation to collection category
    
    * RAC: remove error, errormessage from tag component
    
    * RAC: remove required prop from taggroup
    
    ---------
    
    Co-authored-by: Sebastian Sebald <sebastian.sebald@reservix.de>

--- packages/components/src/TagGroup/Tag.tsx
+++ packages/components/src/TagGroup/Tag.tsx
@@ -1,75 +1,51 @@
-import React, { useRef } from 'react';
-
-import { useButton } from '@react-aria/button';
-import { useFocusRing } from '@react-aria/focus';
-import { AriaTagProps, useTag } from '@react-aria/tag';
-import { mergeProps } from '@react-aria/utils';
-
-import { ListState } from '@react-stately/list';
+import type RAC from 'react-aria-components';
+import { Button, Tag } from 'react-aria-components';
 
 import { cn, useClassNames } from '@marigold/system';
+
+import { TagGroup } from './TagGroup';
 
 interface CloseButtonProps {
   className: string;
 }
 
-export const CloseButton = ({ className, ...props }: CloseButtonProps) => {
-  const ref = useRef(null);
-  const { buttonProps } = useButton({ ...props }, ref);
+export const CloseButton = ({ className }: CloseButtonProps) => {
   return (
-    <button className={className} {...buttonProps}>
+    <Button slot="remove" className={className}>
       <svg viewBox="0 0 20 20" fill="currentColor" width={20} height={20}>
         <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
       </svg>
-    </button>
+    </Button>
   );
 };
 
 // Props
 // ---------------
-export interface TagProps extends AriaTagProps<object> {
-  state: ListState<object>;
+export interface TagProps extends RAC.TagProps {
   variant?: string;
   size?: string;
 }
 
-// Component
-// ---------------
-export const Tag = ({ variant, size, item, state, ...rest }: TagProps) => {
-  const props: AriaTagProps<object> = {
-    item,
-    ...rest,
-  };
-
-  let ref = React.useRef(null);
-  let { focusProps } = useFocusRing({ within: true });
-
-  const { rowProps, gridCellProps, allowsRemoving, removeButtonProps } = useTag(
-    {
-      ...props,
-      item,
-    },
-    state,
-    ref
-  );
+const _Tag = ({ variant, size, children, ...props }: TagProps) => {
+  let textValue = typeof children === 'string' ? children : undefined;
 
   const classNames = useClassNames({ component: 'Tag', variant, size });
 
   return (
-    <span
-      ref={ref}
-      {...mergeProps(rowProps, focusProps)}
-      className={classNames.tag}
-    >
-      <div {...gridCellProps} className={classNames.gridCell}>
-        <span>{item.rendered}</span>
-        {allowsRemoving && (
-          <CloseButton
-            {...removeButtonProps}
-            className={cn('flex items-center', classNames.closeButton)}
-          />
-        )}
-      </div>
-    </span>
+    <Tag textValue={textValue} {...props} className={classNames.tag}>
+      {({ allowsRemoving }) => (
+        <>
+          {children}
+          {allowsRemoving && (
+            <CloseButton
+              className={cn('flex items-center', classNames.closeButton)}
+            />
+          )}
+        </>
+      )}
+    </Tag>
   );
 };
+
+_Tag.Group = TagGroup;
+export { _Tag as Tag };
