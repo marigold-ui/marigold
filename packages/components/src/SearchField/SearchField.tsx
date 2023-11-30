d687ea1063f0ad46e18661cebe89c97c05ab7479
aromko <aromkoapps@gmail.com>
2023-11-30 08:31:59 +0100

    Revert "RAC: use new Input component"
    This reverts commit d05e8c4f141bc384ce1b048a955e8cb78467edb6.

--- packages/components/src/SearchField/SearchField.tsx
+++ packages/components/src/SearchField/SearchField.tsx
@@ -5,7 +5,7 @@ import { SearchField } from 'react-aria-components';
 import { WidthProp } from '@marigold/system';
 
 import { FieldBase, FieldBaseProps } from '../FieldBase/_FieldBase';
-import { Input } from '../Input/Input';
+import { Input } from '../Input/_Input';
 
 // Props
 // ---------------
