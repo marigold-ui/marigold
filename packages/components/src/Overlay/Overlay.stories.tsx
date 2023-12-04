976ae97350b469a54d481154909f18347d79e91b
sarahgm <sarah.gosmann@reservix.de>
2023-12-04 15:02:45 +0100

    typecheck

--- packages/components/src/Overlay/Overlay.stories.tsx
+++ packages/components/src/Overlay/Overlay.stories.tsx
@@ -2,11 +2,6 @@ import { Meta, StoryObj } from '@storybook/react';
 import { forwardRef } from 'react';
 import { Menu, MenuItem, MenuTrigger } from 'react-aria-components';
 
-import { OverlayProvider } from '@react-aria/overlays';
-import { useObjectRef } from '@react-aria/utils';
-
-import { useOverlayTriggerState } from '@react-stately/overlays';
-
 import { Button } from '../Button';
 import { Dialog } from '../Dialog';
 import { Headline } from '../Headline';
