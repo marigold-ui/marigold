ccce03b4df4c8ac17f508d5d966000c1a3805f22
aromko <aromkoapps@gmail.com>
2023-12-05 14:36:21 +0100

    RAC: fix hover style

--- themes/theme-b2b/src/components/Calendar.styles.ts
+++ themes/theme-b2b/src/components/Calendar.styles.ts
@@ -4,7 +4,7 @@ export const Calendar: ThemeComponent<'Calendar'> = {
   calendar: cva('disabled:text-text-disabled bg-white'),
   calendarCell: cva([
     'data-[disabled]:text-text-disabled',
-    'data-[hover]:bg-bg-primary-light',
+    'data-[hovered]:bg-bg-primary-light',
     'data-[selected]:bg-bg-selected-input outline-none data-[selected]:font-semibold data-[selected]:text-white',
   ]),
   calendarControllers: cva([
