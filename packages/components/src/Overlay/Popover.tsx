73d8144ea10a10e5ff9f47c5e2510c6e07bb46c5
OsamaAbdellateef <osama.abd9888@gmail.com>
2023-12-08 12:25:28 +0200

    Resolving some comments

--- packages/components/src/Overlay/Popover.tsx
+++ packages/components/src/Overlay/Popover.tsx
@@ -11,7 +11,7 @@ import { Underlay } from './Underlay';
 export interface PopoverProps
   extends Omit<
     RAC.PopoverProps,
-    'isOpen' | 'isKeyboardDismissDisabled' | 'style'
+    'isOpen' | 'isKeyboardDismissDisabled' | 'style' | 'className'
   > {
   keyboardDismissDisabled?: boolean;
   open?: boolean;
@@ -20,10 +20,7 @@ export interface PopoverProps
 // Component
 // ---------------
 const _Popover = forwardRef<HTMLDivElement, PopoverProps>(
-  (
-    { keyboardDismissDisabled, placement, open, className, children, ...rest },
-    ref
-  ) => {
+  ({ keyboardDismissDisabled, placement, open, children, ...rest }, ref) => {
     const props: RAC.PopoverProps = {
       isKeyboardDismissDisabled: keyboardDismissDisabled,
       isOpen: open,
