d81b20e2333b4ed4f1bc7bfd8f1e9f4291885af7
aromko <aromkoapps@gmail.com>
2023-12-08 13:51:22 +0100

    [DST-254]: replace data-[focused] with rac-focus, replace data-[selected] with rac-selected

--- themes/theme-core/src/components/Checkbox.styles.ts
+++ themes/theme-core/src/components/Checkbox.styles.ts
@@ -14,7 +14,7 @@ export const Checkbox: ThemeComponent<'Checkbox'> = {
   checkbox: cva([
     'border-border-inverted rounded-[2] bg-white p-0.5',
     'group-data-[hovered]/checkbox:border-border-base-hover',
-    'group-[selected]/checkbox:border-border-selected group-data-[selected]/checkbox:bg-bg-selected fill-white group-data-[selected]/checkbox:text-white',
+    'group-[selected]/checkbox:border-border-selected group-rac-selected/checkbox:bg-bg-selected group-rac-selected/checkbox:text-white fill-white',
     'group-[disabled]/checkbox:group-[selected]/checkbox:border-border-base-disabled group-[disabled]/checkbox:group-[selected]/checkbox:bg-bg-inverted-disabled-dark',
     'group-indeterminate/checkbox:border-border-selected group-indeterminate/checkbox:bg-bg-selected group-indeterminate/checkbox:text-text-inverted',
   ]),
