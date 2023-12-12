3952ee0e893704e791bc6a51ed57b3dc80b78ece
sarahgm <38324334+sarahgm@users.noreply.github.com>
2023-12-12 11:00:27 +0100

    RAC: Menu react aria components (#3542)
    Co-authored-by: Sebastian Sebald <sebastian.sebald@gmail.com>
    Co-authored-by: Marcel Köhler <77496890+aromko@users.noreply.github.com>

---
+++ docs/content/components/overlay/menu/menu-selection.demo.tsx
@@ -0,0 +1,14 @@
+import { Menu } from '@marigold/components';
+
+export default () => {
+  <Menu
+    label="Hogwarts Houses"
+    selectionMode="single"
+    selectedKeys={['gryffindor']}
+  >
+    <Menu.Item id="gryffindor">🦁 Gryffindor</Menu.Item>
+    <Menu.Item id="hufflepuff">🦡 Hufflepuff</Menu.Item>
+    <Menu.Item id="ravenclaw">🐦‍⬛ Ravenclaw</Menu.Item>
+    <Menu.Item id="slytherin">🐍 Slytherin</Menu.Item>
+  </Menu>;
+};
--- themes/theme-core/src/components/Menu.styles.ts
+++ themes/theme-core/src/components/Menu.styles.ts
@@ -2,13 +2,16 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const Menu: ThemeComponent<'Menu'> = {
   container: cva([
-    'bg-bg-surface list-none break-words rounded-[2pxs] border p-0',
+    'bg-bg-surface list-none break-words rounded-[2px] border p-0',
     'sm:max-h-[75ch] md:max-h-[75vh] lg:max-h-[45vh]',
-    'overflow-y-auto overflow-x-hidden',
+    'flex flex-col overflow-y-auto overflow-x-hidden',
   ]),
   item: cva([
     'cursor-pointer p-1 focus:outline-0',
-    'hover:text-text-light hover:bg-highlight',
+    'data-[disabled]:text-text-disabled',
+    'data-[hovered]:text-text-light data-[hovered]:bg-highlight ',
+    'text-xs',
+    'data-[selected]:bg-bg-selected',
   ]),
-  section: cva('text-text-dark px-1 py-4 text-xs font-normal'),
+  section: cva('text-text-dark  border-t p-1 text-xs font-normal'),
 };
