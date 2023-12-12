775d10e6dd404b15cf3b4cfeaa64aa0cd59578f1
aromko <aromkoapps@gmail.com>
2023-12-12 10:48:47 +0100

    RAC: hover effect only on no disabled cells

--- themes/theme-core/src/components/Calendar.styles.ts
+++ themes/theme-core/src/components/Calendar.styles.ts
@@ -4,7 +4,7 @@ export const Calendar: ThemeComponent<'Calendar'> = {
   calendar: cva('disabled:text-text-disabled bg-white'),
   calendarCell: cva([
     'data-[disabled]:text-text-disabled',
-    'hover:bg-bg-hover-light',
+    'data-[hovered]:bg-bg-hover-light',
     'data-[selected]:bg-bg-selected outline-none data-[selected]:font-semibold data-[selected]:text-white',
   ]),
   calendarControllers: cva(),
