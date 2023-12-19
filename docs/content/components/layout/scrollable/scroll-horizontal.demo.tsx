655720c68412c7511de483074e7409bbe8a34d45
sarahgm <sarah.gosmann@reservix.de>
2023-12-19 14:49:12 +0100

    save

--- docs/content/components/layout/scrollable/scroll-horizontal.demo.tsx
+++ docs/content/components/layout/scrollable/scroll-horizontal.demo.tsx
@@ -2,7 +2,7 @@ import { Card, Scrollable } from '@marigold/components';
 
 export default () => (
   <Scrollable width="1/2">
-    <div className="flex gap-2">
+    <div className="flex gap-2 p-4">
       <Card>
         <div className="h-[100px] w-[200px]" />
       </Card>
