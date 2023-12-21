68bacf703d45404218e6fe0742225664f2ace709
sarahgm <sarah.gosmann@reservix.de>
2023-12-21 11:25:59 +0100

    chore: Update `tailwindcss-react-aria-components` to v.1.0.0

--- packages/theme-preset/src/create-preset.ts
+++ packages/theme-preset/src/create-preset.ts
@@ -1,5 +1,4 @@
 import deepmerge from 'deepmerge';
-import racSelectorPlugin from 'tailwindcss-react-aria-components';
 import plugin from 'tailwindcss/plugin';
 import type { Config, OptionalConfig } from 'tailwindcss/types/config';
 
@@ -16,8 +15,7 @@ export const createPreset = (name: string, config: Partial<OptionalConfig>) =>
         },
       },
       plugins: [
-        // @ts-expect-error (are the typings wrong?)
-        racSelectorPlugin({ prefix: 'rac' }),
+        require('tailwindcss-react-aria-components')({ prefix: 'rac' }),
         // Grouping
         plugin(({ matchVariant, e }) => {
           matchVariant(
