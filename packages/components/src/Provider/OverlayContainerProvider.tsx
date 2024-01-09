db2e6af052eb064dc4d06861b21cb05a5daaca12
sarahgm <sarah.gosmann@reservix.de>
2024-01-09 12:50:46 +0100

    add comments

--- packages/components/src/Provider/OverlayContainerProvider.tsx
+++ packages/components/src/Provider/OverlayContainerProvider.tsx
@@ -2,6 +2,8 @@ import { createContext, useContext } from 'react';
 
 import { useIsSSR } from '@react-aria/ssr';
 
+// needs an id for the portal container, it will be overgiven as string,
+// if there is no the body is used
 const OverlayContainerContext = createContext<string | undefined>(undefined);
 
 export const OverlayContainerProvider = OverlayContainerContext.Provider;
