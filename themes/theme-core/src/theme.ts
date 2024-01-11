aa66048950e2c7b1cbcead2f55c87ae46395c6ed
sarahgm <sarah.gosmann@reservix.de>
2024-01-11 10:55:27 +0100

    save

--- themes/theme-core/src/theme.ts
+++ themes/theme-core/src/theme.ts
@@ -1,5 +1,5 @@
 import { Theme } from '@marigold/system';
-import { flattenObject } from '@marigold/theme-preset/src/utils';
+import { flattenObject } from '@marigold/theme-preset';
 
 import * as components from './components';
 import { root } from './root';
