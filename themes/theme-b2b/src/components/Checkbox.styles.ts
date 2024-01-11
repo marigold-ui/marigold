72478dc3b01806b4a390b5e3be89be3b9dd479f2
sarahgm <sarah.gosmann@reservix.de>
2024-01-11 15:58:41 +0100

    save

--- themes/theme-b2b/src/components/Checkbox.styles.ts
+++ themes/theme-b2b/src/components/Checkbox.styles.ts
@@ -4,7 +4,7 @@ export const Checkbox: ThemeComponent<'Checkbox'> = {
   checkbox: cva([
     'border-border-base group-data-[hovered]/checkbox:border-border-base-hover rounded-sm',
     'group-focus/checkbox:outline-outline-focus group-focus/checkbox:outline group-focus/checkbox:outline-2 group-focus/checkbox:outline-offset-2',
-    'group-[selected]/checkbox:border-border-selected group-[selected]/checkbox:bg-bg-selected-input group-indeterminate/checkbox:border-border-selected group-indeterminate/checkbox:bg-bg-selected-input group-indeterminate/checkbox:text-text-inverted group-data-[selected]/checkbox:text-text-inverted',
+    'group-[selected]/checkbox:border-border-selected-input group-[selected]/checkbox:bg-bg-selected-input group-indeterminate/checkbox:border-border-selected-input group-indeterminate/checkbox:bg-bg-selected-input group-indeterminate/checkbox:text-text-inverted group-data-[selected]/checkbox:text-text-inverted',
     'group-[disabled]/checkbox:group-[selected]/checkbox:border-border-base-disabled group-[disabled]/checkbox:group-[selected]/checkbox:bg-bg-base-disabled ',
   ]),
   container: cva(''),
