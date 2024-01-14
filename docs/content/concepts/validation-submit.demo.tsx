504fc9e3b0b6b7149d134e71e61655dc1cf9dfa0
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-01-14 22:41:52 +0100

    more

--- docs/content/concepts/validation-submit.demo.tsx
+++ docs/content/concepts/validation-submit.demo.tsx
@@ -1,6 +1,10 @@
-import { Button, Form, Inset, Stack, TextField } from '@marigold/components';
+import { useState } from 'react';
+
+import { Button, Form, Stack, TextField } from '@marigold/components';
 
 export default () => {
+  const [result, setResult] = useState<object>();
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     // Prevent page refresh.
     e.preventDefault();
@@ -9,17 +13,24 @@ export default () => {
     const data = Object.fromEntries(new FormData(e.currentTarget));
 
     // Submit or do whatever you want
-    console.log(data);
+    setResult(data);
   };
 
   return (
-    <Form onSubmit={onSubmit}>
-      <Stack space={1} alignX="left">
-        <TextField label="Promo Code" name="promocode" />
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Stack>
-    </Form>
+    <Stack space={4}>
+      <Form onSubmit={onSubmit}>
+        <Stack space={1} alignX="left">
+          <TextField label="Promo Code" name="promocode" width={44} />
+          <Button variant="primary" size="small" type="submit">
+            Submit
+          </Button>
+        </Stack>
+      </Form>
+      {result && (
+        <pre>
+          <code>{JSON.stringify(result, null, 2)}</code>
+        </pre>
+      )}
+    </Stack>
   );
 };
