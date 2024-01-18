4f78c6af2ef126af0cb5a737af4249c321d9f160
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-01-18 16:04:32 +0100

    story to test

--- packages/components/src/Menu/Menu.stories.tsx
+++ packages/components/src/Menu/Menu.stories.tsx
@@ -5,6 +5,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Key } from '@react-types/shared';
 
 import { Button } from '../Button';
+import { Table } from '../Table';
 import { ActionMenu } from './ActionMenu';
 import { Menu } from './Menu';
 
@@ -225,4 +226,85 @@ export const OpenMenuRemotely: Story = {
       </>
     );
   },
+};
+
+export const MenuWithinTable: Story = {
+  render: args => (
+    <Table
+      aria-label="Table with multiple selection"
+      selectionMode="multiple"
+      size="compact"
+      stretch
+    >
+      <Table.Header>
+        <Table.Column>Name</Table.Column>
+        <Table.Column>Firstname</Table.Column>
+        <Table.Column>House</Table.Column>
+        <Table.Column>Year of birth</Table.Column>
+        <Table.Column>Bla</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row key={1}>
+          <Table.Cell>
+            Ggjfegigiufiuhoighiugguihogufygduigigdoiguiwgiuyfiw
+          </Table.Cell>
+          <Table.Cell>
+            Gigfgeifgeuigeichuegcuyorhugchjrogugiuegiufhufiegchgbhugvuebu
+          </Table.Cell>
+          <Table.Cell>Gryffindor</Table.Cell>
+          <Table.Cell>1980</Table.Cell>
+          <Table.Cell>
+            <ActionMenu
+              onAction={action => alert(`Action: ${action}`)}
+              {...args}
+            >
+              <Menu.Item>Veranstaltung zuweisen</Menu.Item>
+            </ActionMenu>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row key={2}>
+          <Table.Cell>Malfoy</Table.Cell>
+          <Table.Cell>Draco</Table.Cell>
+          <Table.Cell>Slytherin</Table.Cell>
+          <Table.Cell>1980</Table.Cell>
+          <Table.Cell>
+            <ActionMenu
+              onAction={action => alert(`Action: ${action}`)}
+              {...args}
+            >
+              <Menu.Item>Keks</Menu.Item>
+            </ActionMenu>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row key={3}>
+          <Table.Cell>Diggory</Table.Cell>
+          <Table.Cell>Cedric</Table.Cell>
+          <Table.Cell>Hufflepuff</Table.Cell>
+          <Table.Cell>1977</Table.Cell>
+          <Table.Cell>
+            <ActionMenu
+              onAction={action => alert(`Action: ${action}`)}
+              {...args}
+            >
+              <Menu.Item>Keks</Menu.Item>
+            </ActionMenu>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row key={4}>
+          <Table.Cell>Lovegood</Table.Cell>
+          <Table.Cell>Luna</Table.Cell>
+          <Table.Cell>Ravenclaw</Table.Cell>
+          <Table.Cell>1981</Table.Cell>
+          <Table.Cell>
+            <ActionMenu
+              onAction={action => alert(`Action: ${action}`)}
+              {...args}
+            >
+              <Menu.Item>Keks</Menu.Item>
+            </ActionMenu>
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  ),
 };
