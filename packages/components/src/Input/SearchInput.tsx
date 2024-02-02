a034716a3bbc7c6b0f2a37d7e7a0f30e9b899e55
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-02-02 09:44:27 +0100

    jo

---
+++ packages/components/src/Input/SearchInput.tsx
@@ -0,0 +1,45 @@
+import { forwardRef } from 'react';
+import { Button } from 'react-aria-components';
+
+import { cn } from '@marigold/system';
+
+import { Input } from './Input';
+import type { InputProps } from './Input';
+
+// Icon
+// ---------------
+const SearchIcon = (props: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    width={24}
+    height={24}
+    {...props}
+  >
+    <path d="M16.1865 14.5142H15.3057L14.9936 14.2131C16.0862 12.9421 16.744 11.292 16.744 9.497C16.744 5.49443 13.4996 2.25 9.497 2.25C5.49443 2.25 2.25 5.49443 2.25 9.497C2.25 13.4996 5.49443 16.744 9.497 16.744C11.292 16.744 12.9421 16.0862 14.2131 14.9936L14.5142 15.3057V16.1865L20.0888 21.75L21.75 20.0888L16.1865 14.5142ZM9.49701 14.5142C6.72085 14.5142 4.47986 12.2732 4.47986 9.49701C4.47986 6.72085 6.72085 4.47986 9.49701 4.47986C12.2732 4.47986 14.5142 6.72085 14.5142 9.49701C14.5142 12.2732 12.2732 14.5142 9.49701 14.5142Z" />
+  </svg>
+);
+
+// Props
+// ---------------
+export interface SearchInputProps extends Omit<InputProps, 'icon'> {}
+
+// Component
+// ---------------
+export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <Input
+        type="search"
+        className={cn('[&::-webkit-search-cancel-button]:hidden', className)}
+        ref={ref}
+        icon={<SearchIcon />}
+        action={
+          <Button className="group-data-[empty=true]/field:hidden">X</Button>
+        }
+        {...props}
+      />
+    );
+  }
+);
