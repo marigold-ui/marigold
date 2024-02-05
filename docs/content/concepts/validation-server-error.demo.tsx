4464f5cfc65a5ebb41f66fab8afe750622c7dbac
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-02-05 11:24:00 +0100

    add comment about react query

--- docs/content/concepts/validation-server-error.demo.tsx
+++ docs/content/concepts/validation-server-error.demo.tsx
@@ -24,7 +24,12 @@ const SuccessMessage = () => (
 );
 
 const App = () => {
-  // Server communication
+  /**
+   * Server communication
+   *
+   * (We are using `@tanstack/react-query` in this example to interact
+   * with a server. Regular form request via the `action` attribute work too!)
+   */
   const mutation = useMutation<any, ValidationError, string>({
     mutationFn: (email: string) => post('/api/demo/subscribe', { email }),
   });
