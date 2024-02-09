df9c12ed9d959ac6946c1a14d842c85e1927c9eb
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-02-09 08:48:49 +0100

    add notes

---
+++ packages/components/src/Multiselect/Multiselect.tsx
@@ -0,0 +1,7 @@
+/**
+ * - list of removable tags
+ * - combobox to search for an item
+ *
+ * - selecting an item from the combobox -> adds it to the tags, clears combobox input
+ * - combobox only shows unselected items
+ */
