cb058eb4fa04842d8e496d32aa8cfea2739c20c3
sarahgm <sarah.gosmann@reservix.de>
2024-02-21 10:54:49 +0100

    .

--- docs/content/concepts/surface-overlay.demo.tsx
+++ docs/content/concepts/surface-overlay.demo.tsx
@@ -1,20 +1,9 @@
+import { Button, FieldBase } from '@marigold/components';
+
 export default () => {
   return (
     <div className="m-auto w-96">
-      <FieldBase isOpen as={Select} ref={ref} {...props}>
-        <Button
-          className={cn(
-            'flex w-full items-center justify-between gap-1 overflow-hidden',
-            classNames.select
-          )}
-        >
-          <SelectValue />
-          <ChevronDown className="size-4" />
-        </Button>
-        <Popover>
-          <ListBox items={items}>{props.children}</ListBox>
-        </Popover>
-      </FieldBase>
+      <button>click me</button>
     </div>
   );
 };
