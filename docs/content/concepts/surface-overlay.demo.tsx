c8885a0a035aca9b49e67b296bcd03180a1c8ab8
sarahgm <sarah.gosmann@reservix.de>
2024-02-21 13:34:04 +0100

    hmmm

--- docs/content/concepts/surface-overlay.demo.tsx
+++ docs/content/concepts/surface-overlay.demo.tsx
@@ -1,9 +1,22 @@
-import { Button, FieldBase } from '@marigold/components';
+import { Dialog, Headline, Link } from '@marigold/components';
 
 export default () => {
   return (
     <div className="m-auto w-96">
-      <button>click me</button>
+      <Dialog aria-labelledby="example">
+        <Headline level={3}>Overlay</Headline>
+        <p>
+          Overlay is the highest elevation available. It is reserved for a UI
+          that sits over another UI, such as modals, dialogs, dropdown menus,
+          floating toolbars, and floating single-action buttons.
+        </p>
+        <Link
+          href="https://atlassian.design/foundations/elevation/#overlay"
+          target="blank"
+        >
+          source
+        </Link>
+      </Dialog>
     </div>
   );
 };
