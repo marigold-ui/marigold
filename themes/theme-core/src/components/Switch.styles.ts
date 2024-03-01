fbfeb8ab6967e47c62f5cd612a79018bbd90c123
sarahgm <sarah.gosmann@reservix.de>
2024-03-01 16:13:51 +0100

    fix: Switch remove Selected border core theme

--- themes/theme-core/src/components/Switch.styles.ts
+++ themes/theme-core/src/components/Switch.styles.ts
@@ -8,9 +8,5 @@ export const Switch: ThemeComponent<'Switch'> = {
     'group-selected/switch:bg-bg-selected-input group-selected/switch:border-border-selected',
     'group-disabled/switch:bg-bg-base-disabled group-disabled/switch:border-border-base',
   ]),
-  thumb: cva([
-    'size-[20px]',
-    'bg-bg-base',
-    'group-selected/switch:border-border-selected',
-  ]),
+  thumb: cva(['size-[20px]', 'bg-bg-base']),
 };
