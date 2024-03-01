fc7f0bfa56e03de542e9faf6e0951b6901c10381
Sebastian Sebald <sebastian.sebald@reservix.de>
2024-03-01 08:52:15 +0100

    fix([DSTSUP-52]): only allow vertical resizing of textareas in core theme (#3779)
    Co-authored-by: Marcel Köhler <77496890+aromko@users.noreply.github.com>

---
+++ .changeset/smooth-coins-begin.md
@@ -0,0 +1,5 @@
+---
+"@marigold/theme-core": patch
+---
+
+fix([DSTSUP-52]): only allow vertical resizing of textareas in core theme
--- themes/theme-core/src/components/TextArea.styles.ts
+++ themes/theme-core/src/components/TextArea.styles.ts
@@ -8,4 +8,5 @@ export const TextArea: ThemeComponent<'TextArea'> = cva([
   'p-0.5',
   'focus:outline-outline-focus -outline-offset-1 focus:outline focus:outline-2',
   'disabled:bg-bg-inverted-disabled',
+  'resize-y',
 ]);
