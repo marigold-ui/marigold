2af7dc2b801015415a71ed1dd05793fb7eb9ea44
sarahgm <sarah.gosmann@reservix.de>
2024-03-11 15:44:54 +0100

    fix: add some core styles

--- themes/theme-core/src/components/NumberField.styles.ts
+++ themes/theme-core/src/components/NumberField.styles.ts
@@ -2,6 +2,6 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const NumberField: ThemeComponent<'NumberField'> = {
   group: cva(),
-  stepper: cva(),
+  stepper: cva('rac-disabled:bg-none rac-disabled:text-text-base-disabled'),
   input: cva(),
 };
