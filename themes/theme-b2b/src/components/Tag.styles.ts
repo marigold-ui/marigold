0e79c838fea0bc9fcab48dfb51944b9a3f524f7d
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-03-19 12:44:06 +0200

    change border color on select state

--- themes/theme-b2b/src/components/Tag.styles.ts
+++ themes/theme-b2b/src/components/Tag.styles.ts
@@ -3,7 +3,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const Tag: ThemeComponent<'Tag'> = {
   tag: cva([
     'border-border-base flex items-center gap-1 rounded-md border p-1 ',
-    'data-[selected]:bg-bg-selected-input data-[selected]:text-text-inverted',
+    'data-[selected]:bg-bg-selected-input data-[selected]:text-text-inverted data-[selected]:border-border-selected-input',
   ]),
   closeButton: cva('right-1 size-4 cursor-pointer p-0 outline-none'),
   listItems: cva('flex flex-wrap items-center gap-1'),
