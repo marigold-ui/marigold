0c3682faa1875f34ee69a527e8d6a71ec7fd78ae
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-03-19 11:04:38 +0200

    test

--- themes/theme-b2b/src/components/Tag.styles.ts
+++ themes/theme-b2b/src/components/Tag.styles.ts
@@ -2,7 +2,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const Tag: ThemeComponent<'Tag'> = {
   tag: cva([
-    'border-border-base flex items-center gap-1 rounded-md border p-1',
+    'border-border-base flex items-center gap-1 rounded-md border p-1 ',
     'data-[selected]:bg-bg-selected-input data-[selected]:text-text-inverted',
   ]),
   closeButton: cva('right-1 size-4 cursor-pointer p-0 outline-none'),
