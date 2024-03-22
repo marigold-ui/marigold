0c12975fbf3475ade82c41bd2b8427f270ad36a6
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-03-22 17:56:06 +0200

    Replace onSelectionChange with onChange

--- packages/components/src/GridList/GridList.tsx
+++ packages/components/src/GridList/GridList.tsx
@@ -12,8 +12,12 @@ import { cn, useClassNames } from '@marigold/system';
 import { GridListContext } from './Context';
 import { GridListItem } from './GridListItem';
 
+type RemoveProps = 'style' | 'className' | 'onSelectionChange';
+
 export interface GridListProps
-  extends Omit<RAC.GridListProps<object>, 'className' | 'style'> {}
+  extends Omit<RAC.GridListProps<object>, RemoveProps> {
+  onChange?: RAC.GridListProps<object>['onSelectionChange'];
+}
 
 interface GridListComponent
   extends ForwardRefExoticComponent<
@@ -22,26 +26,33 @@ interface GridListComponent
   Item: typeof GridListItem;
 }
 
-const _GridList = forwardRef<HTMLUListElement, GridListProps>((props, ref) => {
-  const classNames = useClassNames({ component: 'ListBox' });
-  console.log('classNames', classNames);
-  return (
-    <GridListContext.Provider value={{ classNames }}>
-      <div className={classNames.container}>
-        <GridList
-          {...props}
-          ref={ref as Ref<HTMLDivElement>}
-          className={cn(
-            'overflow-y-auto sm:max-h-[75vh] lg:max-h-[45vh]',
-            classNames.list
-          )}
-        >
-          {props.children}
-        </GridList>
-      </div>
-    </GridListContext.Provider>
-  );
-}) as GridListComponent;
+const _GridList = forwardRef<HTMLUListElement, GridListProps>(
+  ({ onChange, ...rest }, ref) => {
+    const classNames = useClassNames({ component: 'ListBox' });
+
+    const props: RAC.GridListProps<object> = {
+      onSelectionChange: onChange,
+      ...rest,
+    };
+
+    return (
+      <GridListContext.Provider value={{ classNames }}>
+        <div className={classNames.container}>
+          <GridList
+            {...props}
+            ref={ref as Ref<HTMLDivElement>}
+            className={cn(
+              'overflow-y-auto sm:max-h-[75vh] lg:max-h-[45vh]',
+              classNames.list
+            )}
+          >
+            {props.children}
+          </GridList>
+        </div>
+      </GridListContext.Provider>
+    );
+  }
+) as GridListComponent;
 
 _GridList.Item = GridListItem;
 
