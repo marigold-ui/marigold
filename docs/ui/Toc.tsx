7085690f69ff01becf0dbb6804eeeff4d280dfd1
sarahgm <sarah.gosmann@reservix.de>
2024-04-11 16:46:09 +0200

    make setup the toc

--- docs/ui/Toc.tsx
+++ docs/ui/Toc.tsx
@@ -5,32 +5,26 @@ import { createPortal } from 'react-dom';
 
 import { Headline, Link, List } from '@marigold/components';
 
-export const Toc = ({ items, selector }: any) => {
-  const ref = useRef<Element>();
-  const [, setMounted] = useState(false);
-  const elements = JSON.parse(items) as { anchor: string; title: string }[];
-  console.log(elements);
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      ref.current = document.querySelector(selector) || undefined;
-    }
-    setMounted(true);
-  }, [selector]);
-
-  if (!ref.current || elements.length === 0) {
-    return null;
-  }
-  const toc = document.querySelector('#toc');
-
-  const TocPortal = () => <div>huhuhuhu</div>;
-
-  return createPortal(<TocPortal />, ref.current);
-};
-
 export interface TocProps {
-  items: string[];
+  selector: string;
+  items: string;
 }
 
+export const Toc = ({ items, selector }: TocProps) => {
+  const elements = JSON.parse(items) as { anchor: string; title: string }[];
+
+  console.log(items);
+
+  const toc = document.querySelector('#toc');
+
+  const TocPortal = () => (
+    <div>
+      <Headline level={2}>Table of Contents</Headline>
+    </div>
+  );
+  return createPortal(<TocPortal />, toc as Element);
+};
+
 export const TocContainer = () => {
-  return <div id="toc">huhu</div>;
+  return <div id="toc"></div>;
 };
