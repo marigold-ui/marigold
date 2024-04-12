b16b9fcf187481d57b4eccf8af20a4de90e892bb
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-04-12 11:21:08 +0200

    test

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { b2bTheme, coreTheme, theme } from '@/theme';
 import { MarigoldProvider } from '@/ui';
+import { Suspense } from 'react';
 
 import '@marigold/theme-b2b/styles.css';
 import '@marigold/theme-core/styles.css';
@@ -46,31 +47,34 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className={`${fontSans.className} min-h-screen`}>
-        <MarigoldThemeSwitch themes={themes} initial="b2b">
-          <MarigoldProvider theme={theme}>
-            <div className="flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1 px-4 lg:px-10 xl:px-20">
-                <aside
-                  className={[
-                    'fixed top-14 z-10 -ml-2 hidden h-[calc(100vh-56px)] w-64 overflow-hidden hover:overflow-y-auto md:block xl:w-72',
-                    'scrollbar-thin scrollbar-thumb-secondary-400 scrollbar-thumb-rounded-full scrollbar-track-transparent',
-                    'border-secondary-200 border-r',
-                  ].join(' ')}
-                >
-                  {/* current section navigation sidebar */}
-                  <SectionNavigation />
-                </aside>
-                <div className="px-4 md:pl-80 xl:pl-[360px]">
-                  <MarigoldProvider theme={theme}>
-                    <main className="py-6 xl:py-10">{children}</main>
-                  </MarigoldProvider>
-                  <SiteFooter />
+        <Suspense>
+          <MarigoldThemeSwitch themes={themes} initial="b2b">
+            <MarigoldProvider theme={theme}>
+              <div className="flex min-h-screen flex-col">
+                <SiteHeader />
+                <div className="flex-1 px-4 lg:px-10 xl:px-20">
+                  <aside
+                    className={[
+                      'fixed top-14 z-10 -ml-2 hidden h-[calc(100vh-56px)] w-64 overflow-hidden hover:overflow-y-auto md:block xl:w-72',
+                      'scrollbar-thin scrollbar-thumb-secondary-400 scrollbar-thumb-rounded-full scrollbar-track-transparent',
+                      'border-secondary-200 border-r',
+                    ].join(' ')}
+                  >
+                    {/* current section navigation sidebar */}
+                    <SectionNavigation />
+                  </aside>
+                  <div className="px-4 md:pl-80 xl:pl-[360px]">
+                    <MarigoldProvider theme={theme}>
+                      <main className="py-6 xl:py-10">{children}</main>
+                    </MarigoldProvider>
+                    <SiteFooter />
+                  </div>
                 </div>
               </div>
-            </div>
-          </MarigoldProvider>
-        </MarigoldThemeSwitch>
+            </MarigoldProvider>
+          </MarigoldThemeSwitch>
+        </Suspense>
+
         <Analytics />
       </body>
     </html>
