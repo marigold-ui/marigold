c76de417419c7e12e9c6fcbe7006f55f9e847d4e
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-04-17 08:50:35 +0200

    clean up

--- docs/app/_components/SiteHeader.tsx
+++ docs/app/_components/SiteHeader.tsx
@@ -21,12 +21,3 @@ export const SiteHeader = () => (
     </div>
   </header>
 );
-
-{
-  /* <header className="border-secondary-200 bg-bg-body sticky top-0 z-10 w-full border-b">
-<div className="flex h-14 items-center px-3 xl:px-6">
-  <MobileNavigation />
-  <SiteNavigation />
-</div>
-</header> */
-}
