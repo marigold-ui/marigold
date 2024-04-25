30a3fc11a2707236f03bdf063ef1d91852797961
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-04-25 20:30:22 +0200

    adjust page padding on small screens

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -53,7 +53,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               className={[
                 'top-[--page-header-height]',
                 'py-[--page-sub-nav-padding] xl:py-[--page-sub-nav-padding-xl]',
-                'pl-[--page-padding] xl:pl-[--page-padding-xl]',
+                'pl-[--page-padding-md] xl:pl-[--page-padding-xl]',
                 'h-[calc(100vh-var(--page-header-height))] w-[--page-sub-nav-width] xl:w-[--page-sub-nav-width-xl]',
                 'fixed z-10 hidden overflow-hidden hover:overflow-y-auto md:block',
                 'scrollbar-thin scrollbar-thumb-secondary-400 scrollbar-thumb-rounded-full scrollbar-track-transparent',
@@ -66,7 +66,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <main
               className={[
                 'pt-[--page-main-padding] xl:pt-[--page-main-padding-xl]',
-                'px-[--page-padding] xl:pr-[--page-padding-xl]',
+                'px-[--page-padding] md:px-[--page-padding-md] xl:pr-[--page-padding-xl]',
                 'md:pl-[calc(var(--page-sub-nav-width)+var(--page-main-padding))] xl:pl-[calc(var(--page-sub-nav-width-xl)+var(--page-main-padding-xl))]',
               ].join(' ')}
             >
