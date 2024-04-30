000555a56e05e611847d073d65d8f5212cd9f35f
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-04-30 08:57:40 +0200

    save

--- docs/content/components/form/datepicker/date-picker-controlled.demo.tsx
+++ docs/content/components/form/datepicker/date-picker-controlled.demo.tsx
@@ -1,11 +1,12 @@
-import { parseDate } from '@internationalized/date';
+import { parseAbsoluteToLocal } from '@internationalized/date';
 import type { DateValue } from '@internationalized/date';
 import { useState } from 'react';
 
 import { DatePicker } from '@marigold/components';
 
 export default () => {
-  const [date, setDate] = useState<DateValue>(parseDate('1912-06-23'));
+  const date = new Date().toISOString();
+  const [value, setValue] = useState<DateValue>(parseAbsoluteToLocal(date));
 
-  return <DatePicker label="Date" value={date} onChange={setDate} />;
+  return <DatePicker label="Date" value={value} onChange={setValue} />;
 };
