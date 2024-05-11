ba44df1c904492dba79fc9770328c0cbcb8b2f71
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-05-11 07:57:12 +0200

    initial version

--- packages/components/src/Layout/Layout.stories.tsx
+++ packages/components/src/Layout/Layout.stories.tsx
@@ -1,9 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Layout } from './layout';
+import { Layout } from './Layout';
 
 const meta = {
-  title: 'Layout/Layout',
+  title: 'Components/Layout',
   component: Layout,
 } satisfies Meta<typeof Layout>;
 
@@ -11,5 +11,26 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-  render: args => <Layout>asd</Layout>,
+  render: args => (
+    <Layout
+      {...args}
+      areas={['header header', 'sidebar main', 'footer footer']}
+      columns={[1, 4]}
+      rows={['80px', 'auto', '80px']}
+      space={1}
+    >
+      <Layout.Slot name="header">
+        <div className="size-full bg-slate-600" />
+      </Layout.Slot>
+      <Layout.Slot name="sidebar">
+        <div className="size-full bg-slate-600" />
+      </Layout.Slot>
+      <Layout.Slot name="main">
+        <div className="h-60 w-full bg-slate-600" />
+      </Layout.Slot>
+      <Layout.Slot name="footer">
+        <div className="size-full bg-slate-600" />
+      </Layout.Slot>
+    </Layout>
+  ),
 };
--- packages/components/src/Layout/Layout.tsx
+++ packages/components/src/Layout/Layout.tsx
@@ -7,27 +7,54 @@ import { Slot } from './LayoutSlot';
 
 // Helpers
 // ---------------
+// TODO: What to allow here? cast number so fractions? Allow tailwind values for sizing (e.g. "4" -> "16px"?)
+export type TemplateValue = string | number;
+
 const parseGridAreas = (areas: string[]) =>
   areas.map(area => `"${area}"`).join('\n');
+
+const parseTemplateValue = (values: TemplateValue[]) =>
+  values
+    .map(val => {
+      if (typeof val === 'number') {
+        return `${val}fr`;
+      }
+
+      return val;
+    })
+    .join(' ');
 
 // Props
 // ---------------
 export interface LayoutProps extends GapSpaceProp {
   areas: string[];
   // TODO: make rows/columns more strict (tailwind values?)
-  columns?: string[];
-  rows?: string[];
+  columns: TemplateValue[];
+  rows: TemplateValue[];
   // height?
+  /**
+   * Children of the layout.
+   */
   children?: ReactNode;
 }
 
 // Component
 // ---------------
-export const Layout = ({ children, areas, space = 0 }: LayoutProps) => {
+export const Layout = ({
+  children,
+  areas,
+  columns,
+  rows,
+  space = 0,
+}: LayoutProps) => {
   return (
     <div
       className={cn('grid', gapSpace[space])}
-      style={{ gridTemplateAreas: parseGridAreas(areas) }}
+      style={{
+        gridTemplateAreas: parseGridAreas(areas),
+        gridTemplateColumns: parseTemplateValue(columns),
+        gridTemplateRows: parseTemplateValue(rows),
+      }}
     >
       {children}
     </div>
