f0c0bc18b0ad22984ad344e99222a7119c47ed36
Sebastian Sebald <sebastian.sebald@reservix.de>
2024-05-13 16:41:40 +0200

    refa[DSTSUP-73]: Improve elevation (more distinguisable) (#3889)
    * refa(DSTSUP-73): Improve elevation (more distinguisable)
    
    * Create swift-days-retire.md
    
    * update elevation badge
    
    * update image
    
    * delete elevation story

--- packages/components/src/Overlay/Overlay.stories.tsx
+++ packages/components/src/Overlay/Overlay.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { forwardRef } from 'react';
 import { Menu, MenuItem, MenuTrigger } from 'react-aria-components';
 
@@ -46,27 +46,25 @@ OverlayTray.parameters = {
 };
 
 export const OverlayModal: StoryObj<typeof Modal> = {
-  render: () => {
-    return (
-      <Modal open>
-        <Dialog closeButton aria-labelledby="my-cool-headline">
-          <Headline id="my-cool-headline">This is a headline!</Headline>
-          <Text>
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
-            eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
-            Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit
-            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
-            ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim
-            in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id
-            cursus faucibus, tortor neque egestas augue, eu vulputate magna eros
-            eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan
-            porttitor, facilisis luctus, metus.
-          </Text>
-        </Dialog>
-      </Modal>
-    );
-  },
+  render: () => (
+    <Modal open>
+      <Dialog closeButton aria-labelledby="my-cool-headline">
+        <Headline id="my-cool-headline">This is a headline!</Headline>
+        <Text>
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+          ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+          egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+          eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+          Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet,
+          wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum
+          rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in
+          turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus
+          faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.
+          Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor,
+          facilisis luctus, metus.
+        </Text>
+      </Dialog>
+    </Modal>
+  ),
 };
--- themes/theme-b2b/src/components/Dialog.styles.ts
+++ themes/theme-b2b/src/components/Dialog.styles.ts
@@ -1,8 +1,11 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
+import { ELEVALTION_RING } from '../mixins';
+
 export const Dialog: ThemeComponent<'Dialog'> = {
   closeButton: cva('relative -right-6 -top-2'),
-  container: cva(
-    'font-body bg-bg-surface-overlay shadow-surface-overlay rounded-sm px-8 pb-8 pt-4'
-  ),
+  container: cva([
+    'font-body bg-bg-surface-overlay shadow-surface-overlay rounded-sm px-8 pb-8 pt-4',
+    ELEVALTION_RING,
+  ]),
 };
