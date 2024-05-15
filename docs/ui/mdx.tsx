ed899b45e920ef0c344371e87b914e1979fe4019
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-05-15 08:57:02 +0200

    missed one...

--- docs/ui/mdx.tsx
+++ docs/ui/mdx.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useMDXComponent } from 'next-contentlayer/hooks';
+import { useMDXComponent } from 'next-contentlayer2/hooks';
 import { HTMLAttributes } from 'react';
 
 import Image from 'next/image';
