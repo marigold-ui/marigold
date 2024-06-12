8b39a3ebbca7b306974176aee69f0dd675f9e443
ssarahgm <sarahgosmann@gmail.com>
2024-06-12 15:14:48 +0200

    typecheck

--- packages/components/src/Input/Input.stories.tsx
+++ packages/components/src/Input/Input.stories.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Delete, Search } from '@marigold/icons';
 
 import { Button } from '../Button';
-import { FieldGroup } from '../FieldBase';
 import { Input } from './Input';
 
 const meta = {
