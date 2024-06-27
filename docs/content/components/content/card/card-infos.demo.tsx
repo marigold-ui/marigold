1c3a98fca749edd5f3502a27e1eb185c3f38d37c
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-06-27 12:56:01 +0300

    Adding do and  dont components

--- docs/content/components/content/card/card-infos.demo.tsx
+++ docs/content/components/content/card/card-infos.demo.tsx
@@ -1,25 +1,40 @@
 import { Card, Headline, Link, Stack, Text } from '@marigold/components';
 
+import { Do } from '@/ui/Do';
+import { Dont } from '@/ui/Dont';
+
 export default () => (
-  <Card p={4}>
-    <Stack space={5}>
-      <Headline level={3}>Professor Severus Snape</Headline>
-      <Text>
-        <strong>Professor Severus Snape</strong> (9 January, 1960[1] - 2 May,
-        1998)[2] was an English half-blood[3] wizard serving as Potions Master
-        (1981-1996), Head of Slytherin House (1981-1997), Defence Against the
-        Dark Arts professor (1996-1997), and Headmaster (1997-1998) of the
-        Hogwarts School of Witchcraft and Wizardry as well as a member of the
-        Order of the Phoenix and a Death Eater. His double life played an
-        extremely important role in both of the Wizarding Wars against
-        Voldemort.
-      </Text>
-      <Link
-        href="https://harrypotter.fandom.com/wiki/Severus_Snape"
-        target="blank"
-      >
-        Source
-      </Link>
-    </Stack>
-  </Card>
+  <div className="flex flex-col gap-4">
+    <Card p={4}>
+      <Stack space={5}>
+        <Headline level={3}>Professor Severus Snape</Headline>
+        <Text>
+          <strong>Professor Severus Snape</strong> (9 January, 1960[1] - 2 May,
+          1998)[2] was an English half-blood[3] wizard serving as Potions Master
+          (1981-1996), Head of Slytherin House (1981-1997), Defence Against the
+          Dark Arts professor (1996-1997), and Headmaster (1997-1998) of the
+          Hogwarts School of Witchcraft and Wizardry as well as a member of the
+          Order of the Phoenix and a Death Eater. His double life played an
+          extremely important role in both of the Wizarding Wars against
+          Voldemort.
+        </Text>
+        <Link
+          href="https://harrypotter.fandom.com/wiki/Severus_Snape"
+          target="blank"
+        >
+          Source
+        </Link>
+      </Stack>
+    </Card>
+    <div className="flex gap-4">
+      <Do
+        description="This is the best way to do ... "
+        img="/Elevation_Mockup_B2B.png"
+      />
+      <Dont
+        description="This is the best way to do ... "
+        img="/Elevation_Mockup_B2B.png"
+      />
+    </div>
+  </div>
 );
---
+++ docs/ui/Dont.tsx
@@ -0,0 +1,30 @@
+import { Text } from '@marigold/components';
+
+import { Image } from '@/ui/Image';
+
+interface Props {
+  img: string;
+  description: string;
+}
+
+export const Dont = ({ description, img }: Props) => {
+  return (
+    <div className="border-border flex flex-col border">
+      <Image width={700} height={900} src={img} alt="Don't" />
+      <div className="border-bg-error flex flex-col border-t-8 bg-red-50 p-3">
+        <div className="flex items-center gap-2">
+          <svg
+            width="10px"
+            height="10px"
+            viewBox="0 0 24 24"
+            className="bg-bg-error size-5 flex-none rounded-full fill-white p-1"
+          >
+            <path d="M19.8281 5.74868L18.2513 4.17188L12 10.4232L5.74868 4.17188L4.17188 5.74868L10.4232 12L4.17188 18.2513L5.74868 19.8281L12 13.5768L18.2513 19.8281L19.8281 18.2513L13.5768 12L19.8281 5.74868Z"></path>
+          </svg>
+          DO'NT
+        </div>
+        <Text>{description}</Text>
+      </div>
+    </div>
+  );
+};
