9de4c56f59d1cf86e24abeb9304aec38a830d59f
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-06-27 10:40:18 +0200

    fix types and break shit

--- packages/system/src/components/Formatters/NumericFormat.test.tsx
+++ packages/system/src/components/Formatters/NumericFormat.test.tsx
@@ -8,7 +8,7 @@ import { NumericFormat } from './NumericFormat';
 test('renders Currency', () => {
   render(
     <I18nProvider locale="en-US">
-      <NumericFormat styleFormat="currency" currency="USD" value={20} />
+      <NumericFormat style="currency" currency="USD" value={20} />
     </I18nProvider>
   );
   const currency = screen.getByText('$20.00');
@@ -18,7 +18,7 @@ test('renders Currency', () => {
 test('supports formatting currency in different language', () => {
   render(
     <I18nProvider locale="de-DE">
-      <NumericFormat styleFormat="currency" currency="EUR" value={30} />
+      <NumericFormat style="currency" currency="EUR" value={30} />
     </I18nProvider>
   );
   const currency = screen.getByText('30,00 €');
@@ -38,15 +38,13 @@ test('supports the notation style for formatting numbers', () => {
 });
 
 test('supports the unit of measurement to display alongside the number', () => {
-  render(
-    <NumericFormat value={10} styleFormat={'unit'} unit="mile-per-hour" />
-  );
+  render(<NumericFormat value={10} style="unit" unit="mile-per-hour" />);
   const number = screen.getByText('10 mph');
   expect(number).toBeInTheDocument();
 });
 
 test('renders value in percentage', () => {
-  render(<NumericFormat styleFormat="percent" value={0.2} />);
+  render(<NumericFormat style="percent" value={0.2} />);
   const percentage = screen.getByText('20%');
   expect(percentage).toBeInTheDocument();
 });
