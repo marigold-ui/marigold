d602025c0d1e2e8a0bc00220edd437370f475b34
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-11 15:30:06 +0200

    docs: adjust menu button height

--- themes/theme-docs/src/components/Button.styles.ts
+++ themes/theme-docs/src/components/Button.styles.ts
@@ -4,7 +4,7 @@ export const Button: ThemeComponent<'Button'> = cva('flex gap-2 rounded-sm', {
   variants: {
     variant: {
       ghost: 'text-secondary-700 hover:text-secondary-900 p-0',
-      menu: 'text-secondary-700 hover:bg-secondary-400/20 rounded-lg px-2 py-1',
+      menu: 'text-secondary-700 hover:bg-secondary-400/20 rounded-lg px-1 h-8',
       sunken:
         'text-secondary-600 hover:bg-secondary-400/20 bg-secondary-400/10 h-8 justify-start rounded-lg',
       inverted: 'bg-secondary-100',
