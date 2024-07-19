2e2f05e8407b1c42c611fd0e5a6fa3e673f2f162
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-19 16:58:29 +0200

    eeeeh

--- docs/ui/AppearanceDemo.tsx
+++ docs/ui/AppearanceDemo.tsx
@@ -45,8 +45,6 @@ export const AppearanceDemo = ({
     size: 'default',
   });
 
-  console.log(appearance);
-
   const Wrapper = ({ children }: { children: ReactNode }) =>
     current === 'core' && !disableLabelWidth ? (
       <FieldGroup labelWidth="100px">{children}</FieldGroup>
@@ -56,11 +54,12 @@ export const AppearanceDemo = ({
 
   return (
     <Card variant="content" p={0}>
-      <div className="absolute left-4 top-4">
+      <div className="absolute left-2 top-2 flex gap-2">
         <Select
           label="Variant"
           variant="floating"
           size="small"
+          width={36}
           selectedKey={selected.variant}
           onChange={(val: string) =>
             setSelected({ variant: val, size: selected.size })
@@ -73,6 +72,23 @@ export const AppearanceDemo = ({
             </Select.Option>
           ))}
         </Select>
+        <Select
+          label="Size"
+          variant="floating"
+          size="small"
+          width={28}
+          selectedKey={selected.size}
+          onChange={(val: string) =>
+            setSelected({ variant: selected.variant, size: val })
+          }
+        >
+          <Select.Option id="default">default</Select.Option>
+          {appearance.size.map(v => (
+            <Select.Option key={v} id={v}>
+              {v}
+            </Select.Option>
+          ))}
+        </Select>
       </div>
       <div
         data-theme={current}
@@ -80,7 +96,7 @@ export const AppearanceDemo = ({
       >
         <OverlayContainerProvider value="portalContainer">
           <MarigoldProvider theme={theme}>
-            <div className="not-prose size-full overflow-x-auto p-4">
+            <div className="not-prose size-full overflow-x-auto px-4 pb-4 pt-16">
               <Wrapper>
                 <Component />
               </Wrapper>
