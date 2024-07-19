32ecc22094f41a48784828c3555f34014728db95
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-19 23:29:06 +0200

    remove relative

--- themes/theme-docs/src/components/Field.styles.ts
+++ themes/theme-docs/src/components/Field.styles.ts
@@ -4,7 +4,7 @@ export const Field: ThemeComponent<'Field'> = cva('grid gap-y-0.5', {
   variants: {
     variant: {
       default: 'gap-y-0.5',
-      floating: 'relative grid-cols-1 grid-rows-[auto_auto]',
+      floating: 'grid-cols-1 grid-rows-[auto_auto]',
     },
   },
 });
