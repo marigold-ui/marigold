26f01e4cea0195861c2de7c0fd6ae388cb61abc6
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-22 14:49:24 +0200

    fix([DST-500]): fix popover placement

--- themes/theme-docs/src/components/Popover.styles.ts
+++ themes/theme-docs/src/components/Popover.styles.ts
@@ -1,12 +1,9 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
-export const Popover: ThemeComponent<'Popover'> = cva([''], {
-  variants: {
-    variant: {
-      top: ['mb-1'],
-      bottom: ['mt-1'],
-      right: [''],
-      left: [''],
-    },
-  },
-});
+export const Popover: ThemeComponent<'Popover'> = cva([
+  'animate-in',
+  'placement-t:-translate-y-1',
+  'placement-b:translate-y-1',
+  'placement-r:-translate-x-1',
+  'placement-l:translate-x-1',
+]);
