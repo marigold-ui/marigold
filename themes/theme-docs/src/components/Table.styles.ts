3eb19c6787d7623d0c680e7016da0e490e9dc7fd
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-07-23 10:06:33 +0200

    does this work?

--- themes/theme-docs/src/components/Table.styles.ts
+++ themes/theme-docs/src/components/Table.styles.ts
@@ -1,7 +1,7 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Table: ThemeComponent<'Table'> = {
-  table: cva('my-0 rounded border text-sm'),
+  table: cva('my-0 w-full rounded border text-sm'),
   cell: cva('text-text-primary px-2 py-3.5 text-xs', {
     variants: {
       variant: {
