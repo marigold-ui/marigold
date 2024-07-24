7133f4242fd60298611b2be6794b50c794f0bfde
ssarahgm <sarahgosmann@gmail.com>
2024-07-24 16:39:19 +0200

    update some demos save

--- docs/content/components/form/select/select-basic.demo.tsx
+++ docs/content/components/form/select/select-basic.demo.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
+
 import { Select } from '@marigold/components';
 
-export default () => (
-  <Select label="Favorite character" placeholder="Select your character">
-    <Select.Option>Mario</Select.Option>
-    <Select.Option>Luigi</Select.Option>
-    <Select.Option>Toad</Select.Option>
-    <Select.Option>Yoshi</Select.Option>
-    <Select.Option>Bowser</Select.Option>
-    <Select.Option>Peach</Select.Option>
-  </Select>
-);
+export default () => {
+  const [selected, setSelected] = useState<string | number>('');
+  return (
+    <>
+      <Select label="Genre" placeholder="Select genre" onChange={setSelected}>
+        <Select.Option id="pop">Pop</Select.Option>
+        <Select.Option id="hiphop">Hip Hop</Select.Option>
+        <Select.Option id="rock">Rock</Select.Option>
+        <Select.Option id="schlager">Schlager</Select.Option>
+        <Select.Option id="jazz">Jazz</Select.Option>
+        <Select.Option id="dance">Dance</Select.Option>
+      </Select>
+      <hr />
+      <pre>selected: {selected}</pre>
+    </>
+  );
+};
--- docs/content/components/form/select/select-section.demo.tsx
+++ docs/content/components/form/select/select-section.demo.tsx
@@ -1,16 +1,53 @@
 import { Header, Select } from '@marigold/components';
 
-export default () => (
-  <Select label="Choose your Fandom">
-    <Select.Section>
-      <Header>Fantasy</Header>
-      <Select.Option>Harry Potter</Select.Option>
-      <Select.Option>Lord of the Rings</Select.Option>
-    </Select.Section>
-    <Select.Section>
-      <Header>Sci-Fi</Header>
-      <Select.Option>Star Wars</Select.Option>
-      <Select.Option>Star Trek</Select.Option>
-    </Select.Section>
-  </Select>
-);
+export default () => {
+  const items = [
+    { category: 'Comedy', genres: ['Kabarett', 'Satire', 'Stand Up Comedy'] },
+    {
+      category: 'Classic',
+      genres: ['Chor', 'Kammermusik', 'Kantate', 'Klavierkonzert'],
+    },
+    {
+      category: 'Hardcore',
+      genres: [
+        'Hardcore Punk',
+        'Jazzcore',
+        'Mathcore',
+        'Melodic Hardcore',
+        'Metalcore',
+      ],
+    },
+    {
+      category: 'Metal',
+      genres: ['Black Metal', 'Death Metal', 'Heavy Metal', 'Nu Metal'],
+    },
+    {
+      category: 'Pop',
+      genres: [
+        'Brit-Pop',
+        'Chanson',
+        'Country-Pop',
+        'Dancefloor',
+        'Disco',
+        'New Wave',
+        'Pop-Klassik',
+        'Pop-Rock',
+        'Rave/Madchester',
+        'Synth Pop',
+      ],
+    },
+  ];
+
+  return (
+    <Select label="Genres" items={items}>
+      {items.map(item => (
+        <Select.Section key={item.category}>
+          <Header>{item.category}</Header>
+          {item.genres.map(genre => (
+            <Select.Option>{genre}</Select.Option>
+          ))}
+        </Select.Section>
+      ))}
+    </Select>
+  );
+};
