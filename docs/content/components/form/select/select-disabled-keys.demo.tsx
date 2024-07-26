f9ce842f280cc8a086921c488cb8c8ff5f5195d4
ssarahgm <sarahgosmann@gmail.com>
2024-07-26 08:36:07 +0200

    finish the demo

--- docs/content/components/form/select/select-disabled-keys.demo.tsx
+++ docs/content/components/form/select/select-disabled-keys.demo.tsx
@@ -1,26 +1,29 @@
 import { Select } from '@marigold/components';
 
 export default () => (
-  <Select label="Select your country" disabledKeys={['germany', 'denmark']}>
-    <Select.Option id="Albania">Albania</Select.Option>
-    <Select.Option id="Czech Republic">Czech Republic</Select.Option>
-    <Select.Option id="Denmark">Denmark</Select.Option>
-    <Select.Option id="Egypt">Egypt</Select.Option>
-    <Select.Option id="El Salvador">El Salvador</Select.Option>
-    <Select.Option id="Germany">Germany</Select.Option>
-    <Select.Option id="Hungary">Hungary</Select.Option>
-    <Select.Option id="Iceland">Iceland</Select.Option>
-    <Select.Option id="India">India</Select.Option>
-    <Select.Option id="Netherlands">Netherlands</Select.Option>
-    <Select.Option id="Nigeria">Nigeria</Select.Option>
-    <Select.Option id="Poland">Poland</Select.Option>
-    <Select.Option id="Portugal">Portugal</Select.Option>
-    <Select.Option id="Russia">Russia</Select.Option>
-    <Select.Option id="San Marino">San Marino</Select.Option>
-    <Select.Option id="Saudi Arabia">Saudi Arabia</Select.Option>
-    <Select.Option id="Sweden">Sweden</Select.Option>
-    <Select.Option id="Thailand">Thailand</Select.Option>
-    <Select.Option id="Ukraine">Ukraine</Select.Option>
-    <Select.Option id="United Kingdom">United Kingdom</Select.Option>
+  <Select
+    label="Select your country"
+    disabledKeys={['germany', 'denmark', 'elsalvador']}
+  >
+    <Select.Option id="albania">Albania</Select.Option>
+    <Select.Option id="czechrepublic">Czech Republic</Select.Option>
+    <Select.Option id="denmark">Denmark</Select.Option>
+    <Select.Option id="egypt">Egypt</Select.Option>
+    <Select.Option id="elsalvador">El Salvador</Select.Option>
+    <Select.Option id="germany">Germany</Select.Option>
+    <Select.Option id="hungary">Hungary</Select.Option>
+    <Select.Option id="iceland">Iceland</Select.Option>
+    <Select.Option id="india">India</Select.Option>
+    <Select.Option id="netherlands">Netherlands</Select.Option>
+    <Select.Option id="nigeria">Nigeria</Select.Option>
+    <Select.Option id="poland">Poland</Select.Option>
+    <Select.Option id="portugal">Portugal</Select.Option>
+    <Select.Option id="russia">Russia</Select.Option>
+    <Select.Option id="sanmarino">San Marino</Select.Option>
+    <Select.Option id="saudiarabia">Saudi Arabia</Select.Option>
+    <Select.Option id="sweden">Sweden</Select.Option>
+    <Select.Option id="thailand">Thailand</Select.Option>
+    <Select.Option id="ukraine">Ukraine</Select.Option>
+    <Select.Option id="unitedkingdom">United Kingdom</Select.Option>
   </Select>
 );
