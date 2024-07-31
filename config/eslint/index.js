426513c2f298a7a3177e1c3270d736be08fc69a0
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-07-31 16:46:37 +0300

    Adding comments back

--- config/eslint/index.js
+++ config/eslint/index.js
@@ -20,12 +20,14 @@ module.exports = {
     // Disabled cause it doesn't currently work as of 01/02/2021
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': 'off',
+    // Turned off because we use custom-classnames for our design tokens
     'tailwindcss/no-custom-classname': 'off',
     'tailwindcss/classnames-order': 'error',
   },
   parserOptions: {
     babelOptions: {
       parserOpts: {
+        // Allow imports like `import pkg from './package.json' assert { type: 'json' };`
         plugins: ['importAssertions'],
       },
     },
