1f9baab57813198b9c4709e2e63620795a39de4c
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-08-01 09:26:17 +0200

    feat: add `secondary` button to core theme and fix some token usage

--- themes/theme-core/src/components/Button.styles.ts
+++ themes/theme-core/src/components/Button.styles.ts
@@ -2,7 +2,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const Button: ThemeComponent<'Button'> = cva(
   [
-    'border-border-base bg-bg-inverted text-text-base ease-ease-out h-6 cursor-pointer rounded-sm border px-4 py-0 text-sm leading-[22px] transition-all duration-200 disabled:cursor-none',
+    'border-border-base bg-bg-inverted text-text-base ease-ease-out h-component cursor-pointer rounded-sm border px-4 py-0 text-sm leading-[22px] transition-all duration-200 disabled:cursor-none',
     'disabled:border-border-base-disabled disabled:bg-bg-inverted-disabled disabled:text-text-base-disabled disabled:cursor-not-allowed',
     'outline-outline-focus outline-2 outline-offset-1 focus-visible:outline',
     'hover:bg-bg-inverted-hover',
@@ -12,10 +12,14 @@ export const Button: ThemeComponent<'Button'> = cva(
       variant: {
         primary: [
           'border-border-brand bg-bg-brand text-text-inverted',
-          'hover:bg-bg-brand-hover hover:border-border-brand-hover ',
+          'hover:bg-bg-brand-hover hover:border-border-brand-hover',
+        ],
+        secondary: [
+          'text-text-link border-transparent bg-transparent',
+          'hover:bg-transparent hover:underline',
         ],
         link: [
-          'text-text-link-hover h-auto border-none bg-transparent px-0 text-[13px] leading-none',
+          'text-text-link h-auto border-none bg-transparent px-0 text-[13px] leading-none',
           'hover:bg-transparent hover:underline',
         ],
         text: [
