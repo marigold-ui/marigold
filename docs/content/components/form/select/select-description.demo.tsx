9fbec9b18a13a2eb821371347a50a92d2447f744
ssarahgm <sarahgosmann@gmail.com>
2024-08-02 10:25:16 +0200

    update content

--- docs/content/components/form/select/select-description.demo.tsx
+++ docs/content/components/form/select/select-description.demo.tsx
@@ -5,15 +5,21 @@ export default () => (
     <Select label="Permissions">
       <Select.Option id="read">
         <Text slot="label">Read</Text>
-        <Text slot="description">Read only</Text>
+        <Text slot="description" fontSize="xs">
+          Read only
+        </Text>
       </Select.Option>
       <Select.Option id="write">
         <Text slot="label">Write</Text>
-        <Text slot="description">Read and write only</Text>
+        <Text slot="description" fontSize="xs">
+          Read and write only
+        </Text>
       </Select.Option>
       <Select.Option id="admin">
         <Text slot="label">Admin</Text>
-        <Text slot="description">Full access</Text>
+        <Text slot="description" fontSize="xs">
+          Full access
+        </Text>
       </Select.Option>
     </Select>
   </>
