bbe5a0aa5f31c7259f7a6c2a7e58de21caf0c6d6
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-08-03 23:08:42 +0200

    updates!

---
+++ docs/content/components/form/button/button-icon.demo.tsx
@@ -0,0 +1,14 @@
+import { Button, Inline } from '@marigold/components';
+import { Edit } from '@marigold/icons';
+
+export default () => (
+  <Inline space={5} alignY="center" alignX="center">
+    <Button variant="primary">Edit</Button>
+    <Button variant="primary">
+      <Edit size={16} /> Edit
+    </Button>
+    <Button variant="icon" aria-label="Edit">
+      <Edit />
+    </Button>
+  </Inline>
+);
