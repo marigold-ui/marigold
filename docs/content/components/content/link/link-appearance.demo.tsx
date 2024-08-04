aecaf43f0d3e0daf3ab0a8ef12b0b010d0aad993
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-08-04 22:57:55 +0200

    docs: revise `Link` page

---
+++ docs/content/components/content/link/link-appearance.demo.tsx
@@ -0,0 +1,9 @@
+import { Link } from '@marigold/components';
+
+export default () => (
+  <div className="grid place-items-center">
+    <Link href="https://www.reservix.net/karriere/">
+      Find your job at Reservix
+    </Link>
+  </div>
+);
