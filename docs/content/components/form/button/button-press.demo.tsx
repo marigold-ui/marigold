e0a4db7ec1be2ea0f92be0c48eec6c5b553f3433
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-08-05 15:26:53 +0200

    add on press example adn related

--- docs/content/components/form/button/button-press.demo.tsx
+++ docs/content/components/form/button/button-press.demo.tsx
@@ -3,20 +3,24 @@ import { Button, Stack, Text } from '@marigold/components';
 
 export default () => {
   const [pointerType, setPointerType] = useState('');
+  const [count, setCount] = useState(0);
 
   return (
-    <Stack space={2} alignX="left">
+    <Stack space={3} alignX="left">
       <Button
         variant="secondary"
+        onPress={() => setCount(count + 1)}
         onPressStart={e => setPointerType(e.pointerType)}
         onPressEnd={() => setPointerType('')}
       >
         Press me
       </Button>
       <Text>
+        Number of times presse: {count} (
         {pointerType
           ? `Button is pressed via ${pointerType}.`
           : 'Button not pressed.'}
+        )
       </Text>
     </Stack>
   );
