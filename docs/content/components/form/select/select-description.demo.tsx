1dd5b240916a7929a481ab128047b3caa99831e1
ssarahgm <sarahgosmann@gmail.com>
2024-08-08 09:37:45 +0200

    accessability

--- docs/content/components/form/select/select-description.demo.tsx
+++ docs/content/components/form/select/select-description.demo.tsx
@@ -2,19 +2,19 @@ import { Select, Text } from '@marigold/components';
 
 export default () => (
   <Select label="Permissions" width="fit">
-    <Select.Option id="read">
+    <Select.Option id="read" textValue="Read">
       <Text slot="label">Read</Text>
       <Text slot="description" fontSize="xs">
         Read only
       </Text>
     </Select.Option>
-    <Select.Option id="write">
+    <Select.Option id="write" textValue="Write">
       <Text slot="label">Write</Text>
       <Text slot="description" fontSize="xs">
         Read and write only
       </Text>
     </Select.Option>
-    <Select.Option id="admin">
+    <Select.Option id="admin" textValue="Admin">
       <Text slot="label">Admin</Text>
       <Text slot="description" fontSize="xs">
         Full access
