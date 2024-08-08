3f8ea70b63294b6cc37e34b59e1f97b94ead96b6
ssarahgm <sarahgosmann@gmail.com>
2024-08-08 16:32:00 +0200

    updated demos

--- docs/content/components/layout/inline/inline-horizontally.demo.tsx
+++ docs/content/components/layout/inline/inline-horizontally.demo.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Center, Inline, Stack } from '@marigold/components';
 import { Rectangle } from '@/ui/Rectangle';
 
---
+++ docs/content/components/layout/inline/inline-vertically.demo.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Center, Inline, Stack } from '@marigold/components';
+import { Rectangle } from '@/ui/Rectangle';
+
+export default () => (
+  <Center>
+    <Stack space={8}>
+      <Inline space={3} alignY="top">
+        <Rectangle height="40px" width="40px" />
+        <Rectangle height="60px" width="60px">
+          <div className="text-text-primary-muted m-auto">Top</div>
+        </Rectangle>
+        <Rectangle height="40px" width="40px" />
+      </Inline>
+      <Inline space={3} alignY="center">
+        <Rectangle height="40px" width="40px" />
+        <Rectangle height="60px" width="60px">
+          <div className="text-text-primary-muted m-auto">Center</div>
+        </Rectangle>
+        <Rectangle height="40px" width="40px" />
+      </Inline>
+      <Inline space={3} alignY="bottom">
+        <Rectangle height="40px" width="40px" />
+        <Rectangle height="60px" width="60px">
+          <div className="text-text-primary-muted m-auto">Bottom</div>
+        </Rectangle>
+        <Rectangle height="40px" width="40px" />
+      </Inline>
+    </Stack>
+  </Center>
+);
