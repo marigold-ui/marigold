54125fe74e73e340412d70790edd576b2c8cb9ad
aromko <aromkoapps@gmail.com>
2024-08-12 13:12:20 +0200

    DST-506: edit some comments

--- docs/content/components/content/link/link-appearance.demo.tsx
+++ docs/content/components/content/link/link-appearance.demo.tsx
@@ -1,8 +1,8 @@
-import { Link } from '@marigold/components';
+import { Link, LinkProps } from '@marigold/components';
 
-export default () => (
+export default (props: LinkProps) => (
   <div className="grid place-items-center">
-    <Link href="https://www.reservix.net/karriere/">
+    <Link href="https://www.reservix.net/karriere/" {...props}>
       Find your dream job at Reservix
     </Link>
   </div>
