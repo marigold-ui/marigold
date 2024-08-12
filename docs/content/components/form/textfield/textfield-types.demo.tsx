3cd061f4cd74f941f0de2e605202bced7dcab7f2
aromko <aromkoapps@gmail.com>
2024-08-12 16:42:58 +0200

    DST-508: create component page for text field

--- docs/content/components/form/textfield/textfield-types.demo.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack, TextField } from '@marigold/components';
-
-export default () => (
-  <Stack space={2}>
-    <TextField label="EMail" type="email" />
-    <TextField label="Date" type="date" />
-    <TextField label="Phone Number" type="tel" />
-  </Stack>
-);
