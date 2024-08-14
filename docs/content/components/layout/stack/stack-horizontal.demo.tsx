7de880a52f5d98e3abf5c6cb60101fa385a449b8
ssarahgm <sarahgosmann@gmail.com>
2024-08-14 13:56:48 +0200

    upate

--- docs/content/components/layout/stack/stack-horizontal.demo.tsx
+++ docs/content/components/layout/stack/stack-horizontal.demo.tsx
@@ -2,7 +2,7 @@ import { Divider, Stack } from '@marigold/components';
 import { Rectangle } from '@/ui/Rectangle';
 
 export default () => (
-  <Stack space={4} alignX={undefined}>
+  <Stack space={4}>
     <Stack space={4} alignX="left">
       <Rectangle height="30px" width="50px">
         <div className="text-text-primary-muted m-auto">Left</div>
