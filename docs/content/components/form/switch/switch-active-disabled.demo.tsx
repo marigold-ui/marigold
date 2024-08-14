58cc437b3344d7e893aa5c5aa7c3702d98d5b4f9
aromko <aromkoapps@gmail.com>
2024-08-14 08:33:08 +0200

    DST-511: revise switch component page

--- docs/content/components/form/switch/switch-active-disabled.demo.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, Switch } from '@marigold/components';
-
-export default () => (
-  <Container>
-    <Switch defaultSelected disabled>
-      Disabled active Switch
-    </Switch>
-  </Container>
-);
