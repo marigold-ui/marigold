bc1421a6f1c633b3e59cc89784090616dcf579be
ssarahgm <sarahgosmann@gmail.com>
2024-08-14 16:12:59 +0200

    a more complex demo for nested stacks

--- docs/content/components/layout/stack/stack-nested.demo.tsx
+++ docs/content/components/layout/stack/stack-nested.demo.tsx
@@ -1,21 +1,29 @@
-import { Stack } from '@marigold/components';
+import { Inline, Split, Stack } from '@marigold/components';
 import { Rectangle } from '@/ui/Rectangle';
 
 export default () => (
-  <Stack space={12}>
-    <Stack space={4}>
-      <Rectangle height="50px">
-        <div className="text-text-primary-muted m-auto">First</div>
-      </Rectangle>
-      <Rectangle height="50px" />
-      <Rectangle height="50px" />
-    </Stack>
-    <Stack space={4}>
-      <Rectangle height="50px">
-        <div className="text-text-primary-muted m-auto">Second</div>
-      </Rectangle>
-      <Rectangle height="50px" />
-      <Rectangle height="50px" />
-    </Stack>
+  <Stack space={10}>
+    <div className="border-border bg-bg-body rounded-sm border-2 border-dashed p-2">
+      <Inline space={8}>
+        <Stack space={2} stretch>
+          <Rectangle height="30px" width="100%" />
+          <Rectangle height="30px" width="100%" />
+          <Rectangle height="30px" width="100%" />
+        </Stack>
+        <Stack space={2}>
+          <Rectangle height="30px" width="70px" />
+          <Rectangle height="30px" width="70px" />
+          <Rectangle height="30px" width="70px" />
+        </Stack>
+        <Split />
+        <Stack space={2}>
+          <Rectangle height="30px" width="70px" />
+          <Rectangle height="30px" width="70px" />
+          <Rectangle height="30px" width="70px" />
+        </Stack>
+      </Inline>
+    </div>
+    <Rectangle height="50px" />
+    <Rectangle height="50px" />
   </Stack>
 );
