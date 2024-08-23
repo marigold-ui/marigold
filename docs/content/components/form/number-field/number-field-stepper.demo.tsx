e98c651482c593f0149beeb83680b27e446f4384
aromko <aromkoapps@gmail.com>
2024-08-23 14:01:31 +0200

    DST-517: resolve comments

--- docs/content/components/form/number-field/number-field-stepper.demo.tsx
+++ docs/content/components/form/number-field/number-field-stepper.demo.tsx
@@ -1,4 +1,6 @@
 import {
+  FieldBase,
+  FieldGroup,
   Header,
   Headline,
   NumberField,
@@ -7,31 +9,21 @@ import {
 } from '@marigold/components';
 
 export default () => (
-  <Stack space={5} alignX="center">
+  <Stack space={5}>
     <Header>
       <Headline level={3}>Confirm Guests</Headline>
       <Text>Who’s going on your trip?</Text>
     </Header>
-    <NumberField
-      label="Adults"
-      defaultValue={3}
-      minValue={0}
-      maxValue={20}
-      width="fit"
-    />
-    <NumberField
-      label="Children"
-      defaultValue={0}
-      minValue={0}
-      maxValue={20}
-      width="fit"
-    />
-    <NumberField
-      label="Infants"
-      defaultValue={0}
-      minValue={0}
-      maxValue={20}
-      width="fit"
-    />
+    <FieldGroup labelWidth="50px">
+      <FieldBase label="Adults">
+        <NumberField defaultValue={3} minValue={0} maxValue={20} width="1/6" />
+      </FieldBase>
+      <FieldBase label="Children">
+        <NumberField defaultValue={0} minValue={0} maxValue={20} width="1/6" />
+      </FieldBase>
+      <FieldBase label="Infants">
+        <NumberField defaultValue={0} minValue={0} maxValue={20} width="1/6" />
+      </FieldBase>
+    </FieldGroup>
   </Stack>
 );
--- docs/content/components/form/number-field/number-field-values.demo.tsx
+++
@@ -1,5 +0,0 @@
-import { NumberField } from '@marigold/components';
-
-export default () => (
-  <NumberField label="Length" defaultValue={55} minValue={0} maxValue={100} />
-);
