0f108e9a4ac49b78c216efd8ad3c8339336e8de7
ssarahgm <sarahgosmann@gmail.com>
2024-08-28 10:08:08 +0200

    docs: remove cards around tables in docs

--- docs/ui/Token.tsx
+++ docs/ui/Token.tsx
@@ -105,67 +105,59 @@ export const Breakpoints = () => {
   const breaks = themes[current].screens || {};
 
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-label="breakpoints">
-          <Table.Header>
-            <Table.Column key={'name'}>Name</Table.Column>
-            <Table.Column key={'value'}>Breaks at</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.entries(breaks).map(([key, value]) => (
-              <Table.Row key={key}>
-                <Table.Cell>
-                  <code className="before:content-none after:content-none">
-                    {key}
-                  </code>
-                </Table.Cell>
-                <Table.Cell>{value}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+    <Table aria-label="breakpoints" stretch>
+      <Table.Header>
+        <Table.Column key={'name'}>Name</Table.Column>
+        <Table.Column key={'value'}>Breaks at</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.entries(breaks).map(([key, value]) => (
+          <Table.Row key={key}>
+            <Table.Cell>
+              <code className="before:content-none after:content-none">
+                {key}
+              </code>
+            </Table.Cell>
+            <Table.Cell>{value}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 export const Spacing = () => {
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-label="spaces" variant="noHover">
-          <Table.Header>
-            <Table.Column key={'name'}>Name</Table.Column>
-            <Table.Column key={'value'}>Value</Table.Column>
-            <Table.Column key={'example'}>Example</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.keys(paddingSpace)
-              .sort((a, b) => parseFloat(a) - parseFloat(b))
-              .map(key => (
-                <Table.Row key={key}>
-                  <Table.Cell>
-                    <code className="before:content-none after:content-none">
-                      {key}
-                    </code>
-                  </Table.Cell>
-                  <Table.Cell>{Number(key) * 4}px</Table.Cell>
-                  <Table.Cell>
-                    <div
-                      className={cn(
-                        `pl-${key}`,
-                        'bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)]'
-                      )}
-                    >
-                      <div className="h-3 bg-white"></div>
-                    </div>
-                  </Table.Cell>
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+    <Table aria-label="spaces" variant="noHover" stretch>
+      <Table.Header>
+        <Table.Column key={'name'}>Name</Table.Column>
+        <Table.Column key={'value'}>Value</Table.Column>
+        <Table.Column key={'example'}>Example</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.keys(paddingSpace)
+          .sort((a, b) => parseFloat(a) - parseFloat(b))
+          .map(key => (
+            <Table.Row key={key}>
+              <Table.Cell>
+                <code className="before:content-none after:content-none">
+                  {key}
+                </code>
+              </Table.Cell>
+              <Table.Cell>{Number(key) * 4}px</Table.Cell>
+              <Table.Cell>
+                <div
+                  className={cn(
+                    `pl-${key}`,
+                    'bg-gradient-to-r from-[hsl(29,_37%,_70%)] to-[hsl(29,_37%,_40%)]'
+                  )}
+                >
+                  <div className="h-3 bg-white"></div>
+                </div>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+      </Table.Body>
+    </Table>
   );
 };
--- docs/ui/Typography.tsx
+++ docs/ui/Typography.tsx
@@ -1,5 +1,4 @@
 import {
-  Card,
   Headline,
   MarigoldProvider,
   Table,
@@ -26,169 +25,147 @@ export const Headlines = () => {
   }
 
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-labelledby="typography table">
-          <Table.Header>
-            <Table.Column key={'level'}>Level</Table.Column>
-            <Table.Column key={'example'}>Example</Table.Column>
-            <Table.Column key={'size'}>Styles</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.entries(headline?.size).map(([level, value]) => (
-              <Table.Row key={level}>
-                <Table.Cell>{level}</Table.Cell>
-                <Table.Cell>
-                  <div>
-                    <MarigoldProvider
-                      theme={(current && themes[current]) as Theme}
-                    >
-                      <div className="align-center flex bg-white">
-                        <Headline size={level}>
-                          Discover the Beauty of Marigold
-                        </Headline>
-                      </div>
-                    </MarigoldProvider>
+    <Table aria-labelledby="typography table" stretch>
+      <Table.Header>
+        <Table.Column key={'level'}>Level</Table.Column>
+        <Table.Column key={'example'}>Example</Table.Column>
+        <Table.Column key={'size'}>Styles</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.entries(headline?.size).map(([level, value]) => (
+          <Table.Row key={level}>
+            <Table.Cell>{level}</Table.Cell>
+            <Table.Cell>
+              <div>
+                <MarigoldProvider theme={(current && themes[current]) as Theme}>
+                  <div className="align-center flex bg-white">
+                    <Headline size={level}>
+                      Discover the Beauty of Marigold
+                    </Headline>
                   </div>
-                </Table.Cell>
-                <Table.Cell>{value?.toString()}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+                </MarigoldProvider>
+              </div>
+            </Table.Cell>
+            <Table.Cell>{value?.toString()}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 export const FontSizes = () => {
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-labelledby="typography table">
-          <Table.Header>
-            <Table.Column key={'name'}>Name</Table.Column>
-            <Table.Column key={'value'}>Value</Table.Column>
-            <Table.Column key={'example'}>Example</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.entries(textSize).map(([key, value]) => (
-              <Table.Row key={key}>
-                <Table.Cell>
-                  <code className="before:content-none after:content-none">
-                    {key}
-                  </code>
-                </Table.Cell>
-                <Table.Cell>{value}</Table.Cell>
-                <Table.Cell>
-                  <Text fontSize={key as keyof typeof textSize}>
-                    Marigolds bloom with vibrant colors.
-                  </Text>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+    <Table aria-labelledby="typography table" stretch>
+      <Table.Header>
+        <Table.Column key={'name'}>Name</Table.Column>
+        <Table.Column key={'value'}>Value</Table.Column>
+        <Table.Column key={'example'}>Example</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.entries(textSize).map(([key, value]) => (
+          <Table.Row key={key}>
+            <Table.Cell>
+              <code className="before:content-none after:content-none">
+                {key}
+              </code>
+            </Table.Cell>
+            <Table.Cell>{value}</Table.Cell>
+            <Table.Cell>
+              <Text fontSize={key as keyof typeof textSize}>
+                Marigolds bloom with vibrant colors.
+              </Text>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 export const FontWeights = () => {
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-labelledby="typography table">
-          <Table.Header>
-            <Table.Column key={'name'}>Name</Table.Column>
-            <Table.Column key={'value'}>Value</Table.Column>
-            <Table.Column key={'example'}>Example</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.entries(fontWeight).map(([key, value]) => (
-              <Table.Row key={key}>
-                <Table.Cell>
-                  <code className="before:content-none after:content-none">
-                    {key}
-                  </code>
-                </Table.Cell>
-                <Table.Cell>{value}</Table.Cell>
-                <Table.Cell>
-                  <Text weight={key as keyof typeof fontWeight}>
-                    Marigolds bloom with vibrant colors.
-                  </Text>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+    <Table aria-labelledby="typography table" stretch>
+      <Table.Header>
+        <Table.Column key={'name'}>Name</Table.Column>
+        <Table.Column key={'value'}>Value</Table.Column>
+        <Table.Column key={'example'}>Example</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.entries(fontWeight).map(([key, value]) => (
+          <Table.Row key={key}>
+            <Table.Cell>
+              <code className="before:content-none after:content-none">
+                {key}
+              </code>
+            </Table.Cell>
+            <Table.Cell>{value}</Table.Cell>
+            <Table.Cell>
+              <Text weight={key as keyof typeof fontWeight}>
+                Marigolds bloom with vibrant colors.
+              </Text>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 export const FontStyle = () => {
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-labelledby="typography table">
-          <Table.Header>
-            <Table.Column key={'name'}>Name</Table.Column>
-            <Table.Column key={'value'}>Value</Table.Column>
-            <Table.Column key={'example'}>Example</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.entries(textStyle).map(([key, value]) => (
-              <Table.Row key={key}>
-                <Table.Cell>
-                  <code className="before:content-none after:content-none">
-                    {key}
-                  </code>
-                </Table.Cell>
-                <Table.Cell>{value}</Table.Cell>
-                <Table.Cell>
-                  <Text fontStyle={key as keyof typeof textStyle}>
-                    Marigolds bloom with vibrant colors.
-                  </Text>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+    <Table aria-labelledby="typography table" stretch>
+      <Table.Header>
+        <Table.Column key={'name'}>Name</Table.Column>
+        <Table.Column key={'value'}>Value</Table.Column>
+        <Table.Column key={'example'}>Example</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.entries(textStyle).map(([key, value]) => (
+          <Table.Row key={key}>
+            <Table.Cell>
+              <code className="before:content-none after:content-none">
+                {key}
+              </code>
+            </Table.Cell>
+            <Table.Cell>{value}</Table.Cell>
+            <Table.Cell>
+              <Text fontStyle={key as keyof typeof textStyle}>
+                Marigolds bloom with vibrant colors.
+              </Text>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
 
 export const TextAlign = () => {
   return (
-    <Card>
-      <div className="overflow-auto">
-        <Table aria-labelledby="typography table">
-          <Table.Header>
-            <Table.Column key={'name'}>Name</Table.Column>
-            <Table.Column key={'value'}>Value</Table.Column>
-            <Table.Column key={'example'}>Example</Table.Column>
-          </Table.Header>
-          <Table.Body>
-            {Object.entries(textAlign).map(([key, value]) => (
-              <Table.Row key={key}>
-                <Table.Cell>
-                  <code className="before:content-none after:content-none">
-                    {key}
-                  </code>
-                </Table.Cell>
-                <Table.Cell>{value}</Table.Cell>
-                <Table.Cell>
-                  <Text align={key as keyof typeof textAlign}>
-                    Marigolds bloom with vibrant colors.
-                  </Text>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </div>
-    </Card>
+    <Table aria-labelledby="typography table" stretch>
+      <Table.Header>
+        <Table.Column key={'name'}>Name</Table.Column>
+        <Table.Column key={'value'}>Value</Table.Column>
+        <Table.Column key={'example'}>Example</Table.Column>
+      </Table.Header>
+      <Table.Body>
+        {Object.entries(textAlign).map(([key, value]) => (
+          <Table.Row key={key}>
+            <Table.Cell>
+              <code className="before:content-none after:content-none">
+                {key}
+              </code>
+            </Table.Cell>
+            <Table.Cell>{value}</Table.Cell>
+            <Table.Cell>
+              <Text align={key as keyof typeof textAlign}>
+                Marigolds bloom with vibrant colors.
+              </Text>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 };
