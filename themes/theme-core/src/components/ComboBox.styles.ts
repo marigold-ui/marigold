62f1ebc844868383a6c0697f35fabcfc2422917a
ssarahgm <sarahgosmann@gmail.com>
2024-08-30 09:17:23 +0200

    fix[DSTSUP-86]: adjust select and combobox styles for core theme

--- themes/theme-core/src/components/ComboBox.styles.ts
+++ themes/theme-core/src/components/ComboBox.styles.ts
@@ -1,5 +1,5 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
-export const ComboBox: ThemeComponent<'ComboBox'> = cva(
-  'absolute right-2 size-4 border-none bg-transparent p-0 hover:bg-transparent'
-);
+export const ComboBox: ThemeComponent<'ComboBox'> = cva([
+  'absolute right-2 size-4 border-none bg-transparent disabled:bg-transparent hover:bg-transparent overflow-hidden',
+]);
