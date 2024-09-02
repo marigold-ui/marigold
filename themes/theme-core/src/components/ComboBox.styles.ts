475fa619a7d2157108f7cda7afb4fe9852ffb22c
ssarahgm <sarahgosmann@gmail.com>
2024-09-02 08:09:49 +0200

    add padding

--- themes/theme-core/src/components/ComboBox.styles.ts
+++ themes/theme-core/src/components/ComboBox.styles.ts
@@ -1,5 +1,5 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const ComboBox: ThemeComponent<'ComboBox'> = cva([
-  'absolute right-2 size-4 border-none bg-transparent disabled:bg-transparent hover:bg-transparent overflow-hidden',
+  'absolute right-2 size-4 border-none p-0 bg-transparent disabled:bg-transparent hover:bg-transparent overflow-hidden',
 ]);
