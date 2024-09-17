2b1d6d637aed24ac8fed883810ec6791e09516ce
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-09-17 15:05:12 +0200

    hero layout

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -2,21 +2,25 @@ import { Center, Inline, Link } from '@/ui';
 
 export const Page = () => (
   <main className="grid justify-center p-[--page-padding-md] xl:p-[--page-padding-xl]">
-    <div className="max-w-screen-lg py-20 text-center">
-      <h1 className="text-balance bg-gradient-to-r from-rose-400 to-orange-400 bg-clip-text text-8xl font-extrabold text-transparent">
+    <div className="grid max-w-screen-lg place-items-center pt-28 text-center">
+      <h1 className="text-balance text-8xl font-extrabold">
         Cultivate beautiful user interfaces
       </h1>
-      <p className="text-text-primary/60 text-balance py-14 text-xl font-light leading-relaxed">
+      <p className="text-text-primary/60 text-balance pb-24 pt-10 text-xl font-light leading-relaxed">
         Marigold is a design system for Reservix, providing components and tools
         that let product teams focus on core challenges while creating unified,
         accessible applications.
       </p>
       <Center>
-        <Inline space={4}>
-          <Link variant="primary" href="/introduction/getting-started">
+        <Inline space={6}>
+          <Link
+            variant="primary"
+            size="large"
+            href="/introduction/getting-started"
+          >
             Get started
           </Link>
-          <Link variant="secondary" href="/components">
+          <Link variant="secondary" size="large" href="/components">
             Component
           </Link>
         </Inline>
--- themes/theme-docs/src/components/Link.styles.ts
+++ themes/theme-docs/src/components/Link.styles.ts
@@ -1,21 +1,30 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
-export const Link: ThemeComponent<'Link'> = cva(
-  ['font-medium underline underline-offset-4'],
-  {
-    variants: {
-      variant: {
-        primary: ['bg-text-primary text-white no-underline'],
-        toc: [
-          'text-secondary-500 hover:text-secondary-800 text-xs no-underline',
-          'data-[active=true]:text-secondary-800 font-normal data-[active=true]:font-medium',
-        ],
-        cta: [
-          'h-component rounded-2xl bg-purple-700 px-5 py-1.5',
-          'text-sm font-semibold text-purple-100 no-underline',
-          'transition-all hover:bg-purple-600',
-        ],
-      },
+export const Link: ThemeComponent<'Link'> = cva(['font-medium'], {
+  variants: {
+    variant: {
+      default: 'underline underline-offset-4',
+      primary: 'bg-text-primary text-white',
+      secondary: 'border-text-primary border-2',
+      toc: [
+        'text-secondary-500 hover:text-secondary-800 text-xs',
+        'data-[active=true]:text-secondary-800 font-normal data-[active=true]:font-medium',
+      ],
+      cta: [
+        'h-component rounded-2xl bg-purple-700 px-5 py-1.5',
+        'text-sm font-semibold text-purple-100',
+        'transition-all hover:bg-purple-600',
+      ],
     },
-  }
-);
+    size: {
+      inline: '',
+      small: 'px-3 py-1.5 text-sm',
+      regular: 'px-3 py-2 rounded',
+      large: 'px-8 py-2.5 rounded',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'inline',
+  },
+});
