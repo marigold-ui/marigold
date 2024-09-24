7e15657a5cbe06469543556e99ea0fd6a86cbb09
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-09-24 16:51:47 +0300

    Adding updates

---
+++ docs/content/introduction/form-guideline-states.demo.tsx
@@ -0,0 +1,51 @@
+import {
+  Headline,
+  NumberField,
+  Select,
+  Stack,
+  Switch,
+  TextField,
+  Tiles,
+} from '@marigold/components';
+
+export default () => {
+  return (
+    <Tiles tilesWidth="300px" space={6}>
+      <Stack space={2}>
+        <Headline level={'5'}>Disabled State</Headline>
+        <TextField label="username" placeholder="enter user name" disabled />
+      </Stack>
+      <Stack space={2}>
+        <Headline level={'5'}>Error State</Headline>
+        <NumberField
+          label="Quantity"
+          error
+          errorMessage="Max number of available tickets is 3"
+          value={4}
+        />
+      </Stack>
+      <Stack space={2}>
+        <Headline level={'5'}>ReadOnly State</Headline>
+        <Switch readOnly>Settings Locked</Switch>
+      </Stack>
+
+      <Stack space={2}>
+        <Headline level={'5'}>Required State</Headline>
+        <Select
+          label="Genre"
+          placeholder="Select genre"
+          description="Select the genre you want."
+          width="fit"
+          required
+        >
+          <Select.Option id="pop">Pop</Select.Option>
+          <Select.Option id="hiphop">Hip Hop</Select.Option>
+          <Select.Option id="rock">Rock</Select.Option>
+          <Select.Option id="schlager">Schlager</Select.Option>
+          <Select.Option id="jazz">Jazz</Select.Option>
+          <Select.Option id="dance">Dance</Select.Option>
+        </Select>
+      </Stack>
+    </Tiles>
+  );
+};
