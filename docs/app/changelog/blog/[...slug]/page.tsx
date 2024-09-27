12c6ee09d971d92cc91deae55e82e083502ba990
ssarahgm <sarahgosmann@gmail.com>
2024-09-27 14:12:56 +0200

    save pages

--- docs/app/changelog/blog/[...slug]/page.tsx
+++ docs/app/changelog/blog/[...slug]/page.tsx
@@ -2,39 +2,40 @@
 
 import { allBlogs } from '@/.contentlayer/generated';
 import { Headline } from '@/ui';
-import { usePathname } from 'next/navigation';
+import { notFound, usePathname } from 'next/navigation';
 import { TocContainer } from '@/ui/Toc';
 import { Mdx } from '@/ui/mdx';
 
 const BlogPost = () => {
   const pathname = usePathname();
-  const currentPath = pathname.replace(/^\//, '');
-  const currentPost = allBlogs.find(post => post.slug === currentPath);
+  const currentPost = allBlogs.find(
+    post => post.slug === pathname.replace(/^\//, '')
+  );
+
+  if (!currentPost) {
+    notFound();
+  }
 
   return (
-    <>
-      {currentPost ? (
-        <article
-          key={currentPost.title}
-          className="grid grid-cols-1 gap-x-24 gap-y-14 min-[1400px]:grid-cols-[minmax(min-content,70ch)_1fr]"
-        >
-          <div className="col-span-full">
-            <Headline level={1}>{currentPost.title}</Headline>
-            <div className="text-secondary-400 pt-1">{currentPost.date}</div>
-          </div>
-          <div className="prose max-w-[70ch]">
-            <Mdx
-              className=""
-              title={currentPost.title}
-              code={currentPost.body.code}
-            />
-          </div>
-          <div className="col-start-2 hidden min-[1400px]:block">
-            <TocContainer />
-          </div>
-        </article>
-      ) : null}
-    </>
+    <article
+      key={currentPost.title}
+      className="grid grid-cols-1 gap-x-24 gap-y-14 min-[1400px]:grid-cols-[minmax(min-content,70ch)_1fr]"
+    >
+      <div className="col-span-full">
+        <Headline level={1}>{currentPost.title}</Headline>
+        <div className="text-secondary-400 pt-1">{currentPost.date}</div>
+      </div>
+      <div className="prose max-w-[70ch]">
+        <Mdx
+          className=""
+          title={currentPost.title}
+          code={currentPost.body.code}
+        />
+      </div>
+      <div className="col-start-2 hidden min-[1400px]:block">
+        <TocContainer />
+      </div>
+    </article>
   );
 };
 
