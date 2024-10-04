9ff555bdaabd739959b892d1b4559be6a5ec8924
Sebastian Sebald <sebastian.sebald@reservix.de>
2024-10-04 12:29:44 +0200

    docs([DST-576]): landing page (#4176)
    * restructure
    
    * save
    
    * hero layout
    
    * improve links
    
    * moar
    
    * update
    
    * save
    
    * animations!
    
    * progress
    
    * update
    
    * rename
    
    * done
    
    * next section done
    
    * last section
    
    * save
    
    * readd redirects
    
    * save
    
    * check
    
    * save
    
    * done?
    
    * increase headline
    
    * fix vercel
    
    * make that responsive
    
    * Create serious-tables-prove.md
    
    * whitespace
    
    * typos
    
    * fix lowered card bg
    
    * fix typos

--- themes/theme-docs/src/components/Card.styles.ts
+++ themes/theme-docs/src/components/Card.styles.ts
@@ -1,19 +1,26 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Card: ThemeComponent<'Card'> = cva(
-  ['border-secondary-300 relative w-full overflow-hidden rounded-xl border'],
+  ['relative w-full overflow-hidden rounded-xl'],
   {
     variants: {
       variant: {
-        default: 'bg-bg-surface p-6 shadow',
+        default: 'bg-bg-surface p-6 shadow border border-secondary-300',
         hovering:
-          'bg-bg-surface p-6 shadow transition-shadow hover:cursor-pointer hover:shadow-md',
-        content: 'bg-bg-surface my-6 shadow',
-        outline: 'bg-bg-surface/40 my-6',
+          'bg-bg-surface p-6 shadow transition-shadow hover:cursor-pointer hover:shadow-md border border-secondary-300',
+        content: 'bg-bg-surface my-6 shadow border border-secondary-300',
+        outline: 'bg-bg-surface/40 my-6 border border-secondary-300',
+        lowered: 'bg-bg-surface-lowered p-6',
+        image: '',
+      },
+      size: {
+        default: '',
+        full: 'size-full',
       },
     },
     defaultVariants: {
       variant: 'default',
+      size: 'default',
     },
   }
 );
