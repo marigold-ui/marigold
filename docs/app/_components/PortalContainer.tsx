4348bf16e90c327139c201afb082f26ae276e10d
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-10-05 06:56:57 +0200

    docs: Apply root styles to overlay portal so they are displayed correctly.

--- docs/app/_components/PortalContainer.tsx
+++ docs/app/_components/PortalContainer.tsx
@@ -3,11 +3,12 @@
 import { useThemeSwitch } from '@/ui/ThemeSwitch';
 
 export const PortalContaier = () => {
-  const { current } = useThemeSwitch();
+  const { current, themes } = useThemeSwitch();
+  const theme = themes[current];
 
   return (
     <div data-theme={current}>
-      <div id="portalContainer" className="not-prose" />
+      <div id="portalContainer" className={`not-prose ${theme.root?.()}`} />
     </div>
   );
 };
