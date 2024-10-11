0c868e1980a948361227caa362fd11f38d62655b
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-10-11 12:08:21 +0300

    Separate the state exmples

--- docs/content/getting-started/form-guideline-disabled-state.demo.tsx
+++ docs/content/getting-started/form-guideline-disabled-state.demo.tsx
@@ -1,9 +1,8 @@
-import { Headline, Stack, TextField } from '@marigold/components';
+import { Stack, TextField } from '@marigold/components';
 
 export default () => {
   return (
     <Stack space={2}>
-      <Headline level={'5'}>Disabled State</Headline>
       <TextField label="username" placeholder="enter user name" disabled />
     </Stack>
   );
--- docs/content/getting-started/form-guideline-error-state.demo.tsx
+++ docs/content/getting-started/form-guideline-error-state.demo.tsx
@@ -1,11 +1,12 @@
-import { Headline, NumberField, Stack } from '@marigold/components';
+import { NumberField } from '@marigold/components';
 
-<Stack space={2}>
-  <Headline level={'5'}>Error State</Headline>
-  <NumberField
-    label="Quantity"
-    error
-    errorMessage="Max number of available tickets is 3"
-    value={4}
-  />
-</Stack>;
+export default () => {
+  return (
+    <NumberField
+      label="Quantity"
+      error
+      errorMessage="Max number of available tickets is 3"
+      value={4}
+    />
+  );
+};
