a4ea7a54307eba7ed84a8166ce0e9f7859152552
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-10-11 13:24:41 +0200

    save

--- docs/content/components/form/checkbox/checkbox-indeterminate.demo.tsx
+++ docs/content/components/form/checkbox/checkbox-indeterminate.demo.tsx
@@ -1,7 +1,17 @@
 import { Checkbox } from '@marigold/components';
 
-export default () => (
-  <Checkbox defaultChecked indeterminate>
-    Select all
-  </Checkbox>
-);
+export default () => {
+  return (
+    <Checkbox.Group label="Favorite genres">
+      <Checkbox>All</Checkbox>
+      <Checkbox>Rock</Checkbox>
+      <Checkbox>Pop</Checkbox>
+      <Checkbox>Jazz</Checkbox>
+      <Checkbox>Hip-Hop</Checkbox>
+      <Checkbox>Classical</Checkbox>
+      <Checkbox>Country</Checkbox>
+      <Checkbox>Electronic</Checkbox>
+      <Checkbox>R&B</Checkbox>
+    </Checkbox.Group>
+  );
+};
