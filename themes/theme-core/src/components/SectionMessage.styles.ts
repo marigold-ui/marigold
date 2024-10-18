f6a132c4c1f95fc9ba23a2e57f7f86274a64cb87
sarahgm <38324334+sarahgm@users.noreply.github.com>
2024-10-18 14:37:50 +0200

    feat(components): add close button on `<SectionMessage>` (#4204)

--- themes/theme-core/src/components/SectionMessage.styles.ts
+++ themes/theme-core/src/components/SectionMessage.styles.ts
@@ -2,7 +2,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const SectionMessage: ThemeComponent<'SectionMessage'> = {
   container: cva(
-    'bg-bg-base border border-solid px-4 py-2 text-[13px] [grid-template-areas:"title""content"]',
+    'bg-bg-base border border-solid px-4 py-2 text-[13px] grid-cols-[auto,min-content] [grid-template-areas:"title_close""content_content"]',
     {
       variants: {
         variant: {
@@ -11,6 +11,9 @@ export const SectionMessage: ThemeComponent<'SectionMessage'> = {
           info: 'border-border-info',
           success: 'border-border-success',
         },
+      },
+      defaultVariants: {
+        variant: 'info',
       },
     }
   ),
@@ -23,6 +26,9 @@ export const SectionMessage: ThemeComponent<'SectionMessage'> = {
         success: 'text-text-success',
       },
     },
+    defaultVariants: {
+      variant: 'info',
+    },
   }),
   icon: cva('hidden'),
   content: cva('leading-4', {
@@ -33,6 +39,9 @@ export const SectionMessage: ThemeComponent<'SectionMessage'> = {
         info: 'text-text-info',
         success: 'text-text-success',
       },
+    },
+    defaultVariants: {
+      variant: 'info',
     },
   }),
 };
