68966b1fe0e1e8f7dbf8d0282e65a7da936b8e67
ssarahgm <sarahgosmann@gmail.com>
2024-10-25 15:42:45 +0200

    reduce code

--- packages/components/src/Text/Text.tsx
+++ packages/components/src/Text/Text.tsx
@@ -69,34 +69,12 @@ const _Text = ({
     size,
   });
 
-  const Component = as;
-
-  if (props.slot) {
-    return (
-      <Text
-        {...props}
-        elementType={as}
-        className={cn(
-          'text-[--color] outline-[--outline]',
-          classNames,
-          fontStyle && textStyle[fontStyle],
-          align && textAlign[align],
-          cursor && cursorStyle[cursor],
-          weight && fontWeight[weight],
-          fontSize && textSize[fontSize]
-        )}
-        style={createVar({
-          color: color && getColor(theme, color, color /* fallback */),
-        })}
-      >
-        {children}
-      </Text>
-    );
-  }
+  const Component = props.slot ? Text : as;
 
   return (
     <Component
       {...props}
+      elementType={as}
       className={cn(
         'text-[--color] outline-[--outline]',
         classNames,
