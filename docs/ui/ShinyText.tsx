c7f297c2d025cef6f1ef05f04adb8e5ae0e09790
ssarahgm <sarahgosmann@gmail.com>
2024-10-25 08:23:55 +0200

    remove blank lines

--- docs/ui/ShinyText.tsx
+++ docs/ui/ShinyText.tsx
@@ -23,13 +23,8 @@ export const AnimatedShinyText: FC<AnimatedShinyTextProps> = ({
       }
       className={cn(
         'mx-auto max-w-md text-neutral-600/70',
-
-        // Shine effect
         'animate-shiny-text bg-clip-text bg-no-repeat [background-position:0_0] [background-size:var(--shiny-width)_100%] [transition:background-position_1s_cubic-bezier(.6,.6,0,1)_infinite]',
-
-        // Shine gradient
         'bg-gradient-to-r from-transparent via-black/80 via-50% to-transparent',
-
         className
       )}
     >
