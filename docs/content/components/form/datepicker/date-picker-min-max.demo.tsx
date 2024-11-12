f6a7865ac94ae7590c54b575f8b145808d6471e8
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-11-12 14:11:54 +0200

    Reolsving comments

--- docs/content/components/form/datepicker/date-picker-min-max.demo.tsx
+++ docs/content/components/form/datepicker/date-picker-min-max.demo.tsx
@@ -3,7 +3,7 @@ import { DatePicker } from '@marigold/components';
 
 export default () => (
   <DatePicker
-    label="Select Event Date"
+    label="Event Date"
     description="Choose a date for your ticket purchase. Dates must be between June 5, 2025, and June 20, 2025."
     minValue={new CalendarDate(2025, 6, 5)}
     maxValue={new CalendarDate(2025, 6, 20)}
