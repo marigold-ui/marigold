deae0fd8a629a8cab25f0c732ae5d709921742fd
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-11-12 16:53:33 +0100

    fix demo

--- docs/content/components/overlay/dialog/dialog-title-only.demo.tsx
+++ docs/content/components/overlay/dialog/dialog-title-only.demo.tsx
@@ -1,13 +1,16 @@
 import { Button, Dialog } from '@marigold/components';
 
 export default () => (
-  <Dialog aria-label="delete event">
-    <Dialog.Title>Are you sure you want to delete this event?</Dialog.Title>
-    <Dialog.Actions>
-      <Button variant="secondary" slot="close">
-        Cancel
-      </Button>
-      <Button variant="primary">Delete</Button>
-    </Dialog.Actions>
-  </Dialog>
+  <Dialog.Trigger>
+    <Button variant="primary">Delete</Button>
+    <Dialog aria-label="delete event">
+      <Dialog.Title>Are you sure you want to delete this event?</Dialog.Title>
+      <Dialog.Actions>
+        <Button variant="secondary" slot="close">
+          Cancel
+        </Button>
+        <Button variant="primary">Delete</Button>
+      </Dialog.Actions>
+    </Dialog>
+  </Dialog.Trigger>
 );
