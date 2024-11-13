912cc48047a1b23567356fb784a03bebd9940845
ssarahgm <sarahgosmann@gmail.com>
2024-11-13 15:14:31 +0100

    update demo

--- docs/content/components/layout/columns/fit-width-columns.demo.tsx
+++ docs/content/components/layout/columns/fit-width-columns.demo.tsx
@@ -3,9 +3,12 @@ import { Rectangle } from '@/ui/Rectangle';
 
 export default () => (
   <Columns space={2} columns={[8, 'fit']} collapseAt="40em" stretch={false}>
-    <Rectangle height="100px" width="100%" />
-    <Rectangle height="100px" width="150px">
-      <div className="text-text-primary-muted p-2">Fitting column</div>
+    <Rectangle height="100%" width="100%" />
+    <Rectangle height="100%">
+      <div className="text-text-primary-muted p-2">
+        This is the column that takes the available width of the child, as you
+        can see now.
+      </div>
     </Rectangle>
   </Columns>
 );
