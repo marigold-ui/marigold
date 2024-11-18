fa3f7b83cb4904ec10a0b4a0837977c99b313fc3
ssarahgm <sarahgosmann@gmail.com>
2024-11-18 14:07:42 +0100

    adjust demo

--- docs/content/components/form/button/button-pending.demo.tsx
+++ docs/content/components/form/button/button-pending.demo.tsx
@@ -4,32 +4,37 @@ import { Button, Inline } from '@marigold/components';
 export default () => {
   const [loadSubmit, setLoadSubmit] = useState<boolean>(false);
   const [loadSave, setLoadSave] = useState<boolean>(false);
-  const handleSubmit = async () => {
-    setLoadSubmit(true);
-    try {
-      await new Promise<void>(resolve => setTimeout(resolve, 2000));
-    } finally {
-      setLoadSubmit(false);
-    }
-  };
 
-  const handleSave = async () => {
-    setLoadSave(true);
-    try {
-      await new Promise<void>(resolve => setTimeout(resolve, 6000));
-    } finally {
-      setLoadSave(false);
+  const handlePending = async (action: string) => {
+    if (action === 'save') {
+      setLoadSave(true);
+      try {
+        await new Promise<void>(resolve => setTimeout(resolve, 6000));
+      } finally {
+        setLoadSave(false);
+      }
+    } else {
+      setLoadSubmit(true);
+      try {
+        await new Promise<void>(resolve => setTimeout(resolve, 2000));
+      } finally {
+        setLoadSubmit(false);
+      }
     }
   };
 
   return (
     <Inline space={4}>
-      <Button variant="primary" onPress={() => handleSave()} pending={loadSave}>
+      <Button
+        variant="primary"
+        onPress={() => handlePending('save')}
+        pending={loadSave}
+      >
         {loadSave ? 'Saving' : 'Save'}
       </Button>
       <Button
         variant="primary"
-        onPress={() => handleSubmit()}
+        onPress={() => handlePending('submit')}
         pending={loadSubmit}
       >
         {!loadSubmit && 'Submit'}
