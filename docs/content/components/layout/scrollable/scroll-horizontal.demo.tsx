d20a3c3d2e543ba5bfa39c026dcb7d6779310377
aromko <aromkoapps@gmail.com>
2024-11-20 14:38:13 +0100

    DST-611: use rectangles in scrolling examples

--- docs/content/components/layout/scrollable/scroll-horizontal.demo.tsx
+++ docs/content/components/layout/scrollable/scroll-horizontal.demo.tsx
@@ -1,12 +1,11 @@
-import { Card, Scrollable } from '@marigold/components';
+import { Scrollable } from '@marigold/components';
+import { Rectangle } from '@/ui/Rectangle';
 
 export default () => (
   <Scrollable>
-    <div className="flex gap-2 p-2">
+    <div className="inline-flex gap-2 p-2">
       {Array.from({ length: 10 }).map((_, index) => (
-        <Card key={index}>
-          <div className="h-[100px] w-[200px]" />
-        </Card>
+        <Rectangle key={index} variant="primary" height="100px" width="200px" />
       ))}
     </div>
   </Scrollable>
