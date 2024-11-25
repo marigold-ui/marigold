37f729c86cb3a0a370198efe1214b998ba45f0d5
Sebastian Sebald <sebastian.sebald@reservix.de>
2024-11-25 12:14:25 +0100

    Update docs/content/components/layout/scrollable/scroll-vertical.demo.tsx

--- docs/content/components/layout/scrollable/scroll-vertical.demo.tsx
+++ docs/content/components/layout/scrollable/scroll-vertical.demo.tsx
@@ -5,7 +5,7 @@ export default () => (
   <Scrollable height="150px">
     <div className="flex flex-col items-center gap-2 p-2">
       {Array.from({ length: 10 }).map((_, index) => (
-        <Rectangle key={index} variant="primary" height="100px" width="200px" />
+        <Rectangle key={index} height="100px" width="200px" />
       ))}
     </div>
   </Scrollable>
