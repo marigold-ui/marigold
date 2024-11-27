50e64e77ccd19a626da909aa1e3707a03117a2b4
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-11-27 09:56:06 +0100

    set default aria label

--- packages/components/src/XLoader/BaseLoader.tsx
+++ packages/components/src/XLoader/BaseLoader.tsx
@@ -28,12 +28,18 @@ export const BaseLoader = ({
   variant,
   size,
   children,
+  'aria-label': ariaLabel,
   ...props
 }: LoaderProps) => {
   const className = useClassNames({ component: 'XLoader', variant, size });
 
   return (
-    <ProgressBar className={className.container} isIndeterminate {...props}>
+    <ProgressBar
+      className={className.container}
+      isIndeterminate
+      aria-label={ariaLabel || children ? ariaLabel : 'Loading...'}
+      {...props}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 150 150"
