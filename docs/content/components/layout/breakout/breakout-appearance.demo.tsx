d294bec72d079d26276ab5cffb2741fa0ad1f9a2
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-12-04 22:16:57 +0100

    breakout docs

--- docs/content/components/layout/breakout/breakout-appearance.demo.tsx
+++ docs/content/components/layout/breakout/breakout-appearance.demo.tsx
@@ -8,32 +8,25 @@ import {
 } from '@marigold/components';
 
 export default () => (
-  <Container align="center" space={2}>
-    <Headline level="2">
+  <Container align="center" alignItems="center" space={4}>
+    <Headline level="3">
       Welcome to The Giggle Grounds - Laughville's New Comedy Hub!
     </Headline>
     <Breakout alignX="right">
       <Aspect ratio="ultrawide">
-        <Image src="/venues/the-giggle-grounds.webp" alt="" fit="cover" />
+        <Image src="/venues/comedy-show.webp" alt="" />
       </Aspect>
     </Breakout>
     <Text>
       Laughville, get ready to laugh! Introducing The Giggle Grounds, an outdoor
       amphitheater for comedy under the stars. Located at 123 Main Street, this
       venue blends natural charm with live entertainment for a perfect night
-      out.
-    </Text>
-    <Text>
-      With a capacity of 500, The Giggle Grounds offers an intimate yet lively
-      experience, ideal for enjoying top comedians in an open-air setting.
-      Pricing ranges from $1000 to $5000, making it flexible for various events
-      and budgets.
-    </Text>
-    <Text>
-      Join us at The Giggle Grounds for an unforgettable comedy experience!
-    </Text>
-    <Text>
-      For bookings or more information, visit us at 123 Main Street, Laughville.
+      out. With a capacity of 500, The Giggle Grounds offers an intimate yet
+      lively experience, ideal for enjoying top comedians in an open-air
+      setting. Pricing ranges from $1000 to $5000, making it flexible for
+      various events and budgets. Join us at The Giggle Grounds for an
+      unforgettable comedy experience! For bookings or more information, visit
+      us at 123 Main Street, Laughville.
     </Text>
   </Container>
 );
