b9d6ccd15584b1aa35022e3344ee42036e26f505
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-12-04 09:00:53 +0100

    tests

--- packages/components/src/Container/Container.test.tsx
+++ packages/components/src/Container/Container.test.tsx
@@ -18,6 +18,18 @@ const theme: Theme = {
 };
 const { render } = setup({ theme });
 
+test('defines a content length for text and headlines', () => {
+  render(
+    <>
+      <Container data-testid="container" />
+    </>
+  );
+
+  const container = screen.getByTestId(/container/);
+  expect(container.style.getPropertyValue('--maxTextWidth')).toBeDefined();
+  expect(container.style.getPropertyValue('--maxHeadlineWidth')).toBeDefined();
+});
+
 test('limits with of <Text> and <Headline> children (via CSS var)', () => {
   render(
     <Container data-testid="container">
@@ -37,154 +49,95 @@ test('limits with of <Text> and <Headline> children (via CSS var)', () => {
   expect(text).toHaveClass('max-w-[--maxTextWidth]');
 });
 
-// test('supports default contentType content', () => {
-//   render(
-//     <Container data-testid="container">
-//       <p>Coding makes fun</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toMatchInlineSnapshot(`
-// <div
-//   class="grid grid-cols-[minmax(0,_var(--maxWidth))_1fr_1fr] [&>*]:col-[1] gap-0"
-//   data-testid="container"
-//   style="--maxWidth: 45ch;"
-// >
-//   <p>
-//     Coding makes fun
-//   </p>
-// </div>
-// `);
-// });
+test('supports different lengths for content', () => {
+  render(
+    <>
+      <Container data-testid="container-default" />
+      <Container data-testid="container-long" contentLength="long" />
+    </>
+  );
 
-// test('supports contentType header', () => {
-//   render(
-//     <Container data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toMatchInlineSnapshot(`
-// <div
-//   class="grid grid-cols-[minmax(0,_var(--maxWidth))_1fr_1fr] [&>*]:col-[1] gap-0"
-//   data-testid="container"
-//   style="--maxWidth: 25ch;"
-// >
-//   <p>
-//     sdf
-//   </p>
-// </div>
-// `);
-// });
+  const defaultContainer = screen.getByTestId(/container-default/);
+  const longContainer = screen.getByTestId(/container-long/);
 
-// test('supports short text length', () => {
-//   render(
-//     <Container contentLength="short" data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
+  expect(defaultContainer.style.getPropertyValue('--maxTextWidth')).not.toEqual(
+    longContainer.style.getPropertyValue('--maxTextWidth')
+  );
+  expect(
+    defaultContainer.style.getPropertyValue('--maxHeadlineWidth')
+  ).not.toEqual(longContainer.style.getPropertyValue('--maxHeadlineWidth'));
+});
 
-//   expect(container).toMatchInlineSnapshot(`
-// <div
-//   class="grid grid-cols-[minmax(0,_var(--maxWidth))_1fr_1fr] [&>*]:col-[1] gap-0"
-//   data-testid="container"
-//   style="--maxWidth: 20ch;"
-// >
-//   <p>
-//     sdf
-//   </p>
-// </div>
-// `);
-// });
+test('aligns children on left by default', () => {
+  render(
+    <Container data-testid="container">
+      <Text>some text</Text>
+    </Container>
+  );
 
-// test('supports long text length', () => {
-//   render(
-//     <Container contentLength="long" data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toMatchInlineSnapshot(`
-// <div
-//   class="grid grid-cols-[minmax(0,_var(--maxWidth))_1fr_1fr] [&>*]:col-[1] gap-0"
-//   data-testid="container"
-//   style="--maxWidth: 60ch;"
-// >
-//   <p>
-//     sdf
-//   </p>
-// </div>
-// `);
-// });
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveClass(`*:col-[1]`);
+});
 
-// test('supports default align container left', () => {
-//   render(
-//     <Container data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toHaveClass(`[&>*]:col-[1]`);
-// });
+test('allows to align children to the center', () => {
+  render(
+    <Container data-testid="container" align="center">
+      <Text>some text</Text>
+    </Container>
+  );
 
-// test('supports align container center', () => {
-//   render(
-//     <Container align="center" data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toHaveClass(`[&>*]:col-[2]`);
-// });
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveClass(`*:col-[2]`);
+});
 
-// test('supports align container right', () => {
-//   render(
-//     <Container align="right" data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toHaveClass(`[&>*]:col-[3]`);
-// });
+test('allows to align children to the right', () => {
+  render(
+    <Container data-testid="container" align="right">
+      <Text>some text</Text>
+    </Container>
+  );
 
-// test('supports default align items none', () => {
-//   render(
-//     <Container data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).not.toHaveClass(`place-items`);
-// });
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveClass(`*:col-[3]`);
+});
 
-// test('supports align items center', () => {
-//   render(
-//     <Container alignItems="center" data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toHaveClass(`place-items-center`);
-// });
+test('supports default align items none', () => {
+  render(
+    <Container data-testid="container">
+      <Text>some text</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).not.toHaveClass(`place-items`);
+});
 
-// test('supports align items right', () => {
-//   render(
-//     <Container alignItems="right" data-testid="container">
-//       <p>sdf</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toHaveClass(`place-items-end`);
-// });
+test('supports align items center', () => {
+  render(
+    <Container alignItems="center" data-testid="container">
+      <Text>some text</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveClass(`place-items-center`);
+});
 
-// test('accepts and uses spacing from theme', () => {
-//   render(
-//     <Container space={2} data-testid="container">
-//       <p>one</p>
-//       <p>two</p>
-//     </Container>
-//   );
-//   const container = screen.getByTestId(/container/);
-//   expect(container).toHaveClass(`gap-2`);
-// });
+test('supports align items right', () => {
+  render(
+    <Container alignItems="right" data-testid="container">
+      <Text>some text</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveClass(`place-items-end`);
+});
+
+test('accepts and uses spacing from theme', () => {
+  render(
+    <Container space={2} data-testid="container">
+      <Text>one</Text>
+      <Text>two</Text>
+    </Container>
+  );
+  const container = screen.getByTestId(/container/);
+  expect(container).toHaveClass(`gap-2`);
+});
