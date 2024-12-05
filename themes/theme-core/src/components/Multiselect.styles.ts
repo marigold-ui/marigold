24c4c1f7eb7a0327b27ab4178b3e118cd3d9ca98
OsamaAbdellateef <osama.abd9888@gmail.com>
2024-12-05 12:14:35 +0200

    Adding tag styles
    Co-authored-by: sarahgm <sarahgm@users.noreply.github.com>

--- themes/theme-core/src/components/Multiselect.styles.ts
+++ themes/theme-core/src/components/Multiselect.styles.ts
@@ -14,12 +14,20 @@ export const Multiselect: ThemeComponent<'Multiselect'> = {
     inputSpacing,
     inputDisabled,
     inputError,
-    'flex',
+    'flex items-center gap-1',
     // 'leading-[22px]',
     'has-[input[data-focused=true]]:outline-outline-focus -outline-offset-1 has-[input[data-focused=true]]:outline has-[input[data-focused=true]]:outline-2',
     // 'read-only:border-transparent read-only:bg-transparent',
   ]),
   input: cva('flex-1 data-focused:outline-none outline-none ring-0'),
   button: cva(''),
-  tag: cva(''),
+  tag: cva([
+    'border-border-base bg-bg-base flex items-center gap-1 rounded border ',
+    'data-[selected]:bg-bg-selected-input data-[selected]:text-text-inverted data-[selected]:border-border-selected',
+    'px-1.5 py-[2px] h-4',
+  ]),
+  listItems: cva('flex flex-wrap items-center gap-1'),
+  closeButton: cva(
+    'size-4 cursor-pointer border-none bg-transparent p-0 leading-normal outline-0'
+  ),
 };
