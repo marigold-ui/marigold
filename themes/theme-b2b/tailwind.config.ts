b8178ec2cf1707ead90965d94513ce8d661be720
aromko <aromkoapps@gmail.com>
2025-01-23 15:30:44 +0100

    DST-648: starting replacing config

--- themes/theme-b2b/tailwind.config.ts
+++ themes/theme-b2b/tailwind.config.ts
@@ -2,17 +2,18 @@ import type { Config } from 'tailwindcss';
 import { preset } from './src/preset';
 
 export default {
-  important: '[data-theme="b2b"]',
-  corePlugins: {
+  // important: '[data-theme="b2b"]',
+  /* corePlugins: {
     preflight: false,
   },
+@import "tailwindcss/theme" layer(theme);*/
   content: [
-    'src/root.ts',
-    'src/colors.ts',
-    'src/**/*.*.ts',
-    '../../packages/{components,system}/**/*.{tsx,ts}',
-    '!../../packages/{components,system}/node_modules/**/*.{tsx,ts}',
-    '!../../packages/{components,system}/**/*.{stories,test}.{tsx,ts}',
+    //'src/root.ts',
+    //'src/colors.ts', -> doesn't exist in b2b theme
+    //'src/**/*.*.ts',
+    //'../../packages/{components,system}/**/*.{tsx,ts}',
+    //'!../../packages/{components,system}/node_modules/**/*.{tsx,ts}',
+    //'!../../packages/{components,system}/**/*.{stories,test}.{tsx,ts}',
   ],
   safelist: [{ pattern: /(bg|text|border|shadow)-./ }],
   presets: [preset],
