bdef215b283263d254b5ca2603e465d081ae1329
aromko <aromkoapps@gmail.com>
2025-01-27 13:13:54 +0100

    DST-648: migrate theme-docs

--- themes/theme-docs/src/index.ts
+++ themes/theme-docs/src/index.ts
@@ -1,14 +1,10 @@
 import type { Theme } from '@marigold/system';
 import * as components from './components';
 import { root } from './root';
-import { screens } from './screens';
-import { colors } from './tokens';
 
 export const theme: Theme = {
   name: 'docs',
-  screens,
   root,
-  colors,
   components,
 };
 export default theme;
