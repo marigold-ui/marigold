2d07a67e6ada30a876d8ef6d47a806a1425fb5f8
aromko <aromkoapps@gmail.com>
2025-01-28 14:14:25 +0100

    DST-648: set correct data-theme, split css files, use postcss for building

--- themes/theme-b2b/postcss.config.js
+++ themes/theme-b2b/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
+    'postcss-prefix-selector': { prefix: '[data-theme="b2b"]' },
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
--- themes/theme-core/src/theme.ts
+++ themes/theme-core/src/theme.ts
@@ -2,8 +2,6 @@ import { Theme } from '@marigold/system';
 import * as components from './components';
 import { root } from './root';
 
-export const webFontUrl = [] as const;
-
 export const theme: Theme = {
   name: 'core',
   root,
