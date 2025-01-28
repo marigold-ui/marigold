b66192c4aa041cb02570a9de2fe9c203569231d0
aromko <aromkoapps@gmail.com>
2025-01-28 14:14:25 +0100

    DST-648: set correct data-theme, split css files, use postcss for building

--- themes/theme-core/postcss.config.js
+++ themes/theme-core/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
+    'postcss-prefix-selector': { prefix: '[data-theme="core"]' },
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
