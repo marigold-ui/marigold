aa14dd448da420e2f08f4186283d8ccf9819a7a7
ssarahgm <sarahgosmann@gmail.com>
2025-01-28 15:54:25 +0100

    update font-family in theme.css and remove webfonturl in b2b

--- themes/theme-b2b/src/theme.ts
+++ themes/theme-b2b/src/theme.ts
@@ -2,10 +2,6 @@ import { Theme } from '@marigold/system';
 import * as components from './components';
 import { root } from './root';
 
-export const webFontUrl = [
-  'https://fonts.bunny.net/css?family=inter:400,600,700',
-] as const;
-
 export const theme: Theme = {
   name: 'b2b',
   root,
