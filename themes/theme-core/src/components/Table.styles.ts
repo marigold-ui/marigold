55518e323c0c175d6343ee79c9f18031b5ba6ca6
ssarahgm <sarahgosmann@gmail.com>
2025-01-31 12:58:36 +0100

    fix styles of table component

--- themes/theme-core/src/components/Table.styles.ts
+++ themes/theme-core/src/components/Table.styles.ts
@@ -8,7 +8,7 @@ export const Table: ThemeComponent<'Table'> = {
       'border-x px-2 font-bold',
       'text-text-inverted bg-gray-400',
       'odd:bg-bg-accent',
-      'rac-focus-visible:outline outline-2 outline-outline-focus outline-offset-1',
+      'focus:outline-outline-focus outline-offset-1',
     ],
     {
       variants: {
@@ -26,8 +26,7 @@ export const Table: ThemeComponent<'Table'> = {
   row: cva(
     [
       'group-aria-[multiselectable]/table:[&>*:first-child]:w-12',
-      'selected:bg-bg-selected',
-      'rac-focus-visible:outline outline-2 outline-outline-focus outline-offset-1',
+      'focus:outline-2 outline-outline-focus outline-offset-1',
     ],
     {
       variants: {
@@ -41,7 +40,7 @@ export const Table: ThemeComponent<'Table'> = {
   cell: cva(
     [
       'text-text-base p-2',
-      'rac-focus-visible:outline outline-2 outline-outline-focus outline-offset-1',
+      'focus:outline-2 outline-outline-focus outline-offset-1',
     ],
     {
       variants: {
