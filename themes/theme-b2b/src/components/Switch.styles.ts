4ee3475c3abf6377967b019c9bf82e6523073253
ssarahgm <sarahgosmann@gmail.com>
2025-02-03 14:24:34 +0100

    fix disabled styles for switch

--- themes/theme-b2b/src/components/Switch.styles.ts
+++ themes/theme-b2b/src/components/Switch.styles.ts
@@ -5,7 +5,7 @@ export const Switch: ThemeComponent<'Switch'> = {
   track: cva(
     [
       'bg-bg-inverted/20 border-border-base border',
-      'group-disabled/switch:bg-bg-base-disabled',
+      'group-disabled/switch:group-selected/switch:bg-bg-base-disabled',
       'group-selected/switch:bg-bg-selected-input',
     ],
     {
