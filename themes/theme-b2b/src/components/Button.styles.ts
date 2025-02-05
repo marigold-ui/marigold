f3f2bbf931036a090db841140ce272ea0166bae6
ssarahgm <sarahgosmann@gmail.com>
2025-02-05 14:51:15 +0100

    fix loading state button style

--- themes/theme-b2b/src/components/Button.styles.ts
+++ themes/theme-b2b/src/components/Button.styles.ts
@@ -5,7 +5,7 @@ export const Button: ThemeComponent<'Button'> = cva(
     'rounded-xs border-none px-8 leading-[48px] h-component outline-hidden',
     'focus-visible:outline-outline-focus focus-visible:outline focus-visible:outline-offset-1',
     'disabled:text-text-base-disabled disabled:bg-bg-base-disabled disabled:cursor-not-allowed',
-    'rac-pending:text-text-base-disabled rac-pending:bg-bg-base-disabled rac-pending:cursor-not-allowed',
+    'pending:text-text-base-disabled pending:bg-bg-base-disabled pending:cursor-not-allowed',
   ],
   {
     variants: {
--- themes/theme-core/src/components/Button.styles.ts
+++ themes/theme-core/src/components/Button.styles.ts
@@ -4,7 +4,7 @@ export const Button: ThemeComponent<'Button'> = cva(
   [
     'border-border-base bg-bg-inverted text-text-base ease-ease-out h-component cursor-pointer rounded-xs border px-4 py-0 text-sm leading-[22px] transition-all duration-200 disabled:cursor-none',
     'disabled:border-border-base-disabled disabled:bg-bg-inverted-disabled disabled:text-text-base-disabled disabled:cursor-not-allowed',
-    'rac-pending:border-border-base-disabled rac-pending:bg-bg-inverted-disabled rac-pending:text-text-base-disabled rac-pending:cursor-not-allowed',
+    'pending:border-border-base-disabled pending:bg-bg-inverted-disabled pending:text-text-base-disabled pending:cursor-not-allowed',
     'focus-visible:outline-outline-focus focus-visible:outline focus-visible:outline-offset-1 outline-hidden',
     'hover:bg-bg-inverted-hover',
   ],
