cca824bc153d366252024391ac4028a5fc65eb93
OsamaAbdellateef <osama.abd9888@gmail.com>
2025-02-13 17:38:06 +0200

    Deleting root from theme

--- themes/theme-rui/src/theme.ts
+++ themes/theme-rui/src/theme.ts
@@ -1,9 +1,7 @@
 import { Theme } from '@marigold/system';
 import * as components from './components';
-import { root } from './root';
 
 export const theme: Theme = {
   name: 'rui',
-  root,
   components,
 };
