77860a089396aa615c429a2f3b10e2a3c245d157
Jim Wiedemann <jim.wiedemann@reservix.de>
2025-02-13 09:17:22 +0100

    resolved vercel comments

---
+++ docs/content/components/navigation/accordion/accordion-appearance.demo.tsx
@@ -0,0 +1,10 @@
+import { Accordion } from '@marigold/components';
+
+export default () => {
+  <Accordion>
+    <Accordion.Item>
+      <Accordion.Header title="this is" />
+      <Accordion.Content>how i look</Accordion.Content>
+    </Accordion.Item>
+  </Accordion>;
+};
