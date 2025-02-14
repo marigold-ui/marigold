7b593a50d489523bb3bc7c39aa7542202633ac35
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-02-14 16:42:05 +0100

    some basic styles

--- themes/theme-rui/src/components/Table.styles.ts
+++ themes/theme-rui/src/components/Table.styles.ts
@@ -1,17 +1,31 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Table: ThemeComponent<'Table'> = {
-  table: cva(''),
-  header: cva('', {
-    variants: {
-      variant: {},
-    },
-  }),
-  row: cva('', {
-    variants: {
-      variant: {},
-    },
-  }),
+  table: cva('text-sm'),
+  row: cva(
+    [
+      'border-b border-border transition-colors',
+      // does this selected think work?
+      'data-[state=selected]:bg-muted hover:bg-transparent',
+    ],
+    {
+      variants: {
+        variant: {},
+      },
+    }
+  ),
+  header: cva(
+    [
+      'h-12 px-3 text-left align-middle font-medium text-muted-foreground',
+      // does this checkbox think work?
+      '[&:has([role=checkbox])]:w-px [&:has([role=checkbox])]:pr-0',
+    ],
+    {
+      variants: {
+        variant: {},
+      },
+    }
+  ),
   cell: cva('', {
     variants: {
       variant: {},
