94903facca0fb7ec7cb381a0fbc66ab3336090a9
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-02-15 22:20:39 +0100

    always align right last row

--- themes/theme-rui/src/components/Table.styles.ts
+++ themes/theme-rui/src/components/Table.styles.ts
@@ -2,6 +2,20 @@ import { ThemeComponent, cva } from '@marigold/system';
 
 export const Table: ThemeComponent<'Table'> = {
   table: cva('text-sm'),
+  headerRow: cva('border-b border-border'),
+  header: cva(
+    [
+      'h-12 px-3 text-left align-middle font-medium text-muted-foreground last:text-right',
+      // does this checkbox think work?
+      '[&:has([role=checkbox])]:w-px [&:has([role=checkbox])]:pr-0',
+    ],
+    {
+      variants: {
+        variant: {},
+      },
+    }
+  ),
+  body: cva('[&_tr:last-child]:border-0'),
   row: cva(
     [
       'border-b border-border transition-colors',
@@ -14,22 +28,9 @@ export const Table: ThemeComponent<'Table'> = {
       },
     }
   ),
-  body: cva('[&_tr:last-child]:border-0'),
-  header: cva(
-    [
-      'h-12 px-3 text-left align-middle font-medium text-muted-foreground',
-      // does this checkbox think work?
-      '[&:has([role=checkbox])]:w-px [&:has([role=checkbox])]:pr-0',
-    ],
-    {
-      variants: {
-        variant: {},
-      },
-    }
-  ),
   cell: cva(
     [
-      'p-3 align-middle',
+      'p-3 align-middle last:text-right',
       // does this checkbox think work?
       '[&:has([role=checkbox])]:pr-0',
     ],
