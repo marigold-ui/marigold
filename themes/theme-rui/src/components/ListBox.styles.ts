bb24a3244678440abe5804243e17c7e389c4c924
ssarahgm <sarahgosmann@gmail.com>
2025-02-17 09:48:29 +0100

    add last styles for Select

--- themes/theme-rui/src/components/ListBox.styles.ts
+++ themes/theme-rui/src/components/ListBox.styles.ts
@@ -3,10 +3,10 @@ import { inputDisabled } from './Input.styles';
 
 export const ListBox: ThemeComponent<'ListBox'> = {
   container: cva(['overflow-hidden rounded-lg border border-input-border']),
-  list: cva(['space-y-1 bg-background p-1 text-sm ']),
+  list: cva(['space-y-1 bg-background p-1 text-sm outline-0']),
   option: cva([
-    'relative rounded-md px-2 py-1.5',
     inputDisabled,
+    'relative rounded-md px-2 py-1.5',
     'selected:bg-accent selected:text-accent-foreground',
     'focus:outline-2 focus:outline-ring/70',
   ]),
--- themes/theme-rui/src/components/Popover.styles.ts
+++ themes/theme-rui/src/components/Popover.styles.ts
@@ -1,14 +1,13 @@
 import { type ThemeComponent, cva } from '@marigold/system';
 
 export const Popover: ThemeComponent<'Popover'> = cva([
-  'z-50 overflow-y-auto overflow-x-hidden rounded-lg',
-  'border border-border outline-none',
+  'z-50 overflow-y-auto overflow-x-hidden rounded-lg outline-0',
 
   /** animate stuff missing */
 
   'bg-surface-overlay text-foreground shadow-lg shadow-black/5 ',
-  'placement-t:mb-0.5',
-  'placement-b:mt-0.5',
-  'placement-r:ml-0.5',
-  'placement-l:mr-0.5',
+  'placement-t:mb-1',
+  'placement-b:mt-1',
+  'placement-r:ml-1',
+  'placement-l:mr-1',
 ]);
