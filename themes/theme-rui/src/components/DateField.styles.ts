70a9db1d33a0ff6bd2d153344247d0b1d700445b
ssarahgm <sarahgosmann@gmail.com>
2025-02-17 08:51:13 +0100

    remove duplicated class name

--- themes/theme-rui/src/components/DateField.styles.ts
+++ themes/theme-rui/src/components/DateField.styles.ts
@@ -19,7 +19,7 @@ export const DateField: ThemeComponent<'DateField'> = {
   ]),
   segment: cva([
     inputDisabled,
-    'inline rounded p-0.5 text-foreground caret-transparent outline outline-0 data-[type=literal]:px-0 data-[focused]:data-[placeholder]:text-foreground data-[focused]:text-foreground  data-[type=literal]:text-placeholder ',
+    'inline rounded p-0.5 text-foreground caret-transparent outline-0 data-[type=literal]:px-0 data-[focused]:data-[placeholder]:text-foreground data-[focused]:text-foreground  data-[type=literal]:text-placeholder ',
     'data-[focused]:bg-accent',
     'data-[invalid]:data-[focused]:bg-destructive data-[invalid]:data-[focused]:data-[placeholder]:text-destructive-foreground data-[invalid]:data-[focused]:text-destructive-foreground data-[invalid]:data-[placeholder]:text-destructive data-[invalid]:text-destructive data-[placeholder]:text-placeholder',
   ]),
