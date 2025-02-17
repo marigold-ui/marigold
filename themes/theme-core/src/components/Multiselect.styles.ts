fe4e3e8779bb50fb84548cdad7cd49ee99e4ed54
OsamaAbdellateef <osama.abd9888@gmail.com>
2025-02-17 17:30:09 +0200

    resolving comments - adding styles for option in b2b theme

--- themes/theme-core/src/components/Multiselect.styles.ts
+++ themes/theme-core/src/components/Multiselect.styles.ts
@@ -35,7 +35,6 @@ export const MultiSelect: ThemeComponent<'MultiSelect'> = {
     '[&>*:first-child]:h-full [&>*:first-child]:flex [&>*:first-child]:items-center',
     'px-1.5 py-[2px] h-5',
     'border-border-base bg-bg-base flex items-center gap-1 rounded border ',
-    'data-[selected]:bg-bg-selected-input data-[selected]:text-text-inverted data-[selected]:border-border-selected',
   ]),
   closeButton: cva(
     'size-4 cursor-pointer border-none bg-transparent p-0 leading-normal outline-0'
