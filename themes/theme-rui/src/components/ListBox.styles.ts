178ea7f5a46cbfdbe68b8585af3dee8aad2d3ce5
ssarahgm <sarahgosmann@gmail.com>
2025-02-18 10:54:13 +0100

    add styles for sections

--- themes/theme-rui/src/components/ListBox.styles.ts
+++ themes/theme-rui/src/components/ListBox.styles.ts
@@ -6,10 +6,12 @@ export const ListBox: ThemeComponent<'ListBox'> = {
   list: cva(['space-y-1 bg-background p-1 text-sm outline-0']),
   option: cva([
     inputDisabled,
-    'relative rounded-md px-2 py-1.5',
+    'relative rounded-md px-2 py-1.5 text-sm text-foreground',
     'selected:bg-selected',
     'focus:outline-2 focus:outline-ring/70',
   ]),
-  section: cva(),
-  header: cva(),
+  section: cva(''),
+  header: cva(
+    '[&_header]:px-2 [&_header]:py-1.5 [&_header]:text-xs [&_header]:font-medium [&_header]:text-muted-foreground'
+  ),
 };
