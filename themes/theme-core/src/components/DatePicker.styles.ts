40e6fe1b157034c362a96f1847547aa7336e3748
ssarahgm <sarahgosmann@gmail.com>
2025-02-20 11:16:52 +0100

    improve structure of datepicker styles

--- themes/theme-core/src/components/DatePicker.styles.ts
+++ themes/theme-core/src/components/DatePicker.styles.ts
@@ -1,12 +1,9 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
-export const DatePicker: ThemeComponent<'DatePicker'> = {
-  container: cva(['-mr-2 h-full w-8']),
-  button: cva([
-    'text-text-body/80 relative  border-none bg-transparent px-0 data-disabled:hidden',
-    'flex h-full w-9 items-center justify-center',
-    'data-pressed:text-text-base/80 data-pressed:bg-transparent',
-    'data-focused:border-none data-focused:outline-hidden ',
-    'first:size-5',
-  ]),
-};
+export const DatePicker: ThemeComponent<'DatePicker'> = cva([
+  'text-text-body/80 relative  border-none bg-transparent px-0 data-disabled:hidden',
+  'flex h-full w-9 items-center justify-center',
+  'data-pressed:text-text-base/80 data-pressed:bg-transparent',
+  'data-focused:border-none data-focused:outline-hidden ',
+  'first:size-5',
+]);
---
+++ themes/theme-rui/src/components/DatePicker.styles.ts
@@ -0,0 +1,6 @@
+import { type ThemeComponent, cva } from '@marigold/system';
+
+export const DatePicker: ThemeComponent<'DatePicker'> = cva([
+  'relative',
+  'flex h-full items-center justify-center',
+]);
