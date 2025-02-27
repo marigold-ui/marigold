95e86ae3eb6554ab4067e7fd512acb04c9d6f5ce
ssarahgm <sarahgosmann@gmail.com>
2025-02-27 09:53:24 +0100

    WIP: save selected form story

--- packages/components/src/Form/Form.stories.tsx
+++ packages/components/src/Form/Form.stories.tsx
@@ -1,7 +1,15 @@
+import { CalendarDate } from '@internationalized/date';
 import type { Meta, StoryObj } from '@storybook/react';
+import { Autocomplete } from '../Autocomplete';
+import { Calendar } from '../Calendar';
 import { Checkbox } from '../Checkbox';
+import { Columns } from '../Columns';
+import { ComboBox } from '../ComboBox';
 import { DateField } from '../DateField';
+import { DatePicker } from '../DatePicker';
 import { FieldGroup } from '../FieldBase';
+import { Radio } from '../Radio';
+import { Scrollable } from '../Scrollable';
 import { Select } from '../Select';
 import { Stack } from '../Stack';
 import { TextField } from '../TextField';
@@ -36,6 +44,62 @@ export const Basic: Story = {
             </Stack>
           </Form>
         </div>
+      </FieldGroup>
+    );
+  },
+};
+
+export const Selected: Story = {
+  render: (args, ctx) => {
+    const isCore = ctx.globals.theme === 'core';
+
+    return (
+      <FieldGroup labelWidth={isCore ? '100px' : undefined}>
+        <Form {...args}>
+          <Columns space={5} columns={[1, 1, 1]}>
+            <Stack space={5}>
+              <Checkbox checked>Subscribe to Employee Newsletter</Checkbox>
+              <Radio.Group defaultValue="2" label="Radio Group">
+                <Radio value="1">Option 1</Radio>
+                <Radio value="2">Option 2</Radio>
+              </Radio.Group>
+              <Select
+                label="Department"
+                width={isCore ? 60 : 40}
+                selectedKey={'engineering'}
+              >
+                <Select.Option id="engineering">Engineering</Select.Option>
+                <Select.Option id="design">Design</Select.Option>
+                <Select.Option id="product">Product</Select.Option>
+              </Select>
+            </Stack>
+            <Stack space={5}>
+              <ComboBox
+                label="Department"
+                width={isCore ? 60 : 40}
+                selectedKey={'engineering'}
+              >
+                <ComboBox.Option id="engineering">Engineering</ComboBox.Option>
+                <ComboBox.Option id="design">Design</ComboBox.Option>
+                <ComboBox.Option id="product">Product</ComboBox.Option>
+              </ComboBox>
+              <Autocomplete
+                label="Department"
+                width={isCore ? 60 : 40}
+                selectedKey={'engineering'}
+              >
+                <Autocomplete.Option id="engineering">
+                  Engineering
+                </Autocomplete.Option>
+                <Autocomplete.Option id="design">Design</Autocomplete.Option>
+                <Autocomplete.Option id="product">Product</Autocomplete.Option>
+              </Autocomplete>
+            </Stack>
+            <Stack space={5}>
+              <Calendar defaultValue={new CalendarDate(2025, 8, 16)} />
+            </Stack>
+          </Columns>
+        </Form>
       </FieldGroup>
     );
   },
