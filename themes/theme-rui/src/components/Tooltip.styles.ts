da4e7b35a64de6ddd16c2da5b9cd207eac297eac
Jim Wiedemann <jim.wiedemann@reservix.de>
2025-03-12 15:24:53 +0100

    add tooltip dark as default

--- themes/theme-rui/src/components/Tooltip.styles.ts
+++ themes/theme-rui/src/components/Tooltip.styles.ts
@@ -1,24 +1,50 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Tooltip: ThemeComponent<'Tooltip'> = {
-  container: cva([
-    'text-secondary-foreground relative z-50 max-w-70 rounded-md border border-border px-3 py-1.5 text-sm ',
-    'placement-t:mb-2',
-    'placement-b:mt-2',
-    'placement-r:ml-2',
-    'placement-l:mr-2',
-  ]),
+  container: cva(
+    [
+      'text-brand-foreground bg-brand relative z-50 max-w-70 rounded-md border border-brand px-3 py-1.5 text-sm ',
+      'placement-t:mb-2',
+      'placement-b:mt-2',
+      'placement-r:ml-2',
+      'placement-l:mr-2',
+    ],
+    {
+      variants: {
+        variant: {
+          default: '',
+          white: 'text-secondary-foreground border-border bg-white',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+      },
+    }
+  ),
 
-  arrow: cva([
-    'fill-white  stroke-border',
+  arrow: cva(
+    [
+      'fill-brand  stroke-brand',
 
-    // right
-    'data-[placement=right]:[&_svg]:rotate-90',
+      // right
+      'data-[placement=right]:[&_svg]:rotate-90',
 
-    // left
-    'data-[placement=left]:[&_svg]:-rotate-90',
+      // left
+      'data-[placement=left]:[&_svg]:-rotate-90',
 
-    // bottom
-    'data-[placement=bottom]:[&_svg]:rotate-180',
-  ]),
+      // bottom
+      'data-[placement=bottom]:[&_svg]:rotate-180',
+    ],
+    {
+      variants: {
+        variant: {
+          default: '',
+          white: 'fill-white  stroke-border ',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+      },
+    }
+  ),
 };
