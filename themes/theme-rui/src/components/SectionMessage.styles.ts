4c74d176be727c033091c6511df4ce71ab55becc
ssarahgm <sarahgosmann@gmail.com>
2025-03-12 16:37:44 +0100

    start update section message

--- themes/theme-rui/src/components/SectionMessage.styles.ts
+++ themes/theme-rui/src/components/SectionMessage.styles.ts
@@ -3,18 +3,21 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const SectionMessage: ThemeComponent<'SectionMessage'> = {
   container: cva(
     [
-      'grid-cols-[min-content_auto_min-content] gap-3 [grid-template-areas:"icon_title_close""icon_content_content"]',
+      'grid-cols-[min-content_auto_min-content] gap-4 [grid-template-areas:"icon_title_close""icon_content_content"]',
       //'bg-bg-surface border-y-2 border-l-[16px] border-r-2 border-solid text-sm',
       //'items-center px-4 pb-4 pt-2',
-      'bg-background z-50 max-w-[400px] rounded-md border p-4 shadow-lg ',
+      'bg-background z-50 max-w-[400px] rounded-md border p-4',
     ],
     {
       variants: {
         variant: {
-          success: 'border-border',
-          warning: 'border-border',
-          info: 'border-border',
-          error: 'border-border',
+          success:
+            'border-success-muted-accent bg-success-muted text-success-muted-foreground',
+          warning:
+            'border-warning-muted-accent bg-warning-muted text-warning-muted-foreground',
+          info: 'border-info-muted-accent bg-info-muted text-info-muted-foreground',
+          error:
+            'border-destructive-muted-accent bg-destructive-muted text-destructive-muted-foreground',
         },
       },
       defaultVariants: {
@@ -23,7 +26,19 @@ export const SectionMessage: ThemeComponent<'SectionMessage'> = {
     }
   ),
   title: cva('text-sm font-medium leading-none'),
-  content: cva('text-muted-foreground text-sm'),
+  content: cva('text-muted-foreground text-sm', {
+    variants: {
+      variant: {
+        success: 'text-success-muted-foreground',
+        warning: 'text-warning-muted-foreground',
+        info: 'text-info-muted-foreground',
+        error: 'text-destructive-muted-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'info',
+    },
+  }),
   icon: cva('', {
     variants: {
       variant: {
