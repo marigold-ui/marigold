8d3307c83025a5546169fc4ec770aeb027e1ad89
OsamaAbdellateef <osama.abd9888@gmail.com>
2025-03-13 15:32:55 +0200

    Fixing tests for pagination and select

--- packages/components/src/Pagination/Pagination.test.tsx
+++ packages/components/src/Pagination/Pagination.test.tsx
@@ -100,6 +100,7 @@ describe('Pagination tests', () => {
       <button
         aria-label="Page next"
         class="border-border-base ease-ease-out h-component cursor-pointer rounded-xs border px-4 py-0 text-sm leading-[22px] transition-all duration-200 disabled:border-border-base-disabled disabled:bg-bg-inverted-disabled disabled:text-text-base-disabled disabled:cursor-not-allowed disabled:border disabled:border-solid outline-outline-focus outline-2 outline-offset-1 focus-visible:outline h-auto border-none bg-transparent flex h-8! w-8! items-center justify-center data-[selected=true]:border-0 data-[selected=true]:border-b-2 data-[selected=true]:border-solid data-[selected=true]:border-b-border-selected data-[selected=true]:bg-none data-[selected=true]:text-black data-[selected=true]:font-bold text-gray-700 hover:bg-gray-100"
+        tabindex="0"
         type="button"
       >
         <svg
