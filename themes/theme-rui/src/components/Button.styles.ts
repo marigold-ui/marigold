c10506c99bba0b1240009693115c17332ee610ce
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-17 16:12:02 +0100

    muh outline not working everywhere?

--- themes/theme-rui/src/components/Button.styles.ts
+++ themes/theme-rui/src/components/Button.styles.ts
@@ -3,9 +3,9 @@ import { cva } from '@marigold/system';
 
 export const Button: ThemeComponent<'Button'> = cva(
   [
-    'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors outline-hidden',
+    'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors',
     '[&_svg]:pointer-events-none [&_svg]:shrink-0',
-    'focus-visible:outline-offset-2 focus-visible:outline focus-visible:outline-ring/70',
+    'outline-none focus-visible:outline-offset-2 focus-visible:outline focus-visible:outline-ring/70',
     'h-button px-4 py-2',
     'disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground disabled:border-none disabled:cursor-not-allowed',
     'pending:text-disabled-foreground pending:bg-disabled pending:cursor-not-allowed pending:border-none',
