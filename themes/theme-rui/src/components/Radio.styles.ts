1c5554a508e58ab6033437eb1b512d3832367b85
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-19 11:17:34 +0100

    fix radio focus

--- themes/theme-rui/src/components/Radio.styles.ts
+++ themes/theme-rui/src/components/Radio.styles.ts
@@ -9,7 +9,7 @@ export const Radio: ThemeComponent<'Radio'> = {
   radio: cva([
     'aspect-square size-4 rounded-full',
     'border border-input shadow-sm shadow-black/5',
-    'outline-offset-2 group-[focus-visible]/radio:outline-2 group-[focus-visible]/radio:outline-ring/70',
+    'mixin-ring-focus-visible-radio',
     'group-disabled/radio:border-disabled',
     'group-selected/radio:border-brand group-selected/radio:bg-brand group-selected/radio:text-brand-foreground',
   ]),
