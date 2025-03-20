711df6d054adb415691818ad9eb8c3624eebed9d
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-20 15:33:01 +0100

    adjust popover, make placement work again

--- themes/theme-rui/src/components/Popover.styles.ts
+++ themes/theme-rui/src/components/Popover.styles.ts
@@ -2,14 +2,11 @@ import { type ThemeComponent, cva } from '@marigold/system';
 
 export const Popover: ThemeComponent<'Popover'> = cva([
   'group/popover',
-  'z-50 overflow-y-auto overflow-x-hidden rounded-lg outline-0',
-  'border border-border',
-
+  'z-50 overflow-y-auto overflow-x-hidden rounded-md outline-0 border-input',
   /** animate stuff missing */
-
-  'bg-surface-overlay text-foreground shadow-lg shadow-black/5 ',
-  'placement-t:mb-1',
-  'placement-b:mt-1',
-  'placement-r:ml-1',
-  'placement-l:mr-1',
+  'bg-surface-overlay text-foreground shadow-md',
+  'placement-top:mb-1',
+  'placement-bottom:mt-1',
+  'placement-right:ml-1',
+  'placement-left:mr-1',
 ]);
