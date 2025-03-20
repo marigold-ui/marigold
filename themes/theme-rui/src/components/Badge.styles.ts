96cddefa4b84b0488d8b3f3ee3ce2f065c2320e5
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-20 16:10:10 +0100

    badge make x padd appear same

--- themes/theme-rui/src/components/Badge.styles.ts
+++ themes/theme-rui/src/components/Badge.styles.ts
@@ -4,7 +4,7 @@ const circle = 'before:size-1.5 before:rounded-full';
 
 export const Badge: ThemeComponent<'Badge'> = cva(
   [
-    'inline-flex items-center justify-center rounded-full border px-1.5 text-xs font-medium leading-normal transition-colors',
+    'inline-flex items-center justify-center rounded-full border pl-1.5 pr-2 text-xs font-medium leading-normal transition-colors',
     'mixin-ring-focus-visible',
   ],
   {
