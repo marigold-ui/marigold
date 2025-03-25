caace08bbd138d9c231774e3148ecad557b72a8c
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-25 16:24:44 +0100

    fix section message, remove unnecessary div

--- packages/components/src/SectionMessage/SectionMessage.tsx
+++ packages/components/src/SectionMessage/SectionMessage.tsx
@@ -113,23 +113,21 @@ export const SectionMessage = ({
           {Icon && <Icon />}
         </div>
         {closeButton && (
-          <div className="h-6 w-6 text-center">
-            <button
-              {...buttonProps}
-              ref={buttonRef}
-              aria-label="close"
-              className={cn('[grid-area:close]', classNames.close)}
-              onClick={handleClose}
-            >
-              <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                />
-              </svg>
-            </button>
-          </div>
+          <button
+            {...buttonProps}
+            ref={buttonRef}
+            aria-label="close"
+            className={cn('[grid-area:close]', classNames.close)}
+            onClick={handleClose}
+          >
+            <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              />
+            </svg>
+          </button>
         )}
         {children}
       </div>
