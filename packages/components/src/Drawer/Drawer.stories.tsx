adc93d2b49dc4bc828b4b650b11e0c620f1ea477
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-29 06:58:46 +0100

    add a form story

--- packages/components/src/Drawer/Drawer.stories.tsx
+++ packages/components/src/Drawer/Drawer.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../Button';
+import { Checkbox } from '../Checkbox';
+import { Select } from '../Select';
+import { Slider } from '../Slider';
 import { Stack } from '../Stack';
 import { TextField } from '../TextField';
 import { Drawer, DrawerProps } from './Drawer';
@@ -15,57 +18,94 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-  render: args => {
-    return (
-      <Stack space={8} alignX="left">
-        <Drawer.Trigger>
-          <Button>Open Drawer</Button>
-          <Drawer {...args}>
-            <Drawer.Title>Drawer Title</Drawer.Title>
-            <Drawer.Content>
-              <p className="pb-4">
-                Once upon a time in the quirky world of web design, there lived
-                a lively Drawer Component named Daria. Unlike her staid
-                companions, she loved to make a grand entrance by sliding in
-                from the left, declaring, "Surprise! I've got filters and puns
-                galore!"
-              </p>
+  render: args => (
+    <Stack space={8} alignX="left">
+      <Drawer.Trigger>
+        <Button>Open Drawer</Button>
+        <Drawer {...args}>
+          <Drawer.Title>Drawer Title</Drawer.Title>
+          <Drawer.Content>
+            <p className="pb-4">
+              Once upon a time in the quirky world of web design, there lived a
+              lively Drawer Component named Daria. Unlike her staid companions,
+              she loved to make a grand entrance by sliding in from the left,
+              declaring, "Surprise! I've got filters and puns galore!"
+            </p>
 
-              <p className="pb-4">
-                Every time a user clicked, Daria would pop out with flair,
-                showing off her hidden treasures while her neighboring
-                components—Header the Bold and Footer the Wise—watched in amused
-                awe. They’d chuckle as Daria’s witty remarks filled the space
-                between content sections.
-              </p>
+            <p className="pb-4">
+              Every time a user clicked, Daria would pop out with flair, showing
+              off her hidden treasures while her neighboring components—Header
+              the Bold and Footer the Wise—watched in amused awe. They’d chuckle
+              as Daria’s witty remarks filled the space between content
+              sections.
+            </p>
 
-              <p className="pb-4">
-                One day, Daria got a bit too excited. In a burst of enthusiasm,
-                she slid out so fast that she disrupted the perfect grid layout!
-                The Main Content, usually so composed, was thrown into disarray,
-                muttering about lost pixels and wayward margins.
-              </p>
+            <p className="pb-4">
+              One day, Daria got a bit too excited. In a burst of enthusiasm,
+              she slid out so fast that she disrupted the perfect grid layout!
+              The Main Content, usually so composed, was thrown into disarray,
+              muttering about lost pixels and wayward margins.
+            </p>
 
-              <p className="pb-4">
-                But instead of sulking, Daria quipped, "Looks like I just broke
-                the grid—guess I'm too stylish to be confined!" The users,
-                entertained by her antics, cheered on as she restored order with
-                a graceful slide back, proving that even a mischievous drawer
-                could be the hero of responsive design.
-              </p>
-            </Drawer.Content>
-            <Drawer.Actions>
-              <Button slot="close">Close</Button>
-              <Button variant="primary">Save</Button>
-            </Drawer.Actions>
-          </Drawer>
-        </Drawer.Trigger>
-        <TextField
-          label="Name"
-          description="Can you interact with me when the drawer is open?"
-          width={80}
-        />
-      </Stack>
-    );
-  },
+            <p className="pb-4">
+              But instead of sulking, Daria quipped, "Looks like I just broke
+              the grid—guess I'm too stylish to be confined!" The users,
+              entertained by her antics, cheered on as she restored order with a
+              graceful slide back, proving that even a mischievous drawer could
+              be the hero of responsive design.
+            </p>
+          </Drawer.Content>
+          <Drawer.Actions>
+            <Button slot="close">Close</Button>
+            <Button variant="primary">Save</Button>
+          </Drawer.Actions>
+        </Drawer>
+      </Drawer.Trigger>
+      <TextField
+        label="Name"
+        description="Can you interact with me when the drawer is open?"
+        width={80}
+      />
+    </Stack>
+  ),
+};
+
+export const WithForms = {
+  render: args => (
+    <Drawer.Trigger>
+      <Button>Open Drawer</Button>
+      <Drawer {...args}>
+        <Drawer.Title>Drawer Title</Drawer.Title>
+        <Drawer.Content>
+          <Stack space={8}>
+            <Slider
+              {...args}
+              formatOptions={{ style: 'currency', currency: 'EUR' }}
+              minValue={10}
+              maxValue={140}
+              defaultValue={[30, 60]}
+              thumbLabels={['min', 'max']}
+            >
+              Price
+            </Slider>
+            <Select label="Category">
+              <Select.Option id="all">All</Select.Option>
+              <Select.Option id="classic">Classic</Select.Option>
+              <Select.Option id="rock">Rock</Select.Option>
+              <Select.Option id="pop">Pop</Select.Option>
+              <Select.Option id="jazz">Jazz</Select.Option>
+            </Select>
+            <Checkbox.Group label="Amenities">
+              <Checkbox value="fast-lane">Fast Lane</Checkbox>
+              <Checkbox value="parking">VIP Parking</Checkbox>
+            </Checkbox.Group>
+          </Stack>
+        </Drawer.Content>
+        <Drawer.Actions>
+          <Button slot="close">Close</Button>
+          <Button variant="primary">Save</Button>
+        </Drawer.Actions>
+      </Drawer>
+    </Drawer.Trigger>
+  ),
 };
