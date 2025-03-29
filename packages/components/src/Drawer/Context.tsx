0f3abf82c6dee8695b49b9f9556fc0d65a060f15
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-03-29 08:26:47 +0100

    fix props table generation

--- packages/components/src/Drawer/Context.tsx
+++ packages/components/src/Drawer/Context.tsx
@@ -1,10 +1,13 @@
 import { createContext, useContext } from 'react';
 
-export const DrawerContext = createContext<{
+export interface DrawerContextProps {
   variant?: string;
   size?: string;
-}>({
+}
+
+export const DrawerContext = createContext<DrawerContextProps>({
   variant: undefined,
   size: undefined,
 });
+
 export const useDrawerContext = () => useContext(DrawerContext);
