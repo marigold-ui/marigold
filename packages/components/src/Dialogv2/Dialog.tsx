8d44950f4312b2d6c14ced75672209f8b2c03998
OsamaAbdellateef <osama.abd9888@gmail.com>
2025-04-01 17:54:13 +0200

    edit close button

--- packages/components/src/Dialogv2/Dialog.tsx
+++ packages/components/src/Dialogv2/Dialog.tsx
@@ -54,24 +54,24 @@ interface CloseButtonProps {
 const CloseButton = ({ className }: CloseButtonProps) => {
   const ctx = useContext(OverlayTriggerStateContext);
   return (
-    <div className="absolute top-4 right-4 ml-4">
-      <button
-        className={cn(
-          'h-4 w-4 cursor-pointer border-none p-0 leading-normal outline-0',
-          className
-        )}
-        onClick={ctx?.close}
-        slot="dismiss-button"
-      >
-        <svg viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-          />
-        </svg>
-      </button>
-    </div>
+    // <div className="absolute top-4 right-4 ml-4">
+    <button
+      className={cn(
+        'h-4 w-4 cursor-pointer border-none p-0 leading-normal outline-0',
+        className
+      )}
+      onClick={ctx?.close}
+      slot="dismiss-button"
+    >
+      <svg viewBox="0 0 20 20" fill="currentColor">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+        />
+      </svg>
+    </button>
+    // </div>
   );
 };
 
