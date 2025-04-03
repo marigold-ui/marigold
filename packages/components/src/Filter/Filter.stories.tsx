6039c3b42611eaeb5ef2f091cf2045274200565c
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-04-03 16:06:58 +0200

    meh?

--- packages/components/src/Filter/Filter.stories.tsx
+++ packages/components/src/Filter/Filter.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Filter } from './Filter';
+import { Checkbox } from '../Checkbox';
+import { Select } from '../Select';
+import { Slider } from '../Slider';
+import { Filter, useFilters } from './Filter';
 
 const meta = {
   title: 'Components/Filter',
@@ -11,6 +14,44 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+// Display some resulst and use filter from filter context to filter them
+const Results = () => {
+  const filterCtx = useFilters();
+
+  return null;
+};
+
 export const Basic: Story = {
-  render: args => <Filter {...args}></Filter>,
+  render: args => (
+    <Filter.Provider {...args}>
+      <Filter.Button />
+      <Filter>
+        <Filter.Title>Aka Drawer Title</Filter.Title>
+        <Filter.Content>
+          <Slider
+            formatOptions={{ style: 'currency', currency: 'EUR' }}
+            minValue={10}
+            maxValue={140}
+            defaultValue={[30, 60]}
+            thumbLabels={['min', 'max']}
+          >
+            Price
+          </Slider>
+          <Select label="Category">
+            <Select.Option id="all">All</Select.Option>
+            <Select.Option id="classic">Classic</Select.Option>
+            <Select.Option id="rock">Rock</Select.Option>
+            <Select.Option id="pop">Pop</Select.Option>
+            <Select.Option id="jazz">Jazz</Select.Option>
+          </Select>
+          <Checkbox.Group label="Amenities">
+            <Checkbox value="fast-lane">Fast Lane</Checkbox>
+            <Checkbox value="parking">VIP Parking</Checkbox>
+          </Checkbox.Group>
+        </Filter.Content>
+      </Filter>
+      <Filter.List />
+      <Results />
+    </Filter.Provider>
+  ),
 };
--- packages/components/src/Filter/Filter.tsx
+++ packages/components/src/Filter/Filter.tsx
@@ -1,7 +1,22 @@
-import type { ReactNode } from 'react';
+import type { PropsWithChildren, ReactNode } from 'react';
+import { Button } from '../Button';
+
+export const useFilters = () => ({});
 
 export interface FilterProps {
   children?: ReactNode;
 }
 
 export const Filter = ({ children }: FilterProps) => <div>{children}</div>;
+
+// Provides the context to update filter
+Filter.Provider = ({ children }: PropsWithChildren) => <div>{children}</div>;
+
+// Display applied filter -> Tags + TagGroup
+Filter.List = ({ children }: PropsWithChildren) => <div>{children}</div>;
+
+// Basically the drawer parts?
+Filter.Title = ({ children }: PropsWithChildren) => <div>{children}</div>;
+Filter.Content = ({ children }: PropsWithChildren) => <div>{children}</div>;
+Filter.Actions = ({ children }: PropsWithChildren) => <div>{children}</div>;
+Filter.Button = () => <Button>Filter</Button>;
