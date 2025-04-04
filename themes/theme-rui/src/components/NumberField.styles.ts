25185ef275b5d6c3dff76ebaf6968f1b7ddb9de4
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-04-04 11:35:51 +0200

    fix focus styles number input

--- themes/theme-rui/src/components/NumberField.styles.ts
+++ themes/theme-rui/src/components/NumberField.styles.ts
@@ -1,10 +1,11 @@
 import { type ThemeComponent, cva } from '@marigold/system';
-import { inputInvalid } from './Input.styles';
+import { inputInvalid, inputReadOnly } from './Input.styles';
 
 export const NumberField: ThemeComponent<'NumberField'> = {
   group: cva([
     'mixin-ring-has-focus-visible',
     inputInvalid,
+    inputReadOnly,
     'relative inline-flex w-full -mb-0.5 items-center overflow-hidden whitespace-nowrap rounded-lg h-input grow text-center tabular-nums text-foreground',
     'border border-input text-sm shadow-sm shadow-black/5 transition-shadow',
     'data-invalid:data-[focus-within]:border-destructive data-invalid:data-[focus-within]:ring-destructive/20',
@@ -15,7 +16,8 @@ export const NumberField: ThemeComponent<'NumberField'> = {
     // Make sure focus ring is only around the component and not the stepper buttons
     'border-input! first-of-type:border-r! last-of-type:border-l!',
   ]),
-  input: cva(
-    'border-none shadow-none rounded-none outline-offset-0 text-center focus-visible:ring-0'
-  ),
+  input: cva([
+    'border-none shadow-none rounded-none text-center',
+    'disabled:text-disabled-foreground disabled:bg-disabled',
+  ]),
 };
