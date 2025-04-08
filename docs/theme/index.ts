ef477d7813d973ca8ad0334700eed4415db85ffc
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-04-08 11:25:11 +0200

    docs([DST-722]): Add RUI to the docs

--- docs/theme/index.ts
+++ docs/theme/index.ts
@@ -3,5 +3,6 @@
 import b2bTheme from '@marigold/theme-b2b';
 import coreTheme from '@marigold/theme-core';
 import theme from '@marigold/theme-docs';
+import ruiTheme from '@marigold/theme-rui';
 
-export { theme, b2bTheme, coreTheme };
+export { theme, b2bTheme, coreTheme, ruiTheme };
