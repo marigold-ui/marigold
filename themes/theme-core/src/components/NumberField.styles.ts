df9618764444fab9ccaed4ba742b7f150ace1316
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-04-10 13:38:20 +0200

    fix([DSTSUP-117]): Fix overlapping `<NumberField>`

--- themes/theme-core/src/components/NumberField.styles.ts
+++ themes/theme-core/src/components/NumberField.styles.ts
@@ -1,8 +1,28 @@
 import { ThemeComponent, cva } from '@marigold/system';
-import { Input } from './Input.styles';
+import {
+  inputBox,
+  inputDisabled,
+  inputError,
+  inputHeight,
+  inputSpacing,
+} from './Input.styles';
 
 export const NumberField: ThemeComponent<'NumberField'> = {
   group: cva('h-component'),
   stepper: cva('rac-disabled:bg-none rac-disabled:text-text-base-disabled'),
-  input: Input.input,
+  input: cva([
+    inputBox,
+    inputHeight,
+    inputSpacing,
+    inputDisabled,
+    inputError,
+    'min-w-0 w-full',
+    'leading-[22px]',
+    'focus:outline-outline-focus -outline-offset-1 focus:outline',
+    'read-only:border-transparent read-only:bg-transparent',
+    // Extra padding for when an icon/action is present
+    'group-[icon]/input:pl-5',
+    'group-[action]/input:pr-8',
+    'placeholder:text-text-inverted-disabled',
+  ]),
 };
