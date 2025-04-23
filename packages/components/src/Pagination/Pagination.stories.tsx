707c1ec3bb67e8bc54b4b613123608c917ee0983
ssarahgm <sarahgosmann@gmail.com>
2025-04-23 14:35:55 +0200

    typecheck

--- packages/components/src/Pagination/Pagination.stories.tsx
+++ packages/components/src/Pagination/Pagination.stories.tsx
@@ -1,6 +1,6 @@
 import { useState } from '@storybook/preview-api';
 import type { Meta, StoryObj } from '@storybook/react';
-import { expect, fireEvent, userEvent, waitFor, within } from '@storybook/test';
+import { expect, userEvent, within } from '@storybook/test';
 import React from 'react';
 import {
   FieldGroup,
