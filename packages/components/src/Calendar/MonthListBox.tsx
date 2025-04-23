512d8f0806ce918cb4344be76852026dd424e384
OsamaAbdellateef <osama.abd9888@gmail.com>
2025-04-23 13:17:18 +0200

    resolve comments -> Adding changeset

--- packages/components/src/Calendar/MonthListBox.tsx
+++ packages/components/src/Calendar/MonthListBox.tsx
@@ -1,11 +1,37 @@
 import { Dispatch, Key, SetStateAction, useContext } from 'react';
-import { CalendarStateContext } from 'react-aria-components';
+import {
+  CalendarStateContext,
+  ListBox,
+  ListBoxItem,
+} from 'react-aria-components';
+import { FocusScope, useFocusManager } from '@react-aria/focus';
 import { Button } from '../Button';
 import { useFormattedMonths } from './useFormattedMonths';
 
 interface MonthDropdownProps {
   setSelectedDropdown: Dispatch<SetStateAction<string | undefined>>;
 }
+
+const ButtonComponent = props => {
+  const focusManager = useFocusManager();
+
+  let onKeyDown = e => {
+    switch (e.key) {
+      case 'ArrowRight':
+        focusManager?.focusFirst({ wrap: true });
+        break;
+      case 'ArrowLeft':
+        focusManager.focusPrevious({ wrap: true });
+        break;
+    }
+  };
+
+  return (
+    <Button {...props} onKeyDown={onKeyDown}>
+      {props.children}
+    </Button>
+  );
+};
 
 const MonthListBox = ({ setSelectedDropdown }: MonthDropdownProps) => {
   const state = useContext(CalendarStateContext)!;
@@ -18,31 +44,33 @@ const MonthListBox = ({ setSelectedDropdown }: MonthDropdownProps) => {
   };
 
   return (
-    <ul
+    <ListBox
       data-testid="monthOptions"
       className="grid h-full max-h-[300px] min-w-[300px] grid-cols-3 gap-y-10 overflow-y-scroll p-2"
     >
-      {months.map((month, index) => {
-        return (
-          <li className="flex justify-center" key={index}>
-            <Button
-              slot="previous"
-              variant={
-                index === state.focusedDate.month - 1 ? 'secondary' : 'text'
-              }
-              size="small"
-              onPress={() => {
-                onChange(index);
-                setSelectedDropdown(undefined);
-              }}
-              key={index + 1}
-            >
-              {month.substring(0, 3)}
-            </Button>
-          </li>
-        );
-      })}
-    </ul>
+      <FocusScope autoFocus restoreFocus contain>
+        {months.map((month, index) => {
+          return (
+            <ListBoxItem className="flex justify-center" key={index}>
+              <ButtonComponent
+                slot="previous"
+                variant={
+                  index === state.focusedDate.month - 1 ? 'secondary' : 'text'
+                }
+                size="small"
+                onPress={() => {
+                  onChange(index);
+                  setSelectedDropdown(undefined);
+                }}
+                key={index + 1}
+              >
+                {month.substring(0, 3)}
+              </ButtonComponent>
+            </ListBoxItem>
+          );
+        })}
+      </FocusScope>
+    </ListBox>
   );
 };
 
