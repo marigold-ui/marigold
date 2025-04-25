d1033fd9913e332bf14e52debeb8d7045499866a
aromko <aromkoapps@gmail.com>
2025-04-25 08:39:06 +0200

    [DST-775]: add automated a11y test setup

--- config/storybook/.storybook/test-runner.ts
+++ config/storybook/.storybook/test-runner.ts
@@ -1,6 +1,5 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
-import { getStoryContext } from '@storybook/test-runner';
-import { checkA11y, configureAxe, injectAxe } from 'axe-playwright';
+import { checkA11y, injectAxe } from 'axe-playwright';
 
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
@@ -10,22 +9,8 @@ const config: TestRunnerConfig = {
   async preVisit(page) {
     await injectAxe(page);
   },
-  async postVisit(page, context) {
-    // Get the entire context of a story, including parameters, args, argTypes, etc.
-    const storyContext = await getStoryContext(page, context);
-
-    // Apply story-level a11y rules
-    await configureAxe(page, {
-      rules: storyContext.parameters?.a11y?.config?.rules,
-    });
-
-    const element = storyContext.parameters?.a11y?.element ?? 'body';
-    await checkA11y(page, element, {
-      detailedReport: true,
-      detailedReportOptions: {
-        html: true,
-      },
-    });
+  async postVisit(page) {
+    await checkA11y(page, '#storybook-root', {}, true, 'html');
   },
 };
 
