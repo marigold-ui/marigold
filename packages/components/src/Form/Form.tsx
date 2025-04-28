dd2d9eb95b5a291ea51f817a1caae5b51d2bbbec
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-04-28 16:28:22 +0200

    feat([DST-799]): Add `unstyled` and `maxWidth` to `<Form>`

--- packages/components/src/Form/Form.tsx
+++ packages/components/src/Form/Form.tsx
@@ -1,2 +1,23 @@
-export { Form } from 'react-aria-components';
-export type { FormProps } from 'react-aria-components';
+import { Form } from 'react-aria-components';
+import type RAC from 'react-aria-components';
+import { cn, maxWidth as twMaxWidth } from '@marigold/system';
+import type { MaxWidthProp } from '@marigold/system';
+
+export interface FormProps
+  extends Omit<RAC.FormProps, 'className' | 'style'>,
+    MaxWidthProp {
+  /**
+   * Removes the form's visual container so that it does not impact the layout,
+   * letting child elements render naturally.
+   */
+  unstyled?: boolean;
+}
+
+const _Form = ({ unstyled, maxWidth = 'full', ...props }: FormProps) => (
+  <Form
+    {...props}
+    className={cn(twMaxWidth[maxWidth], unstyled && 'contents')}
+  />
+);
+
+export { _Form as Form };
