9ef135bb6f05e4338e6c65d79fdf86c91fd32bea
Marcel Köhler <77496890+aromko@users.noreply.github.com>
2025-04-29 09:22:32 +0200

    DRAFT:[DST-775]: automate-accessibility-tests (#4562)
    Co-authored-by: Sebastian Sebald <sebastian.sebald@reservix.de>

--- config/storybook/setupTest.ts
+++ config/storybook/setupTest.ts
@@ -1,8 +1,12 @@
+import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
 import { setProjectAnnotations } from '@storybook/react';
 import { beforeAll } from 'vitest';
 import * as previewAnnotations from './.storybook/preview';
 
-const annotations = setProjectAnnotations([previewAnnotations]);
+const annotations = setProjectAnnotations([
+  a11yAddonAnnotations,
+  previewAnnotations,
+]);
 
 // Run Storybook's beforeAll hook
 beforeAll(annotations.beforeAll);
