9b1a1ecb2b7e3529850a66dcd9198dbeaed49dbe
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-05-06 15:18:49 +0200

    adjust modal styles

--- themes/theme-rui/src/components/Modal.styles.ts
+++ themes/theme-rui/src/components/Modal.styles.ts
@@ -9,13 +9,13 @@ export const Modal: ThemeComponent<'Modal'> = cva(
   {
     variants: {
       size: {
+        xsmall: '[--dialog-width:480px]', // "xs" breakpoint
         small: '[--dialog-width:640px]', // sm breakpoint
         medium: '[--dialog-width:768px]', // md breakpoint
-        large: '[--dialog-width:1024px]', // lg breakpoint
       },
     },
     defaultVariants: {
-      size: 'medium',
+      size: 'small',
     },
   }
 );
