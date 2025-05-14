ea6eaa9978b75e90b07204980bbacd55567a8793
ssarahgm <sarahgosmann@gmail.com>
2025-05-14 10:51:09 +0200

    add cursor pointer

--- themes/theme-rui/src/components/Menu.styles.ts
+++ themes/theme-rui/src/components/Menu.styles.ts
@@ -10,7 +10,7 @@ export const Menu: ThemeComponent<'Menu'> = {
   ),
   button: cva([
     'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md h-button px-4 py-2',
-    'text-sm font-medium outline-none',
+    'text-sm font-medium outline-none cursor-pointer',
     'border border-input bg-background shadow-xs',
     'hover:bg-hover hover:text-foreground',
     'transition-[color,box-shadow]',
