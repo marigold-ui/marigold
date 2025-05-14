b641732ce7c57d8c95625dc076134f3db88a3a45
ssarahgm <sarahgosmann@gmail.com>
2025-05-14 11:25:51 +0200

    cursor pointer

--- themes/theme-rui/src/components/Menu.styles.ts
+++ themes/theme-rui/src/components/Menu.styles.ts
@@ -3,7 +3,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const Menu: ThemeComponent<'Menu'> = {
   container: cva(['text-foreground z-50 overflow-hidden rounded-md p-1']),
   item: cva([
-    'focus:bg-focus focus:text-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none disabled:text-disabled-foreground',
+    'focus:bg-focus focus:text-foreground relative flex cursor-pointer items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none disabled:text-disabled-foreground',
   ]),
   section: cva(
     'text-muted-foreground px-2 py-1.5 text-xs font-medium border-t border-t-border in-first:border-t-0'
