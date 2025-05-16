9cab4b82bc67b55810b3e6c9b9918fc81b60f11a
ssarahgm <sarahgosmann@gmail.com>
2025-05-16 14:49:22 +0200

    typecheck

--- packages/components/src/Multiselect/Multiselect.test.tsx
+++ packages/components/src/Multiselect/Multiselect.test.tsx
@@ -1,7 +1,6 @@
 import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { Multiselect } from './Multiselect';
 import * as stories from './Multiselect.stories';
 
 const { Basic } = composeStories(stories);
