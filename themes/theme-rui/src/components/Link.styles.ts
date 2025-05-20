a93f22aaf7b95f95d0b3240c55df193685bf7566
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-05-20 15:54:08 +0200

    refa([DST-859]): Improve `Link`styles

--- themes/theme-rui/src/components/Link.styles.ts
+++ themes/theme-rui/src/components/Link.styles.ts
@@ -1,7 +1,7 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
 export const Link: ThemeComponent<'Link'> = cva(
-  'underline aria-[disabled]:cursor-not-allowed py-2 underline-offset-4',
+  'font-medium underline aria-[disabled]:cursor-not-allowed py-2 underline-offset-4',
   {
     variants: {
       variant: {
