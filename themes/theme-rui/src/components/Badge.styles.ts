8f91283c73069de0d6666e7a37903d674e34d0b1
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-05-20 14:45:26 +0200

    refa([DST-858]): remove border and circle from badges

--- themes/theme-rui/src/components/Badge.styles.ts
+++ themes/theme-rui/src/components/Badge.styles.ts
@@ -1,33 +1,19 @@
 import { type ThemeComponent, cva } from '@marigold/system';
 
-const circle = 'before:size-1.5 before:rounded-full pr-2';
-
 export const Badge: ThemeComponent<'Badge'> = cva(
   [
-    'inline-flex items-center justify-center rounded-full border px-1.5 text-xs font-medium leading-normal transition-colors',
+    'inline-flex items-center justify-center rounded-full px-2 text-xs font-medium leading-normal transition-colors',
     'focus-visible:util-focus-ring outline-none',
   ],
   {
     variants: {
       variant: {
-        default: 'border-border bg-white',
-        primary: 'bg-brand text-brand-foreground border-transparent',
-        success: [
-          'bg-success-muted text-success-muted-foreground border-success-muted-accent before:bg-success-muted-accent gap-1.5',
-          circle,
-        ],
-        warning: [
-          'bg-warning-muted text-warning-muted-foreground border-warning-muted-accent before:bg-warning-muted-accent gap-1.5',
-          circle,
-        ],
-        info: [
-          'bg-info-muted text-info-muted-foreground border-info-muted-accent before:bg-info-muted-accent gap-1.5',
-          circle,
-        ],
-        error: [
-          'bg-destructive-muted text-destructive-muted-foreground border-destructive-muted-accent before:bg-destructive-muted-accent gap-1.5',
-          circle,
-        ],
+        default: 'bg-muted text-foreground',
+        primary: 'bg-brand text-brand-foreground ',
+        success: ['bg-success-muted text-success-muted-foreground'],
+        warning: ['bg-warning-muted text-warning-muted-foreground'],
+        info: ['bg-info-muted text-info-muted-foreground'],
+        error: ['bg-destructive-muted text-destructive-muted-foreground'],
       },
     },
     defaultVariants: {
