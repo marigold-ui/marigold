f671a1c964e928f69afd442fc8b003edf0f2feb5
OsamaAbdellateef <osama.abd9888@gmail.com>
2025-05-21 15:33:38 +0300

    adding legacy indicator

--- docs/app/_components/ThemeMenu.tsx
+++ docs/app/_components/ThemeMenu.tsx
@@ -21,7 +21,19 @@ export const ThemeMenu = () => {
     >
       {Object.keys(themes).map(name => (
         <Menu.Item key={name} id={name}>
-          {name.toUpperCase()}
+          <div className="flex items-center justify-between gap-2">
+            {name.toUpperCase()}
+            {['b2b', 'core'].includes(name) && (
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                role="presentation"
+              >
+                <path d="M2.25 20.3097H21.75L12 3.46875L2.25 20.3097ZM12.8864 17.2606H11.1136V15.4879H12.8864V17.2606ZM12.8864 13.7151H11.1136V10.1697H12.8864V13.7151Z"></path>
+              </svg>
+            )}
+          </div>
         </Menu.Item>
       ))}
     </Menu>
