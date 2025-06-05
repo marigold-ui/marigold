f5022711f31c551dcb97dadea3ec2f42ad93bdc6
pauleitenbichler <pauleitenbichler@gmail.com>
2025-06-05 09:54:12 +0200

    [DST-731]: Improve Code after CR

--- docs/content/components/overlay/ContextualHelp/contextualhelp-appearance.demo.tsx
+++ docs/content/components/overlay/ContextualHelp/contextualhelp-appearance.demo.tsx
@@ -1,16 +1,18 @@
-'use client';
-
 import { ContextualHelp } from '@marigold/components';
+import { Headline } from '@marigold/components';
+import { Text } from '@marigold/components';
+import { Link } from '@marigold/components';
 
 export default () => (
   <ContextualHelp>
-    <h3 className="mb-1 text-base font-bold">Was ist das?</h3>
-    <p className="text-sm">
-      Dieses Feature erklärt dir wichtige Funktionen direkt im Kontext der
-      Seite. <br />
-      <a href="https://www.marigold-ui.io/components/overview?theme=rui">
-        Zur Dokumentation
-      </a>
-    </p>
+    <Headline size="level-3">Whats this?</Headline>
+    <Text size="sm">
+      This Component explains the most important thinks in the This feature
+      explains important functions to you directly in the context of the page.
+      <br />
+      <Link href="https://www.marigold-ui.io/components/overview?theme=rui">
+        To the documentation
+      </Link>
+    </Text>
   </ContextualHelp>
 );
--- packages/components/src/ContextualHelp/ContextualHelp.stories.tsx
+++ packages/components/src/ContextualHelp/ContextualHelp.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta } from '@storybook/react';
 import React from 'react';
+import { Headline } from '../Headline';
+import { Link } from '../Link';
+import { Text } from '../Text';
 import { ContextualHelp } from './ContextualHelp';
 
 const meta = {
@@ -23,7 +26,7 @@ const meta = {
       control: 'boolean',
       defaultValue: false,
     },
-    isOpen: {
+    open: {
       control: 'boolean',
     },
     onOpenChange: {
@@ -40,14 +43,15 @@ export default meta;
 export const Basic = (props: React.ComponentProps<typeof ContextualHelp>) => (
   <div className="flex h-96 items-center justify-center">
     <ContextualHelp {...props}>
-      <h3 className="mb-1 text-base font-bold">Was ist das?</h3>
-      <p className="text-sm">
-        Dieses Feature erklärt dir wichtige Funktionen direkt im Kontext der
-        Seite. <br />
-        <a href="https://www.marigold-ui.io/components/overview?theme=rui">
-          Zur Dokumentation
-        </a>
-      </p>
+      <Headline size="level-3">Whats this?</Headline>
+      <Text size="sm">
+        This Component explains the most important thinks in the This feature
+        explains important functions to you directly in the context of the page.
+        <br />
+        <Link href="https://www.marigold-ui.io/components/overview?theme=rui">
+          To the documentation
+        </Link>
+      </Text>
     </ContextualHelp>
   </div>
 );
