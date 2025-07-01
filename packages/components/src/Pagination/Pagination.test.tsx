0737e065bc646b32fbd3500b7e8415ddaa5961c0
OsamaAbdellatif <osama.abd9888@gmail.com>
2025-07-01 15:22:14 +0300

    fix: replace pagination styles testing with main theme "rui"

--- packages/components/src/Pagination/Pagination.test.tsx
+++ packages/components/src/Pagination/Pagination.test.tsx
@@ -198,7 +198,7 @@ describe('Pagination tests', () => {
       'flex items-center justify-center space-x-2'
     );
     expect(button[0]).toHaveClass(
-      'border-border-base text-text-base ease-ease-out h-component cursor-pointer rounded-xs border px-4 py-0 text-sm leading-[22px] transition-all duration-200 disabled:border-border-base-disabled disabled:bg-bg-inverted-disabled disabled:text-text-base-disabled disabled:cursor-not-allowed disabled:border disabled:border-solid outline-outline-focus focus:outline-2 focus:outline-offset-1 h-auto border-none bg-transparent flex h-8! w-8 items-center justify-center wrap data-[selected=true]:border-0 data-[selected=true]:border-b-2 data-[selected=true]:border-solid data-[selected=true]:border-b-border-selected data-[selected=true]:bg-none data-[selected=true]:text-text-base data-[selected=true]:font-bold hover:bg-gray-100'
+      'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:util-focus-ring outline-none disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground [&_svg]:pointer-events-none [&_svg]:shrink-0 hover:bg-hover hover:text-hover-foreground h-9 py-2 gap-1 px-2.5 cursor-pointer'
     );
   });
 
