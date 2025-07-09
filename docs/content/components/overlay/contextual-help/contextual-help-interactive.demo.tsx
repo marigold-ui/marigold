9cee94758110b18a5c1e4a6ddc6c4b8a43acd7b1
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-07-09 11:35:52 +0200

    some more fixes

--- docs/content/components/overlay/contextual-help/contextual-help-interactive.demo.tsx
+++ docs/content/components/overlay/contextual-help/contextual-help-interactive.demo.tsx
@@ -1,16 +1,26 @@
 'use client';
 
-import { Button, ContextualHelp } from '@marigold/components';
+import {
+  Button,
+  ContextualHelp,
+  Inline,
+  Stack,
+  Text,
+} from '@marigold/components';
 
 export default () => (
   <ContextualHelp>
     <ContextualHelp.Title>Need more help?</ContextualHelp.Title>
     <ContextualHelp.Content>
-      You can access detailed documentation or contact our support. <br />
-      <div className="flex gap-2">
-        <Button variant="text">Docs</Button>
-        <Button variant="secondary">Support</Button>
-      </div>
+      <Stack space={2}>
+        <Text>
+          You can access detailed documentation or contact our support.
+        </Text>
+        <Inline space={2}>
+          <Button variant="ghost">Docs</Button>
+          <Button>Support</Button>
+        </Inline>
+      </Stack>
     </ContextualHelp.Content>
   </ContextualHelp>
 );
