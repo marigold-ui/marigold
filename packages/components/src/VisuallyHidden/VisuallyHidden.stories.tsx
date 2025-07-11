11646ac6b55eb867f12644817922285492b8b122
ssarahgm <sarahgosmann@gmail.com>
2025-07-11 12:02:39 +0200

    fix props

--- packages/components/src/VisuallyHidden/VisuallyHidden.stories.tsx
+++ packages/components/src/VisuallyHidden/VisuallyHidden.stories.tsx
@@ -6,13 +6,19 @@ import { VisuallyHidden } from './VisuallyHidden';
 const meta = {
   title: 'Components/VisuallyHidden',
   component: VisuallyHidden,
+  argTypes: {
+    children: {
+      control: { type: 'text' },
+      description: 'Children of the Visually Hidden.',
+    },
+  },
 } satisfies Meta<typeof VisuallyHidden>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-  render: ({ children, ...args }) => (
+  render: ({ ...args }) => (
     <>
       <Text>The Text below is visually hidden</Text>
       <VisuallyHidden {...args}>Invisible!</VisuallyHidden>
