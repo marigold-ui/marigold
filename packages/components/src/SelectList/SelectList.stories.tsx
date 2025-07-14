ad5ff4153f6d53c65bbbf6ea23a7141253b845f1
OsamaAbdellatif <osama.abd9888@gmail.com>
2025-07-14 14:33:36 +0300

    feat: add SelectList.Action component and update SelectListItem styling

--- packages/components/src/SelectList/SelectList.stories.tsx
+++ packages/components/src/SelectList/SelectList.stories.tsx
@@ -81,8 +81,7 @@ export const Action: Story = {
     >
       {(item: { id: number; name: string }) => (
         <SelectList.Item textValue={item.name}>
-          {item.name}
-          <div className="ml-auto">
+          <SelectList.Action>
             <Button
               aria-label="Info"
               onPress={() => alert(`Info for ${item.name}...`)}
@@ -91,7 +90,8 @@ export const Action: Story = {
                 <path d="M12 2.85938C6.95437 2.85938 2.85938 6.95437 2.85938 12C2.85938 17.0456 6.95437 21.1406 12 21.1406C17.0456 21.1406 21.1406 17.0456 21.1406 12C21.1406 6.95437 17.0456 2.85938 12 2.85938ZM12.7875 15.9374H11.2125V11.2124H12.7875V15.9374ZM12.7875 9.6375H11.2125V8.0625H12.7875V9.6375Z"></path>
               </svg>
             </Button>
-          </div>
+          </SelectList.Action>
+          {item.name}
         </SelectList.Item>
       )}
     </SelectList>
