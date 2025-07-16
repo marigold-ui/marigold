3ef38e46207e7734293e0644c2ea824a0e8d0485
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-07-16 23:10:17 +0200

    save

---
+++ docs/content/patterns/admin-master-mark/admin-master-mark-anatomy.demo.tsx
@@ -0,0 +1,8 @@
+import { Badge, Inline } from '@marigold/components';
+
+export default () => (
+  <Inline space={4} alignX="center">
+    <Badge variant="admin">Admin</Badge>
+    <Badge variant="master">Master</Badge>
+  </Inline>
+);
