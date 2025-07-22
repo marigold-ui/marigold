9311338b2361b6e2511ba4c67fa5f3b5f3e78806
Sebastian Sebald <sebastian.sebald@reservix.de>
2025-07-22 08:35:37 +0200

    docs([DST-936]): Update tooltip page (#4703)

--- docs/content/components/overlay/tooltip/tooltip-placement.demo.tsx
+++
@@ -1,8 +0,0 @@
-import { Button, Tooltip } from '@marigold/components';
-
-export default () => (
-  <Tooltip.Trigger>
-    <Button>Edit Ticket</Button>
-    <Tooltip placement="right">Make changes to the ticket details.</Tooltip>
-  </Tooltip.Trigger>
-);
---
+++ docs/content/components/overlay/tooltip/tooltip-usage.demo.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Button, Tooltip } from '@marigold/components';
+import { DesignTicket } from '@marigold/icons';
+
+export default () => (
+  <Tooltip.Trigger>
+    <Button variant="icon" size="large">
+      <DesignTicket aria-hidden="true" />
+    </Button>
+    <Tooltip>Design Ticket</Tooltip>
+  </Tooltip.Trigger>
+);
