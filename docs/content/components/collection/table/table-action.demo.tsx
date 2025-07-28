209ddee1abd1fdb74b7b8d041769b11d071f86bc
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-07-28 15:49:55 +0200

    update

--- docs/content/components/collection/table/table-action.demo.tsx
+++ docs/content/components/collection/table/table-action.demo.tsx
@@ -1,69 +1,24 @@
-import React from 'react';
-import { ActionMenu, I18nProvider, Menu, Table } from '@marigold/components';
-import { DateFormat } from '@marigold/system';
-
-const rows = [
-  {
-    event: 'Music Festival',
-    date: '2023-08-25',
-    ticketnr: '123456789',
-    id: 1,
-  },
-  {
-    event: 'Red Carpet Theater',
-    date: '2023-09-10',
-    ticketnr: '987654321',
-    id: 2,
-  },
-  {
-    event: 'Conference',
-    date: '2023-10-05',
-    ticketnr: '246813579',
-    id: 3,
-  },
-  {
-    event: 'Sports Tournament',
-    date: '2023-11-20',
-    ticketnr: '135792468',
-    id: 4,
-  },
-  {
-    event: 'Opera',
-    date: '2023-05-15',
-    ticketnr: '128216789',
-    id: 5,
-  },
-] as const;
+import { venues } from '@/lib/data/venues';
+import { Button, Table } from '@marigold/components';
 
 export default () => (
-  <Table
-    aria-label="Data Table"
-    selectionMode="multiple"
-    size="compact"
-    variant="linedTable"
-  >
+  <Table aria-label="Venue List" stretch>
     <Table.Header>
       <Table.Column>Event</Table.Column>
       <Table.Column>Date</Table.Column>
       <Table.Column align="right">Ticket Number</Table.Column>
       <Table.Column>Action</Table.Column>
     </Table.Header>
-    <Table.Body items={rows}>
-      {rows.map(item => (
+    <Table.Body>
+      {venues.slice(0, 3).map(item => (
         <Table.Row key={item.id}>
-          <Table.Cell>{item.event}</Table.Cell>
+          <Table.Cell>{item.name}</Table.Cell>
           <Table.Cell>
-            <I18nProvider locale="de-DE">
-              <DateFormat dateStyle="full" value={new Date(`${item.date}`)} />
-            </I18nProvider>
+            {item.street}, {item.city}
           </Table.Cell>
-          <Table.Cell>{item.ticketnr}</Table.Cell>
+          <Table.Cell>{item.rating}</Table.Cell>
           <Table.Cell>
-            <ActionMenu onAction={action => alert(`Action: ${action}`)}>
-              <Menu.Item id="edit">Open in editor</Menu.Item>
-              <Menu.Item id="event">Go to event</Menu.Item>
-              <Menu.Item id="delete">Delete</Menu.Item>
-            </ActionMenu>
+            <Button>View Details</Button>
           </Table.Cell>
         </Table.Row>
       ))}
