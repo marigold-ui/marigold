e4224da2c74b6581a39e0063466083148942dcdc
Sebastian Sebald <sebastian.sebald@reservix.de>
2025-08-01 08:47:25 +0200

    docs([DST-875]): Rework row action guideliens for tables (#4729)
    * text
    
    * Create strange-dryers-hide.md
    
    * update
    
    * update
    
    * save
    
    * update link
    
    * cleanup and finish
    
    * remove manually added classes
    
    * update small buttons

--- docs/content/components/collection/table/table-links.demo.tsx
+++ docs/content/components/collection/table/table-links.demo.tsx
@@ -1,44 +1,25 @@
+import { venues } from '@/lib/data/venues';
 import { Link, Table } from '@marigold/components';
 
 export default () => (
   <Table aria-label="Event Links Table" stretch>
     <Table.Header>
-      <Table.Column>Event</Table.Column>
-      <Table.Column>Details</Table.Column>
-      <Table.Column>Website</Table.Column>
+      <Table.Column>Venue</Table.Column>
+      <Table.Column>Address</Table.Column>
+      <Table.Column>Rating</Table.Column>
     </Table.Header>
     <Table.Body>
-      <Table.Row>
-        <Table.Cell>Annual Tech Conference</Table.Cell>
-        <Table.Cell>
-          Join industry leaders for a day of talks and networking.
-        </Table.Cell>
-        <Table.Cell>
-          <Link href="https://techconf.example.com" target="_blank">
-            techconf.example.com
-          </Link>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>React Advanced Workshop</Table.Cell>
-        <Table.Cell>
-          Hands-on coding session for intermediate React developers.
-        </Table.Cell>
-        <Table.Cell>
-          <Link href="https://reactworkshop.example.com" target="_blank">
-            reactworkshop.example.com
-          </Link>
-        </Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>Frontend Community Meetup</Table.Cell>
-        <Table.Cell>Monthly meetup for frontend enthusiasts.</Table.Cell>
-        <Table.Cell>
-          <Link href="https://frontendmeetup.example.com" target="_blank">
-            frontendmeetup.example.com
-          </Link>
-        </Table.Cell>
-      </Table.Row>
+      {venues.slice(0, 4).map(venue => (
+        <Table.Row key={venue.id}>
+          <Table.Cell>
+            <Link href={`#`}>{venue.name}</Link>
+          </Table.Cell>
+          <Table.Cell>
+            {venue.street}, {venue.city}
+          </Table.Cell>
+          <Table.Cell>{venue.rating}</Table.Cell>
+        </Table.Row>
+      ))}
     </Table.Body>
   </Table>
 );
