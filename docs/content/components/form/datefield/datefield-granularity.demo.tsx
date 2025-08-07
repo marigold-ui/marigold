b30727889bb62ad79e726ab4efc802e1b6edc28a
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-07 13:52:20 +0200

    save

---
+++ docs/content/components/form/datefield/datefield-granularity.demo.tsx
@@ -0,0 +1,9 @@
+import { DateField } from '@marigold/components';
+
+export default () => (
+  <DateField
+    label="Event Date"
+    description="Select the date for your ticket event."
+    granularity="second"
+  />
+);
---
+++ docs/content/components/form/datefield/datefield-timezone.demo.tsx
@@ -0,0 +1,8 @@
+import { DateField } from '@marigold/components';
+
+export default () => (
+  <DateField
+    label="Event Date"
+    description="Select the date for your ticket event."
+  />
+);
