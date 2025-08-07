3af0dcf777c44b6b3399c2396d9d6c298c8f1e64
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-07 15:35:05 +0200

    done!

--- docs/content/components/form/datefield/datefield-granularity.demo.tsx
+++ docs/content/components/form/datefield/datefield-granularity.demo.tsx
@@ -1,9 +1,17 @@
-import { DateField } from '@marigold/components';
+import { parseAbsoluteToLocal, parseDateTime } from '@internationalized/date';
+import { DateField, Stack } from '@marigold/components';
 
 export default () => (
-  <DateField
-    label="Event Date"
-    description="Select the date for your ticket event."
-    granularity="second"
-  />
+  <Stack space={8}>
+    <DateField
+      label="Date and Time"
+      granularity="second"
+      defaultValue={parseDateTime('2025-07-23T08:29')}
+    />
+    <DateField
+      label="Date and Time with Timezone"
+      granularity="second"
+      defaultValue={parseAbsoluteToLocal('2024-03-15T05:25:21Z')}
+    />
+  </Stack>
 );
--- docs/content/components/form/datefield/datefield-timezone.demo.tsx
+++
@@ -1,8 +0,0 @@
-import { DateField } from '@marigold/components';
-
-export default () => (
-  <DateField
-    label="Event Date"
-    description="Select the date for your ticket event."
-  />
-);
