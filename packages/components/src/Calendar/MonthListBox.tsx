75fa1cf13213f1635655335460709ec8dd3f00eb
Sinan Akdesir <sinan.akdesir@reservix.de>
2025-08-08 16:07:07 +0200

    refactor parts of calendar

--- packages/components/src/Calendar/MonthListBox.tsx
+++ packages/components/src/Calendar/MonthListBox.tsx
@@ -1,9 +1,7 @@
 import { Dispatch, Key, SetStateAction, useContext } from 'react';
-import { CalendarStateContext } from 'react-aria-components';
-import { Button } from 'react-aria-components';
-import { DateValue } from 'react-aria-components';
-import { cn } from '@marigold/system';
+import { CalendarStateContext, DateValue } from 'react-aria-components';
 import { useCalendarContext } from './Context';
+import { ListBox } from './ListBox';
 import { useFormattedMonths } from './useFormattedMonths';
 
 interface MonthDropdownProps {
@@ -26,44 +24,22 @@ const MonthListBox = ({
   const maxMonth =
     maxValue && maxValue.year === currentYear ? maxValue.month : 12;
 
-  const onPress = (index: Key, isDisabled: boolean) => {
-    if (isDisabled) return;
-    const date = state.focusedDate.set({ month: Number(index) + 1 });
-    state.setFocusedDate(date);
-    setSelectedDropdown(undefined);
-  };
-
   const { classNames } = useCalendarContext();
 
   return (
-    <ul
-      data-testid="monthOptions"
-      className="grid h-full max-h-[300px] min-w-[300px] grid-cols-3 gap-y-10 p-2"
-    >
-      {months.map((month, index) => {
-        const monthNum = index + 1;
-        const isDisabled = monthNum < minMonth || monthNum > maxMonth;
-        const isSelected = index === state.focusedDate.month - 1;
-        return (
-          <li className="flex justify-center" key={index}>
-            <Button
-              slot="previous"
-              onPress={() => onPress(index, isDisabled)}
-              key={index + 1}
-              aria-current={isSelected}
-              isDisabled={isDisabled}
-              className={cn(
-                classNames.calendarListboxButton,
-                'inline-flex items-center justify-center gap-[0.5ch]',
-                isDisabled && 'cursor-not-allowed opacity-50'
-              )}
-            >
-              {month.substring(0, 3)}
-            </Button>
-          </li>
-        );
-      })}
-    </ul>
+    <ListBox
+      items={months}
+      isDisabled={(_, monthIndex) =>
+        monthIndex + 1 < minMonth || monthIndex + 1 > maxMonth
+      }
+      isSelected={(_, monthIndex) => monthIndex === state.focusedDate.month - 1}
+      onSelect={(_, monthIndex) => {
+        state.setFocusedDate(state.focusedDate.set({ month: monthIndex + 1 }));
+        setSelectedDropdown(undefined);
+      }}
+      format={month => month.substring(0, 3)}
+      buttonClassName={classNames.calendarListboxButton}
+    />
   );
 };
 
