2d52eaf4df433c4bfb10e34936b75e366957a837
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-08 10:44:16 +0200

    update

--- docs/content/patterns/navigation/topnavigation.demo.tsx
+++ docs/content/patterns/navigation/topnavigation.demo.tsx
@@ -3,32 +3,39 @@ import { Logout } from '@marigold/icons';
 import { Logo } from './Logo';
 
 export default () => (
-  <div className="border-border w-full border-b pt-2 pl-2">
-    <Inline space={12}>
-      <div className="py-2">
-        <Logo />
-      </div>
-      <div className="self-end *:-mb-px">
-        <Tabs selectedKey={'home'}>
-          <Tabs.List aria-label="Navigation">
-            <Tabs.Item id="home" href="https://www.marigold-ui.io">
-              Home
-            </Tabs.Item>
-            <Tabs.Item id="settings" href="https://www.reservix.de">
-              Settings
-            </Tabs.Item>
-            <Tabs.Item id="messages" href="https://www.reservix.net">
-              Messages
-            </Tabs.Item>
-          </Tabs.List>
-        </Tabs>
-      </div>
-      <Split />
-      <Inline space={2} alignY="center">
-        <Button variant="ghost">
-          <Logout size={16} /> Logout
-        </Button>
+  <>
+    {/* Container */}
+    <div className="border-border w-full border-b pt-2 pl-2">
+      <Inline space={12}>
+        {/* Logo area */}
+        <div className="py-2">
+          <Logo />
+        </div>
+        {/* Navigation links */}
+        <div className="self-end *:-mb-px">
+          <Tabs selectedKey={'home'}>
+            <Tabs.List aria-label="Navigation">
+              <Tabs.Item id="home" href="https://www.marigold-ui.io">
+                Home
+              </Tabs.Item>
+              <Tabs.Item id="settings" href="https://www.reservix.de">
+                Settings
+              </Tabs.Item>
+              <Tabs.Item id="messages" href="https://www.reservix.net">
+                Messages
+              </Tabs.Item>
+            </Tabs.List>
+          </Tabs>
+        </div>
+        {/* Spacer */}
+        <Split />
+        {/* Action area */}
+        <Inline space={2} alignY="center">
+          <Button variant="ghost">
+            <Logout size={16} /> Logout
+          </Button>
+        </Inline>
       </Inline>
-    </Inline>
-  </div>
+    </div>
+  </>
 );
