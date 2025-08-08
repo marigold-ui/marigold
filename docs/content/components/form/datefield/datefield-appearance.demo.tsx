47a05e959dffb8e8f8951b82872f0bbfb8f7645e
Sebastian Sebald <sebastian.sebald@reservix.de>
2025-08-08 13:49:20 +0200

    docs([DST-938]): Update `DateField` docs to show how to display time and timezone (#4759)
    * save
    
    * save
    
    * done!
    
    * Create curvy-timers-hunt.md

--- docs/content/components/form/datefield/datefield-appearance.demo.tsx
+++ docs/content/components/form/datefield/datefield-appearance.demo.tsx
@@ -1,10 +1,3 @@
-import { CalendarDate } from '@internationalized/date';
 import { DateField } from '@marigold/components';
 
-export default () => (
-  <DateField
-    label="Event Date"
-    defaultValue={new CalendarDate(2023, 10, 15)} // Updated to a future date relevant for ticketing
-    description="Select the date for your ticket event."
-  />
-);
+export default () => <DateField label="Event Date" width={40} />;
