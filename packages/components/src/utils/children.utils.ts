26dc9c97d27bcc13c4cf596d024eeab94f81297b
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-14 08:12:27 +0200

    rename

--- packages/components/src/utils/children.utils.ts
+++ packages/components/src/utils/children.utils.ts
@@ -2,11 +2,11 @@ import type { ReactNode } from 'react';
 import { Children } from 'react';
 
 export const splitChildren = (children: ReactNode, at?: number) => {
-  const c = Children.toArray(children);
+  const childArray = Children.toArray(children);
 
   if (at === undefined) {
-    return [c, []];
+    return [childArray, []];
   }
 
-  return [c.slice(0, at), c.slice(at)];
+  return [childArray.slice(0, at), childArray.slice(at)];
 };
