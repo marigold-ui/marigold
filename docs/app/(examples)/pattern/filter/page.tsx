a395f3703d4c0b7a25804adeee62095c83a27067
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-14 09:45:13 +0200

    save

--- docs/app/(examples)/pattern/filter/page.tsx
+++ docs/app/(examples)/pattern/filter/page.tsx
@@ -2,10 +2,7 @@
  * TODO: slider needs a "name" attribute, separate arial-label from name of thumb
  * TODO: need more visual separation between the filters in the drawer
  * TODO: close drawer after applying filters?
- * TODO: Numeric format only in system not in components package?
- * TODO: a "clear all" button for taggroup component
  * TODO: indicate when there is a unapplied filters (drawer)?
- * TODO: TagGroup can not be used in a form, because it has no name attribute
  */
 import { Stack } from '@/ui';
 import { AppliedFilter } from './applied-filter';
