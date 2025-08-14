6fa4a5b84eac7631ef013ecfc64a00e1e426a7b7
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-14 22:32:48 +0200

    save

--- docs/content/patterns/filter/filter-revert.demo.tsx
+++ docs/content/patterns/filter/filter-revert.demo.tsx
@@ -1,35 +1,52 @@
-import { Button, NumberField, Slider } from '@marigold/components';
+import { Undo2 } from 'lucide-react';
+import {
+  Button,
+  Inline,
+  NumberField,
+  Slider,
+  Stack,
+  Tooltip,
+} from '@marigold/components';
+
+const Reset = () => (
+  <Tooltip.Trigger>
+    <Button size="small" variant="link" slot={null}>
+      <Undo2 className="size-4" strokeWidth={2.5} />
+    </Button>
+    <Tooltip>Reset filter</Tooltip>
+  </Tooltip.Trigger>
+);
 
 export default () => (
-  <div className="item-center mx-auto grid max-w-1/2 place-items-center gap-12 py-4">
-    <Slider
-      label={
-        <>
-          <>Ticket Price</>
-          <Button variant="link">Reset filter</Button>
-        </>
-      }
-      defaultValue={[30, 80]}
-      step={10}
-      maxValue={150}
-      formatOptions={{
-        style: 'currency',
-        currency: 'EUR',
-        minimumFractionDigits: 0,
-      }}
-    />
-    <NumberField
-      label={
-        <div className="flex w-full items-center justify-between">
-          <>Min. available Tickets</>
-          <Button variant="link" slot={null}>
-            Reset filter
-          </Button>
-        </div>
-      }
-      defaultValue={2}
-      minValue={0}
-      maxValue={150}
-    />
+  <div className="mx-auto w-1/2">
+    <Stack space={6}>
+      <NumberField
+        label={
+          <Inline space={2}>
+            <>Min. available Tickets</>
+            <Reset />
+          </Inline>
+        }
+        defaultValue={2}
+        minValue={0}
+        maxValue={150}
+      />
+      <Slider
+        label={
+          <Inline space={2}>
+            <>Ticket Price</>
+            <Reset />
+          </Inline>
+        }
+        defaultValue={[30, 80]}
+        step={10}
+        maxValue={150}
+        formatOptions={{
+          style: 'currency',
+          currency: 'EUR',
+          minimumFractionDigits: 0,
+        }}
+      />
+    </Stack>
   </div>
 );
