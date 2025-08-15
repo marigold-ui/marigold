ab32fc13f545639d5f0726777779d1d46335e2c0
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-15 11:19:34 +0200

    fix(Pagination): correctly apply classnames

--- themes/theme-rui/src/components/Pagination.styles.ts
+++ themes/theme-rui/src/components/Pagination.styles.ts
@@ -1,22 +1,23 @@
 import { ThemeComponent, cva } from '@marigold/system';
 
-const button =
-  'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors' +
-  'focus-visible:util-focus-ring outline-none' +
-  'disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground' +
-  '[&_svg]:pointer-events-none [&_svg]:shrink-0' +
-  'hover:bg-hover hover:text-hover-foreground' +
-  'cursor-pointer';
+const button = [
+  'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors',
+  'focus-visible:util-focus-ring outline-none',
+  'disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground',
+  '[&_svg]:pointer-events-none [&_svg]:shrink-0',
+  'hover:bg-hover hover:text-hover-foreground',
+  'cursor-pointer',
+];
 
 export const Pagination: ThemeComponent<'Pagination'> = {
   container: cva('flex items-center justify-center space-x-2'),
   navigationButton: cva([
-    button,
+    ...button,
     'disabled:pointer-events-none disabled:bg-disabled disabled:text-disabled-foreground',
     'h-9 py-2 gap-1 px-2.5',
   ]),
   pageButton: cva([
-    button,
+    ...button,
     'bg-background size-9',
     'data-[selected=true]:border data-[selected=true]:border-input data-[selected=true]:shadow-xs',
   ]),
