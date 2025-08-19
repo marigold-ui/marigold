3e6f0eaf628003e3709d589a15b4c2bc861625f1
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-08-19 14:33:53 +0200

    docs(Textarea): Revise `<TextArea>` docs

--- docs/content/components/form/text-area/text-area-req.demo.tsx
+++
@@ -1,3 +0,0 @@
-import { TextArea } from '@marigold/components';
-
-export default () => <TextArea label="Required Textarea" required rows={6} />;
---
+++ docs/content/components/form/textarea/textarea-appearance.demo.tsx
@@ -0,0 +1,6 @@
+import type { TextAreaProps } from '@marigold/components';
+import { TextArea } from '@marigold/components';
+
+export default (props: TextAreaProps) => (
+  <TextArea label="Comment" width="1/2" {...props} />
+);
