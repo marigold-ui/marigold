4947cf1af30eb89aa894fd1112cdec02f6b9d0e1
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-09-17 13:55:30 +0200

    fix appearance demos

--- docs/content/components/actions/button/button-appearance.demo.tsx
+++ docs/content/components/actions/button/button-appearance.demo.tsx
@@ -1,4 +1,7 @@
+import { Save } from 'lucide-react';
 import type { ButtonProps } from '@marigold/components';
 import { Button } from '@marigold/components';
 
-export default (props: ButtonProps) => <Button {...props}>Press me</Button>;
+export default (props: ButtonProps) => (
+  <Button {...props}>{props.size === 'icon' ? <Save /> : 'Press me'}</Button>
+);
