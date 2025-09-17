01f3352cbed90cd6f3c341b48c919c174990fe01
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-09-17 11:45:26 +0200

    add guidelines

---
+++ docs/content/components/actions/link-button/linkbutton-appearance.demo.tsx
@@ -0,0 +1,6 @@
+import type { LinkButtonProps } from '@marigold/components';
+import { LinkButton } from '@marigold/components';
+
+export default (props: LinkButtonProps) => (
+  <LinkButton {...props}>Press me</LinkButton>
+);
