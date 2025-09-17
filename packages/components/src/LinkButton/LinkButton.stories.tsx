ccbeea43e7f3e786dd7478ceff13f4a64c5f7118
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-09-17 10:15:48 +0200

    fixes

--- packages/components/src/LinkButton/LinkButton.stories.tsx
+++ packages/components/src/LinkButton/LinkButton.stories.tsx
@@ -4,6 +4,22 @@ import { LinkButton, LinkButtonProps } from './LinkButton';
 const meta: Meta<LinkButtonProps> = {
   title: 'Components/LinkButton',
   component: LinkButton,
+  argTypes: {
+    size: {
+      control: {
+        type: 'radio',
+      },
+      description: 'Size of the button',
+      options: ['default', 'small', 'large', 'icon'],
+    },
+    variant: {
+      control: {
+        type: 'radio',
+      },
+      options: ['primary', 'secondary', 'destructive', 'ghost', 'icon', 'text'],
+      description: 'Variant of the button',
+    },
+  },
   args: {
     children: 'Link Button',
     href: 'https://marigold-ui.io',
