55ac284a953257a32d09823dee85196b65b501c6
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-09-26 09:06:17 +0200

    demos

--- docs/content/components/layout/inline/inline-horizontally.demo.tsx
+++ docs/content/components/layout/inline/inline-horizontally.demo.tsx
@@ -27,5 +27,29 @@ export default () => (
       </Rectangle>
       <Rectangle height="60px" width="60px" />
     </Inline>
+    <Divider />
+    <Inline space={3} alignX="between">
+      <Rectangle height="60px" width="60px" />
+      <Rectangle height="60px" width="60px">
+        <div className="text-text-primary-muted m-auto">Between</div>
+      </Rectangle>
+      <Rectangle height="60px" width="60px" />
+    </Inline>
+    <Divider />
+    <Inline space={3} alignX="around">
+      <Rectangle height="60px" width="60px" />
+      <Rectangle height="60px" width="60px">
+        <div className="text-text-primary-muted m-auto">Around</div>
+      </Rectangle>
+      <Rectangle height="60px" width="60px" />
+    </Inline>
+    <Divider />
+    <Inline space={3} alignX="evenly">
+      <Rectangle height="60px" width="60px" />
+      <Rectangle height="60px" width="60px">
+        <div className="text-text-primary-muted m-auto">Evenly</div>
+      </Rectangle>
+      <Rectangle height="60px" width="60px" />
+    </Inline>
   </Stack>
 );
