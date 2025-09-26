46bc554a2d0b8e0e231b85bcec2037f0641e7a81
ssarahgm <sarahgosmann@gmail.com>
2025-09-26 14:06:59 +0200

    add dedicated accordion section

--- docs/app/(examples)/pattern/form/page.tsx
+++ docs/app/(examples)/pattern/form/page.tsx
@@ -1,26 +1,28 @@
 'use client';
 
-import { Button, Inline, Stack } from '@marigold/components';
+import { Button, Form, Inline, Stack } from '@marigold/components';
 import { EventDetails } from './eventDetails';
 import { LocationSettings } from './locationSettings';
 import { OrganizerInfo } from './organizerInfo';
 import { RegistrationSettings } from './registrationSettings';
 
 const FormPage = () => (
-  <div className="mx-auto w-[800px] py-12">
-    <Stack space={14}>
-      <EventDetails />
-      <LocationSettings />
-      <Stack space={4}>
-        <OrganizerInfo />
-        <RegistrationSettings />
-      </Stack>
+  <div className="flex justify-center py-20">
+    <Form maxWidth="container">
+      <Stack space={14}>
+        <EventDetails />
+        <LocationSettings />
+        <Stack space={4}>
+          <OrganizerInfo />
+          <RegistrationSettings />
+        </Stack>
 
-      <Inline space={4}>
-        <Button variant="primary">Save</Button>
-        <Button variant="secondary">Cancel</Button>
-      </Inline>
-    </Stack>
+        <Inline space={4}>
+          <Button variant="primary">Save</Button>
+          <Button variant="secondary">Cancel</Button>
+        </Inline>
+      </Stack>
+    </Form>
   </div>
 );
 
