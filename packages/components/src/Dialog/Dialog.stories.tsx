97588264c6c3e880a12a4c9cc82b46d3fb18f2ec
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-09-26 23:13:08 +0200

    add tests

--- packages/components/src/Dialog/Dialog.stories.tsx
+++ packages/components/src/Dialog/Dialog.stories.tsx
@@ -2,15 +2,9 @@ import type { StoryObj } from '@storybook/react';
 import { useState } from 'storybook/preview-api';
 import { Button } from '../Button';
 import { Menu } from '../Menu';
-import { Stack } from '../Stack';
 import { Text } from '../Text';
 import { TextField } from '../TextField';
-import {
-  ConfirmationDialog,
-  ConfirmationDialogProps,
-} from './ConfirmationDialog';
 import { Dialog } from './Dialog';
-import { useConfirmation } from './useConfirmation';
 
 const meta = {
   title: 'Components/Dialog',
@@ -148,51 +142,6 @@ export const OpenFromMenu: Story = {
           )}
         </Dialog>
       </>
-    );
-  },
-};
-
-export const Confirmation: StoryObj<ConfirmationDialogProps> = {
-  render: ({ ...args }) => (
-    <ConfirmationDialog.Trigger {...args}>
-      <Button>Open</Button>
-      <ConfirmationDialog title="Confirmation" confirmationLabel="Confirm">
-        Are you sure you want to proceed with this action?
-      </ConfirmationDialog>
-    </ConfirmationDialog.Trigger>
-  ),
-};
-
-export const UseConfirmation: StoryObj = {
-  render: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const confirm = useConfirmation();
-
-    const onConfirmation = async () => {
-      await confirm({
-        title: 'Enable notifications',
-        content: 'Would you like to receive notifications for upcoming events?',
-        confirmationLabel: 'Enable',
-        cancelLabel: 'Cancel',
-      });
-    };
-
-    const onDelete = async () => {
-      await confirm({
-        variant: 'destructive',
-        title: 'Delete item',
-        content: 'Are you sure you want to delete this item?',
-        confirmationLabel: 'Delete',
-      });
-    };
-
-    return (
-      <Stack space={4} alignX="left">
-        <Button onPress={onConfirmation}>Confirm</Button>
-        <Button variant="destructive" onPress={onDelete}>
-          Delete
-        </Button>
-      </Stack>
     );
   },
 };
