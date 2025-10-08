2bf620263b5742a50366d2de2bac1fdabb822080
marekgieldatsh <64067674+marekgieldatsh@users.noreply.github.com>
2025-10-08 15:28:13 +0200

    remove barrel file structure  system and icons (#4879)

--- packages/system/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './useClassNames';
-export * from './useResponsiveValue';
-export * from './useStateProps';
-export * from './useTheme';
-export * from './useSmallScreen';
-export * from './extendTheme';
