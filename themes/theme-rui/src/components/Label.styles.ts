abcf279bb6e4efcd2a0d10d372e7067ba99ff88b
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-10-17 14:38:08 +0200

    adjust label indicator

--- themes/theme-rui/src/components/Label.styles.ts
+++ themes/theme-rui/src/components/Label.styles.ts
@@ -2,9 +2,9 @@ import { type ThemeComponent, cva } from '@marigold/system';
 
 export const Label: ThemeComponent<'Label'> = {
   container: cva([
-    'flex items-center',
+    'flex items-center gap-1',
     'text-sm font-medium leading-none text-foreground',
     'group-disabled/field:cursor-not-allowed group-disabled/field:text-disabled-foreground',
   ]),
-  indicator: cva('group-required/field:block text-destructive'),
+  indicator: cva('group-required/field:block text-destructive -ml-1'),
 };
