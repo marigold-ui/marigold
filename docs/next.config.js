b2955ff4f3420111a819727d670e48e8bb616b99
aromko <aromkoapps@gmail.com>
2025-10-23 22:05:30 +0200

    [DST-150]: cleanup pnpm-lock.yaml and dependencies. Removed `next-router-mock`, added `mdx-bundler`, and streamlined build/dev scripts in `package.json`.

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,3 @@
-const { withContentlayer } = require('next-contentlayer2');
 const pkg = require('./package.json');
 
 /** @type {import('next').NextConfig} */
@@ -45,4 +44,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
