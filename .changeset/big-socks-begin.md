13b5c4bb063b5ab19547665e2c3fd3f7e0402ee4
Sebastian Sebald <sebastian.sebald@reservix.de>
2025-11-07 12:32:55 +0100

    fix(DST-1112): Show focus ring of `<CloseButton>` only when focused with keyboard (#4926)
    * fix(DST-1112): Show focus ring of `<CloseButton>` only when focused with keyboard
    
    * Create big-socks-begin.md

---
+++ .changeset/big-socks-begin.md
@@ -0,0 +1,5 @@
+---
+"@marigold/theme-rui": patch
+---
+
+fix(DST-1112): Show focus ring of `<CloseButton>` only when focused with keyboard
--- themes/theme-rui/src/components/CloseButton.styles.ts
+++ themes/theme-rui/src/components/CloseButton.styles.ts
@@ -5,7 +5,7 @@ export const CloseButton: ThemeComponent<'CloseButton'> = cva([
   'flex items-center justify-center whitespace-nowrap',
   'cursor-pointer',
   'transition-[color,box-shadow]',
-  'util-focus-ring',
+  'focus-visible:util-focus-ring outline-none',
   'rounded-full',
   'duration-150 active:scale-[0.98] pressed:scale-[0.98] transition-transform',
   '[&_svg]:size-4 [&_svg]:opacity-60 [&_svg]:transition-opacity hover:[&_svg]:opacity-100',
