135da2261891ca01a17dcd2056019eb727562a46
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-11-13 13:56:32 +0100

    adjust styles

--- themes/theme-rui/src/components/Button.styles.ts
+++ themes/theme-rui/src/components/Button.styles.ts
@@ -5,9 +5,9 @@ export const Button: ThemeComponent<'Button'> = cva(
   [
     'inline-flex items-center justify-center gap-2',
     'whitespace-nowrap rounded-md font-medium transition-[color,box-shadow,transform]',
-    'duration-150 active:scale-[0.98] pressed:scale-[0.98]',
+    'duration-150 active:scale-[0.97] pressed:scale-[0.97]',
     '[&_svg]:pointer-events-none [&_svg]:shrink-0',
-    'focus-visible:util-focus-ring outline-none disabled:util-disabled',
+    'focus-visible:state-focus outline-none disabled:state-disabled',
     'pending:text-disabled-foreground pending:bg-disabled pending:cursor-not-allowed pending:border-none',
     'cursor-pointer',
   ],
