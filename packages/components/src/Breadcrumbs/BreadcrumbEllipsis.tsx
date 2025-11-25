bd743c790456da0f38e65d016f48bb86f2e263af
ssarahgm <sarahgosmann@gmail.com>
2025-11-25 10:11:26 +0100

    update translations

--- packages/components/src/Breadcrumbs/BreadcrumbEllipsis.tsx
+++ packages/components/src/Breadcrumbs/BreadcrumbEllipsis.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Menu, MenuItem, MenuTrigger } from 'react-aria-components';
+import { useLocalizedStringFormatter } from '@react-aria/i18n';
 import { useClassNames } from '@marigold/system';
 import { IconButton } from '../IconButton/IconButton';
 import { Popover } from '../Overlay/Popover';
+import { intlMessages } from '../intl/messages';
 import { BreadcrumbsItemProps } from './BreadcrumbsItem';
 
 interface BreadcrumbEllipsisProps extends React.ComponentProps<'span'> {
@@ -14,13 +16,16 @@ export const BreadcrumbEllipsis = ({
   hiddenItems = [],
   disabled = false,
 }: BreadcrumbEllipsisProps) => {
+  const stringFormatter = useLocalizedStringFormatter(intlMessages, 'marigold');
   const { container, item: menuItem } = useClassNames({
     component: 'Menu',
   });
 
   return (
     <MenuTrigger>
-      <IconButton aria-label="These breadcrumbs are hidden">...</IconButton>
+      <IconButton aria-label={stringFormatter.format('hiddenBreadcrumbs')}>
+        ...
+      </IconButton>
       <Popover>
         <Menu className={container}>
           {hiddenItems.map((item, index) => {
