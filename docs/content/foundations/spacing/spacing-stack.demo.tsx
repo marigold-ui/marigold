5f82809322fc877e8546c0418baeff1586b9daae
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-11-28 15:20:10 +0100

    save

--- docs/content/foundations/spacing/spacing-stack.demo.tsx
+++ docs/content/foundations/spacing/spacing-stack.demo.tsx
@@ -2,10 +2,12 @@ import { Stack } from '@marigold/components';
 import { Rectangle } from '@/ui/Rectangle';
 
 export default () => (
-  <Stack alignX="left">
+  <Stack alignX="left" space="peer">
     <Rectangle className="text-sm" width="auto">
       Label
     </Rectangle>
-    <Rectangle className="text-sm">Input</Rectangle>
+    <Rectangle className="text-sm" width="50%">
+      Input
+    </Rectangle>
   </Stack>
 );
