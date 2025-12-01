9fa7f231be8c1c9f17136168ef574cdccc18f419
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-12-01 23:20:24 +0100

    save

---
+++ docs/content/foundations/spacing/spacing-application.tsx
@@ -0,0 +1,11 @@
+import { Card, Stack } from '@marigold/components';
+
+export default () => {
+  return (
+    <Stack space="peer">
+      <Card>
+        <Stack></Stack>
+      </Card>
+    </Stack>
+  );
+};
