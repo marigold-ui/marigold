388a138101b70fc35f4e1ff4e78ba3f42b57bfbd
Sebastian Sebald <sebastian.sebald@gmail.com>
2025-12-01 15:17:46 +0100

    save

--- docs/ui/Wireframe.tsx
+++ docs/ui/Wireframe.tsx
@@ -7,9 +7,12 @@ export interface WireframeProps {
 export const Box = ({ className }: WireframeProps) => (
   <div
     className={cn(
-      'z-10',
-      'bg-secondary-50/25 relative size-full bg-clip-padding',
+      'relative z-10 size-full bg-clip-padding',
       'border-secondary-300 rounded-lg border shadow-xs',
+      // fill
+      'to-secondary-100/80 from-secondary-50/30 bg-linear-to-b',
+      'shadow-[inset_0_1px_0_0_--theme(--color-white/90%),inset_0_2px_4px_--theme(--color-black/3%)]',
+      // ::before
       'before:absolute before:inset-0 before:rounded-[calc(var(--radius-lg)-1px)]',
       'before:pointer-events-none before:shadow-[0_1px_--theme(--color-black/12%)]',
       className
