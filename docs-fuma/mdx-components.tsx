0f942d479224bfc228ee12d77bb3d39047acb1a4
OsamaAbdellatif <osama.abd9888@gmail.com>
2025-12-30 00:37:09 +0200

    Add resources

--- docs-fuma/mdx-components.tsx
+++ docs-fuma/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
@@ -5,5 +6,19 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    pre: props => (
+      <CodeBlock>
+        <Pre {...props} className="bg-red-300">
+          {props.children}
+        </Pre>
+      </CodeBlock>
+    ),
+    figure: props => (
+      <CodeBlock>
+        <figure {...props} className="Osama">
+          {props.children}
+        </figure>
+      </CodeBlock>
+    ),
   };
 }
