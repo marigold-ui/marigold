e840196cbf6b9120ac1c5ebc67383b4e65997258
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-17 07:06:52 +0100

    ui state improvements

--- themes/theme-rui/src/styles.stories.tsx
+++ themes/theme-rui/src/styles.stories.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react';
-import { Inline, Stack } from '@marigold/components';
+import { Headline, Inline, Stack } from '@marigold/components';
 import preview from '../../../.storybook/preview';
 
 const meta = preview.meta({
@@ -20,6 +20,7 @@ const Base = ({
 export const Surface = meta.story({
   render: () => (
     <Stack space="group">
+      <Headline level="3">Surface</Headline>
       <Inline space="peer">
         <Base className="ui-surface">raised</Base>
         <Base className="ui-surface ui-elevation-overlay">overlay</Base>
@@ -27,16 +28,42 @@ export const Surface = meta.story({
           overlay / error
         </Base>
       </Inline>
+      <Headline level="3">With Tailwind Classes</Headline>
+      <Inline space="peer">
+        <Base className="ui-surface ui-elevation-overlay shadow-fuchsia-600">
+          overlay / tw color
+        </Base>
+        <Base className="ui-surface ui-elevation-overlay inset-shadow-sm/50 inset-shadow-fuchsia-600">
+          overlay / tw shadow
+        </Base>
+      </Inline>
+      <Headline level="3">States</Headline>
       <Inline space="peer">
         <Base className="ui-surface ui-state-focus">focus</Base>
         <Base className="ui-surface ui-state-disabled">disabled</Base>
         <Base className="ui-surface ui-state-error">error</Base>
         <Base className="ui-surface ui-state-readonly">readonly</Base>
       </Inline>
-      <Inline>
+      <Headline level="3">Input</Headline>
+      <Inline space="peer">
         <input
-          className="ui-surface invalid:ui-state-error focus:ui-state-focus p-squish-relaxed"
+          className="ui-surface invalid:ui-state-error focus:ui-state-focus p-squish-relaxed text-sm"
+          placeholder="default"
+        />
+        <input
+          className="ui-surface invalid:ui-state-error focus:ui-state-focus p-squish-relaxed text-sm"
+          placeholder="invalid"
           required
+        />
+        <input
+          className="ui-surface disabled:ui-state-disabled focus:ui-state-focus p-squish-relaxed text-sm"
+          placeholder="disabled"
+          disabled
+        />
+        <input
+          className="ui-surface read-only:ui-state-readonly focus:ui-state-focus p-squish-relaxed text-sm"
+          placeholder="readonly"
+          readOnly
         />
       </Inline>
     </Stack>
