b762f92f2bfe0a8a7c32236b0670327ebde2a836
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-01-20 16:32:28 +0100

    fix sorting story

--- packages/components/src/TableView/TableView.stories.tsx
+++ packages/components/src/TableView/TableView.stories.tsx
@@ -328,62 +328,79 @@ export const Empty = meta.story({
 
 export const Sorting = meta.story({
   render: args => {
+    const columns = [
+      { name: 'Name', id: 'name', isRowHeader: true },
+      { name: 'Height', id: 'height', isRowHeader: false },
+      { name: 'Mass', id: 'mass', isRowHeader: false },
+      { name: 'Birth Year', id: 'birth_year', isRowHeader: false },
+    ] as const;
+
     const data = [
       {
+        id: '1',
         name: 'Luke Skywalker',
         height: '172',
         mass: '77',
         birth_year: '19BBY',
       },
       {
+        id: '2',
         name: 'C-3PO',
         height: '167',
         mass: '75',
         birth_year: '112BBY',
       },
       {
+        id: '3',
         name: 'R2-D2',
         height: '96',
         mass: '32',
         birth_year: '33BBY',
       },
       {
+        id: '4',
         name: 'Darth Vader',
         height: '202',
         mass: '136',
         birth_year: '41.9BBY',
       },
       {
+        id: '5',
         name: 'Leia Organa',
         height: '150',
         mass: '49',
         birth_year: '19BBY',
       },
       {
+        id: '6',
         name: 'Owen Lars',
         height: '178',
         mass: '120',
         birth_year: '52BBY',
       },
       {
+        id: '7',
         name: 'Beru Whitesun lars',
         height: '165',
         mass: '75',
         birth_year: '47BBY',
       },
       {
+        id: '8',
         name: 'R5-D4',
         height: '97',
         mass: '32',
         birth_year: 'unknown',
       },
       {
+        id: '9',
         name: 'Biggs Darklighter',
         height: '183',
         mass: '84',
         birth_year: '24BBY',
       },
       {
+        id: '10',
         name: 'Obi-Wan Kenobi',
         height: '182',
         mass: '77',
@@ -419,26 +436,21 @@ export const Sorting = meta.story({
           selectionMode="multiple"
           {...args}
         >
-          <TableView.Header>
-            <TableView.Column key="name" allowsSorting>
-              Name
-            </TableView.Column>
-            <TableView.Column key="height" allowsSorting>
-              Height
-            </TableView.Column>
-            <TableView.Column key="mass" allowsSorting>
-              Mass
-            </TableView.Column>
-            <TableView.Column key="birth_year" allowsSorting>
-              Birth Year
-            </TableView.Column>
+          <TableView.Header columns={columns}>
+            {column => (
+              <TableView.Column
+                isRowHeader={column.isRowHeader}
+                id={column.id}
+                allowsSorting
+              >
+                {column.name}
+              </TableView.Column>
+            )}
           </TableView.Header>
           <TableView.Body items={list}>
             {item => (
-              <TableView.Row key={item.name}>
-                {columnKey => (
-                  <TableView.Cell>{(item as any)[columnKey]}</TableView.Cell>
-                )}
+              <TableView.Row columns={columns}>
+                {column => <TableView.Cell>{item[column.id]}</TableView.Cell>}
               </TableView.Row>
             )}
           </TableView.Body>
