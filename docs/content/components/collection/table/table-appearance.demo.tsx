b9d7bf238d596ab0ecff4970bde8d3d8ec778b69
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-02-02 19:54:21 +0100

    save

--- docs/content/components/collection/table/table-appearance.demo.tsx
+++ docs/content/components/collection/table/table-appearance.demo.tsx
@@ -3,16 +3,20 @@ import type { TableProps } from '@marigold/components';
 import { Table, Text } from '@marigold/components';
 
 const columns = [
-  { name: 'Venue', key: 'name' },
-  { name: 'Type', key: 'type' },
-  { name: 'City', key: 'city' },
-  { name: 'Capacity', key: 'capacity' },
-];
+  { name: 'Venue', key: 'name', width: '1fr', align: 'left' },
+  { name: 'Type', key: 'type', width: '1fr', align: 'left' },
+  { name: 'City', key: 'city', width: '1fr', align: 'left' },
+  { name: 'Capacity', key: 'capacity', width: 100, align: 'right' },
+] as const;
 
 export default (props: TableProps) => (
   <Table {...props}>
     <Table.Header columns={columns}>
-      {column => <Table.Column>{column.name}</Table.Column>}
+      {column => (
+        <Table.Column width={column.width} align={column.align}>
+          {column.name}
+        </Table.Column>
+      )}
     </Table.Header>
     <Table.Body items={venues.slice(0, 5)}>
       {item => (
@@ -24,7 +28,7 @@ export default (props: TableProps) => (
           <Table.Cell>
             {item.city}, {item.country}
           </Table.Cell>
-          <Table.Cell>{item.capacity.toLocaleString()}</Table.Cell>
+          <Table.Cell>{item.capacity}</Table.Cell>
         </Table.Row>
       )}
     </Table.Body>
