94fff1f2c77ca9c1fdb910e2ebadb45864b4950b
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-02-03 14:56:41 +0100

    save

--- packages/components/src/Table/TableRow.tsx
+++ packages/components/src/Table/TableRow.tsx
@@ -7,25 +7,35 @@ import {
   Row,
   useTableOptions,
 } from 'react-aria-components';
-import { cn } from '@marigold/system';
+import { cn, useClassNames } from '@marigold/system';
 import { Checkbox } from '../Checkbox/Checkbox';
 import { useTableContext } from './Context';
 
 type RemovedProps = 'className' | 'style';
 
-export type TableRowProps<T extends object = object> = Omit<
+export interface TableRowProps<T extends object = object> extends Omit<
   RAC.RowProps<T>,
   RemovedProps
->;
+> {
+  variant?: 'grid' | 'default' | 'muted' | (string & {});
+  size?: 'compact' | 'default' | 'spacious' | (string & {});
+}
 
 const TableRow = <T extends object>({
   id,
   columns,
   children,
+  variant: variantProp,
+  size: sizeProp,
   ...otherProps
 }: TableRowProps<T>) => {
   let { selectionBehavior, allowsDragging } = useTableOptions();
-  const { classNames } = useTableContext();
+  const context = useTableContext();
+  const classNames = useClassNames({
+    component: 'Table',
+    variant: variantProp ?? context.variant,
+    size: sizeProp ?? context.size,
+  });
 
   return (
     <Row id={id} className={cn('group/row', classNames.row)} {...otherProps}>
