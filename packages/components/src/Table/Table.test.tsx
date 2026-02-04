57c64107608d16e2657df71d0ac2e0890aa7ad47
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-02-04 23:58:10 +0100

    remove unused

--- packages/components/src/Table/Table.test.tsx
+++ packages/components/src/Table/Table.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import { vi } from 'vitest';
 import { Table } from './Table';
 import {
