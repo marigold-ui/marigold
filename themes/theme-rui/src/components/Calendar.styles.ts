d1f2f607a7fd400462cc0445905097e3f4a1e112
ssarahgm <sarahgosmann@gmail.com>
2026-02-05 15:18:39 +0100

    update dependencies

--- themes/theme-rui/src/components/Calendar.styles.ts
+++ themes/theme-rui/src/components/Calendar.styles.ts
@@ -6,7 +6,7 @@ export const Calendar: ThemeComponent<'Calendar'> = {
     // In a Popover
     'group-data-trigger/popover:shadow-elevation-overlay',
     // In a Tray
-    'group-[[role=dialog]]/tray:shadow-none group-[[role=dialog]]/tray:border-0 group-[[role=dialog]]/tray:p-0',
+    'group-[[role=dialog]]/tray:shadow-none group-[[role=dialog]]/tray:border-0 group-[[role=dialog]]/tray:p-0 group-[[role=dialog]]/tray:place-self-center',
   ]),
   calendarContainer: cva('flex gap-4'),
   calendarMonth: cva('min-w-[250px] flex-1'),
