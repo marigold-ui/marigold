c954c747474a2412511b75457d42d57a5b7b38f1
ssarahgm <sarahgosmann@gmail.com>
2026-02-05 11:04:03 +0100

    use renderwithoverlay

--- packages/components/src/Tray/Tray.test.tsx
+++ packages/components/src/Tray/Tray.test.tsx
@@ -1,14 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithOverlay } from '../test.utils';
 import { Basic } from './Tray.stories';
 
 test('renders trigger button', () => {
-  render(<Basic.Component />);
+  renderWithOverlay(<Basic.Component />);
 
   expect(screen.getByRole('button', { name: 'Open Tray' })).toBeInTheDocument();
 });
 
 test('tray content is not visible when closed', () => {
-  render(<Basic.Component />);
+  renderWithOverlay(<Basic.Component />);
 
   expect(screen.queryByText('Tray Title')).not.toBeInTheDocument();
   expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
