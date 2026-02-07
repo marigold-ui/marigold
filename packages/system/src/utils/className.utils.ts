6710fe8816bf3cbf8e6afa8887114c7265ded2e3
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-02-07 06:20:51 +0100

    save

--- packages/system/src/utils/className.utils.ts
+++ packages/system/src/utils/className.utils.ts
@@ -28,28 +28,16 @@ interface CvaConfig<T extends ConfigSchema = ConfigSchema> {
     ClassProp)[];
 }
 
-/**
- * Typed version of CVA's cva function using Marigold's type parameters.
- * CVA restricts its generic parameters to internal use, so this interface
- * describes its runtime behavior to bridge our generic wrapper.
- */
-interface CreateStyles {
-  <T extends ConfigSchema>(
-    config: CvaConfig<T>
-  ): (
-    props?: T extends ConfigSchema ? ConfigVariants<T> & ClassProp : ClassProp
-  ) => string;
-}
-
-const createStyles = _cva as unknown as CreateStyles;
-
 type Props<T> = T extends ConfigSchema
   ? ConfigVariants<T> & ClassProp
   : ClassProp;
 
-// Updated CVA wrapper with new signature
 export const cva = <T extends ConfigSchema>(config?: CvaConfig<T>) => {
-  const styleFn = config ? createStyles(config) : undefined;
+  const styleFn = config
+    ? (_cva(config as Parameters<typeof _cva>[0]) as (
+        props?: Props<T>
+      ) => string)
+    : undefined;
 
   function styles(props?: Props<T>) {
     return twMerge(styleFn ? styleFn(props) : '');
