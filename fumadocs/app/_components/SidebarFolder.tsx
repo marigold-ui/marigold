4d7e1ab9ac13cb1cc1fe0e9597a54af6ba29c3c2
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-02-19 13:54:24 +0100

    migrate

--- fumadocs/app/_components/SidebarFolder.tsx
+++ fumadocs/app/_components/SidebarFolder.tsx
@@ -14,21 +14,19 @@ import { useTreePath } from 'fumadocs-ui/contexts/tree';
 import { ComponentProps, type ReactNode } from 'react';
 import { cva } from '@marigold/system';
 
-const itemVariants = cva(
-  'relative flex flex-row items-center gap-2 rounded-lg p-2 text-start text-fd-muted-foreground wrap-anywhere [&_svg]:size-4 [&_svg]:shrink-0',
-  {
-    variants: {
-      variant: {
-        link: 'transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none data-[active=true]:bg-fd-primary/10 data-[active=true]:text-fd-primary data-[active=true]:hover:transition-colors',
-        button:
-          'transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none',
-      },
-      highlight: {
-        true: "data-[active=true]:before:content-[''] data-[active=true]:before:bg-fd-primary data-[active=true]:before:absolute data-[active=true]:before:w-px data-[active=true]:before:inset-y-2.5 data-[active=true]:before:start-2.5",
-      },
+const itemVariants = cva({
+  base: 'relative flex flex-row items-center gap-2 rounded-lg p-2 text-start text-fd-muted-foreground wrap-anywhere [&_svg]:size-4 [&_svg]:shrink-0',
+  variants: {
+    variant: {
+      link: 'transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none data-[active=true]:bg-fd-primary/10 data-[active=true]:text-fd-primary data-[active=true]:hover:transition-colors',
+      button:
+        'transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none',
     },
-  }
-);
+    highlight: {
+      true: "data-[active=true]:before:content-[''] data-[active=true]:before:bg-fd-primary data-[active=true]:before:absolute data-[active=true]:before:w-px data-[active=true]:before:inset-y-2.5 data-[active=true]:before:start-2.5",
+    },
+  },
+});
 
 const getItemOffset = (depth: number) => {
   return `calc(${2 + 3 * depth} * var(--spacing))`;
@@ -69,7 +67,7 @@ const StyledSidebarFolderTrigger = ({
   return (
     <BaseSidebarFolderTrigger
       className={cn(
-        itemVariants({ variant: collapsible ? 'button' : null }),
+        itemVariants({ variant: collapsible ? 'button' : undefined }),
         'w-full',
         className
       )}
