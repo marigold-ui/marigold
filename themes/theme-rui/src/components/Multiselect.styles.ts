d7c892eec2b80565e0e43e31f7e5386598d73a67
ssarahgm <sarahgosmann@gmail.com>
2026-02-19 13:12:41 +0100

    add elevation in missing multiselect

--- themes/theme-rui/src/components/Multiselect.styles.ts
+++ themes/theme-rui/src/components/Multiselect.styles.ts
@@ -3,7 +3,7 @@ import { ThemeComponent, cva } from '@marigold/system';
 export const MultiSelect: ThemeComponent<'MultiSelect'> = {
   field: cva('space-y-2'),
   container: cva([
-    'ui-surface',
+    'ui-surface shadow-elevation-border',
     'px-3 text-sm text-foreground transition-shadow',
     'border border-input rounded-lg outline-hidden',
     'aria-disabled:bg-disabled aria-disabled:text-disabled-foreground aria-disabled:hover:border-input aria-disabled:border-input aria-disabled:cursor-not-allowed',
