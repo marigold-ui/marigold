3d14e30639f71be12a03570ab54524580c709cb5
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-02-19 12:37:48 +0100

    adjust tag field styles

--- themes/theme-rui/src/components/TagField.styles.ts
+++ themes/theme-rui/src/components/TagField.styles.ts
@@ -5,10 +5,10 @@ export const TagField: ThemeComponent<'TagField'> = {
     'ui-surface shadow-elevation-border ui-input h-fit min-h-input',
     'cursor-pointer py-1',
     'group-disabled/field:ui-state-disabled',
-    'has-focus:ui-state-focus',
+    '[&:has(>button[data-focus-visible])]:ui-state-focus',
     // Need to set error ring manually to override focus ring
     'group-invalid/field:ui-state-error',
-    'has-focus:group-invalid/field:ring-destructive/20',
+    '[&:has(>button[data-focus-visible])]:group-invalid/field:ring-destructive/20',
   ]),
   tagGroup: cva('flex flex-1 flex-wrap items-center gap-1'),
   listItems: cva('flex flex-wrap gap-1'),
