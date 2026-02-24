f9031a4c3e797356d283036312a0471caba7e936
OsamaAbdellatif <osama.abd9888@gmail.com>
2026-02-24 16:44:55 +0200

    hot fix

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -2,6 +2,8 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Suspense } from 'react';
 import { Inter } from 'next/font/google';
+import { MarigoldProvider } from '@marigold/components';
+import { theme } from '@marigold/theme-docs';
 import './global.css';
 
 const inter = Inter({
@@ -13,7 +15,9 @@ const Layout = ({ children }: LayoutProps<'/'>) => {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex min-h-screen flex-col">
         <Suspense>
-          <RootProvider>{children}</RootProvider>
+          <MarigoldProvider theme={theme}>
+            <RootProvider>{children}</RootProvider>
+          </MarigoldProvider>
           <div id="portalContainer" data-theme="rui" className="not-prose" />
         </Suspense>
       </body>
