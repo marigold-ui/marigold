ea2be248cf589698ef5c1200f97678c05798e11e
ssarahgm <sarahgosmann@gmail.com>
2026-02-26 16:02:29 +0100

    docs([DST-1234]): Update Fumadocs with favicons and metadata

--- fumadocs/app/layout.tsx
+++ fumadocs/app/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Suspense } from 'react';
 import { Inter } from 'next/font/google';
@@ -10,6 +9,25 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+// Metadata
+// ---------------
+const FAV_ICONS = {
+  development: '/logo-dev.svg',
+  preview: '/logo-preview.svg',
+};
+
+export const metadata = {
+  title: 'Marigold Design System',
+  description: "Documentation of Reservix' Design System",
+  icons: {
+    icon:
+      // @ts-expect-error TS2538
+      FAV_ICONS[process.env.NEXT_PUBLIC_VERCEL_ENV] || '/logo.svg',
+  },
+};
+
+// Layout
+// ---------------
 const Layout = ({ children }: LayoutProps<'/'>) => {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
