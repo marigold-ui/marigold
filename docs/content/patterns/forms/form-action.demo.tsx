73e931a1c56f54209a0c42e41f7802bdae527716
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-03-01 22:53:26 +0100

    save

--- docs/content/patterns/forms/form-action.demo.tsx
+++ docs/content/patterns/forms/form-action.demo.tsx
@@ -4,6 +4,6 @@ export default () => (
   <Inline space="related" alignY="center">
     <Button variant="primary">Save</Button>
     <Button variant="secondary">Save as Draft</Button>
-    <Button variant="secondary">Cancel</Button>
+    <Button variant="ghost">Cancel</Button>
   </Inline>
 );
