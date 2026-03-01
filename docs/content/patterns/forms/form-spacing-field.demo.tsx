c177dc1988b236bc619ca823bff977a0f1e04bd9
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-03-01 11:36:18 +0100

    save

--- docs/content/patterns/forms/form-spacing-field.demo.tsx
+++ docs/content/patterns/forms/form-spacing-field.demo.tsx
@@ -1,10 +1,4 @@
-import {
-  Headline,
-  Inline,
-  Inset,
-  Stack,
-  TextField,
-} from '@marigold/components';
+import { Headline, Stack, TextField } from '@marigold/components';
 import { VisualSpacing } from '@/ui/VisualSpacing';
 
 export default () => (
@@ -14,18 +8,18 @@ export default () => (
     <VisualSpacing space="regular" orientation="vertical" />
     <TextField label="Street" required />
     <VisualSpacing space="regular" orientation="vertical" />
-    <Inline space="related">
+    <Stack space="related">
       <TextField label="Postal Code" width={20} />
-      <VisualSpacing space="related" orientation="horizontal" />
+      <VisualSpacing space="related" orientation="vertical" />
       <TextField label="City" width={40} />
-    </Inline>
+    </Stack>
     <VisualSpacing space="group" orientation="vertical" />
-    <Inset spaceY="group">
+    <Stack space="group">
       <Stack space="regular">
         <TextField label="Phone" />
         <VisualSpacing space="regular" orientation="vertical" />
         <TextField label="Email" type="email" />
       </Stack>
-    </Inset>
+    </Stack>
   </Stack>
 );
