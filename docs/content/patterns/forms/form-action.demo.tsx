fedc472faaca43d6ac81f862b000686fbcdce604
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-03-01 19:29:51 +0100

    save

--- docs/content/patterns/forms/form-action.demo.tsx
+++ docs/content/patterns/forms/form-action.demo.tsx
@@ -3,6 +3,7 @@ import { Button, Inline } from '@marigold/components';
 export default () => (
   <Inline space="related" alignY="center">
     <Button variant="primary">Save</Button>
+    <Button variant="secondary">Save as Draft</Button>
     <Button variant="secondary">Cancel</Button>
   </Inline>
 );
