94a7c5f7d84e64f095123253518ccaca6e7ab0c2
aromko <aromkoapps@gmail.com>
2026-03-03 17:17:34 +0100

    [DSTSUP-233]: add card shadows to master- and adminmark

--- themes/theme-rui/src/components/Card.styles.ts
+++ themes/theme-rui/src/components/Card.styles.ts
@@ -2,7 +2,7 @@ import { cva } from '@marigold/system';
 import type { ThemeComponent } from '@marigold/system';
 
 export const Card: ThemeComponent<'Card'> = cva({
-  base: ['p-4 rounded-md'],
+  base: ['p-4 rounded-md shadow-elevation-raised'],
   variants: {
     variant: {
       default: 'ui-surface shadow-elevation-raised',
