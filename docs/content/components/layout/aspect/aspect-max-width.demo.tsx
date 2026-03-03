e855307a6a22922a20ab91d072f1ddb6d6911978
Osama Abdul Latif <62595605+OsamaAbdellateef@users.noreply.github.com>
2026-03-03 16:22:07 +0200

    docs: migrate layout components (#5191)

--- docs/content/components/layout/aspect/aspect-max-width.demo.tsx
+++
@@ -1,7 +0,0 @@
-import { Aspect } from '@marigold/components';
-
-export default () => (
-  <Aspect ratio="square" maxWidth="20vw">
-    <img src="/marigold-abstract.webp" alt="an abstract marigold flower" />
-  </Aspect>
-);
