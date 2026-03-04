56880f96065d66111474b68658b71c6462a5a56e
aromko <aromkoapps@gmail.com>
2026-03-04 13:37:48 +0100

    [DSTSUP-1125]: undo opacity for non context based icons

--- themes/theme-rui/src/components/Select.styles.ts
+++ themes/theme-rui/src/components/Select.styles.ts
@@ -1,7 +1,7 @@
 import { type ThemeComponent, cva } from '@marigold/system';
 
 export const Select: ThemeComponent<'Select'> = {
-  icon: cva({ base: 'text-muted-foreground' }),
+  icon: cva({ base: 'text-muted-foreground/80' }),
   select: cva({
     base: [
       'ui-surface shadow-elevation-border ui-input h-input ',
