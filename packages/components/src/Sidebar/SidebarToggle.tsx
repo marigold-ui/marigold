bfbd9c3656cadd3a60279ef5ff9ec6cd8be27507
Sebastian Sebald <sebastian.sebald@gmail.com>
2026-03-04 16:06:24 +0100

    make it ssr safe

--- packages/components/src/Sidebar/SidebarToggle.tsx
+++ packages/components/src/Sidebar/SidebarToggle.tsx
@@ -1,19 +1,18 @@
 import { Button } from 'react-aria-components';
 import { useLocalizedStringFormatter } from '@react-aria/i18n';
+import { useIsSSR } from '@react-aria/ssr';
 import { cn } from '@marigold/system';
 import { Tooltip } from '../Tooltip/Tooltip';
 import { intlMessages } from '../intl/messages';
 import { useSidebar } from './Context';
 import { SidebarToggleIcon } from './SidebarToggleIcon';
 
-const isMac =
-  typeof navigator !== 'undefined' &&
-  /Mac|iPhone|iPad/.test(navigator.userAgent);
-
 export const SidebarToggle = () => {
   const { toggleSidebar, state, classNames } = useSidebar();
   const stringFormatter = useLocalizedStringFormatter(intlMessages);
+  const isSSR = useIsSSR();
 
+  const isMac = !isSSR && /Mac|iPhone|iPad/.test(navigator.userAgent);
   const shortcut = isMac ? '⌘B' : 'Ctrl+B';
 
   return (
