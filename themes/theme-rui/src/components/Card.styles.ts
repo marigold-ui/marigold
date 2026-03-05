8b038f7264bc1734443c011899066a47bc159dcb
aromko <aromkoapps@gmail.com>
2026-03-05 10:36:13 +0100

    [DSTSUP-233]: remove duplicated styles

--- themes/theme-rui/src/components/Card.styles.ts
+++ themes/theme-rui/src/components/Card.styles.ts
@@ -5,7 +5,7 @@ export const Card: ThemeComponent<'Card'> = cva({
   base: ['p-4 rounded-md shadow-elevation-raised'],
   variants: {
     variant: {
-      default: 'ui-surface shadow-elevation-raised',
+      default: 'ui-surface',
       master: 'bg-access-master/30 border border-access-master',
       admin: 'bg-access-admin/30 border border-access-admin',
     },
