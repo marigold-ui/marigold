3be2d245f42626e179bd467aae291e0f591c8617
OsamaAbdellatif <osama.abd9888@gmail.com>
2026-03-16 14:18:58 +0200

    remove unwanted providers

--- docs/app/providers.tsx
+++ docs/app/providers.tsx
@@ -2,13 +2,7 @@
 
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import type { ReactNode } from 'react';
-import { MarigoldProvider } from '@marigold/components';
-import theme from '@marigold/theme-rui';
 
 export const Providers = ({ children }: { children: ReactNode }) => {
-  return (
-    <MarigoldProvider theme={theme}>
-      <RootProvider>{children}</RootProvider>
-    </MarigoldProvider>
-  );
+  return <RootProvider>{children}</RootProvider>;
 };
