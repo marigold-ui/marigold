8ef22c1edca317d088b4b6f06f30470f2c9b5232
OsamaAbdellatif <osama.abd9888@gmail.com>
2026-03-24 00:29:18 +0200

    remove un applied class

--- docs/content/components/application/provider/provider-root-styles.demo.tsx
+++ docs/content/components/application/provider/provider-root-styles.demo.tsx
@@ -2,7 +2,7 @@ import { Card, Inset, MarigoldProvider } from '@marigold/components';
 import theme from '@marigold/theme-rui';
 
 export default () => (
-  <MarigoldProvider theme={theme} className="bg-surface-sunken rounded-lg">
+  <MarigoldProvider theme={theme} className="rounded-lg">
     <Inset space={6}>
       <Card p={4}>
         This card will always be on a sunken surface with rounded corners.
