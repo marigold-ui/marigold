8221a78d0ad5a0c0a3337c3e75cfaa4e1ba6e571
ssarahgm <sarahgosmann@gmail.com>
2026-03-26 15:50:38 +0100

    update disabled color and remove text-hover-foreground only 2 usages

--- themes/theme-rui/src/components/Toast.styles.ts
+++ themes/theme-rui/src/components/Toast.styles.ts
@@ -26,7 +26,7 @@ export const Toast: ThemeComponent<'Toast'> = {
       'ml-2',
       'flex items-center justify-center',
       'size-5 rounded transition-[color,box-shadow] outline-none',
-      'focus-visible:ui-state-focus outline-none text-secondary hover:text-hover-foreground',
+      'focus-visible:ui-state-focus outline-none text-secondary',
     ],
   }),
   icon: cva({
