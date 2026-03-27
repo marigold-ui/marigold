e567623e649840d71fb230e4cdec8b7627647a7b
ssarahgm <sarahgosmann@gmail.com>
2026-03-27 15:07:08 +0100

    update

--- themes/theme-rui/src/components/Slider.styles.ts
+++ themes/theme-rui/src/components/Slider.styles.ts
@@ -17,7 +17,7 @@ export const Slider: ThemeComponent<'Slider'> = {
     base: [
       'block h-5 w-5 rounded-full border-2 border-primary bg-surface transition-colors',
       'focus-visible:ui-state-focus outline-none',
-      'disabled:cursor-not-allowed disabled:border-disabled-surface-foreground',
+      'disabled:cursor-not-allowed disabled:border-disabled',
     ],
   }),
   output: cva({ base: 'text-foreground text-sm' }),
