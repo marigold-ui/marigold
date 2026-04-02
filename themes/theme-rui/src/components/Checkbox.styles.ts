4f8da41fa57f170a51afa24005afcc87bd02fe25
ssarahgm <sarahgosmann@gmail.com>
2026-04-02 11:45:20 +0200

    update bg change when hovering

--- themes/theme-rui/src/components/Checkbox.styles.ts
+++ themes/theme-rui/src/components/Checkbox.styles.ts
@@ -9,6 +9,7 @@ export const Checkbox: ThemeComponent<'Checkbox'> = {
       'group-disabled/checkbox:group-selected/checkbox:bg-disabled-surface group-disabled/checkbox:border-disabled-surface! group-disabled/checkbox:text-disabled group-disabled/checkbox:cursor-not-allowed',
       'group-selected/checkbox:border-primary group-selected/checkbox:bg-primary group-selected/checkbox:text-primary-foreground',
       'group-[indeterminate]/checkbox:border-primary group-[indeterminate]/checkbox:bg-primary group-[indeterminate]/checkbox:text-primary-foreground',
+      'group-hover/checkbox:bg-hover group-hover/checkbox:group-disabled/checkbox:bg-disabled-surface',
     ],
   }),
   container: cva({ base: 'cursor-pointer read-only:cursor-default gap-2' }),
