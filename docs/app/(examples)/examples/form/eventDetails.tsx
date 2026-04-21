607d5929955854af6ea85b5c91f47129acd1c37c
ssarahgm <sarahgosmann@gmail.com>
2026-04-21 14:23:02 +0200

    update

--- docs/app/(examples)/examples/form/eventDetails.tsx
+++ docs/app/(examples)/examples/form/eventDetails.tsx
@@ -32,7 +32,7 @@ export const EventDetails = () => {
       <Select
         label="Event Type"
         placeholder="Select event type"
-        width={'fit'}
+        width="1/2"
         required
       >
         <Select.Option id="conference">Conference</Select.Option>
@@ -51,7 +51,7 @@ export const EventDetails = () => {
               <Select
                 label="Event Visibility"
                 placeholder="Select visibility"
-                width={'fit'}
+                width="1/2"
               >
                 <Select.Option id="public">Public</Select.Option>
                 <Select.Option id="private">Private</Select.Option>
