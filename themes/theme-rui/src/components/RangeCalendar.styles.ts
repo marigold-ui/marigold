5ef57b6ac74499893693980c011ef8d612f09c63
aromko <aromkoapps@gmail.com>
2026-05-04 10:21:01 +0200

    [DST-1134]: refa stylings

--- themes/theme-rui/src/components/RangeCalendar.styles.ts
+++ themes/theme-rui/src/components/RangeCalendar.styles.ts
@@ -1,15 +1,8 @@
 import { type ThemeComponent, cva } from '@marigold/system';
+import { Calendar } from './Calendar.styles';
 
 export const RangeCalendar: ThemeComponent<'RangeCalendar'> = {
-  calendar: cva({
-    base: [
-      'min-h-[350px] ui-surface shadow-elevation-border p-2',
-      'group-data-trigger/popover:shadow-elevation-overlay',
-      'group-[[role=dialog]]/tray:shadow-none group-[[role=dialog]]/tray:border-0 group-[[role=dialog]]/tray:p-0 group-[[role=dialog]]/tray:place-self-center',
-    ],
-  }),
-  calendarContainer: cva({ base: 'flex gap-4' }),
-  calendarMonth: cva({ base: 'min-w-[250px] flex-1' }),
+  ...Calendar,
   calendarCell: cva({
     base: [
       'relative flex h-9 w-full items-center justify-center whitespace-nowrap rounded-none justify-self-center',
@@ -26,44 +19,8 @@ export const RangeCalendar: ThemeComponent<'RangeCalendar'> = {
       'outside-month:hidden',
     ],
   }),
-  calendarControllers: cva({
-    base: [
-      'inline-flex items-center justify-center gap-[0.5ch]',
-      'size-9 rounded-lg',
-      'text-muted-foreground',
-      'focus-visible:ui-state-focus outline-none',
-    ],
-  }),
-  calendarHeader: cva({
-    base: [
-      'size-9 rounded-lg p-0 text-xs font-medium text-muted-foreground text-center',
-    ],
-  }),
   calendarGrid: cva({
     base: 'border-collapse border-spacing-0 [&_td]:p-0 [&_td]:group-[[role=dialog]]/tray:p-0',
-  }),
-  calendarHeading: cva({
-    base: 'text-sm font-medium',
-  }),
-  calendarListboxButton: cva({
-    base: [
-      'rounded-md text-sm font-medium transition-[color,box-shadow]',
-      'px-4 py-2',
-      'focus-visible:ui-state-focus outline-none',
-      'cursor-pointer',
-      'hover:bg-hover',
-      'aria-selected:bg-brand aria-selected:text-brand-foreground aria-selected:shadow-elevation-border aria-selected:hover:bg-brand/90',
-    ],
-  }),
-  select: cva({
-    base: [
-      '[&svg]:text-muted-foreground',
-      'flex w-full px-3 py-2 rounded-lg shadow-elevation-border border border-input bg-background text-sm text-foreground transition-shadow',
-      'focus-visible:ui-state-focus outline-none',
-      'h-input',
-      'disabled:cursor-not-allowed disabled:text-disabled-foreground disabled:bg-disabled',
-      'cursor-pointer',
-    ],
   }),
   errorMessage: cva({
     base: 'text-error text-xs mt-2',
