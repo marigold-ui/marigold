0113ed0896bafa04e3e727014583107f8d235253
OsamaAbdellatif <osama.abd9888@gmail.com>
2026-05-12 17:49:36 +0300

    feat(docs): add card demos for whitespace and stacked card usage

--- docs/content/components/collection/card/card-stacked.demo.tsx
+++ docs/content/components/collection/card/card-stacked.demo.tsx
@@ -1,13 +1,13 @@
 import { people } from '@/lib/data/people';
-import { Divider, Headline, Inline, Stack, Text } from '@marigold/components';
+import { Card, Headline, Inline, Stack, Text } from '@marigold/components';
 
-const team = people.slice(0, 5);
+const team = people.slice(0, 4);
 
 export default () => (
-  <div className="bg-bg-surface rounded-xl p-6">
-    <Stack space="regular">
-      {team.map((person, index) => (
-        <Stack key={person.id} space="related">
+  <Stack space="regular">
+    {team.map(person => (
+      <Card key={person.id} stretch>
+        <Card.Body>
           <Inline space="related" alignY="center">
             <img
               src={person.avatar}
@@ -21,9 +21,8 @@ export default () => (
               </Text>
             </Stack>
           </Inline>
-          {index < team.length - 1 && <Divider />}
-        </Stack>
-      ))}
-    </Stack>
-  </div>
+        </Card.Body>
+      </Card>
+    ))}
+  </Stack>
 );
