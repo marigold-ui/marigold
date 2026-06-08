2d19414ae03f82d6f542aef6d0fa90cee4685e89
OsamaAbdellatif <osama.abd9888@gmail.com>
2026-06-08 15:59:02 +0300

    fix: update Stack space prop to use "related" for better layout consistency

--- docs/content/components/form/file-field/file-field-small.demo.tsx
+++ docs/content/components/form/file-field/file-field-small.demo.tsx
@@ -1,7 +1,7 @@
 import { FileField, Select, Stack } from '@marigold/components';
 
 export default () => (
-  <Stack space={2}>
+  <Stack space={'related'}>
     <Select label="Document type" size="small">
       <Select.Option id="terms">Terms and conditions</Select.Option>
       <Select.Option id="privacy">Privacy policy</Select.Option>
